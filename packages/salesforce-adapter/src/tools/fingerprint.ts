f3b38494838a20e9a0c960429191e8e3ac74a63b
Yariv Brosh <yariv.brosh@gmail.com>
2019-06-18 15:56:27 +0300

    Jsforce type extensions with example of using describeValueType
    * Switched dependency to our own jsforce forked repo.
    * Switched dependency from @types/jsforce to our own jsforce-types repo which contains a copy of @types/jsforce with our additions.
    * Added the prefix URI to DescribeValueType param. (Should be moved to SalesforceClient.ts once it is inside)

--- packages/salesforce-adapter/src/tools/fingerprint.ts
+++ packages/salesforce-adapter/src/tools/fingerprint.ts
@@ -1,24 +1,32 @@
+import * as winston from 'winston'
 import {
-  Connection,
   ConnectionOptions,
   DescribeMetadataResult,
   MetadataObject,
-  FileProperties
+  FileProperties,
+  Connection,
+  DescribeValueTypeResult,
+  ValueTypeField
 } from 'jsforce'
-import * as winston from 'winston'
+
+const developerEdition = 'Developer Edition'
 
 /**
  * This method runs on your salesforce account and prints an "inventory" of the various entities it contains.
  */
 async function main(): Promise<void> {
+  // Instantiate the logging mechanism
   const logger = winston.createLogger({
-    level: 'info',
     format: winston.format.combine(
       winston.format.splat(),
       winston.format.colorize(),
       winston.format.simple()
     ),
-    transports: [new winston.transports.Console()]
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.simple()
+      })
+    ]
   })
 
   // Process arguments. Slice the 2 first params which are the path to nodejs and the location of the script
@@ -37,18 +45,42 @@ async function main(): Promise<void> {
     connectionOptions = { version: '45.0' }
   }
   const conn = new Connection(connectionOptions)
-  const loginResult = await conn.login(userName, password)
+  await conn.login(userName, password, (err, user) => {
+    if (err) {
+      logger.error('Could not establish a connection: %s', err.message)
+      process.exit(1)
+    } else {
+      logger.info(
+        `UserInfo:\n org Id:${user.organizationId}, User Id:${user.id}, URL:${user.url}`
+      )
+    }
+  })
 
-  logger.info(
-    `UserInfo:\n org Id:${loginResult.organizationId}, User Id:${loginResult.id}, URL:${loginResult.url}`
-  )
+  // Verifying the organization is a Sandbox
+  if (!sandbox) {
+    // If not, verifying that its type is "Developer Edition".
+    // Query for organization data
+    await conn.query(
+      'SELECT Id, OrganizationType, Name, instanceName, isSandbox FROM Organization',
+      undefined,
+      (err, res: any) => {
+        if (err) {
+          logger.error(
+            'Cannot determine if organization is a Developer Edition or not. Failed to fetch organization object with error: %s',
+            err.message
+          )
+          process.exit(1)
+        } else if (res.records[0].OrganizationType !== developerEdition) {
+          logger.error(
+            'Your organization is neither a sandbox nor a Developer Edition, therefore the program will quit to prevent massive API calls to your production account'
+          )
+          process.exit(1)
+        }
+      }
+    )
+  }
 
-  // Query for organization data
-  const result: any = await conn.query(
-    'SELECT Id, OrganizationType, Name, instanceName, isSandbox FROM Organization'
-  )
-  logger.info(result.records[0].OrganizationType)
-
+  logger.info('============METADATA API=================')
   const metaResults: DescribeMetadataResult = await conn.metadata.describe()
   const sortedObjects: MetadataObject[] = metaResults.metadataObjects.sort(
     (a, b) => {
@@ -56,10 +88,39 @@ async function main(): Promise<void> {
     }
   )
 
+  // Cannot run async statements inside an array forEach() loop
+  // eslint-disable-next-line no-restricted-syntax
+  for (const obj of sortedObjects) {
+    logger.info('------------------------------------')
+    logger.info('Analyze metadata type %s\n', obj.xmlName)
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const results: DescribeValueTypeResult = await conn.metadata.describeValueType(
+        `{http://soap.sforce.com/2006/04/metadata}${obj.xmlName}`
+      )
+      if (!results) {
+        logger.info('Failed to describe value type %s', obj.xmlName)
+      } else {
+        const sortedFields: ValueTypeField[] = results.ValueTypeFields.sort(
+          (a, b) => {
+            return a.name.localeCompare(b.name)
+          }
+        )
+        // eslint-disable-next-line no-restricted-syntax
+        for (const field of sortedFields) {
+          logger.info(field.name)
+        }
+      }
+      // eslint-disable-next-line no-empty
+    } catch (error) {}
+  }
+
+  // Cannot run async statements inside an array forEach() loop
   // eslint-disable-next-line no-restricted-syntax
   for (const obj of sortedObjects) {
     logger.info(obj.xmlName)
     try {
+      // We want to perform this synchronously, hence the await inside the loop
       // eslint-disable-next-line no-await-in-loop
       const results = await conn.metadata.list({
         type: obj.xmlName

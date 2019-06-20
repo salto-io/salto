02e4c89d0bd5416432f5ab03ade449fdd8a692ed
Ori Moisis <ori.moisis@salto.io>
2019-06-20 12:14:57 +0300

    remove build from npm test commands and increase coverage requirements

--- packages/salto/jest.config.js
+++ packages/salto/jest.config.js
@@ -21,15 +21,15 @@ module.exports = {
     '!**/node_modules/**',
     '!<rootDir>/*.config.js',
     '!<rootDir>/(test|dist|coverage)/**',
-    '!<rootDir>/src/parser/wasm_exec.js',     // External source file
+    '!<rootDir>/src/parser/wasm_exec.js', // External source file
   ],
   coverageThreshold: {
     // Slowly start increasing here, never decrease!
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
-    }
-  }
+      branches: 85,
+      functions: 59,
+      lines: 50,
+      statements: 50,
+    },
+  },
 }

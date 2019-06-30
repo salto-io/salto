2c473fb345d9bcf76fac2cbbced498f4ef25bba0
ori-moisis <ori.moisis@salto.io>
2019-06-30 15:47:04 +0300

    speed up wasm execution by creating wasm instances from module instead of from data (#31)

--- packages/salto/src/parser/hcl.ts
+++ packages/salto/src/parser/hcl.ts
@@ -8,30 +8,33 @@ import './wasm_exec'
 const go = new Go()
 
 class HCLParser {
-  private wasmData: Promise<Buffer> | null = null
+  private wasmModule: Promise<WebAssembly.Module> | null = null
 
-  get wasmModule(): Promise<WebAssembly.ResultObject> {
-    if (this.wasmData === null) {
+  get wasmInstance(): Promise<WebAssembly.Instance> {
+    if (this.wasmModule === null) {
       // Load web assembly module data once in the life of a parser
-      this.wasmData = (async () => {
+      this.wasmModule = (async () => {
         // Relative path from source location
         const modulePath = path.join(__dirname, '..', '..', 'hcl.wasm')
-        return fs.readFile(modulePath)
+        const data = await fs.readFile(modulePath)
+        // Not sure why eslint ignores this definition from webassembly.d.ts,
+        // but this doesn't work without the following disable
+        // eslint-disable-next-line no-undef
+        const wasmObj = await WebAssembly.instantiate(data, go.importObject)
+        return wasmObj.module
       })()
     }
 
     // Not sure why eslint ignores this definition from webassembly.d.ts,
-    //  but this doesn't work without the following disable
+    // but this doesn't work without the following disable
     // eslint-disable-next-line no-undef
-    return this.wasmData.then(data => WebAssembly.instantiate(data, go.importObject))
+    return this.wasmModule.then(module => WebAssembly.instantiate(module, go.importObject))
   }
 
   async Parse(
     src: Buffer,
     filename: string,
   ): Promise<{ body: HCLBlock; errors: string[] }> {
-    const wasmModule = await this.wasmModule
-
     try {
       // Setup arguments to parse function
       global.hclParserArgs = {
@@ -39,7 +42,7 @@ class HCLParser {
         filename,
       }
       // Call parse function from go
-      await go.run(wasmModule.instance)
+      await go.run(await this.wasmInstance)
       // Return value should be populated by the above call
       return {
         body: global.hclParserReturn.value,

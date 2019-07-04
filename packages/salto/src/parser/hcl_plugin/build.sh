7c9031f907414220557b7b9da922cf8b4d4c6cd1
ori-moisis <ori.moisis@salto.io>
2019-07-04 15:35:28 +0300

    Fix web assembly build to include all needed things in dist folder (#43)
    It was missing wasm_exec.js which is required for running the HCL plugin
    it was not copied by tsc because it ignores .js files

--- packages/salto/src/parser/hcl_plugin/build.sh
+++ packages/salto/src/parser/hcl_plugin/build.sh
@@ -6,19 +6,27 @@ function abspath {
 
 if [ $# -lt 1 ]
 then
-	echo "Usage build.sh <destination> [other destination]"
+	echo "Usage build.sh <destination_dir>"
 	exit 1
 fi
 
+
 dest=$(abspath $1)
+js_dest=$dest/src/parser
 
 pushd $(dirname $0) > /dev/null
 echo -n 'Building go HCL plugin... '
-env GOOS=js GOARCH=wasm go build -o $dest
+
+# Build wasm
+env GOOS=js GOARCH=wasm go build -o $dest/hcl.wasm
+
+# Copy wasm to package root so that tests can run
+cp $dest/hcl.wasm ../../..
+
+# Copy wasm_exec.js to destination
+mkdir -p $js_dest
+cp ../wasm_exec.js $js_dest
+
 echo 'Done'
 popd > /dev/null
 
-if [ $# -eq 2 ]
-then
-	cp $dest $2
-fi

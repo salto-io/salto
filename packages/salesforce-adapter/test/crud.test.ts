8a52e0fac548a473869d0636c4704c3aff493c18
Oren Digmi <oren.digmi@salto.io>
2019-07-31 14:51:34 +0300

    Added unit test for (failure) add instance scenario

--- packages/salesforce-adapter/test/crud.test.ts
+++ packages/salesforce-adapter/test/crud.test.ts
@@ -100,7 +100,36 @@ describe('Test SalesforceAdapter CRUD', () => {
       expect(mockCreate.mock.calls[0][1].token).toBeUndefined()
     })
 
-    it('Should add new element', async () => {
+    it('should fail add new salesforce instance', async () => {
+    SalesforceClient.prototype.create = jest
+      .fn()
+      .mockImplementationOnce(async () => ({
+        success: false,
+        fullName: 'Test__c',
+        errors: [
+          {
+            message: 'Failed to add Test__c',
+          },
+          {
+            message: 'Additional message',
+          },
+        ],
+      }))
+
+    return expect(
+      adapter().add(
+        new InstanceElement(mockElemID, new ObjectType({
+          elemID: mockElemID,
+          fields: {},
+          annotations: {},
+          annotationsValues: {},
+        }),
+        {})
+      )
+    ).rejects.toEqual(new Error('Failed to add Test__c\nAdditional message'))
+  })
+
+  it('Should add new element', async () => {
       const result = await adapter().add(
         new ObjectType({
           elemID: mockElemID,

307059ad4770be5283dbc4c94b8f68d832bbccf9
Roi Ronn <roi.ronn@salto.io>
2019-07-31 11:50:30 +0300

    cr fixes

--- packages/salto/src/blueprints/validator.ts
+++ packages/salto/src/blueprints/validator.ts
@@ -13,72 +13,63 @@ const primitiveValidators = {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const validateValueType = (value: any, scheme: Type): void => {
+const validateValue = (value: any, scheme: Type): string[] => {
   if ((isPrimitiveType(scheme) && !primitiveValidators[scheme.primitive](value))
      || (isObjectType(scheme) && !_.isPlainObject(value))) {
-    throw new Error(`Invalid value type for ${scheme.elemID.getFullName()} : ${value}`)
+    return [`Invalid value type for ${scheme.elemID.getFullName()} : ${value}`]
   }
-}
-
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const validateValue = (value: any, scheme: Type): void => {
-  validateValueType(value, scheme)
   if (isObjectType(scheme)) {
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    _.forEach(value, (v, k) => scheme.fields[k] && validateFieldValue(v, scheme.fields[k]))
+    return Object.keys(value).map(
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      k => scheme.fields[k] && validateFieldValue(value[k], scheme.fields[k])
+    ).filter(e => e).reduce((acc, e) => [...acc, ...e], [])
   }
+  return []
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const validateFieldValue = (value: any, field: Field): void => {
+const validateFieldValue = (value: any, field: Field): string[] => {
   if (field.isList) {
     if (!_.isArray(value)) {
-      throw new Error(`Invalid value type for ${field.elemID.getFullName()}: expected list`)
+      return [`Invalid value type for ${field.elemID.getFullName()}: expected list`]
     }
-    value.forEach(v => validateValue(v, field.type))
-  } else {
-    validateValue(value, field.type)
+    return value.map(v => validateValue(v, field.type)).reduce((acc, e) => [...acc, ...e], [])
   }
+  return validateValue(value, field.type)
 }
 
-const validateField = (field: Field): void => {
-  _.forEach(
-    field.annotationsValues,
-    (v, k) => field.type.annotations[k] && validateValue(v, field.type.annotations[k])
+const validateField = (field: Field): string[] => Object.keys(field.annotationsValues).map(
+  k => field.type.annotations[k] && validateValue(
+    field.annotationsValues[k],
+    field.type.annotations[k]
   )
-}
+).filter(e => e).reduce((acc, e) => [...acc, ...e], [])
 
-const validateType = (element: Type): void => {
-  _.forEach(
-    element.annotationsValues,
-    (v, k) => element.annotations[k] && validateValue(v, element.annotations[k])
-  )
+const validateType = (element: Type): string[] => {
+  const errors = Object.keys(element.annotationsValues).map(
+    k => element.annotations[k] && validateValue(
+      element.annotationsValues[k],
+      element.annotations[k]
+    )
+  ).filter(e => e).reduce((acc, e) => [...acc, ...e], [])
   if (isObjectType(element)) {
-    Object.values(element.fields).forEach(validateField)
+    return [
+      ...errors,
+      ...Object.values(element.fields).map(validateField).reduce((acc, e) => [...acc, ...e], []),
+    ]
   }
-}
-
-const validateInstanceElement = (element: InstanceElement): void => {
-  validateValue(element.value, element.type)
-}
-
-const validateElements = (elements: Element[]): string[] => {
-  const errors: string[] = []
-
-  elements.forEach(element => {
-    try {
-      if (isInstanceElement(element)) {
-        validateInstanceElement(element)
-      } else {
-        validateType(element as Type)
-      }
-    } catch (err) {
-      errors.push(err.message)
-    }
-  })
-
   return errors
 }
+
+const validateInstanceElement = (
+  element: InstanceElement
+): string[] => validateValue(element.value, element.type)
+
+const validateElements = (elements: Element[]): string[] => elements.map(element => {
+  if (isInstanceElement(element)) {
+    return validateInstanceElement(element)
+  }
+  return validateType(element as Type)
+}).reduce((acc, e) => [...acc, ...e], [])
 
 export default validateElements

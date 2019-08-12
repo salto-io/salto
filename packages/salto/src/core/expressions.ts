f33091eb401da44b47c14143a31f2f5b327d821f
Roi Ronn <roi.ronn@salto.io>
2019-08-12 11:01:19 +0300

    wip

--- packages/salto/src/core/expressions.ts
+++ packages/salto/src/core/expressions.ts
@@ -4,6 +4,19 @@ import {
   ElemID, Element, isObjectType, isInstanceElement, isType, Value,
 } from 'adapter-api'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ResolvedRef = any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Value = any
+
+interface Expression {
+  resolve(contextElements: Element[], visited?: string[]): ResolvedRef
+}
+
+type TemplatePart = string|Expression
+
+const isExpression = (value: Value): value is Expression => _.isFunction(value.resolve)
+
 export class ReferenceExpression {
   static readonly TRAVERSAL_SEPERATOR = '.'
   traversal: string
@@ -14,10 +27,6 @@ export class ReferenceExpression {
     this.root = new ElemID(nameParts[0], ...nameParts.slice(1))
     this.path = traversalParts.slice(1)
     this.traversal = traversalParts.join(ReferenceExpression.TRAVERSAL_SEPERATOR)
-  }
-
-  static isReferenceExpresion(value: Value): value is ReferenceExpression {
-    return value instanceof ReferenceExpression
   }
 
   // This is only wrapped as function so that the error validation would be simpler
@@ -51,15 +60,27 @@ export class ReferenceExpression {
       throw new Error(`Can not resolve reference ${this.traversal}`)
     }
 
-    return ReferenceExpression.isReferenceExpresion(value)
+    return isExpression(value)
       ? value.resolve(contextElements, [this.traversal, ...visited])
       : value
   }
 }
 
+export class TemplateExpression {
+  parts: TemplatePart[]
+
+  constructor(parts: TemplatePart[]) {
+    this.parts = parts
+  }
+
+  resolve(contextElements: Element[], visited: string[] = []): ResolvedRef {
+    return this.parts.map(p => (isExpression(p) ? p.resolve(contextElements, visited) : p)).join('')
+  }
+}
+
 export const resolve = (element: Element, contextElements: Element[]): Element => {
-  const referenceCloner = (v: Value): Value => (
-    ReferenceExpression.isReferenceExpresion(v) ? v.resolve(contextElements) : undefined
+  const referenceCloner = (v: Value): ResolvedRef => (
+    isExpression(v) ? v.resolve(contextElements) : undefined
   )
 
   if (isInstanceElement(element)) {

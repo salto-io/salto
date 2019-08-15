f1a7f5e83899035032b05ca835ed3bf9422da4d1
roironn <roi.ronn@salto.io>
2019-08-15 22:02:25 +0300

    Revert "added parser cache (#84)"
    This reverts commit 40a9c86364b2b9fbe01d479f9c849c92cf99d09b.

--- packages/salto/src/state/state.ts
+++ packages/salto/src/state/state.ts
@@ -3,7 +3,8 @@ import { Element } from 'adapter-api'
 import * as fs from 'async-file'
 import * as path from 'path'
 import os from 'os'
-import { serialize, deserialize } from './serializer'
+import Parser from '../parser/salto'
+import { getAllElements } from '../blueprints/blueprint'
 
 const STATEPATH = path.join(os.homedir(), '.salto/latest_state.bp')
 /**
@@ -48,7 +49,7 @@ export default class State {
     public async flush(): Promise<void> {
       // If state is not loaded we don't have anything to save
       if (!this.state) return
-      const buffer = serialize(this.state)
+      const buffer = await Parser.dump(this.state)
       await fs.createDirectory(path.dirname(this.statePath))
       await fs.writeFile(this.statePath, buffer)
     }
@@ -66,13 +67,15 @@ export default class State {
      * @returns the elements that represent the last saved state
      */
     private async read(): Promise<Element[]> {
+      let buffer: Buffer
       try {
         const exists = await fs.exists(this.statePath)
         if (!exists) {
           return []
         }
-        const data = await fs.readFile(this.statePath, 'utf8')
-        return deserialize(data)
+        buffer = await fs.readFile(this.statePath, 'utf8')
+        // force the await here so errors will be thrown in proper place
+        return (await getAllElements([{ buffer, filename: this.statePath }]))
       } catch (err) {
         throw new Error(`Failed to load state: ${err}`)
       }

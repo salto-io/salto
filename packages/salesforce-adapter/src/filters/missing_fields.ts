e16cd9057da2025c8757cb0a3ebbaba48ccccf1c
ori-moisis <ori.moisis@gmail.com>
2019-08-27 14:30:16 +0300

    add missing fields filter (#110)

---
+++ packages/salesforce-adapter/src/filters/missing_fields.ts
@@ -0,0 +1,81 @@
+import _ from 'lodash'
+import {
+  isObjectType, Field, Values, Type, isType, BuiltinTypes,
+} from 'adapter-api'
+import Filter from './filter'
+
+interface MissingField {
+  name: string
+  type: Type | string
+  annotationValues?: Values
+  isList?: boolean
+}
+
+const allMissingFields: Record<string, MissingField[]> = {
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  filter_item: [
+    {
+      name: 'operation',
+      type: BuiltinTypes.STRING,
+      annotationValues: {
+        [Type.RESTRICTION]: {
+          values: [
+            'equals', 'notEqual', 'lessThan', 'greaterThan', 'lessOrEqual', 'greaterOrEqual',
+            'contains', 'notContain', 'startsWith', 'includes', 'excludes', 'within',
+          ],
+        },
+      },
+    },
+  ],
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  lead_convert_settings: [
+    {
+      name: 'object_mapping',
+      type: 'object_mapping',
+      isList: true,
+    },
+  ],
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  rule_entry: [
+    {
+      name: 'assigned_to_type',
+      type: BuiltinTypes.STRING,
+      annotationValues: {
+        [Type.RESTRICTION]: { values: ['User', 'Queue'] },
+      },
+    },
+  ],
+}
+
+export const makeFilter = (missingFields: Record<string, MissingField[]>): Filter => ({
+  onDiscover: async function onDiscover(_client, elements) {
+    // We need a mapping of all the types so we can replace type names with the correct types
+    const typeMap = _(elements)
+      .filter(isType)
+      .map(t => [t.elemID.name, t])
+      .fromPairs()
+      .value()
+
+    // Add missing fields to types
+    elements.filter(isObjectType).forEach(elem => {
+      const fieldsToAdd = missingFields[elem.elemID.name]
+      if (fieldsToAdd !== undefined) {
+        _.assign(elem.fields, _(fieldsToAdd)
+          .map(f => [f.name, new Field(
+            elem.elemID,
+            f.name,
+            isType(f.type) ? f.type : typeMap[f.type],
+            f.annotationValues || {},
+            f.isList === true,
+          )])
+          .fromPairs()
+          .value())
+      }
+    })
+  },
+  onAdd: async (_client, _elem) => [],
+  onUpdate: async (_client, _elem1, _elem2) => [],
+  onRemove: async (_client, _elem) => [],
+})
+
+export default makeFilter(allMissingFields)
---
+++ packages/salesforce-adapter/test/filters/missing_fields.test.ts
@@ -0,0 +1,90 @@
+import {
+  ObjectType, ElemID, Field, BuiltinTypes,
+} from 'adapter-api'
+import { makeFilter } from '../../src/filters/missing_fields'
+import SalesforceClient from '../../src/client/client'
+import * as constants from '../../src/constants'
+
+jest.mock('../../src/client/client')
+
+describe('Test layout filter', () => {
+  const client = new SalesforceClient('', '', false)
+  const filter = makeFilter({
+    test: [
+      {
+        name: 'lst',
+        type: BuiltinTypes.STRING,
+        annotationValues: { dummy: true },
+        isList: true,
+      },
+      {
+        name: 'complex',
+        type: 'complex_type',
+      },
+    ],
+  })
+
+  const mockObjId = new ElemID(constants.SALESFORCE, 'test')
+  const mockType = new ObjectType({
+    elemID: mockObjId,
+    fields: {
+      existing: new Field(mockObjId, 'existing', BuiltinTypes.STRING),
+    },
+  })
+  const complexType = new ObjectType({
+    elemID: new ElemID(constants.SALESFORCE, 'complex_type'),
+    annotationsValues: { marker: 'here' },
+  })
+
+  let testElements: ObjectType[]
+
+  beforeEach(() => {
+    testElements = [
+      mockType.clone(),
+      complexType,
+    ]
+  })
+
+  describe('on discover', () => {
+    beforeEach(() => filter.onDiscover(client, testElements))
+
+    it('should add primitive list fields', async () => {
+      const [testType] = testElements
+      expect(testType.fields.lst).toBeDefined()
+      expect(testType.fields.lst.isList).toBe(true)
+      expect(testType.fields.lst.getAnnotationsValues()).toEqual({ dummy: true })
+      expect(testType.fields.lst.type).toEqual(BuiltinTypes.STRING)
+    })
+
+    it('should add fields by type name', async () => {
+      const [testType] = testElements
+      expect(testType.fields.complex).toBeDefined()
+      expect(testType.fields.complex.isList).toBe(false)
+      expect(testType.fields.complex.getAnnotationsValues()).toEqual({})
+      expect(testType.fields.complex.type).toEqual(complexType)
+    })
+
+    it('should keep existing fields unchanged', async () => {
+      const [testType] = testElements
+      expect(testType.fields.existing).toEqual(mockType.fields.existing)
+    })
+  })
+
+  describe('on add', () => {
+    it('should have no effect', async () => {
+      expect(await filter.onAdd(client, mockType)).toHaveLength(0)
+    })
+  })
+
+  describe('on update', () => {
+    it('should have no effect', async () => {
+      expect(await filter.onUpdate(client, mockType, mockType)).toHaveLength(0)
+    })
+  })
+
+  describe('on remove', () => {
+    it('should have no effect', async () => {
+      expect(await filter.onRemove(client, mockType)).toHaveLength(0)
+    })
+  })
+})

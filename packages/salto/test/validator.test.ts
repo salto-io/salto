c90363629910ea4583727ad752abf66560703f60
Oren Digmi <oren.digmi@salto.io>
2019-08-27 19:37:06 +0300

    Refactor validator.test

--- packages/salto/test/validator.test.ts
+++ packages/salto/test/validator.test.ts
@@ -60,7 +60,7 @@ describe('Elements validation', () => {
   describe('validate types', () => {
     it('should validate a correct type', () => {
       const errors = validateElements([BuiltinTypes.STRING, simpleType, nestedType])
-      expect(errors.length).toBe(0)
+      expect(errors).toHaveLength(0)
     })
 
     it('should allow unspecified annotations', () => {
@@ -69,33 +69,35 @@ describe('Elements validation', () => {
       extType.fields.nested.getAnnotationsValues().unspecnum = 1
       extType.fields.nested.getAnnotationsValues().unspecbool = false
       const errors = validateElements([extType])
-      expect(errors.length).toBe(0)
+      expect(errors).toHaveLength(0)
     })
+
     it('should return error on bad str primitive type', () => {
       const badStr = _.cloneDeep(nestedType)
       badStr.fields.nested.getAnnotationsValues().annostr = 1
       const errors = validateElements([badStr])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
+
     it('should return error on bad num primitive type', () => {
       const badNum = _.cloneDeep(nestedType)
       badNum.fields.nested.getAnnotationsValues().annonum = 'str'
       const errors = validateElements([badNum])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on bad bool primitive type', () => {
       const badBool = _.cloneDeep(nestedType)
       badBool.fields.nested.getAnnotationsValues().annoboolean = 1
       const errors = validateElements([badBool])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on nested annotation mismatch', () => {
       const badNested = _.cloneDeep(nestedType)
       badNested.getAnnotationsValues().nested = { str: 1 }
       const errors = validateElements([badNested])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error object/primitive mismatch', () => {
@@ -105,95 +107,88 @@ describe('Elements validation', () => {
       const badObj = _.cloneDeep(nestedType)
       badObj.getAnnotationsValues().nested = 'not an object'
       const errors = validateElements([badObj, badPrim])
-      expect(errors.length).toBe(2)
+      expect(errors).toHaveLength(2)
     })
   })
 
   describe('validate instances', () => {
+    let extInst: InstanceElement
+
+    beforeEach(() => {
+      extInst = _.cloneDeep(nestedInstance)
+    })
+
     it('should validate a correct type', () => {
       const errors = validateElements([nestedInstance])
-      expect(errors.length).toBe(0)
+      expect(errors).toHaveLength(0)
     })
 
     it('should allow unspecified values', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.unspecstr = 'unspec'
       extInst.value.unspecnum = 1
       extInst.value.unspecbool = false
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(0)
+      expect(errors).toHaveLength(0)
     })
 
     it('should return error on bad str primitive type', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.flatstr = 1
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on bad num primitive type', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.flatnum = 'str'
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on bad bool primitive type', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.flatbool = 'str'
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on nested string value mismatch', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.nested.str = 1
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on nested num value mismatch', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.nested.num = 'str'
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error on nested bool value mismatch', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.nested.bool = 'str'
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error object/primitive mismatch', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.nested = 'str'
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error list/primitive mismatch', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.list = 'not a list'
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error list/object mismatch', () => {
-      // const badList = _.cloneDeep(nestedInstance)
-      // badList.value.list = {}
-      const badObj = _.cloneDeep(nestedInstance)
-      badObj.value = { nested: [] }
-      const errors = validateElements([badObj])
-      expect(errors.length).toBe(1)
+      extInst.value = { nested: [] }
+      const errors = validateElements([extInst])
+      expect(errors).toHaveLength(1)
     })
 
     it('should return error list item mismatch', () => {
-      const extInst = _.cloneDeep(nestedInstance)
       extInst.value.list.push(1)
       const errors = validateElements([extInst])
-      expect(errors.length).toBe(1)
+      expect(errors).toHaveLength(1)
     })
   })
 })

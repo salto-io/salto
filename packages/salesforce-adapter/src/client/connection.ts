196e9e5365dd315271572fc95207a7ec76378f76
Yariv Brosh <yarivbrosh@Yarivs-MacBook-Pro.local>
2019-09-03 12:00:24 +0300

    eslint fix

--- packages/salesforce-adapter/src/client/connection.ts
+++ packages/salesforce-adapter/src/client/connection.ts
@@ -37,5 +37,6 @@ export default interface Connection {
   metadata: Metadata
   soap: Soap
   describeGlobal(): Promise<Global>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   query(soql: string): Promise<QueryResult<any>>
 }

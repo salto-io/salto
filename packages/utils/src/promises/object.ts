e4ddff4913a733c3605ef63c0cb9287a88b7c9af
Roy Razon <roy.razon@salto.io>
2019-09-04 23:03:19 +0300

    converted comments to jsdoc

--- packages/utils/src/promises/object.ts
+++ packages/utils/src/promises/object.ts
@@ -1,7 +1,9 @@
 import _ from 'lodash'
 
-// transforms an object's values using an async mapper function
-// returns an object whose values are the resolved results of the mapper
+/**
+ * transforms an object's values using an async mapper function
+ * @returns an object whose values are the resolved results of the mapper
+ */
 export const mapValuesAsync = async <TVal1, TVal2>(
   o: Record<string, TVal1>,
   mapper: (val: TVal1, key: string) => Promise<TVal2>
@@ -16,8 +18,11 @@ export const mapValuesAsync = async <TVal1, TVal2>(
   return _.fromPairs(pairs)
 }
 
-// transform an object whos values are promises into an object
+/**
+ * transforms an object whos values are promises
+ * @returns an object
 // whos values are the result of resolving the promises
+ */
 export const resolveValues = <TVal>(
   o: Record<string, Promise<TVal>>
 ): Promise<Record<string, TVal>> => mapValuesAsync(o, _.identity)

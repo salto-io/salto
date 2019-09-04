e62a5a1f094e67e3369a026b65f5336dd76dc22b
Roy Razon <roy.razon@salto.io>
2019-09-04 11:06:04 +0300

    CR fix: removed mapValuesAsync

--- packages/salto/src/core/adapters/adapters.ts
+++ packages/salto/src/core/adapters/adapters.ts
@@ -4,12 +4,6 @@ import {
 } from 'adapter-api'
 import adapterCreators from './creators'
 
-const mapValuesAsync = async <V1, V2>(
-  o: Record<string, V1>, mapper: (v: V1) => Promise<V2>
-): Promise<Record<string, V2>> => Promise.all(
-  Object.keys(o).map(key => mapper(o[key]).then(v => [key, v]))
-).then(_.fromPairs)
-
 const initAdapters = async (
   elements: Element[],
   fillConfig: (t: ObjectType) => Promise<InstanceElement>
@@ -29,12 +23,14 @@ const initAdapters = async (
     return config
   }
 
-  const adapters = await mapValuesAsync(
-    adapterCreators,
-    async (
-      { create, configType }
-    ): Promise<Adapter> => create({ config: await findConfig(configType) }),
+  const adapterEntries = await Promise.all(
+    Object.entries(adapterCreators).map(async ([adapterName, creator]) => {
+      const adapter = await creator.create({ config: await findConfig(creator.configType) })
+      return [adapterName, adapter]
+    })
   )
+
+  const adapters = _.fromPairs(adapterEntries)
 
   return [adapters, newConfigs]
 }

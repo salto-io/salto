21e290649980feee2faf73e0305da69d4dc00e25
Roi Ronn <roi.ronn@salto.io>
2019-09-06 02:32:40 +0300

    additional changes

--- packages/salto/src/parser/salto.ts
+++ packages/salto/src/parser/salto.ts
@@ -9,6 +9,18 @@ import evaluate from './expressions'
 
 export type SourceMap = collections.map.DefaultMap<string, SourceRange[]>
 
+interface SourcePos {
+  line: number
+  col: number
+  byte: number
+}
+
+interface SourceRange {
+  filename: string
+  start: SourcePos
+  end: SourcePos
+}
+
 enum Keywords {
   TYPE_DEFINITION = 'type',
   LIST_DEFINITION = 'list',

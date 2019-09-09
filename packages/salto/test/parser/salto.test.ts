213ceb93ec8c1394c1a7f35b35c99217de5be11b
Ori Moisis <ori.moisis@salto.io>
2019-09-09 16:02:28 +0300

    add support for serializing parts of an element separately

--- packages/salto/test/parser/salto.test.ts
+++ packages/salto/test/parser/salto.test.ts
@@ -380,64 +380,88 @@ describe('Salto Dump', () => {
     }
   )
 
-  let body: string
+  describe('dump elements', () => {
+    let body: string
 
-  beforeAll(async () => {
-    body = await Parser.dump([strType, numType, boolType, fieldType, model, instance, config])
-  })
-
-  it('dumps primitive types', () => {
-    expect(body).toMatch(/type salesforce_string is string {/)
-    expect(body).toMatch(/type salesforce_number is number {/)
-    expect(body).toMatch(/type salesforce_bool is boolean {/)
-  })
-
-  it('dumps primitive field type annotations', () => {
-    expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?number alice {/s)
-    expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?number bob {/s)
-    expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?boolean tom {/s)
-    expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?string jerry {/s)
-  })
-
-  it('dumps instance elements', () => {
-    expect(body).toMatch(/salesforce_test me {/)
-  })
-
-  it('dumps config elements', () => {
-    expect(body).toMatch(/salesforce_test {/)
-  })
-
-  describe('dumped model', () => {
-    it('has correct block type and label', () => {
-      expect(body).toMatch(/type salesforce_test {/)
+    beforeAll(async () => {
+      body = await Parser.dump([strType, numType, boolType, fieldType, model, instance, config])
     })
-    it('has complex attributes', () => {
-      expect(body).toMatch(
-        /lead_convert_settings = {\s*account = \[\s*{\s*input\s*=\s*"bla",\s*output\s*=\s*"foo"\s*}\s*\]\s*}/m,
-      )
+
+    it('dumps primitive types', () => {
+      expect(body).toMatch(/type salesforce_string is string {/)
+      expect(body).toMatch(/type salesforce_number is number {/)
+      expect(body).toMatch(/type salesforce_bool is boolean {/)
     })
-    it('has fields', () => {
-      expect(body).toMatch(
-        /salesforce_string name {\s+label = "Name"\s+}/m,
-      )
-      expect(body).toMatch(
-        /salesforce_number num {/m,
-      )
-      expect(body).toMatch(
-        /list salesforce_string list {/m
-      )
+
+    it('dumps primitive field type annotations', () => {
+      expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?number alice {/s)
+      expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?number bob {/s)
+      expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?boolean tom {/s)
+      expect(body).toMatch(/type salesforce_field is number {.*?annotations {.*?string jerry {/s)
     })
-    it('can be parsed back', async () => {
-      const { elements, errors } = await Parser.parse(Buffer.from(body), 'none')
-      expect(errors.length).toEqual(0)
-      expect(elements.length).toEqual(7)
-      expect(elements[0]).toEqual(strType)
-      expect(elements[1]).toEqual(numType)
-      expect(elements[2]).toEqual(boolType)
-      TestHelpers.expectTypesToMatch(elements[3] as Type, fieldType)
-      TestHelpers.expectTypesToMatch(elements[4] as Type, model)
-      TestHelpers.expectInstancesToMatch(elements[5] as InstanceElement, instance)
-      TestHelpers.expectInstancesToMatch(elements[6] as InstanceElement, config)
+
+    it('dumps instance elements', () => {
+      expect(body).toMatch(/salesforce_test me {/)
+    })
+
+    it('dumps config elements', () => {
+      expect(body).toMatch(/salesforce_test {/)
+    })
+
+    describe('dumped model', () => {
+      it('has correct block type and label', () => {
+        expect(body).toMatch(/type salesforce_test {/)
+      })
+      it('has complex attributes', () => {
+        expect(body).toMatch(
+          /lead_convert_settings = {\s*account = \[\s*{\s*input\s*=\s*"bla",\s*output\s*=\s*"foo"\s*}\s*\]\s*}/m,
+        )
+      })
+      it('has fields', () => {
+        expect(body).toMatch(
+          /salesforce_string name {\s+label = "Name"\s+}/m,
+        )
+        expect(body).toMatch(
+          /salesforce_number num {/m,
+        )
+        expect(body).toMatch(
+          /list salesforce_string list {/m
+        )
+      })
+      it('can be parsed back', async () => {
+        const { elements, errors } = await Parser.parse(Buffer.from(body), 'none')
+        expect(errors.length).toEqual(0)
+        expect(elements.length).toEqual(7)
+        expect(elements[0]).toEqual(strType)
+        expect(elements[1]).toEqual(numType)
+        expect(elements[2]).toEqual(boolType)
+        TestHelpers.expectTypesToMatch(elements[3] as Type, fieldType)
+        TestHelpers.expectTypesToMatch(elements[4] as Type, model)
+        TestHelpers.expectInstancesToMatch(elements[5] as InstanceElement, instance)
+        TestHelpers.expectInstancesToMatch(elements[6] as InstanceElement, config)
+      })
+    })
+  })
+  describe('dump field', () => {
+    let body: string
+
+    beforeAll(async () => {
+      body = await Parser.dump(model.fields.name)
+    })
+
+    it('should contain only field', () => {
+      expect(body).toMatch(/^salesforce_string name {\s+label = "Name"\s+}$/m)
+    })
+  })
+  describe('dump attribute', () => {
+    let body: string
+
+    beforeAll(async () => {
+      body = await Parser.dump({ attr: 'value' })
+    })
+
+    it('should contain only attribute', () => {
+      expect(body).toMatch(/^attr\s+=\s+"value"$/m)
     })
   })
 })

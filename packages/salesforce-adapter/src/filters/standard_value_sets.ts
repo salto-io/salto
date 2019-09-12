ce78460f2d58eba7f11f7d7813772b3a672e29e7
Gilad Foyer <gilad.foyer@gmail.com>
2019-09-12 11:53:49 +0300

    Support Standard Value Set instances
    SalesForce API has a special representation and handling for standard value sets that can be shared among a few standard types
    (e.g. LeadSource value set is used in Account.AccountSource, Lead.LeadSourceand Opportunity.Source)
    Since there's no way to retrieve a list of all of them with the Metadata API,
    we will probably have to rely on this document:
    https://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/standardvalueset_names.htm
    (see JIRA task SALTO-144)
    This commit allows discovery and update of standard value sets
    references
    StandardValueSets bp’s will be created during the discovery process and stored under records/standard_value_set folder
    Updating refennces to standard values set is done bycreating a mapping
    from unique encoded strings of SF standard value set values (sort of naive hash)
    to the standard value set name.
    Next, all standard picklist fields are found their values  annotation
    is replaced with standard value set refernce (if one was found from the
    mapping)
    
    JIRA: SALTO-145 SALTO-146

---
+++ packages/salesforce-adapter/src/filters/standard_value_sets.ts
@@ -0,0 +1,209 @@
+import _ from 'lodash'
+import { MetadataInfo } from 'jsforce'
+import {
+  Element, ObjectType, InstanceElement, isObjectType, Field,
+} from 'adapter-api'
+import { collections } from '@salto/lowerdash'
+import SalesforceClient from '../client/client'
+import { FilterCreator } from '../filter'
+import {
+  CUSTOM_OBJECT, FIELD_TYPE_NAMES,
+  PICKLIST_VALUES, SALESFORCE_CUSTOM_SUFFIX,
+} from '../constants'
+import {
+  metadataType, apiName, createInstanceElement,
+} from '../transformer'
+
+const { makeArray } = collections.array
+
+
+export const STANDARD_VALUE_SET = 'StandardValueSet'
+export const STANDARD_VALUE = 'standard_value'
+
+
+type StandardValuesSets = Set<string>
+
+/*
+ * Standard values sets and references are specified in this API apendix:
+ * https://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/standardvalueset_names.htm
+ */
+
+const STANDARD_VALUE_SETS: StandardValuesSets = new Set<string>([
+  'AccountContactMultiRoles',
+  'AccountContactRole',
+  'AccountOwnership',
+  'AccountRating',
+  'AccountType',
+  'AddressCountryCode',
+  'AddressStateCode',
+  'AssetStatus',
+  'CampaignMemberStatus',
+  'CampaignStatus',
+  'CampaignType',
+  'CaseContactRole',
+  'CaseOrigin',
+  'CasePriority',
+  'CaseReason',
+  'CaseStatus',
+  'CaseType',
+  'ContactRole',
+  'ContractContactRole',
+  'ContractStatus',
+  'EntitlementType',
+  'EventSubject',
+  'EventType',
+  'FiscalYearPeriodName',
+  'FiscalYearPeriodPrefix',
+  'FiscalYearQuarterName',
+  'FiscalYearQuarterPrefix',
+  'IdeaCategory',
+  'IdeaMultiCategory',
+  'IdeaStatus',
+  'IdeaThemeStatus',
+  'Industry',
+  'InvoiceStatus',
+  'LeadSource',
+  'LeadStatus',
+  'OpportunityCompetitor',
+  'OpportunityStage',
+  'OpportunityType',
+  'OrderStatus',
+  'OrderType',
+  'PartnerRole',
+  'Product2Family',
+  'QuestionOrigin',
+  'QuickTextCategory',
+  'QuickTextChannel',
+  'QuoteStatus',
+  'SalesTeamRole',
+  'Salutation',
+  'ServiceContractApprovalStatus',
+  'SocialPostClassification',
+  'SocialPostEngagementLevel',
+  'SocialPostReviewedStatus',
+  'SolutionStatus',
+  'TaskPriority',
+  'TaskStatus',
+  'TaskSubject',
+  'TaskType',
+  'WorkOrderLineItemStatus',
+  'WorkOrderPriority',
+  'WorkOrderStatus',
+])
+
+const encodeValues = (values: string[]): string =>
+  values.sort().join(';')
+
+export const extractFullNamesFromValueList = (values: {full_name: string}[]): string[] =>
+  values.map(v => v.full_name)
+
+const svsValuesToRef = (svsInstances: InstanceElement[]): Record<string, string> => _.fromPairs(
+  svsInstances
+    .filter(i => i.value[STANDARD_VALUE])
+    .map(i => {
+      const standardValue = makeArray(i.value[STANDARD_VALUE])
+      return [encodeValues(extractFullNamesFromValueList(standardValue)), i.elemID.getFullName()]
+    })
+)
+
+const isStandardPickList = (f: Field): boolean =>
+  (f.type.elemID.name === FIELD_TYPE_NAMES.PICKLIST
+    || f.type.elemID.name === FIELD_TYPE_NAMES.MULTIPICKLIST)
+  && !apiName(f).endsWith(SALESFORCE_CUSTOM_SUFFIX)
+
+const calculatePicklistFieldsToUpdate = (
+  custObjectFields: Record<string, Field>,
+  svsValuesToName: Record<string, string>
+): Record<string, Field> => _.mapValues(custObjectFields, (f: Field) => {
+  if (!isStandardPickList(f) || _.isEmpty(f.annotations[PICKLIST_VALUES])) {
+    return f
+  }
+
+  const encodedPlVals = encodeValues(f.annotations[PICKLIST_VALUES])
+  const foundStandardValueSet = svsValuesToName[encodedPlVals]
+
+  if (!foundStandardValueSet) {
+    return f
+  }
+  const newField = f.clone()
+  newField.annotations[PICKLIST_VALUES] = foundStandardValueSet
+  return newField
+})
+
+
+const createStandardValueSetInstances = (
+  valueSets: MetadataInfo[],
+  svsMetadataType: ObjectType
+): InstanceElement[] => valueSets
+  .filter(vs => vs.fullName)
+  .map((svs: MetadataInfo) =>
+    createInstanceElement(svs, svsMetadataType))
+
+const findStandardValueSetType = (elements: Element[]): ObjectType | undefined =>
+  _.find(
+    elements,
+    (element: Element) => metadataType(element) === STANDARD_VALUE_SET
+  ) as ObjectType | undefined
+
+const discoverStandardValueSets = async (
+  standardValueSets: Set<string>,
+  client: SalesforceClient): Promise<MetadataInfo[]> =>
+  _.flatten(await Promise.all(
+    [...standardValueSets].map(
+      (svsName: string) => client.readMetadata(STANDARD_VALUE_SET, svsName)
+    )
+  ))
+
+const createSVSInstances = async (
+  standardValueSetNames: Set<string>,
+  client: SalesforceClient,
+  svsMetadataType: ObjectType): Promise<InstanceElement[]> => {
+  const valueSets = await discoverStandardValueSets(standardValueSetNames, client)
+  const svsInstances = createStandardValueSetInstances(valueSets, svsMetadataType)
+  return svsInstances
+}
+
+const updateSVSReferences = (elements: Element[], svsInstances: InstanceElement[]): void => {
+  const svsValuesToName = svsValuesToRef(svsInstances)
+  const customObjectTypeElements = elements
+    .filter(isObjectType)
+    .filter(o => metadataType(o) === CUSTOM_OBJECT)
+
+  customObjectTypeElements.forEach((custObjType: ObjectType) => {
+    const fieldsToUpdate = calculatePicklistFieldsToUpdate(custObjType.fields, svsValuesToName)
+    _.assign(custObjType, { fields: fieldsToUpdate })
+  })
+}
+
+
+/**
+* Declare the StandardValueSets filter that
+* adds the fixed collection of standard value sets in SFDC
+* and modify reference in discovered elements that uses them.
+*/
+export const makeFilter = (
+  standardValueSetNames: StandardValuesSets
+): FilterCreator => ({ client }) => ({
+  /**
+   * Upon discover, retrieve standard value sets and
+   * modify references to them in discovered elements
+   *
+   * @param elements the already discovered elements
+   */
+  onDiscover: async (elements: Element[]): Promise<void> => {
+    const svsMetadataType: ObjectType | undefined = findStandardValueSetType(elements)
+    if (svsMetadataType !== undefined) {
+      const svsInstances = await createSVSInstances(standardValueSetNames, client, svsMetadataType)
+      elements.push(...svsInstances)
+      updateSVSReferences(elements, svsInstances)
+    } else {
+      // [GF] No StandardValueSet MetadataType was found.
+      // Is this considered an error?
+      // Not sure about handling this case,
+      // we want to at least log this for sure.
+    }
+  },
+})
+
+
+export default makeFilter(STANDARD_VALUE_SETS)
---
+++ packages/salesforce-adapter/test/filters/standard_value_sets.test.ts
@@ -0,0 +1,142 @@
+import {
+  ObjectType, ElemID, Element, InstanceElement, Field, Type,
+} from 'adapter-api'
+import { MetadataInfo } from 'jsforce'
+import * as constants from '../../src/constants'
+import { FilterWith } from '../../src/filter'
+import mockClient from '../client'
+import {
+  makeFilter, extractFullNamesFromValueList, STANDARD_VALUE_SET, STANDARD_VALUE,
+} from '../../src/filters/standard_value_sets'
+import SalesforceClient from '../../src/client/client'
+import { Types } from '../../src/transformer'
+
+const createStandardValueSetMetadataInfo = (name: string, values: string[]): MetadataInfo =>
+  ({
+    fullName: name,
+    sorted: false,
+    standardValue: values.map(v => (
+      {
+        fullName: v,
+        default: 'false',
+        label: v,
+      })),
+  } as MetadataInfo)
+
+
+const createPicklistObjectType = (
+  mockElemID: ElemID,
+  apiName: string,
+  pickListValues: string[],
+  isMultiPicklist = false
+): ObjectType => new ObjectType({
+  elemID: mockElemID,
+  fields: {
+    state: new Field(
+      mockElemID,
+      'simps',
+      isMultiPicklist
+        ? Types.salesforceDataTypes[constants.FIELD_TYPE_NAMES.MULTIPICKLIST]
+        : Types.salesforceDataTypes[constants.FIELD_TYPE_NAMES.PICKLIST], {
+        [Type.REQUIRED]: false,
+        [Type.DEFAULT]: 'Bart',
+        [constants.API_NAME]: apiName,
+        label: 'test label',
+        values: pickListValues,
+      }
+    ),
+  },
+  annotations: {
+    [constants.METADATA_TYPE]: constants.CUSTOM_OBJECT,
+  },
+})
+
+/* eslint-disable jest/no-focused-tests */
+describe('Standard Value Sets filter', () => {
+  type FilterType = FilterWith<'onDiscover'>
+  const { client } = mockClient()
+  const mockSVSType = new ObjectType({
+    annotationTypes: {},
+    elemID: new ElemID(constants.SALESFORCE, 'standard_value_set'),
+  })
+  mockSVSType.annotations[constants.METADATA_TYPE] = STANDARD_VALUE_SET
+
+
+  const filterCreator = (sfClient: SalesforceClient): FilterType =>
+    makeFilter(new Set<string>(['Simpsons', 'Numbers']))({ client: sfClient }) as FilterType
+
+  let filter: FilterType
+
+  beforeEach(() => {
+    client.readMetadata = jest.fn()
+      .mockImplementationOnce(() => createStandardValueSetMetadataInfo('Simpsons', ['Bart', 'Homer', 'Lisa']))
+      .mockImplementationOnce(() => createStandardValueSetMetadataInfo('Numbers', ['One', 'Two', 'Three']))
+    filter = filterCreator(client)
+  })
+
+
+  it('should do nothing if no standard value set element was found', async () => {
+    const elements: Element[] = []
+    await filter.onDiscover(elements)
+    expect(client.readMetadata).toHaveBeenCalledTimes(0)
+    expect(elements.length).toBe(0)
+  })
+
+  it('should add standard value set instances', async () => {
+    const elements: Element[] = [mockSVSType.clone()]
+    await filter.onDiscover(elements)
+    expect(client.readMetadata).toHaveBeenCalledTimes(2)
+    expect(elements.length).toBe(3)
+    const simpsonsSvs = elements[1]
+    expect(simpsonsSvs.elemID.name).toBe('standard_value_set_simpsons')
+    expect(simpsonsSvs.path).toEqual(['records', 'standard_value_set', 'simpsons'])
+    expect(extractFullNamesFromValueList((simpsonsSvs as InstanceElement).value[STANDARD_VALUE])).toEqual(['Bart', 'Homer', 'Lisa'])
+    const numbersSvs = elements[2]
+    expect(numbersSvs.elemID.name).toBe('standard_value_set_numbers')
+    expect(numbersSvs.path).toEqual(['records', 'standard_value_set', 'numbers'])
+    expect(extractFullNamesFromValueList((numbersSvs as InstanceElement).value[STANDARD_VALUE])).toEqual(['One', 'Two', 'Three'])
+  })
+  it('should replace value list with references for standard picklist fields', async () => {
+    const apiName = 'simps'
+    const pickListValues = ['Bart', 'Homer', 'Lisa']
+    const mockElemID = new ElemID(constants.SALESFORCE, 'test')
+    const typeElement = createPicklistObjectType(mockElemID, apiName, pickListValues)
+    const elements: Element[] = [mockSVSType.clone(), typeElement]
+    await filter.onDiscover(elements)
+    expect(elements.length).toBe(4)
+    expect(typeElement.fields.state.annotations[constants.PICKLIST_VALUES]).toBe('salesforce_standard_value_set_simpsons')
+  })
+
+  it('should replace value list with references for standard multipicklist fields', async () => {
+    const apiName = 'simps'
+    const pickListValues = ['Bart', 'Homer', 'Lisa']
+    const mockElemID = new ElemID(constants.SALESFORCE, 'test')
+    const typeElement = createPicklistObjectType(mockElemID, apiName, pickListValues, true)
+    const elements: Element[] = [mockSVSType.clone(), typeElement]
+    await filter.onDiscover(elements)
+    expect(elements.length).toBe(4)
+    expect(typeElement.fields.state.annotations[constants.PICKLIST_VALUES]).toBe('salesforce_standard_value_set_simpsons')
+  })
+
+  it('should not replace value list with references for custom picklist fields', async () => {
+    const apiName = 'simpsy__c'
+    const pickListValues = ['Bart', 'Homer', 'Lisa']
+    const mockElemID = new ElemID(constants.SALESFORCE, 'test')
+    const typeElement = createPicklistObjectType(mockElemID, apiName, pickListValues)
+    const elements: Element[] = [mockSVSType.clone(), typeElement]
+    await filter.onDiscover(elements)
+    expect(elements.length).toBe(4)
+    expect(typeElement.fields.state.annotations[constants.PICKLIST_VALUES]).toEqual(pickListValues)
+  })
+
+  it('should not replace value list with references for standard picklist fields if svs with values not found', async () => {
+    const apiName = 'simps'
+    const pickListValues = ['Marge', 'Homer', 'Lisa']
+    const mockElemID = new ElemID(constants.SALESFORCE, 'test')
+    const typeElement = createPicklistObjectType(mockElemID, apiName, pickListValues)
+    const elements: Element[] = [mockSVSType.clone(), typeElement]
+    await filter.onDiscover(elements)
+    expect(elements.length).toBe(4)
+    expect(typeElement.fields.state.annotations[constants.PICKLIST_VALUES]).toEqual(pickListValues)
+  })
+})

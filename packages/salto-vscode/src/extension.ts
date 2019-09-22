62c9fad0b656017689ce9bfba31730671c6c9236
Roi Ronn <roi.ronn@salto.io>
2019-09-22 15:40:03 +0300

    cr fixes

--- packages/salto-vscode/src/extension.ts
+++ packages/salto-vscode/src/extension.ts
@@ -63,10 +63,12 @@ const createCompletionsProvider = (
     position: vscode.Position
   ) => {
     const workspace = workspaces[workspaceName]
-    const context = getPositionContext(workspace, doc.getText(), doc.fileName, {
-      line: position.line + 1,
-      col: position.character,
-    })
+    const context = getPositionContext(
+      workspace, 
+      doc.getText(), 
+      doc.fileName, 
+      vsPosToSaltoPos(position)
+    )
     const line = doc.lineAt(position).text.substr(0, position.character)
     return provideWorkspaceCompletionItems(workspace, context, line).map(
       ({ label, reInvoke, insertText }) => {

b4b8770109ba745cbeac2ea8120886790b8fdec4
hadard <hadar.davidovich@salto.io>
2019-09-24 13:49:53 +0300

    fix for SALTO-93 - remove restictions on flow_metadata_value.name field (#203)

---
+++ packages/salesforce-adapter/src/filters/flow.ts
@@ -0,0 +1,29 @@
+
+import _ from 'lodash'
+import {
+  Element, isObjectType, Type,
+} from 'adapter-api'
+import { FilterWith } from '../filter'
+
+export const FLOW_METADATA_VALUE_TYPE_NAME = 'flow_metadata_value'
+
+/**
+ * Create filter that handles flow type/instances corner case.
+ */
+const filterCreator = (): FilterWith<'onDiscover'> => ({
+  /**
+   * Upon discover remove restrictions from flow_metadata_value.name.
+   *
+   * @param elements the already discovered elements
+   */
+  onDiscover: async (elements: Element[]): Promise<void> => {
+    // fix flow_metadata_value - remove resrictions from name, see: SALTO-93
+    const flowMetadataValue = _(elements).filter(isObjectType)
+      .find(e => e.elemID.name === FLOW_METADATA_VALUE_TYPE_NAME)
+    if (flowMetadataValue && flowMetadataValue.fields.name) {
+      delete flowMetadataValue.fields.name.annotations[Type.RESTRICTION]
+    }
+  },
+})
+
+export default filterCreator
---
+++ packages/salesforce-adapter/test/filters/flow.test.ts
@@ -0,0 +1,25 @@
+import {
+  ObjectType, ElemID, Field, BuiltinTypes, Type,
+} from 'adapter-api'
+import filterCreator from '../../src/filters/flow'
+import { SALESFORCE } from '../../src/constants'
+
+describe('flow filter', () => {
+  const filter = filterCreator()
+  const elemID = new ElemID(SALESFORCE, 'flow_metadata_value')
+  const mockFlow = new ObjectType({
+    elemID,
+    fields: {
+      name: new Field(elemID, 'name', BuiltinTypes.STRING,
+        {
+          [Type.RESTRICTION]: ['ObjectType', 'TriggerType', 'ObjectVariable', 'OldObjectVariable',
+            'RecursiveCountVariable', 'EventType'],
+        }),
+    },
+  })
+
+  it('remove restrictions from flow_metadata_value.name', () => {
+    filter.onDiscover([mockFlow])
+    expect(mockFlow.fields.name.annotations[Type.RESTRICTION]).toBeUndefined()
+  })
+})

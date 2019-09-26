6b259fcf78b55565dbd0b633d5e75fdd073c61d7
Adam Rosenthal <adamro87@gmail.com>
2019-09-26 16:01:03 +0300

    Fix a bug with empty LeadConvertSettings (#220)

--- packages/salesforce-adapter/src/filters/lead_convert_settings.ts
+++ packages/salesforce-adapter/src/filters/lead_convert_settings.ts
@@ -41,24 +41,24 @@ const filterCreator: FilterCreator = ({ client }) => ({
       .find(e => isTypeName(e, LEAD_CONVERT_SETTINGS_TYPE))
 
     if (lead && convertType) {
-      lead.annotationTypes[CONVERT_SETTINGS_ANNOTATION] = convertType
-      const value = convertInstance ? convertInstance.value : {}
-      // Remove fullName from the value and type
-      delete value[bpCase(METADATA_OBJECT_NAME_FIELD)]
       delete convertType.fields[bpCase(METADATA_OBJECT_NAME_FIELD)]
+      lead.annotationTypes[CONVERT_SETTINGS_ANNOTATION] = convertType
 
-      // Fix list values where needed - convert_list filter is not running on annotations.
-      // As annotations are created case by case in the adapter I think it's ok to keep
-      // manual fix behavior for now.
-      value[OBJECT_MAPPING_FIELD] = makeArray(value[OBJECT_MAPPING_FIELD])
-      _.forEach(value[OBJECT_MAPPING_FIELD], mapping => {
-        mapping[MAPPING_FIELDS_FIELD] = makeArray(mapping[MAPPING_FIELDS_FIELD])
-      })
-
-      lead.annotate({ [CONVERT_SETTINGS_ANNOTATION]: transform(value, convertType, false) || {} })
-
-      // Remove the instance if it exists
       if (convertInstance) {
+        const { value } = convertInstance
+        // Remove fullName from the value and type
+        delete value[bpCase(METADATA_OBJECT_NAME_FIELD)]
+
+        // Fix list values where needed - convert_list filter is not running on annotations.
+        // As annotations are created case by case in the adapter I think it's ok to keep
+        // manual fix behavior for now.
+        value[OBJECT_MAPPING_FIELD] = makeArray(value[OBJECT_MAPPING_FIELD])
+        _.forEach(value[OBJECT_MAPPING_FIELD], mapping => {
+          mapping[MAPPING_FIELDS_FIELD] = makeArray(mapping[MAPPING_FIELDS_FIELD])
+        })
+
+        lead.annotate({ [CONVERT_SETTINGS_ANNOTATION]: transform(value, convertType, false) || {} })
+
         const index = elements.findIndex(e => e.elemID.name === convertInstance.elemID.name)
         elements.splice(index, 1)
       }

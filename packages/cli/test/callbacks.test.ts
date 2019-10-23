1bae7570f225b72356f76ba9933da749781edf37
Adam Rosenthal <adamro87@gmail.com>
2019-10-23 14:35:26 +0300

    Use the password input for specific user inputs in the cli instead of shown text (#228)

--- packages/cli/test/callbacks.test.ts
+++ packages/cli/test/callbacks.test.ts
@@ -4,11 +4,13 @@ import { getFieldInputType } from '../src/callbacks'
 
 describe('callbacks', () => {
   it('should create proper inquirer field', async () => {
-    const stRes = getFieldInputType(BuiltinTypes.STRING)
-    const iRes = getFieldInputType(BuiltinTypes.NUMBER)
-    const bRes = getFieldInputType(BuiltinTypes.BOOLEAN)
+    const stRes = getFieldInputType(BuiltinTypes.STRING, 'st')
+    const iRes = getFieldInputType(BuiltinTypes.NUMBER, 'i')
+    const bRes = getFieldInputType(BuiltinTypes.BOOLEAN, 'b')
+    const passRes = getFieldInputType(BuiltinTypes.STRING, 'password')
     expect(iRes).toBe('number')
     expect(bRes).toBe('confirm')
     expect(stRes).toBe('input')
+    expect(passRes).toBe('password')
   })
 })

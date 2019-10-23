e85130896aa982b91fca79e14dd82667b48b6479
Roi Ronn <roi.ronn@salto.io>
2019-10-23 08:47:08 +0300

    changed ext pakcage script to use rsync

--- packages/salto-vscode/scripts/package.sh
+++ packages/salto-vscode/scripts/package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ./tmp_pkg && vsce package --yarn -o ./tmp_pkg/salto.vsix
 cd ./tmp_pkg && unzip salto.vsix 
-cp -r ../node_modules/ extension/node_modules/ 
+rsync -r ../node_modules/ extension/node_modules/ 
 zip -ur salto.vsix extension 
 cd ..
 mkdir -p ./pkg 

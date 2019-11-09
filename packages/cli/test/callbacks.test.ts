c22aa293d3e3196b58db20d80953f55e91af15b7
Hadar Davidovich <hadar.davidovich@salto.io>
2019-11-09 21:22:55 +0200

    getApprovedChanges should return changes in case of interactive=false

--- packages/cli/test/callbacks.test.ts
+++ packages/cli/test/callbacks.test.ts
@@ -1,9 +1,13 @@
 
 import { BuiltinTypes } from 'adapter-api'
-import { getFieldInputType } from '../src/callbacks'
+import { getFieldInputType, getApprovedChanges } from '../src/callbacks'
+import { dummyChanges } from './mocks'
 
+jest.mock('inquirer', () => ({
+  prompt: jest.fn().mockImplementation(() => Promise.resolve({ 0: 'yes', 1: 'no' })),
+}))
 describe('callbacks', () => {
-  it('should create proper inquirer field', async () => {
+  it('should create proper inquirer field', () => {
     const stRes = getFieldInputType(BuiltinTypes.STRING, 'st')
     const iRes = getFieldInputType(BuiltinTypes.NUMBER, 'i')
     const bRes = getFieldInputType(BuiltinTypes.BOOLEAN, 'b')
@@ -12,5 +16,24 @@ describe('callbacks', () => {
     expect(bRes).toBe('confirm')
     expect(stRes).toBe('input')
     expect(passRes).toBe('password')
+  })
+
+  describe('getApprovedChanges', () => {
+    const fetchChanges = dummyChanges.map(c => ({ change: c, serviceChange: c }))
+    it('should return all non conflict changes interactive=false', async () => {
+      const approved = await getApprovedChanges(fetchChanges, false)
+      expect(approved).toHaveLength(fetchChanges.length)
+    })
+
+    it('should return only approved changes interactive=true', async () => {
+      const approved = await getApprovedChanges(fetchChanges, true)
+      expect(approved).toHaveLength(1)
+    })
+
+    it('should ask for conflicts return only approved changes interactive=true', async () => {
+      const approved = await getApprovedChanges(fetchChanges
+        .map(c => ({ ...c, pendingChange: c.change })), false)
+      expect(approved).toHaveLength(1)
+    })
   })
 })

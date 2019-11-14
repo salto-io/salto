bb2452684288f45d728cbb4950945ca9437ed24e
Yariv Brosh <yarivbrosh@Yarivs-MacBook-Pro.local>
2019-11-14 21:09:33 +0200

    CR fixes + tests

--- packages/salesforce-adapter/src/filters/workflow_actions.ts
+++ packages/salesforce-adapter/src/filters/workflow_actions.ts
@@ -1,5 +1,5 @@
 import {
-  Element, ObjectType, ElemID, Field, findObjectType, findInstances, isInstanceElement,
+  Element, ObjectType, ElemID, Field, findObjectType, findInstances, isInstanceElement, FieldMap,
 } from 'adapter-api'
 import _ from 'lodash'
 import wu from 'wu'
@@ -7,20 +7,35 @@ import { SALESFORCE } from '../constants'
 import { FilterCreator } from '../filter'
 
 export const ACTIONS = 'actions'
-export const WORKFLOW_ALERT = 'workflow_alert'
-export const WORKFLOW_FIELD_UPDATE = 'workflow_field_update'
-export const WORKFLOW_OUTBOUND_MESSAGE = 'workflow_outbound_message'
-export const WORKFLOW_KNOWLEDGE_PUBLISH = 'workflow_knowledge_publish'
-export const WORKFLOW_TASK = 'workflow_task'
-export const WORKFLOW_SEND = 'workflow_send'
 export const WORKFLOW_RULE = 'workflow_rule'
 export const WORKFLOW_ACTIONS = 'workflow_actions'
-export const ALERTS_FIELD_NAME = 'alerts'
-export const FIELD_UPDATES_FIELD_NAME = 'field_updates'
-export const OUTBOUND_MESSAGES_FIELD_NAME = 'outbound_messages'
-export const TASKS_FIELD_NAME = 'tasks'
-export const KNOWLEDGE_PUBLISHES_FIELD_NAME = 'knowledge_publishes'
-export const SEND_FIELD_NAME = 'send'
+export const WORKFLOW_FIELD_TO_TYPE_MAP = {
+  ALERT: ['alerts', 'workflow_alert'],
+  FIELD_UPDATE: ['field_updates', 'workflow_field_update'],
+  OUTBOUND_MESSAGE: ['outbound_messages', 'workflow_outbound_message'],
+  KNOWLEDGE_PUBLISH: ['knowledge_publishes', 'workflow_knowledge_publish'],
+  TASK: ['tasks', 'workflow_task'],
+  SEND: ['send', 'workflow_send'],
+}
+
+const FIELDS_TO_ASSIGN = [
+  WORKFLOW_FIELD_TO_TYPE_MAP.ALERT[0],
+  WORKFLOW_FIELD_TO_TYPE_MAP.FIELD_UPDATE[0],
+  WORKFLOW_FIELD_TO_TYPE_MAP.OUTBOUND_MESSAGE[0],
+  WORKFLOW_FIELD_TO_TYPE_MAP.KNOWLEDGE_PUBLISH[0],
+  WORKFLOW_FIELD_TO_TYPE_MAP.TASK[0],
+  WORKFLOW_FIELD_TO_TYPE_MAP.SEND[0],
+]
+
+const OBJECT_TYPES_TO_REMOVE = new Set(
+  [WORKFLOW_FIELD_TO_TYPE_MAP.ALERT[1],
+    WORKFLOW_FIELD_TO_TYPE_MAP.FIELD_UPDATE[1],
+    WORKFLOW_FIELD_TO_TYPE_MAP.OUTBOUND_MESSAGE[1],
+    WORKFLOW_FIELD_TO_TYPE_MAP.KNOWLEDGE_PUBLISH[1],
+    WORKFLOW_FIELD_TO_TYPE_MAP.TASK[1],
+    WORKFLOW_FIELD_TO_TYPE_MAP.SEND[1],
+    WORKFLOW_RULE]
+)
 
 /**
  * Embed actions inside workflow records
@@ -47,47 +62,27 @@ const filterCreator: FilterCreator = () => ({
         return
       }
 
+      const getFields = (
+        elems: Element[],
+        fieldsToObjectTypesMap: Record<string, string[]>
+      ): FieldMap => {
+        const actionsElemId = new ElemID(SALESFORCE, WORKFLOW_ACTIONS)
+        const result: FieldMap = {}
+        Object.values(fieldsToObjectTypesMap).forEach(mapping => {
+          result[mapping[0]] = new Field(
+            actionsElemId, mapping[0], findObjectType(
+              elems,
+              new ElemID(SALESFORCE, mapping[1])
+            ) as ObjectType, {}, true
+          )
+        })
+        return result
+      }
+
       const actionsElemId = new ElemID(SALESFORCE, WORKFLOW_ACTIONS)
       const workflowActionsType = new ObjectType({
         elemID: actionsElemId,
-        fields: {
-          [ALERTS_FIELD_NAME]: new Field(
-            actionsElemId, ALERTS_FIELD_NAME, findObjectType(
-              elements,
-              new ElemID(SALESFORCE, WORKFLOW_ALERT)
-            ) as ObjectType, {}, true
-          ),
-          [FIELD_UPDATES_FIELD_NAME]: new Field(
-            actionsElemId, FIELD_UPDATES_FIELD_NAME, findObjectType(
-              elements,
-              new ElemID(SALESFORCE, WORKFLOW_FIELD_UPDATE)
-            ) as ObjectType, {}, true
-          ),
-          [OUTBOUND_MESSAGES_FIELD_NAME]: new Field(
-            actionsElemId, OUTBOUND_MESSAGES_FIELD_NAME, findObjectType(
-              elements,
-              new ElemID(SALESFORCE, WORKFLOW_OUTBOUND_MESSAGE)
-            ) as ObjectType, {}, true
-          ),
-          [TASKS_FIELD_NAME]: new Field(
-            actionsElemId, TASKS_FIELD_NAME, findObjectType(
-              elements,
-              new ElemID(SALESFORCE, WORKFLOW_TASK)
-            ) as ObjectType, {}, true
-          ),
-          [KNOWLEDGE_PUBLISHES_FIELD_NAME]: new Field(
-            actionsElemId, KNOWLEDGE_PUBLISHES_FIELD_NAME, findObjectType(
-              elements,
-              new ElemID(SALESFORCE, WORKFLOW_KNOWLEDGE_PUBLISH)
-            ) as ObjectType, {}, true
-          ),
-          [SEND_FIELD_NAME]: new Field(
-            actionsElemId, SEND_FIELD_NAME, findObjectType(
-              elements,
-              new ElemID(SALESFORCE, WORKFLOW_SEND)
-            ) as ObjectType, {}, true
-          ),
-        },
+        fields: getFields(elements, WORKFLOW_FIELD_TO_TYPE_MAP),
       })
       workflowActionsType.path = ['types', 'subtypes', WORKFLOW_ACTIONS]
       const actionField = workFlowRuleObjectType.fields[ACTIONS]
@@ -101,23 +96,16 @@ const filterCreator: FilterCreator = () => ({
      */
     const changeWorkFlowInstances = (elements: Element[]): void => {
       const workflows = findInstances(elements, new ElemID(SALESFORCE, 'workflow'))
-      wu(workflows).forEach(workflow => {
-        workflow.value.rules.actions = {}
-        const { actions } = workflow.value.rules
-        const fieldsToAssign = [ALERTS_FIELD_NAME,
-          FIELD_UPDATES_FIELD_NAME,
-          OUTBOUND_MESSAGES_FIELD_NAME,
-          TASKS_FIELD_NAME,
-          KNOWLEDGE_PUBLISHES_FIELD_NAME,
-          SEND_FIELD_NAME]
-        fieldsToAssign.forEach(field => {
-          if (workflow.value[field]) {
-            _.assign(
-              actions,
-              { [field]: workflow.value[field] }
-            )
-          }
-        })
+      wu(workflows).map(w => w.value).forEach(value => {
+        value.rules.actions = {}
+        const { actions } = value.rules
+        const workflowFields = _(FIELDS_TO_ASSIGN)
+          .map(field => [field, value[field]])
+          .fromPairs()
+          .pickBy() // filter undefined values
+          .value()
+
+        Object.assign(actions, workflowFields)
       })
     }
 
@@ -127,15 +115,8 @@ const filterCreator: FilterCreator = () => ({
      * @param elements the list of elements
      */
     const removeNestedInstances = (elements: Element[]): void => {
-      _.remove(elements, elem => isInstanceElement(elem) && (
-        [WORKFLOW_ALERT,
-          WORKFLOW_FIELD_UPDATE,
-          WORKFLOW_OUTBOUND_MESSAGE,
-          WORKFLOW_KNOWLEDGE_PUBLISH,
-          WORKFLOW_TASK,
-          WORKFLOW_SEND,
-          WORKFLOW_RULE].includes(elem.type.elemID.name)
-      ))
+      _.remove(elements,
+        elem => isInstanceElement(elem) && OBJECT_TYPES_TO_REMOVE.has(elem.type.elemID.name))
     }
 
     changeWorkFlowRule(allElements)

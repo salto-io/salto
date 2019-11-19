534c7ac25bc903135ac6cbb229de564599e58500
roironn <roi.ronn@salto.io>
2019-11-19 12:57:01 +0200

    order before element by after element in vscode diff (#439)

--- packages/salto-vscode/src/format.ts
+++ packages/salto-vscode/src/format.ts
@@ -1,10 +1,58 @@
 import * as Diff from 'diff'
 import { Diff2Html } from 'diff2html'
-import { Element, getChangeElement } from 'adapter-api'
+import { Element, getChangeElement, isObjectType, isInstanceElement, isField, ObjectType, isPrimitiveType, PrimitiveType, Field, Value } from 'adapter-api'
 import { dump, PlanItem } from 'salto'
 import wu from 'wu'
+import _ from 'lodash'
 
 export type UnifiedDiff = string
+
+const orderByAfterElement = (target: Element, ref: Element | undefined): Element => {
+  const orderMapBy = (
+    targetMap: Record<string, Value|Element|Field>,
+    refMap: Record<string, Value|Element|Field>
+  ): Record<string, Value|Element|Field> => _(targetMap)
+    .toPairs()
+    .sortBy(([key, _v]) => _.keys(refMap).indexOf(key))
+    .fromPairs()
+    .value()
+
+  if (isObjectType(target) && isObjectType(ref)) {
+    return new ObjectType({
+      elemID: target.elemID,
+      fields: orderMapBy(target.fields, ref.fields),
+      annotationTypes: orderMapBy(target.annotationTypes, ref.annotationTypes),
+      annotations: orderMapBy(target.annotations, ref.annotations),
+    })
+  }
+
+  if (isPrimitiveType(target) && isPrimitiveType(ref)) {
+    return new PrimitiveType({
+      elemID: target.elemID,
+      primitive: target.primitive,
+      annotationTypes: orderMapBy(target.annotationTypes, ref.annotationTypes),
+      annotations: orderMapBy(target.annotations, ref.annotations),
+    })
+  }
+
+  if (isField(target) && isField(ref)) {
+    return new Field(
+      target.parentID,
+      target.name,
+      target.type,
+      orderMapBy(target.annotations, ref.annotations),
+      target.isList
+    )
+  }
+
+  if (isInstanceElement(target) && isInstanceElement(ref)) {
+    const res = target.clone()
+    res.value = orderMapBy(target.value, ref.value)
+    return res
+  }
+
+  return target
+}
 
 export const getActionName = (
   change: PlanItem,
@@ -22,11 +70,15 @@ export const getActionName = (
 
 export const createChangeDiff = async (
   stepIndex: number,
-  change: PlanItem
+  change: PlanItem,
 ): Promise<UnifiedDiff> => {
   const changeData = change.parent().data as {before?: Element; after?: Element}
-  const beforeHCL = changeData.before ? dump([changeData.before]) : ''
-  const afterHCL = changeData.after ? dump([changeData.after]) : ''
+  const beforeHCL = changeData.before
+    ? dump([orderByAfterElement(changeData.before, changeData.after)])
+    : ''
+  const afterHCL = changeData.after
+    ? dump([changeData.after])
+    : ''
   const step = `Step ${stepIndex} - `
   const patchName = `${step}${getActionName(change)}`
   return Diff.createPatch(

57e907a9943012105855fb6c62af2fb63df13a3b
Oren Digmi <oren.digmi@salto.io>
2019-12-01 17:17:04 +0200

    Added MadKudu wrapper for the HubSpot API

--- packages/hubspot-adapter/src/main.ts
+++ packages/hubspot-adapter/src/main.ts
@@ -1,4 +1,13 @@
-export const firstFunc = (): void => {
+import {
+  RequestPromise,
+} from 'requestretry'
+import HubspotClient from './client/client'
+
+export const firstFunc = (): RequestPromise => {
 // eslint-disable-next-line no-console
   console.log('Welcome to hubspot adapter')
+
+  const testClient = new HubspotClient('d0b5e073-694e-4335-a9d6-9f8fdf4eb9d5')
+
+  return testClient.getAllContacts()
 }

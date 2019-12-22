eaf1834fc8e0124b4d8f2ad0de7e0ab9ee61470d
Omri Litvak <omri.litvak@salto.io>
2019-12-22 16:43:18 +0200

    Salto-394: Support XSD Types for instances

--- packages/salesforce-adapter/test/filters/convert_types.test.ts
+++ packages/salesforce-adapter/test/filters/convert_types.test.ts
@@ -23,12 +23,21 @@ describe('convert types filter', () => {
       numAsNum: new Field(mockObjId, 'numAsNum', BuiltinTypes.NUMBER),
       numAsStr: new Field(mockObjId, 'numAsStr', BuiltinTypes.NUMBER),
       nullStr: new Field(mockObjId, 'nullStr', BuiltinTypes.STRING),
+      values: new Field(mockObjId, 'values', new ObjectType({
+        elemID: mockObjId,
+        fields: {
+          field: new Field(mockObjId, 'field', BuiltinTypes.STRING),
+          value: new Field(mockObjId, 'value', BuiltinTypes.STRING),
+        },
+      }), {}, true),
       numArray: new Field(mockObjId, 'numArray', BuiltinTypes.NUMBER, {}, true),
       picklist: new Field(mockObjId, 'picklist', BuiltinTypes.STRING,
         { [Type.VALUES]: ['a', 'b', 'c'], [Type.RESTRICTION]: { [Type.ENFORCE_VALUE]: true } }),
     },
   })
-
+  const XSI_TYPE = 'xsi_type'
+  const UNDERSCORE = '_'
+  const EMPTY_STRING = ''
   const mockInstance = new InstanceElement(
     'test_inst_with_list',
     mockType,
@@ -40,7 +49,42 @@ describe('convert types filter', () => {
       numAsStr: '12',
       numAsNum: 12,
       // eslint-disable-next-line @typescript-eslint/camelcase
-      nullStr: { _: { xsi_nil: 'true' } },
+      nullStr: { '': { xsi_nil: 'true' } },
+      values: [{
+        field: 'xsdString',
+        value: {
+          [UNDERSCORE]: 'stringVal',
+          [EMPTY_STRING]: { [XSI_TYPE]: 'xsd:string' },
+        },
+      },
+      {
+        field: 'xsdBoolean',
+        value: {
+          [UNDERSCORE]: 'false',
+          [EMPTY_STRING]: { [XSI_TYPE]: 'xsd:boolean' },
+        },
+      },
+      {
+        field: 'xsdInt',
+        value: {
+          [UNDERSCORE]: '6',
+          [EMPTY_STRING]: { [XSI_TYPE]: 'xsd:int' },
+        },
+      },
+      {
+        field: 'xsdLong',
+        value: {
+          [UNDERSCORE]: '6.1',
+          [EMPTY_STRING]: { [XSI_TYPE]: 'xsd:long' },
+        },
+      },
+      {
+        field: 'xsdDouble',
+        value: {
+          [UNDERSCORE]: '6.2',
+          [EMPTY_STRING]: { [XSI_TYPE]: 'xsd:double' },
+        },
+      }],
       numArray: ['12', '13', '14'],
       picklist: '0',
     },
@@ -87,6 +131,31 @@ describe('convert types filter', () => {
 
       it('should convert nulls', () => {
         expect(inst.value.nullStr).toBe(undefined)
+      })
+
+      it('should convert xsd string', () => {
+        expect(inst.value.values[0].field).toEqual('xsdString')
+        expect(inst.value.values[0].value).toEqual('stringVal')
+      })
+
+      it('should convert xsd boolean', () => {
+        expect(inst.value.values[1].field).toEqual('xsdBoolean')
+        expect(inst.value.values[1].value).toEqual('false')
+      })
+
+      it('should convert xsd int', () => {
+        expect(inst.value.values[2].field).toEqual('xsdInt')
+        expect(inst.value.values[2].value).toEqual('6')
+      })
+
+      it('should convert xsd long', () => {
+        expect(inst.value.values[3].field).toEqual('xsdLong')
+        expect(inst.value.values[3].value).toEqual('6.1')
+      })
+
+      it('should convert xsd double', () => {
+        expect(inst.value.values[4].field).toEqual('xsdDouble')
+        expect(inst.value.values[4].value).toEqual('6.2')
       })
 
       it('should not change settings', () => {

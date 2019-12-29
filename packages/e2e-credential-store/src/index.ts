ab01170042e676b2b97cf9acd58e90aa904b9b6f
Roy Razon <roy.razon@salto.io>
2019-12-29 15:07:23 +0200

    e2e minor improvements (#546)
    - less log output from E2E jest environment
    - extracted global property salesforceCredentials from generic e2e_credentials_store JestEnvironment to its use in salesforce-adapter.

--- packages/e2e-credential-store/src/index.ts
+++ packages/e2e-credential-store/src/index.ts
@@ -1,6 +1,9 @@
 import cliMain from './cli'
+import { CredsSpec as CredsSpec_ } from './jest-environment/creds'
 
 export { default as createEnvUtils } from './jest-environment/process_env'
 export { default as JestEnvironment } from './jest-environment'
+export type CredsSpec<TCreds> = CredsSpec_<TCreds>
+
 export * from './types'
 export const cli = { main: cliMain }
--- packages/salesforce-adapter/e2e_test/jest_environment.ts
+++ packages/salesforce-adapter/e2e_test/jest_environment.ts
@@ -1,23 +1,26 @@
 import { logger } from '@salto/logging'
-import { JestEnvironment, createEnvUtils } from '@salto/e2e-credentials-store'
+import { JestEnvironment, createEnvUtils, CredsSpec } from '@salto/e2e-credentials-store'
 import { Credentials, validateCredentials } from '../src/client/client'
 
 const log = logger(module)
 
+export const credsSpec: CredsSpec<Credentials> = {
+  envHasCreds: env => 'SF_USER' in env,
+  fromEnv: env => {
+    const envUtils = createEnvUtils(env)
+    return {
+      username: envUtils.required('SF_USER'),
+      password: envUtils.required('SF_PASSWORD'),
+      apiToken: env.SF_TOKEN ?? '',
+      isSandbox: envUtils.bool('SF_SANDBOX'),
+    }
+  },
+  validate: validateCredentials,
+  typeName: 'salesforce',
+  globalProp: 'salesforceCredentials',
+}
+
 export default JestEnvironment<Credentials>({
   logBaseName: log.namespace,
-  credsSpec: {
-    envHasCreds: env => 'SF_USER' in env,
-    fromEnv: env => {
-      const envUtils = createEnvUtils(env)
-      return {
-        username: envUtils.required('SF_USER'),
-        password: envUtils.required('SF_PASSWORD'),
-        apiToken: env.SF_TOKEN ?? '',
-        isSandbox: envUtils.bool('SF_SANDBOX'),
-      }
-    },
-    validate: validateCredentials,
-    typeName: 'salesforce',
-  },
+  credsSpec,
 })

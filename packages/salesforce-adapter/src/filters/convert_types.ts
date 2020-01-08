4ec5c0d0c13b7c5ddf323227818c69a25e1d6f25
Oren Digmi <oren.digmi@salto.io>
2020-01-08 03:33:11 +0200

    Moved transform func to adapter-api

--- packages/salesforce-adapter/src/filters/convert_types.ts
+++ packages/salesforce-adapter/src/filters/convert_types.ts
@@ -1,81 +1,10 @@
-import _ from 'lodash'
 import {
-  Element, isObjectType, PrimitiveTypes, Values, ObjectType,
-  isPrimitiveType, isInstanceElement,
+  Element, isObjectType, ObjectType,
+  isInstanceElement, transform,
 } from 'adapter-api'
 import { FilterCreator } from '../filter'
-import { convertXsdTypeFuncMap, PrimitiveValue } from '../transformers/transformer'
+import { transformPrimitive } from '../transformers/transformer'
 
-
-const transformPrimitive = (val: PrimitiveValue, primitive: PrimitiveTypes):
-  PrimitiveValue | undefined => {
-  // Salesforce returns nulls as objects like { $: { 'xsi:nil': 'true' } }
-  // our key name transform replaces '$' with '' and ':' with '_'
-  if (_.isObject(val) && _.get(val, ['', 'xsi_nil']) === 'true') {
-    // We transform null to undefined as currently we don't support null in Salto language
-    // and the undefined values are omitted later in the code
-    return undefined
-  }
-  // (Salto-394) Salesforce returns objects like:
-  // { "_": "fieldValue", "$": { "xsi:type": "xsd:string" } }
-  // our key name transform replaces '$' with '' and ':' with '_'
-  if (_.isObject(val) && Object.keys(val).includes('_')) {
-    const convertFunc = convertXsdTypeFuncMap[_.get(val, ['', 'xsi_type'])] || (v => v)
-    return transformPrimitive(convertFunc(_.get(val, '_')), primitive)
-  }
-  switch (primitive) {
-    case PrimitiveTypes.NUMBER:
-      return Number(val)
-    case PrimitiveTypes.BOOLEAN:
-      return val.toString().toLowerCase() === 'true'
-    case PrimitiveTypes.STRING:
-      return val.toString().length === 0 ? undefined : val.toString()
-    default:
-      return val
-  }
-}
-
-export const transform = (obj: Values, type: ObjectType, strict = true): Values | undefined => {
-  const result = _(obj).mapValues((value, key) => {
-    // we get lists of empty strings that we would like to filter out
-    if (_.isArray(value) && value.every(s => s === '')) {
-      return undefined
-    }
-    // we get empty strings that we would like to filter out
-    if (value === '') {
-      return undefined
-    }
-
-    const field = type.fields[key]
-    if (field !== undefined) {
-      const fieldType = field.type
-      if (isObjectType(fieldType)) {
-        return _.isArray(value)
-          ? value.map(v => transform(v, fieldType, strict))
-            .filter(v => !_.isEmpty(v))
-          : transform(value, fieldType, strict)
-      }
-      if (isPrimitiveType(fieldType)) {
-        return _.isArray(value)
-          ? value.map(v => transformPrimitive(v, fieldType.primitive))
-            .filter(v => !_.isArrayLike(v) || !_.isEmpty(v))
-          : transformPrimitive(value, fieldType.primitive)
-      }
-    }
-    // We are not returning the value if it's not fit the type definition.
-    // We saw cases where we got for jsforce values empty values in unexpected
-    // format for example:
-    // "layoutColumns":["","",""] where layoutColumns suppose to be list of object
-    // with LayoutItem and reserve fields.
-    // return undefined
-    if (strict) {
-      return undefined
-    }
-    return value
-  }).omitBy(_.isUndefined)
-    .value()
-  return _.isEmpty(result) ? undefined : result
-}
 
 /**
  * Convert types of values in instance elements to match the expected types according to the
@@ -93,7 +22,11 @@ const filterCreator: FilterCreator = () => ({
       .filter(isInstanceElement)
       .filter(instance => isObjectType(instance.type))
       .forEach(instance => {
-        instance.value = transform(instance.value, instance.type as ObjectType) || {}
+        instance.value = transform(
+          instance.value,
+          instance.type as ObjectType,
+          transformPrimitive
+        ) || {}
       })
   },
 })

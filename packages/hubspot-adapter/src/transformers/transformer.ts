7b37a1dff59ec2b38953aad4e03b6c4ed03bbf03
Oren Digmi <oren.digmi@salto.io>
2020-01-09 16:32:01 +0200

    Changed funcs access level

--- packages/hubspot-adapter/src/transformers/transformer.ts
+++ packages/hubspot-adapter/src/transformers/transformer.ts
@@ -20,7 +20,7 @@ const propertyElemID = new ElemID(HUBSPOT, OBJECTS_NAMES.PROPERTY)
 const optionsElemID = new ElemID(HUBSPOT, OBJECTS_NAMES.OPTIONS)
 
 export class Types {
-  public static optionsType: ObjectType =
+  private static optionsType: ObjectType =
     new ObjectType({
       elemID: optionsElemID,
       fields: {
@@ -74,10 +74,10 @@ export class Types {
           },
         ),
       },
-      path: ['hubspot', 'types', 'subtypes', optionsElemID.name],
+      path: [HUBSPOT, 'types', 'subtypes', optionsElemID.name],
     })
 
-  public static propertyType: ObjectType =
+  private static propertyType: ObjectType =
     new ObjectType({
       elemID: propertyElemID,
       fields: {
@@ -168,10 +168,10 @@ export class Types {
           true,
         ),
       },
-      path: ['hubspot', 'types', 'subtypes', propertyElemID.name],
+      path: [HUBSPOT, 'types', 'subtypes', propertyElemID.name],
     })
 
-  public static propertyGroupType: ObjectType =
+  private static propertyGroupType: ObjectType =
     new ObjectType({
       elemID: propertyGroupElemID,
       fields: {
@@ -198,7 +198,7 @@ export class Types {
           },
         ),
       },
-      path: ['hubspot', 'types', 'subtypes', propertyGroupElemID.name],
+      path: [HUBSPOT, 'types', 'subtypes', propertyGroupElemID.name],
     })
 
 
@@ -328,7 +328,7 @@ export class Types {
     Types.optionsType,
   ]
 
-  public static fieldTypes: Record<string, Type> = {
+  private static fieldTypes: Record<string, Type> = {
     [FIELD_TYPES.TEXTAREA]: new PrimitiveType({
       elemID: new ElemID(HUBSPOT, FIELD_TYPES.TEXTAREA),
       primitive: PrimitiveTypes.STRING,
@@ -391,7 +391,7 @@ const createInstanceName = (
  * @param info
  * @param infoType
  */
-export const fromHubspotObject = (
+const fromHubspotObject = (
   info: HubspotMetadata,
   infoType: ObjectType
 ): Values => {

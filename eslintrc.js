855c31c8360548f75e960d5657ea729ed05180e8
Amit Goldberg <amit.goldberg@salto.io>
2020-02-06 17:36:52 +0200

    SALTO-535: apply Apache 2.0 OSS license

--- eslintrc.js
+++ eslintrc.js
@@ -32,8 +32,27 @@ module.exports = {
         project: './tsconfig.json',
         tsconfigRootDir: './',
     },
-    plugins: ['react', 'import', 'jest', '@typescript-eslint'],
+    plugins: ['react', 'import', 'jest', '@typescript-eslint', 'header'],
     rules: {
+        'header/header': [2, "block",
+            [
+                "",
+                "*                      Copyright 2020 Salto Labs Ltd.",
+                "*",
+                "* Licensed under the Apache License, Version 2.0 (the \"License\");",
+                "* you may not use this file except in compliance with",
+                "* the License.  You may obtain a copy of the License at",
+                "*",
+                "*     http://www.apache.org/licenses/LICENSE-2.0",
+                "*",
+                "* Unless required by applicable law or agreed to in writing, software",
+                "* distributed under the License is distributed on an \"AS IS\" BASIS,",
+                "* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",
+                "* See the License for the specific language governing permissions and",
+                "* limitations under the License.",
+                ""
+            ]
+        ],
         'linebreak-style': ['error', isWindows ? 'windows' : 'unix'],
         quotes: [
             'error',

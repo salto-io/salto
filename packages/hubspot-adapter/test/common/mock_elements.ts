0620468cd6423927a2bc64d5b874bc19db4a37d9
Amit Goldberg <amit.goldberg@salto.io>
2020-02-09 23:21:54 +0200

    header added to all ts (linted) files

--- packages/hubspot-adapter/test/common/mock_elements.ts
+++ packages/hubspot-adapter/test/common/mock_elements.ts
@@ -1,6 +1,18 @@
-/**
- * Forms Mock Instances
- */
+/*
+*                      Copyright 2020 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 const firstFormMock = {
   portalId: 6774238,
   guid: '3e2e7ef3-d0d4-418f-92e0-ad40ae2b622c',
--- packages/lowerdash/src/types.ts
+++ packages/lowerdash/src/types.ts
@@ -1,8 +1,18 @@
-//
-// AtLeastOne, RequiredMember, HasMember
-//
-
-// https://stackoverflow.com/a/48244432
+/*
+*                      Copyright 2020 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K> }> = Partial<T> & U[keyof U]
 
 export type RequiredMember<T, M extends keyof T> = {

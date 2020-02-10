2a8bfedee741637d5dcd66a5605d4bab93ca630f
Amit Goldberg <amit.goldberg@salto.io>
2020-02-10 17:24:51 +0200

    apply apache 2 oss license (#676)
      * For the linted files (with eslint), we're using the eslint-plugin-header plugin. This can validate AND auto-fix source files.
      * For other .js files, we're using a short bash script that only validates the source files.
      * NOTICES is checked against a generated NOTICES file to see that it's up to date.

--- packages/cli/package_native.js
+++ packages/cli/package_native.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
+
+/*
+*                      Copyright 2020 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
@@ -100,3 +116,4 @@ const doNexe = (input) => new Promise((resolve, reject) => {
   await doNexe(bundle)
   console.log('Done!')
 })().catch(handleError)
+

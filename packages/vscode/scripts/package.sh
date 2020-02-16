e1d104fd1095ea939c27247f397209f398481b96
erikzaadi <erik.zaadi@gmail.com>
2020-02-16 15:33:56 +0200

    Make vscode extension build from local vsce
    Ensures that the build works even if vsce is not installed globally.

--- packages/vscode/scripts/package.sh
+++ packages/vscode/scripts/package.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-mkdir -p ./tmp_pkg && vsce package --yarn -o ./tmp_pkg/salto.vsix
-cd ./tmp_pkg && unzip salto.vsix 
-cp -r ../node_modules/* extension/node_modules/ 
-zip -ur salto.vsix extension 
-cd ..
-mkdir -p ./pkg 
-cp ./tmp_pkg/salto.vsix ./pkg/salto.vsix 
-rm -rf ./tmp_pkg
+SCRIPT_BASE="$( cd -P "$( dirname "$0" )" && pwd )"
+PACKAGE_ROOT="${SCRIPT_BASE}/.."
+TMP_PACKAGE_ROOT="${PACKAGE_ROOT}/tmp_pkg"
+mkdir -p ${TMP_PACKAGE_ROOT} && ${PACKAGE_ROOT}/node_modules/.bin/vsce package --yarn -o ${TMP_PACKAGE_ROOT}/salto.vsix
+cd ${TMP_PACKAGE_ROOT} && unzip salto.vsix
+cp -r ${PACKAGE_ROOT}/node_modules/* ${TMP_PACKAGE_ROOT}/extension/node_modules/
+zip -ur ${TMP_PACKAGE_ROOT}/salto.vsix ./extension
+cd ${PACKAGE_ROOT}
+mkdir -p ${PACKAGE_ROOT}/pkg
+cp ${TMP_PACKAGE_ROOT}/salto.vsix ${PACKAGE_ROOT}/pkg/salto.vsix
+rm -rf ${TMP_PACKAGE_ROOT}

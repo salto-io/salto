7557e43f6a2c0c69010ac6b482b65a7682bf2eec
Noam Hammer <noam9995@gmail.com>
2020-02-23 20:44:40 +0200

    fix reference expressions resolved bug

--- packages/core/src/serializer/elements.ts
+++ packages/core/src/serializer/elements.ts
@@ -52,6 +52,11 @@ export const serialize = (elements: Element[]): string => {
       o[SALTO_CLASS_FIELD] = e.constructor.name
       return o
     }
+    if (isReferenceExpression(e)) {
+      const o = new ReferenceExpression(e.elemId) as ReferenceExpression & ClassName
+      o[SALTO_CLASS_FIELD] = e.constructor.name
+      return o
+    }
     // We need to sort objects so that the state file won't change for the same data.
     if (_.isPlainObject(e)) {
       return _(e).toPairs().sortBy().fromPairs()

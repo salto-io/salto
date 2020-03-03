082d63f451ad21d42430ccee830b9de4b71ad781
Noam Hammer <noam9995@gmail.com>
2020-03-03 20:08:36 +0200

    .

--- packages/cli/src/workspace.ts
+++ packages/cli/src/workspace.ts
@@ -78,7 +78,9 @@ export const loadWorkspace = async (workingDir: string, cliOutput: CliOutput,
 export const updateWorkspace = async (ws: Workspace, cliOutput: CliOutput,
   changes: readonly FetchChange[], strict = false): Promise<boolean> => {
   if (changes.length > 0) {
-    log.info(`going to update workspace with ${changes.length} changes`)
+    log.info('going to update workspace with %d changes and print %d of them',
+      changes.length,
+      changes.length > MAX_DETAIL_CHANGES_TO_LOG ? changes.length : MAX_DETAIL_CHANGES_TO_LOG)
     if (!await ws.isEmpty(true)) {
       formatDetailedChanges([changes.slice(0, MAX_DETAIL_CHANGES_TO_LOG).map(c => c.change)])
         .split('\n')

a89bbd2f9f21b2d0316c436983fcfc35187f1a86
Roy Razon <roy.razon@salto.io>
2020-03-05 11:44:41 +0200

    fix publish script

---
+++ .circleci/scripts/publish_to_npm_if_version_changed.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GIT_BASE_REVISION=${1-}
+if [ -z "$GIT_BASE_REVISION" ]; then
+  echo >&2 "usage: $0 git_base_revision"
+  exit 1
+fi
+
+CURRENT_VERSION="$(jq -r .version < lerna.json)"
+PREV_VERSION="$(git show ${GIT_BASE_REVISION}:lerna.json | jq -r .version)"
+if [ "$CURRENT_VERSION" != "$PREV_VERSION" ]; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+  yarn lerna-publish -y --ignore-scripts
+  VERSION_TAG="v${CURRENT_VERSION}"
+  git tag $VERSION_TAG
+  git push origin $VERSION_TAG
+fi
+

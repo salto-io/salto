02209c3b470177472d195eeba4f24a278e63ab75
Roy Razon <roy.razon@salto.io>
2020-03-05 15:48:22 +0200

    fix: publish scripts (3nd time) (#768)
    - editing .npmrc created a dirty git local copy
    - "git push origin" asked for interactive confirmation

--- .circleci/scripts/publish_to_npm_if_version_changed.sh
+++ .circleci/scripts/publish_to_npm_if_version_changed.sh
@@ -11,9 +11,19 @@ fi
 CURRENT_VERSION="$(jq -j .version lerna.json)"
 PREV_VERSION="$(git show ${GIT_BASE_REVISION}:lerna.json | jq -j .version)"
 if [ "$CURRENT_VERSION" != "$PREV_VERSION" ]; then
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+  # set token at npmrc - without making the git local copy dirty
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+  echo ".npmrc" >> .git/info/exclude
+  git update-index --assume-unchanged .npmrc
+
   yarn lerna-publish -y --ignore-scripts
+
   VERSION_TAG="v${CURRENT_VERSION}"
+
   git tag $VERSION_TAG
+
+  # prevent SSH fingerprint prompt on git push
+  ssh -o StrictHostKeyChecking=no git@github.com
+  
   git push origin $VERSION_TAG
 fi

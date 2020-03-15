17d9764970fc56f3a6c31a17c5ba6ca89513e959
Roy Razon <roy.razon@salto.io>
2020-03-15 13:19:56 +0200

    fix: vscode extension name cannot be scoped (#823)
    * fix: vscode extension name cannot be scoped
    
    https://github.com/Microsoft/vscode-vsce/issues/186
    
    also: fixed package script to fail on error

--- packages/vscode/scripts/package.sh
+++ packages/vscode/scripts/package.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -xeuo pipefail
+
 mkdir -p ./tmp_pkg && vsce package --yarn -o ./tmp_pkg/salto.vsix
-cd ./tmp_pkg && unzip salto.vsix 
-cp -r ../node_modules/* extension/node_modules/ 
-zip -ur salto.vsix extension 
+cd ./tmp_pkg && unzip salto.vsix
+cp -r ../node_modules/* extension/node_modules/
+zip -ur salto.vsix extension
 cd ..
-mkdir -p ./pkg 
-cp ./tmp_pkg/salto.vsix ./pkg/salto.vsix 
+mkdir -p ./pkg
+cp ./tmp_pkg/salto.vsix ./pkg/salto.vsix
 rm -rf ./tmp_pkg

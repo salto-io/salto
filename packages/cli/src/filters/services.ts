076beb0ecaa2127a02c2d463de337d1013af6c89
Amit Goldfarb <62199348+amitgoldarb@users.noreply.github.com>
2020-03-31 19:01:04 +0300

    fixed PR comments

--- packages/cli/src/filters/services.ts
+++ packages/cli/src/filters/services.ts
@@ -18,6 +18,7 @@ import yargs from 'yargs'
 import { loadConfig, currentEnvConfig } from '@salto-io/core'
 import { ParsedCliInput } from '../types'
 import { ParserFilter, ParsedCliInputFilter } from '../filter'
+import { EnvironmentArgs } from '../commands/env'
 
 export interface ServiceCmdArgs {
   command: string
@@ -50,7 +51,7 @@ export const serviceCmdFilter: ServiceCmdFilter = {
         }>): true => {
         if (args.command && nameRequiredCommands.includes(args.command)) {
           if (_.isEmpty(args.name)) {
-            throw new Error(`Missing required argument: name\n\nExample usage: @salto-io/core services ${args.command} salesforce`)
+            throw new Error(`Missing required argument: name\n\nExample usage: 'salto services ${args.command} salesforce'`)
           }
         }
         return true
@@ -64,7 +65,7 @@ export const serviceCmdFilter: ServiceCmdFilter = {
   },
 }
 
-export interface ServicesArgs { services: string[]; env: string }
+export type ServicesArgs = { services: string[] } & EnvironmentArgs
 
 export type ServicesParsedCliInput = ParsedCliInput<ServicesArgs>
 
@@ -93,7 +94,7 @@ export const servicesFilter: ServicesFilter = {
     const workspaceServices = currentEnvConfig(workspaceConfig).services
     if (workspaceServices.length === 0) {
       throw new Error(
-        `No services are configured for env=${workspaceConfig.currentEnv}. Use salto services add'.`
+        `No services are configured for env=${workspaceConfig.currentEnv}. Use 'salto services add'.`
       )
     }
     // This assumes the default value for input services is all configured
--- packages/core/src/workspace/config.ts
+++ packages/core/src/workspace/config.ts
@@ -305,9 +305,7 @@ const readConfig = async (baseDir: string): Promise<PartialConfig> =>
   parseConfig(await configSource(localDirectoryStore(getConfigDir(baseDir)))
     .get(CONFIG_FILENAME))
 
-export const loadConfig = async (
-  lookupDir: string):
-  Promise<Config> => {
+export const loadConfig = async (lookupDir: string): Promise<Config> => {
   const baseDir = await baseDirFromLookup(lookupDir)
   const config = await readConfig(baseDir)
   const completeCfg = await completeConfig(baseDir, config)

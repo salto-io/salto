952d18e4d3812900389d7cd1ea477e29bc0b304d
Adam Rosenthal <adamro87@gmail.com>
2020-04-19 17:13:06 +0300

    Working version with update/add + fixes

--- packages/hubspot-adapter/test/filters/useridentifier.test.ts
+++ packages/hubspot-adapter/test/filters/useridentifier.test.ts
@@ -15,81 +15,13 @@
 */
 import { RequestPromise } from 'requestretry'
 import { OnFetchFilter } from 'src/filter'
-import { ObjectType, ElemID, Field, ListType, Values, InstanceElement, BuiltinTypes } from '@salto-io/adapter-api'
-import { HUBSPOT } from '../../src/constants'
-import { Types } from '../../src/transformers/transformer'
+import { Values, InstanceElement } from '@salto-io/adapter-api'
+import { useridentifierObjectType } from '../common/mock_elements'
 import mockClient from '../client'
 import filterCreator from '../../src/filters/useridentifier'
 
 describe('useridentifier filter test', () => {
   let filter: OnFetchFilter
-  const objectElemID = new ElemID(HUBSPOT, 'object')
-  const innerObjectElemID = new ElemID(HUBSPOT, 'innerObject')
-  const simpleField = (parentElemID: ElemID): Field =>
-    new Field(
-      parentElemID, 'simple',
-      Types.userIdentifierType, {
-        name: 'simple',
-        _readOnly: false,
-      },
-    )
-  const stringListField = (parentElemID: ElemID): Field =>
-    new Field(
-      parentElemID, 'stringList',
-      new ListType(Types.userIdentifierType), {
-        name: 'stringList',
-        _readOnly: false,
-      },
-    )
-  const stringArrayField = (parentElemID: ElemID): Field =>
-    new Field(
-      parentElemID, 'stringArray',
-      new ListType(Types.userIdentifierType), {
-        name: 'stringArray',
-        _readOnly: false,
-      },
-    )
-  const strField = (parentElemID: ElemID): Field =>
-    new Field(
-      parentElemID, 'str', BuiltinTypes.STRING, {
-        name: 'str',
-      }
-    )
-  const innerObject = new ObjectType(
-    {
-      elemID: innerObjectElemID,
-      fields: {
-        str: strField(innerObjectElemID),
-        simple: simpleField(innerObjectElemID),
-        stringList: stringListField(innerObjectElemID),
-        stringArray: stringArrayField(innerObjectElemID),
-      },
-    }
-  )
-  const objectType = new ObjectType(
-    {
-      elemID: objectElemID,
-      fields: {
-        str: strField(objectElemID),
-        simple: simpleField(objectElemID),
-        stringList: stringListField(objectElemID),
-        stringArray: stringArrayField(objectElemID),
-        objField: new Field(
-          objectElemID, 'objField', innerObject, {
-            name: 'objField',
-            _readOnly: false,
-          },
-        ),
-        listOfObjField: new Field(
-          objectElemID, 'listOfObjField',
-          new ListType(innerObject), {
-            name: 'listOfObjField',
-            _readOnly: false,
-          },
-        ),
-      },
-    }
-  )
   const getOwnerById = async (id: number | string): Promise<RequestPromise> => {
     switch (id) {
       case '12':
@@ -122,24 +54,28 @@ describe('useridentifier filter test', () => {
     instanceValues = {
       str: '12',
       simple: '12',
-      stringList: '12,34,56',
+      simpleNum: '101',
+      stringList: '12,34,56,non@owner.com',
       stringArray: ['12', '34', '56'],
       objField: {
         str: '12',
         simple: '12',
-        stringList: '12,34,56',
+        simpleNum: '101',
+        stringList: '12,34,56,non@owner.com',
         stringArray: ['12', '34', '56'],
       },
       listOfObjField: [
         {
           str: '12',
           simple: '12',
-          stringList: '12,34,56',
+          simpleNum: '101',
+          stringList: '12,34,56,non@owner.com',
           stringArray: ['12', '34', '56'],
         },
         {
           str: '34',
           simple: '34',
+          simpleNum: '101',
           stringList: '34,56,12',
           stringArray: ['34', '56', '12'],
         },
@@ -147,7 +83,7 @@ describe('useridentifier filter test', () => {
     } as Values
     objectInstance = new InstanceElement(
       'objectInstance',
-      objectType,
+      useridentifierObjectType,
       instanceValues,
     )
     const { client } = mockClient()
@@ -165,12 +101,16 @@ describe('useridentifier filter test', () => {
     expect(objectInstance.value.simple).toEqual('a@b.com')
   })
 
+  it('should convert unknown-identifier to string at base level of object', () => {
+    expect(objectInstance.value.simpleNum).toEqual('101')
+  })
+
   it('should convert to user identifier list when value is an array', () => {
     expect(objectInstance.value.stringArray).toEqual(['a@b.com', 'c@d.com', 'e@f.com'])
   })
 
   it('should convert to user identifier list when value is string', () => {
-    expect(objectInstance.value.stringList).toEqual(['a@b.com', 'c@d.com', 'e@f.com'])
+    expect(objectInstance.value.stringList).toEqual(['a@b.com', 'c@d.com', 'e@f.com', 'non@owner.com'])
   })
 
   it('should not effect non-useridentifier field values inside an object field', () => {
@@ -181,12 +121,16 @@ describe('useridentifier filter test', () => {
     expect(objectInstance.value.objField.simple).toEqual('a@b.com')
   })
 
+  it('should convert unknown-identifier to string inside an object field', () => {
+    expect(objectInstance.value.objField.simpleNum).toEqual('101')
+  })
+
   it('should convert to user identifier list when value is an array inside an object field', () => {
     expect(objectInstance.value.objField.stringArray).toEqual(['a@b.com', 'c@d.com', 'e@f.com'])
   })
 
   it('should convert to user identifier list when value is string inside an object field', () => {
-    expect(objectInstance.value.objField.stringList).toEqual(['a@b.com', 'c@d.com', 'e@f.com'])
+    expect(objectInstance.value.objField.stringList).toEqual(['a@b.com', 'c@d.com', 'e@f.com', 'non@owner.com'])
   })
 
   it('should not effect non-useridentifier field values inside a list of object fields', () => {
@@ -197,16 +141,15 @@ describe('useridentifier filter test', () => {
     expect(objectInstance.value.listOfObjField[0].simple).toEqual('a@b.com')
   })
 
+  it('should convert unknown-identifier to string inside a list of object fields', () => {
+    expect(objectInstance.value.listOfObjField[0].simpleNum).toEqual('101')
+  })
+
   it('should convert to user identifier list when value is an array inside a list of object fields', () => {
     expect(objectInstance.value.listOfObjField[0].stringArray).toEqual(['a@b.com', 'c@d.com', 'e@f.com'])
   })
 
   it('should convert to user identifier list when value is string inside a list of object fields', () => {
-    expect(objectInstance.value.listOfObjField[0].stringList).toEqual(['a@b.com', 'c@d.com', 'e@f.com'])
+    expect(objectInstance.value.listOfObjField[0].stringList).toEqual(['a@b.com', 'c@d.com', 'e@f.com', 'non@owner.com'])
   })
-
-  // TODO:
-  // 1. Actual number
-  // 2. Number that does not exist in the map
-  // 3. Non-number (email)
 })

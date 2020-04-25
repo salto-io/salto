361280ee613b719e5680f2ee2412d883664036bc
Amit Goldberg <amit.goldberg@salto.io>
2020-04-25 20:03:34 +0300

    split script to functions

--- .circleci/scripts/tag_repo_and_publish.sh
+++ .circleci/scripts/tag_repo_and_publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 GITHUB_API_ENDPOINT=https://api.github.com
 GITHUB_UPLOADS_ENDPOINT=https://uploads.github.com
@@ -16,54 +16,62 @@ if [ -z "$NPM_TOKEN" ]; then
   exit 1
 fi
 
+if [ -z "$FILES_TO_UPLOAD" ]; then
+  echo >&2 "missing FILES_TO_UPLOAD environment variable (filenames to upload to the Github release)"
+  exit 1
+fi
+
 CURRENT_VERSION="$(jq -j .version lerna.json)"
-
-##################
-# Push a new tag #
-##################
 VERSION_TAG="v${CURRENT_VERSION}"
-git tag $VERSION_TAG
 
-# prevent SSH fingerprint prompt on git push
-ssh -o StrictHostKeyChecking=no git@github.com || true
-git push origin $VERSION_TAG
+push_new_git_tag() {
+  echo "tagging and pushing new git tag: ${VERSION_TAG}"
+  git tag $VERSION_TAG
 
-##################
-# Publish to NPM #
-##################
-# set token at npmrc - without making the git local copy dirty
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
-echo ".npmrc" >> .git/info/exclude
-git update-index --assume-unchanged .npmrc
+  # prevent SSH fingerprint prompt on git push
+  ssh -o StrictHostKeyChecking=no git@github.com || true
+  git push origin $VERSION_TAG
+}
 
-yarn lerna-publish -y --ignore-scripts
+publish_packages_to_npm() {
+  echo "publishing to npm"
+  # set token at npmrc - without making the git local copy dirty
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+  echo ".npmrc" >> .git/info/exclude
+  git update-index --assume-unchanged .npmrc
 
-###############################
-# Publish a release to Github #
-###############################
+  yarn lerna-publish -y --ignore-scripts
+}
 
-# https://developer.github.com/v3/repos/releases/#create-a-release
-new_release_json=$(echo -e "
-{
-  \"tag_name\": \"${CIRCLE_TAG}\",
-  \"prerelease\": false,
-  \"draft\": false,
-  \"name\": \"Salto ${CIRCLE_TAG}\"
-}" | jq -cM)
+create_release_in_github() {
+  echo "creating a github release"
+  # https://developer.github.com/v3/repos/releases/#create-a-release
+  new_release_json=$(echo -e "
+  {
+    \"tag_name\": \"${CIRCLE_TAG}\",
+    \"prerelease\": false,
+    \"draft\": false,
+    \"name\": \"Salto ${CIRCLE_TAG}\"
+  }" | jq -cM)
 
-release_result=$(curl -L -XPOST --fail \
-  "${GITHUB_API_ENDPOINT}/repos/${REPO}/releases" \
-  -d "$new_release_json" \
-  -H "Authorization: token ${GITHUB_AUTH_TOKEN}" \
-  -H "Content-Type: application/json")
-
-# attach assets to the release
-release_id=$(echo "$release_result" | jq .id -j)
-for f in $FILES_TO_UPLOAD; do
-  curl "${GITHUB_UPLOADS_ENDPOINT}/repos/${REPO}/releases/${release_id}/assets?name=$(basename $f)" \
-    -L -XPOST --fail \
-    --data-binary @"$f" \
+  release_result=$(curl -L -XPOST --fail \
+    "${GITHUB_API_ENDPOINT}/repos/${REPO}/releases" \
+    -d "$new_release_json" \
     -H "Authorization: token ${GITHUB_AUTH_TOKEN}" \
-    -H "Content-Type: application/octet-stream" &
-done
-wait
+    -H "Content-Type: application/json")
+
+  # attach assets to the release
+  release_id=$(echo "$release_result" | jq .id -j)
+  for f in $FILES_TO_UPLOAD; do
+    curl "${GITHUB_UPLOADS_ENDPOINT}/repos/${REPO}/releases/${release_id}/assets?name=$(basename $f)" \
+      -L -XPOST --fail \
+      --data-binary @"$f" \
+      -H "Authorization: token ${GITHUB_AUTH_TOKEN}" \
+      -H "Content-Type: application/octet-stream" &
+  done
+  wait
+}
+
+push_new_git_tag
+publish_packages_to_npm
+create_release_in_github

3ac96a54c35055eb520b5860a414f52e8d04f65a
Amit Goldberg <amit.goldberg@salto.io>
2020-04-28 00:17:00 +0300

    prepend env vars with DOCS_

--- docs/build.sh
+++ docs/build.sh
@@ -12,10 +12,10 @@ function usage () {
     The script generates an html file, uploading it to S3 and
     invalidating cloudfront cache.
     Available environment variables:
-    S3_BUCKETS:           A comma separated string of  bucket names
-                          you want to upload the generated file to.
-    CLOUDFRONT_DIST_IDS:  A comma separated string of Cloudfront distribution IDs you want to create
-                          distribution IDs you want to create cache invalidations in.
+    DOCS_S3_BUCKETS:          A comma separated string of  bucket names
+                              you want to upload the generated file to.
+    DOCS_CLOUDFRONT_DIST_IDS: A comma separated string of Cloudfront distribution IDs you want to create
+                              distribution IDs you want to create cache invalidations in.
     "
     exit 0
 }
@@ -58,26 +58,26 @@ rm "$temp_md_file"
 
 # Upload newly generated file to S3 buckets
 
-if [ -z "$S3_BUCKETS" ]; then
-  echo "S3_BUCKETS was not specified - not uploading generated files"
+if [ -z "$DOCS_S3_BUCKETS" ]; then
+  echo "DOCS_S3_BUCKETS was not specified - not uploading generated files"
   exit 0
 fi
 
 echo "uploading ${OUTPUT} to S3 buckets"
 IFS=","
-for bucket in $S3_BUCKETS; do
+for bucket in $DOCS_S3_BUCKETS; do
   aws s3 cp "$OUTPUT" "s3://${bucket}"
 done
 
 # Invalidate cloudfront caches
 
-if [ -z "$CLOUDFRONT_DIST_IDS" ]; then
-  echo "$CLOUDFRONT_DIST_IDS (distribution ids) were not specified, not creating invalidations"
+if [ -z "$DOCS_CLOUDFRONT_DIST_IDS" ]; then
+  echo "$DOCS_CLOUDFRONT_DIST_IDS (distribution ids) were not specified, not creating invalidations"
   exit 0
 fi
 
 echo "invalidating cloudfront distributions"
 IFS=","
-for dist_id in $CLOUDFRONT_DIST_IDS; do
+for dist_id in $DOCS_CLOUDFRONT_DIST_IDS; do
   aws cloudfront create-invalidation --distribution-id "$dist_id" --paths '/*'
 done

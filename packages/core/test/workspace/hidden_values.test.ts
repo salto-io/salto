cfea4e2547892817a996ec61c374d8c9b273b7dd
Oren Digmi <oren.digmi@salto.io>
2020-05-11 16:20:52 +0300

    Added tests to addHiddenValues

--- packages/core/test/workspace/hidden_values.test.ts
+++ packages/core/test/workspace/hidden_values.test.ts
@@ -19,6 +19,7 @@ import {
   ObjectType,
 } from '@salto-io/adapter-api'
 import {
+  addHiddenValues,
   removeHiddenValues,
 } from '../../src/workspace/hidden_values'
 
@@ -69,6 +70,13 @@ describe('hidden_values.ts', () => {
         innerObject,
         { [CORE_ANNOTATIONS.HIDDEN]: true }
       ),
+      numHidden: new Field(
+        anotherTypeID,
+        'numHidden',
+        BuiltinTypes.NUMBER,
+        { [CORE_ANNOTATIONS.HIDDEN]: true }
+      ),
+      addedField: new Field(anotherTypeID, 'addedField', BuiltinTypes.STRING),
     },
     path: ['records', 'hidden'],
   })
@@ -97,6 +105,7 @@ describe('hidden_values.ts', () => {
     notHidden: 'notHidden',
     hidden: 'Hidden',
     val: 'val',
+    numHidden: 123,
     objField: {
       num: 1212,
       stringList: 'test1,tes2,3',
@@ -125,6 +134,7 @@ describe('hidden_values.ts', () => {
       delete instanceAfterHiddenRemoved.value.hidden
       delete instanceAfterHiddenRemoved.value.listOfObjects[0].hiddenStr
       delete instanceAfterHiddenRemoved.value.objField.hiddenStr
+      delete instanceAfterHiddenRemoved.value.numHidden
 
       const clonedHiddenInstance = hiddenInstance.clone()
 
@@ -139,6 +149,59 @@ describe('hidden_values.ts', () => {
 
       it('should not done in-place', () => {
         expect(clonedHiddenInstance.isEqual(hiddenInstance)).toBeTruthy()
+      })
+    })
+  })
+
+  describe('addHiddenValues func', () => {
+    describe('instances', () => {
+      const workspaceInstance = hiddenInstance.clone()
+
+      // workspace elements should not contain hidden values
+      delete workspaceInstance.value.hidden
+      delete workspaceInstance.value.listOfObjects[0].hiddenStr
+      delete workspaceInstance.value.objField.hiddenStr
+
+      // workspace changes
+      workspaceInstance.value.notHidden = 'notHiddenChanged'
+      workspaceInstance.value.addedField = 'addedField'
+      workspaceInstance.value.listOfObjects[0].num = 12345
+      workspaceInstance.value.numHidden = 11111
+
+
+      const stateInstance = hiddenInstance.clone()
+
+      let resp: Element[]
+      let instanceAfterHiddenAddition: InstanceElement
+      beforeAll(async () => {
+        resp = addHiddenValues([workspaceInstance], [stateInstance])
+        instanceAfterHiddenAddition = resp[0] as InstanceElement
+      })
+
+      it('should add hidden values from state elements', () => {
+        expect(instanceAfterHiddenAddition.value.hidden).toEqual(stateInstance.value.hidden)
+        expect(instanceAfterHiddenAddition.value.listOfObjects[0].hiddenStr)
+          .toEqual(stateInstance.value.listOfObjects[0].hiddenStr)
+        expect(instanceAfterHiddenAddition.value.objField.hiddenStr)
+          .toEqual(stateInstance.value.objField.hiddenStr)
+      })
+
+      it('should ignore hidden values from workspace element', () => {
+        expect(instanceAfterHiddenAddition.value.numHidden)
+          .toEqual(stateInstance.value.numHidden)
+      })
+
+      it('should not change workspace (not hidden) element values', () => {
+        expect(workspaceInstance.value.notHidden)
+          .toEqual(instanceAfterHiddenAddition.value.notHidden)
+        expect(workspaceInstance.value.reg)
+          .toEqual(instanceAfterHiddenAddition.value.reg)
+        expect(workspaceInstance.value.listOfObjects[0].num)
+          .toEqual(instanceAfterHiddenAddition.value.listOfObjects[0].num)
+      })
+
+      it('should not done in-place', () => {
+        expect(stateInstance.isEqual(hiddenInstance)).toBeTruthy()
       })
     })
   })

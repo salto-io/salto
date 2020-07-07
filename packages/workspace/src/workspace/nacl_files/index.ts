83809d44b3b1d695685fd52d82e6d52966b29f83
Noam Hammer <noam9995@gmail.com>
2020-07-07 12:42:28 +0300

    export parsed nacl files

--- packages/workspace/src/workspace/nacl_files/index.ts
+++ packages/workspace/src/workspace/nacl_files/index.ts
@@ -13,4 +13,5 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-export { NaclFile, FILE_EXTENSION, NaclFilesSource, naclFilesSource } from './nacl_files_source'
+export { NaclFile, FILE_EXTENSION, NaclFilesSource, naclFilesSource,
+  ParsedNaclFile, getParsedNaclFiles } from './nacl_files_source'

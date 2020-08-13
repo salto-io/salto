b13cdb395a0fd3578cd1bb430c3d5aac015ed01a
Nitzan Haim <nitzanhaim@gmail.com>
2020-08-13 18:33:43 +0300

    added services.ts & tests files after renaming 'services' to 'service'

---
+++ packages/cli/src/commands/service.ts
@@ -0,0 +1,184 @@
+/*
+*                      Copyright 2020 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import _ from 'lodash'
+import { EOL } from 'os'
+import {
+  addAdapter,
+  getLoginStatuses,
+  LoginStatus,
+  updateCredentials,
+  loadLocalWorkspace,
+} from '@salto-io/core'
+import { Workspace } from '@salto-io/workspace'
+
+import { InstanceElement, ObjectType } from '@salto-io/adapter-api'
+import { environmentFilter } from '../filters/env'
+import { createCommandBuilder } from '../command_builder'
+import { CliOutput, ParsedCliInput, CliCommand, CliExitCode, WriteStream } from '../types'
+import { getCredentialsFromUser } from '../callbacks'
+import { serviceCmdFilter, ServiceCmdArgs } from '../filters/service'
+import {
+  formatServiceConfigured, formatServiceNotConfigured, formatConfiguredServices,
+  formatLoginUpdated, formatLoginOverride, formatServiceAdded,
+  formatServiceAlreadyAdded, formatCredentialsHeader, formatLoginToServiceFailed,
+} from '../formatter'
+import { EnvironmentArgs } from './env'
+
+const getLoginInputFlow = async (
+  workspace: Workspace,
+  configType: ObjectType,
+  getLoginInput: (configType: ObjectType) => Promise<InstanceElement>,
+  stdout: WriteStream
+): Promise<void> => {
+  stdout.write(formatCredentialsHeader(configType.elemID.adapter))
+  const newConfig = await getLoginInput(configType)
+  await updateCredentials(workspace, newConfig)
+  stdout.write(EOL)
+  stdout.write(formatLoginUpdated)
+}
+
+const loadWorkspace = async (workspaceDir: string, inputEnvironment?: string):
+Promise<Workspace> => {
+  const workspace = await loadLocalWorkspace(workspaceDir)
+  if (!_.isUndefined(inputEnvironment)) {
+    await workspace.setCurrentEnv(inputEnvironment, false)
+  }
+  return workspace
+}
+
+const addService = async (
+  workspaceDir: string,
+  { stdout, stderr }: CliOutput,
+  getLoginInput: (configType: ObjectType) => Promise<InstanceElement>,
+  serviceName: string,
+  inputEnvironment?: string,
+): Promise<CliExitCode> => {
+  const workspace = await loadWorkspace(workspaceDir, inputEnvironment)
+  if (workspace.services().includes(serviceName)) {
+    stderr.write(formatServiceAlreadyAdded(serviceName))
+    return CliExitCode.UserInputError
+  }
+
+  const adapterCredentialsType = await addAdapter(workspace, serviceName)
+  stdout.write(formatServiceAdded(serviceName))
+
+  try {
+    await getLoginInputFlow(workspace, adapterCredentialsType, getLoginInput, stdout)
+  } catch (e) {
+    stderr.write(formatLoginToServiceFailed(serviceName, e.message))
+  }
+
+  return CliExitCode.Success
+}
+
+const listServices = async (
+  workspaceDir: string,
+  cliOutput: CliOutput,
+  serviceName: string,
+  inputEnvironment?: string,
+): Promise<CliExitCode> => {
+  const workspace = await loadWorkspace(workspaceDir, inputEnvironment)
+  if (_.isEmpty(serviceName)) {
+    cliOutput.stdout.write(formatConfiguredServices(workspace.services()))
+  } else if (workspace.services().includes(serviceName)) {
+    cliOutput.stdout.write(formatServiceConfigured(serviceName))
+  } else {
+    cliOutput.stdout.write(formatServiceNotConfigured(serviceName))
+  }
+  return CliExitCode.Success
+}
+
+const loginService = async (
+  workspaceDir: string,
+  { stdout, stderr }: CliOutput,
+  getLoginInput: (configType: ObjectType) => Promise<InstanceElement>,
+  serviceName: string,
+  inputEnvironment?: string,
+): Promise<CliExitCode> => {
+  const workspace = await loadWorkspace(workspaceDir, inputEnvironment)
+  if (!workspace.services().includes(serviceName)) {
+    stderr.write(formatServiceNotConfigured(serviceName))
+    return CliExitCode.AppError
+  }
+  const serviceLoginStatus = (await getLoginStatuses(
+    workspace,
+    [serviceName]
+  ))[serviceName] as LoginStatus
+  if (serviceLoginStatus.isLoggedIn) {
+    stdout.write(formatLoginOverride)
+  }
+  await getLoginInputFlow(workspace, serviceLoginStatus.configType, getLoginInput, stdout)
+  return CliExitCode.Success
+}
+
+export const command = (
+  workspaceDir: string,
+  commandName: string,
+  { stdout, stderr }: CliOutput,
+  getLoginInput: (configType: ObjectType) => Promise<InstanceElement>,
+  serviceName = '',
+  inputEnvironment?: string,
+): CliCommand => ({
+  async execute(): Promise<CliExitCode> {
+    switch (commandName) {
+      case 'add':
+        return addService(
+          workspaceDir,
+          { stdout, stderr },
+          getLoginInput,
+          serviceName,
+          inputEnvironment
+        )
+      case 'list':
+        return listServices(workspaceDir, { stdout, stderr }, serviceName, inputEnvironment)
+      case 'login':
+        return loginService(
+          workspaceDir,
+          { stdout, stderr },
+          getLoginInput,
+          serviceName,
+          inputEnvironment
+        )
+      default:
+        throw new Error('Unknown service management command')
+    }
+  },
+})
+
+type ServiceArgs = {} & ServiceCmdArgs & EnvironmentArgs
+
+type ServiceParsedCliInput = ParsedCliInput<ServiceArgs>
+
+const servicesBuilder = createCommandBuilder({
+  options: {
+    command: 'service <command> [name]',
+    description: 'Manage your environment services',
+  },
+
+  filters: [serviceCmdFilter, environmentFilter],
+  async build(input: ServiceParsedCliInput, output: CliOutput) {
+    return command(
+      '.',
+      input.args.command,
+      output,
+      getCredentialsFromUser,
+      input.args.name,
+      input.args.env,
+    )
+  },
+})
+
+export default servicesBuilder
---
+++ packages/cli/test/filters/service.test.ts
@@ -0,0 +1,161 @@
+/*
+*                      Copyright 2020 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { createCommandBuilder, YargsCommandBuilder } from '../../src/command_builder'
+import { CliExitCode } from '../../src/types'
+import { MockCliOutput, cli, mockLoadWorkspace } from '../mocks'
+import { servicesFilter, serviceCmdFilter } from '../../src/filters/service'
+
+
+jest.mock('@salto-io/core', () => ({
+  ...jest.requireActual('@salto-io/core'),
+  loadLocalWorkspace: jest.fn().mockImplementation(() => mockLoadWorkspace('name')),
+}))
+describe('services filter', () => {
+  let out: MockCliOutput
+  let buildFunc: jest.Mock
+  let builder: YargsCommandBuilder
+
+  describe('verify services filter', () => {
+    beforeEach(async () => {
+      buildFunc = jest.fn(() =>
+        Promise.resolve({ execute: () => Promise.resolve(CliExitCode.Success) })) as jest.Mock
+      builder = createCommandBuilder({
+        options: {
+          command: 'testCommand',
+          aliases: ['t'],
+          description: 'tests the command parser',
+          keyed: {
+            'test-opt': {
+              boolean: true,
+              demandOption: true,
+            },
+          },
+        },
+
+        filters: [servicesFilter],
+
+        build: buildFunc,
+      })
+    })
+
+    const runCli = (args: string): Promise<MockCliOutput> =>
+      cli({
+        commandBuilders: [builder] as YargsCommandBuilder[],
+        args,
+      })
+
+    describe('yargs configuration', () => {
+      it('does not override the original options', async () => {
+        out = await runCli('t .') // existing option 'test-opt' not specified
+        expect(out.err).toMatch(/\bMissing required argument: test-opt\b/)
+      })
+
+      it('Shows the help option', async () => {
+        expect(out.err).toMatch(/--help\b/)
+      })
+    })
+
+    describe('command builder', () => {
+      describe('when service option is not used', () => {
+        beforeEach(async () => {
+          await runCli('t --test-opt')
+        })
+
+        it('service should be workspace services', () => {
+          expect(buildFunc.mock.calls[0][0]).toEqual(
+            expect.objectContaining({
+              args: {
+                $0: 'salto',
+                _: ['t'],
+                services: ['salesforce', 'hubspot'],
+                'test-opt': true,
+                testOpt: true,
+              },
+            })
+          )
+        })
+      })
+
+      describe('when called with a service that exists', () => {
+        beforeEach(async () => {
+          await runCli('t --test-opt -s salesforce')
+        })
+
+        it('service should be inputted services', () => {
+          expect(buildFunc.mock.calls[0][0]).toEqual(
+            expect.objectContaining({
+              args:
+              {
+                $0: 'salto',
+                _: ['t'],
+                s: ['salesforce'],
+                services: ['salesforce'],
+                'test-opt': true,
+                testOpt: true,
+              },
+            })
+          )
+        })
+      })
+
+      describe('when called with a service that is not configured', () => {
+        beforeEach(async () => {
+          out = await runCli('t --test-opt -s abcdefg')
+        })
+
+        it('should fail with service not configured error', async () => {
+          expect(out.err).toMatch('Not all services (abcdefg) are set up for this workspace')
+        })
+      })
+    })
+  })
+  describe('verify service cmd filter', () => {
+    beforeEach(async () => {
+      buildFunc = jest.fn(() =>
+        Promise.resolve({ execute: () => Promise.resolve(CliExitCode.Success) })) as jest.Mock
+      builder = createCommandBuilder({
+        options: {
+          command: 'service <command> [name]',
+          description: 'tests the command parser',
+        },
+        filters: [serviceCmdFilter],
+        build: buildFunc,
+      })
+    })
+
+    const runCli = (args: string): Promise<MockCliOutput> =>
+      cli({
+        commandBuilders: [builder] as YargsCommandBuilder[],
+        args,
+      })
+    describe('should fail when commands called incorrectly', () => {
+      it('fail with add', async () => {
+        out = await runCli('service add')
+        expect(out.err).toMatch('Error: Missing required argument: name\n\nExample usage: \'salto service add salesforce\'')
+      })
+      it('fail with login', async () => {
+        out = await runCli('service login')
+        expect(out.err).toMatch('Error: Missing required argument: name\n\nExample usage: \'salto service login salesforce\'')
+      })
+      describe('Should succeed', () => {
+        it('succeed with add', async () => {
+          out = await runCli('service add sales force')
+          expect(out.err).toMatch('')
+        })
+      })
+    })
+  })
+})

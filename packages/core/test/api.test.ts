694d29bc05da720766bae7044edc625ed5713fcb
Ofir <ofir.elias@salto.io>
2020-08-20 16:40:14 +0300

    Core UT fixed

--- packages/core/test/api.test.ts
+++ packages/core/test/api.test.ts
@@ -470,21 +470,21 @@ describe('api.ts', () => {
   describe('diff', () => {
     const ws = mockWorkspace({ name: 'diff' })
     it('should return changes', async () => {
-      const changes = await api.diff(ws, ws.currentEnv(), 'other', false, false)
+      const changes = await api.diff(ws, 'active', 'other', false, false)
       expect(changes).toHaveLength(1)
-      expect(ws.elements).toHaveBeenCalledWith(false)
+      expect(ws.elements).toHaveBeenCalledWith(false, 'active')
       expect(ws.elements).toHaveBeenCalledWith(false, 'other')
     })
     it('should get the data from state when the state flag is true', async () => {
-      const changes = await api.diff(ws, ws.currentEnv(), 'other', false, true)
+      const changes = await api.diff(ws, 'active', 'other', false, true)
       expect(changes).toHaveLength(1)
-      expect(ws.state).toHaveBeenCalledWith()
+      expect(ws.state).toHaveBeenCalledWith('active')
       expect(ws.state).toHaveBeenCalledWith('other')
     })
     it('should get hidden types when flag is true', async () => {
-      const changes = await api.diff(ws, ws.currentEnv(), 'other', false, true)
+      const changes = await api.diff(ws, 'active', 'other', false, true)
       expect(changes).toHaveLength(1)
-      expect(ws.elements).toHaveBeenCalledWith(false)
+      expect(ws.elements).toHaveBeenCalledWith(false, 'active')
       expect(ws.elements).toHaveBeenCalledWith(false, 'other')
     })
   })

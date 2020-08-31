3f8a8eed7923c87fc730805e177d5c3bf50e0e6e
Omri Litvak <omri.litvak@salto.io>
2020-08-31 13:10:22 +0300

    mock request-promise-native package to fix UTs & E2E

--- jest.setup_netsuite.js
+++ jest.setup_netsuite.js
@@ -13,29 +13,4 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-module.exports = {
-  verbose: true,
-  testEnvironment: 'node',
-  testMatch: [
-    process.env['RUN_E2E_TESTS']
-      ? '<rootDir>/dist/e2e_test/**/*.test.js'
-      : '<rootDir>/dist/test/**/*.test.js'
-  ],
-  testRunner: "jest-circus/runner",
-  collectCoverage: true,
-  coverageReporters: ['json', 'lcov', 'text', 'clover', 'json-summary'],
-  collectCoverageFrom: [
-    '**/*.js',
-    '**/*.jsx',
-    '!**/node_modules/**',
-    '!*.config.js',
-    '!coverage/**',
-    '!dist/test/**',
-    '!dist/e2e_test/**',
-    
-  ],
-  setupFiles: [
-    `${__dirname}/jest.setup_netsuite.js`,
-  ],
-}
-
+jest.mock('request-promise-native');

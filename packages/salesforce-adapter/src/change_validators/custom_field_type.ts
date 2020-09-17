786cfda770d6fd3b6e4dab6d99cae2476f39f673
Nitzan Haim <nitzanhaim@gmail.com>
2020-09-17 13:55:19 +0300

    refactored filtering funcs to use the casting performed by isFieldChange

--- packages/salesforce-adapter/src/change_validators/custom_field_type.ts
+++ packages/salesforce-adapter/src/change_validators/custom_field_type.ts
@@ -14,53 +14,46 @@
 * limitations under the License.
 */
 import {
-  ChangeDataType, ChangeError, Field, getChangeElement, isAdditionOrModificationChange,
-  ChangeValidator, isField, Change, isAdditionChange,
+  ChangeError, Field, getChangeElement, isAdditionOrModificationChange,
+  ChangeValidator, Change, isAdditionChange, isFieldChange,
 } from '@salto-io/adapter-api'
 import { CUSTOM_FIELD_UPDATE_CREATE_ALLOWED_TYPES, FIELD_TYPE_NAMES, COMPOUND_FIELD_TYPE_NAMES } from '../constants'
 import { isCustomObject } from '../transformers/transformer'
 
-const isCustomField = (changedElement: ChangeDataType): changedElement is Field =>
-  (isField(changedElement) && isCustomObject(changedElement.parent))
+const isCustomFieldChange = (change: Change<Field>): boolean =>
+  (isCustomObject(getChangeElement(change).parent))
 
-const isInvalidCustomFieldType = (change: Change<ChangeDataType>): boolean => {
-  const afterElement = getChangeElement(change)
-  if (isCustomField(afterElement)) {
-    const afterFieldType = afterElement.type.elemID.typeName as
-    FIELD_TYPE_NAMES | COMPOUND_FIELD_TYPE_NAMES
-    const isAfterTypeAllowed = CUSTOM_FIELD_UPDATE_CREATE_ALLOWED_TYPES.includes(afterFieldType)
-    if (isAfterTypeAllowed) {
-      return false
-    } if (isAdditionChange(change)) {
-      return true
-    }
-
-    // modification change and the target type is invalid
-    const beforeElement = change.data.before
-    return isField(beforeElement)
-      && beforeElement.type.elemID.typeName !== afterFieldType
+const isInvalidTypeChange = (change: Change<Field>): boolean => {
+  const afterFieldType = getChangeElement(change).type.elemID.typeName as
+  FIELD_TYPE_NAMES | COMPOUND_FIELD_TYPE_NAMES
+  const isAfterTypeAllowed = CUSTOM_FIELD_UPDATE_CREATE_ALLOWED_TYPES.includes(afterFieldType)
+  if (isAfterTypeAllowed) {
+    return false
+  } if (isAdditionChange(change)) {
+    return true
   }
-  return false
+
+  // it's a modification change and the target type is invalid
+  return change.data.before.type.elemID.typeName !== afterFieldType
 }
 
-const createChangeError = (change: Change<ChangeDataType>): ChangeError => {
-  const field = getChangeElement(change) as Field
-  return {
-    elemID: field.elemID,
-    severity: 'Error',
-    message: `You cannot create or modify a custom field type to ${field.type.elemID.typeName}. Field: ${field.name}`,
-    detailedMessage: `You cannot create or modify a custom field type to ${field.type.elemID.typeName}. Valid types can be found at:\nhttps://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/meta_field_types.htm#meta_type_fieldtype`,
-  }
-}
-
+const createChangeError = (changedElement: Field): ChangeError => ({
+  elemID: changedElement.elemID,
+  severity: 'Error',
+  message: `You cannot create or modify a custom field type to ${changedElement.type.elemID.typeName}. Field: ${changedElement.name}`,
+  detailedMessage: `You cannot create or modify a custom field type to ${changedElement.type.elemID.typeName}. Valid types can be found at:\nhttps://developer.salesforce.com/docs/atlas.en-us.api_meta.meta/api_meta/meta_field_types.htm#meta_type_fieldtype`,
+})
 
 /**
-   * It is forbidden to add or modify a field with unknown type.
-   * A missing type means this field is not accessible on Salesforce.
+   * Modification of a custom field type is restriced to certain types,
+   * as well as the type of new custom fields.
    */
 const changeValidator: ChangeValidator = async changes => changes
   .filter(isAdditionOrModificationChange)
-  .filter(isInvalidCustomFieldType)
+  .filter(isFieldChange)
+  .filter(isCustomFieldChange)
+  .filter(isInvalidTypeChange)
+  .map(getChangeElement)
   .map(createChangeError)
 
 export default changeValidator

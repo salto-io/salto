51d8700f4ded6af76707fc5fdc8144450d588894
Guy Tish <guytish@gmail.com>
2020-11-23 14:12:17 +0200

    Added basic test

---
+++ packages/marketo-adapter/test/common/mock_elements.ts
@@ -0,0 +1,56 @@
+/*
+*                      Copyright 2020 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+const firstLeadMock = {
+  id: 1,
+  dataType: 'string',
+  displayName: 'Test Lead',
+  rest: {
+    name: 'test',
+  },
+  soap: {
+    name: 'test',
+  },
+}
+
+export const leadsMockArray = [
+  firstLeadMock,
+] as unknown
+
+const firstCustomObjectMock = {
+  name: 'test',
+  idField: 'test',
+  displayName: 'Test',
+  pluralName: 'tests',
+  description: 'unit test',
+  dedupeFields: ['dedupe'],
+  searchableFields: [],
+  fields: [
+    {
+      name: 'field',
+      dataType: 'string',
+    },
+  ],
+  relationships: [],
+  createdAt: '',
+  updatedAt: '',
+  apiName: 'test',
+  version: 'draft',
+}
+
+export const customObjectsMockArray = [
+  firstCustomObjectMock,
+] as unknown

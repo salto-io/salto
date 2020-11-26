fae45fcacfe905dbd382481e495e6020ad268691
Nitzan Haim <nitzanhaim@gmail.com>
2020-11-26 10:27:05 +0200

    added missing fields, types and references in EntitlementProccess type

--- packages/salesforce-adapter/src/transformers/reference_mapping.ts
+++ packages/salesforce-adapter/src/transformers/reference_mapping.ts
@@ -301,6 +301,10 @@ export const fieldNameToTypeMappingDefs: FieldReferenceDefinition[] = [
     target: { type: CUSTOM_OBJECT },
   },
   {
+    src: { field: 'businessHours', parentTypes: ['EntitlementProcessMilestoneItem'] },
+    target: { name: 'BusinessHoursEntry', type: CUSTOM_FIELD },
+  },
+  {
     src: { field: CPQ_LOOKUP_OBJECT_NAME, parentTypes: [CPQ_PRICE_RULE, CPQ_PRODUCT_RULE] },
     target: { type: CUSTOM_OBJECT },
   },

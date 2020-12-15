c1e4a86d65effec191c9aee74e4d329ab6a8109b
Nitzan Haim <nitzanhaim@gmail.com>
2020-12-15 16:15:33 +0200

    Revert "reverted commit 8afdbced5d9021a36a009e56aa406b129bd5c7eb"
    This reverts commit f2d01f3cd5c78d38008c42fa6c514315b6a5a88c.

--- packages/salesforce-adapter/src/adapter.ts
+++ packages/salesforce-adapter/src/adapter.ts
@@ -295,6 +295,7 @@ export default class SalesforceAdapter implements AdapterOperations {
       'PermissionSetTabSetting',
       'PermissionSetUserPermission',
       'KnowledgeSitesSettings',
+      'EntitlementProcessMilestoneItem',
     ],
     nestedMetadataTypes = {
       CustomLabels: {

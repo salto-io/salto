e5279d1efa8a0a2e533121a6125bbdf9fd1e0019
Yanai <yanaise@gmail.com>
2020-12-19 05:24:33 -0500

    Add flush ability to map, as well as remote map tests

--- packages/core/src/core/db/remote_map.ts
+++ packages/core/src/core/db/remote_map.ts
@@ -14,24 +14,29 @@
 * limitations under the License.
 */
 import { Element, ElemID } from '@salto-io/adapter-api'
+import leveldown from 'leveldown'
 import rocksdb from 'rocksdb'
 import { serialization } from '@salto-io/workspace'
 import { promisify } from 'util'
 import LRU from 'lru-cache'
 
 const LRU_OPTIONS = { max: 500 }
-const DB_LOCATION = '/tmp/salto_db'
+export const DB_LOCATION = '/tmp/salto_db'
 const NAMESPACE_SEPARATOR = '_'
+const TEMP_PREFIX = 'TEMP'
 
 const { serialize, deserialize } = serialization
 const BATCH_WRITE_INTERVAL = 1000
 
-type RemoteMap = {
-  get: (key: ElemID) => Promise<Element>
+export type RemoteMap = {
+  get: (key: ElemID) => Promise<Element | undefined>
   getAll: () => AsyncIterator<Element>
   set: (key: ElemID, element: Element) => Promise<void>
   putAll: (elements: AsyncIterable<Element>) => Promise<void>
   list: () => AsyncIterator<ElemID>
+  destroy: () => void
+  close: () => Promise<void>
+  flush: () => Promise<void>
 }
 
 type RocksDBValue = string | Buffer | undefined
@@ -50,87 +55,174 @@ export const createRemoteMap = async (namespace: string): Promise<RemoteMap> => 
   const cache = new LRU<ElemID, Element>(LRU_OPTIONS)
   const keyToDBKey = (key: ElemID): string =>
     namespace.concat(NAMESPACE_SEPARATOR).concat(key.getFullName())
-  return {
-    get: async (key: ElemID): Promise<Element> => new Promise(resolve => {
-      if (cache.has(key)) {
-        resolve(cache.get(key) as Element)
-      } else {
-        db.get(keyToDBKey(key), async (_error, value) => {
-          const ret = (await deserialize(value.toString()))[0]
-          cache.set(key, ret)
-          resolve(ret)
-        })
+  const keyToTempDBKey = (key: ElemID): string =>
+    TEMP_PREFIX.concat(namespace.concat(NAMESPACE_SEPARATOR).concat(key.getFullName()))
+  const putAllImpl = async (elements: AsyncIterable<Element>,
+    temp = true): Promise<void> => {
+    let i = 0
+    let batch = db.batch()
+    for await (const element of elements) {
+      i += 1
+      cache.set(element.elemID, element)
+      batch.put(temp ? keyToTempDBKey(element
+        .elemID) : keyToDBKey(element.elemID), serialize([element]))
+      if (i % BATCH_WRITE_INTERVAL === 0) {
+        await promisify(batch.write.bind(batch))()
+        batch = db.batch()
       }
-    }),
-    getAll: (): AsyncIterator<Element> => {
-      const valueIter = db.iterator({
-        keys: false,
-        gte: namespace.concat(NAMESPACE_SEPARATOR),
-        lte: namespace.concat(String.fromCharCode(NAMESPACE_SEPARATOR.charCodeAt(0) + 1)),
-      })
-      return {
-        next: async () => {
-          let done = false
-          let curVal: RocksDBValue
-          await new Promise<void>(resolve => {
+    }
+    if (i % BATCH_WRITE_INTERVAL !== 0) {
+      await promisify(batch.write.bind(batch))()
+    }
+  }
+  const getAllImpl = (tempOnly = false): AsyncIterator<Element> => {
+    const tempValueIter = db.iterator({
+      keys: false,
+      gte: TEMP_PREFIX.concat(namespace.concat(NAMESPACE_SEPARATOR)),
+      lte: TEMP_PREFIX.concat(namespace
+        .concat(String.fromCharCode(NAMESPACE_SEPARATOR.charCodeAt(0) + 1))),
+    })
+    const valueIter = db.iterator({
+      keys: false,
+      gte: namespace.concat(NAMESPACE_SEPARATOR),
+      lte: namespace.concat(String.fromCharCode(NAMESPACE_SEPARATOR.charCodeAt(0) + 1)),
+    })
+    let done = false
+    let tempDone = false
+    return {
+      next: async () => {
+        let curVal: RocksDBValue
+        await new Promise<void>(resolve => {
+          if (!tempDone) {
+            tempValueIter.next((_err, _key, value) => {
+              tempDone = value === undefined
+              if (!tempDone) {
+                curVal = value
+                resolve()
+              } else {
+                valueIter.next((_innerErr, _innerKey, innerValue) => {
+                  done = innerValue === undefined
+                  curVal = innerValue
+                  resolve()
+                })
+              }
+            })
+          } else {
             valueIter.next((_err, _key, value) => {
               done = value === undefined
               curVal = value
               resolve()
             })
-          })
-          return {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            value: curVal ? (await deserialize(curVal.toString()))[0] : undefined as any,
-            done,
           }
-        },
+        })
+        if (tempOnly) {
+          done = tempDone
+        }
+        return {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          value: curVal ? (await deserialize(curVal.toString()))[0] : undefined as any,
+          done,
+        }
+      },
+    }
+  }
+  return {
+    get: async (key: ElemID): Promise<Element | undefined> => new Promise(resolve => {
+      if (cache.has(key)) {
+        resolve(cache.get(key) as Element)
+      } else {
+        const resolveRet = async (value: Buffer | string): Promise<void> => {
+          const ret = (await deserialize(value.toString()))[0]
+          cache.set(key, ret)
+          resolve(ret)
+        }
+        db.get(keyToTempDBKey(key), async (error, value) => {
+          if (error) {
+            db.get(keyToDBKey(key), async (innerError, innerValue) => {
+              if (innerError) {
+                resolve(undefined)
+              } else {
+                await resolveRet(innerValue)
+              }
+            })
+          } else {
+            await resolveRet(value)
+          }
+        })
       }
-    },
+    }),
+    getAll: getAllImpl,
     set: async (key: ElemID, element: Element): Promise<void> => new Promise(resolve => {
       cache.set(key, element)
-      db.put(keyToDBKey(key), serialize([element]), () => { resolve() })
+      db.put(keyToTempDBKey(key), serialize([element]), () => { resolve() })
     }),
-    putAll: async (elements: AsyncIterable<Element>) => {
-      let i = 0
-      let batch = db.batch()
-      for await (const element of elements) {
-        i += 1
-        cache.set(element.elemID, element)
-        batch.put(keyToDBKey(element.elemID), serialize([element]))
-        if (i % BATCH_WRITE_INTERVAL === 0) {
-          await promisify(batch.write.bind(batch))()
-          batch = db.batch()
-        }
-      }
-      if (i % BATCH_WRITE_INTERVAL !== 0) {
-        await promisify(batch.write.bind(batch))()
-      }
-    },
+    putAll: putAllImpl,
     list: () => {
+      const tempKeyIter = db.iterator({
+        values: false,
+        gte: TEMP_PREFIX.concat(namespace.concat(NAMESPACE_SEPARATOR)),
+        lte: TEMP_PREFIX.concat(namespace.concat(String
+          .fromCharCode(NAMESPACE_SEPARATOR.charCodeAt(0) + 1))),
+      })
       const keyIter = db.iterator({
         values: false,
         gte: namespace.concat(NAMESPACE_SEPARATOR),
         lte: namespace.concat(String.fromCharCode(NAMESPACE_SEPARATOR.charCodeAt(0) + 1)),
       })
+      let done = false
+      let tempDone = false
       return {
         next: async () => {
-          let done = false
           let value: RocksDBValue
           await new Promise<void>(resolve => {
-            keyIter.next((_err, next) => {
-              done = next === undefined
-              value = next
-              resolve()
-            })
+            if (!tempDone) {
+              tempKeyIter.next((_err, next) => {
+                tempDone = next === undefined
+                if (!tempDone) {
+                  value = next
+                  resolve()
+                } else {
+                  keyIter.next((_innerErr, innerNext) => {
+                    done = innerNext === undefined
+                    value = innerNext
+                    resolve()
+                  })
+                }
+              })
+            } else {
+              keyIter.next((_err, next) => {
+                done = next === undefined
+                value = next
+                resolve()
+              })
+            }
           })
           return {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            value: value ? ElemID.fromFullName(value.toString().replace(namespace.concat(NAMESPACE_SEPARATOR), '')) : undefined as any,
+            value: value ? ElemID.fromFullName(value.toString().replace(namespace.concat(NAMESPACE_SEPARATOR), '')
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              .replace(TEMP_PREFIX, '')) : undefined as any,
             done,
           }
         },
       }
+    },
+    flush: async () => {
+      await putAllImpl({ [Symbol.asyncIterator]: () => getAllImpl(true) }, false)
+      return new Promise<void>(resolve => {
+        db.clear({
+          gte: TEMP_PREFIX,
+          lte: TEMP_PREFIX.substring(0, TEMP_PREFIX.length - 1).concat(String
+            .fromCharCode(TEMP_PREFIX[TEMP_PREFIX.length - 1].charCodeAt(0) + 1)),
+        }, () => {
+          resolve()
+        })
+      })
+    },
+    close: () => promisify(db.close.bind(db))(),
+    destroy: () => {
+      leveldown.destroy(DB_LOCATION, _error => {
+        // no error handling atm
+      })
     },
   }
 }

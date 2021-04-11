bac9275cbcd083ca534a32be44f91f02f7852da8
Adam Rosenthal <adamro87@gmail.com>
2021-04-11 18:09:03 +0300

    Logic impl

--- packages/salesforce-adapter/src/filters/custom_object_instances_references.ts
+++ packages/salesforce-adapter/src/filters/custom_object_instances_references.ts
@@ -17,78 +17,135 @@ import _ from 'lodash'
 import { collections, values as lowerdashValues } from '@salto-io/lowerdash'
 import { transformValues, TransformFunc } from '@salto-io/adapter-utils'
 import { logger } from '@salto-io/logging'
-import {
-  Element, Values, ObjectType, Field, InstanceElement,
-  ReferenceExpression,
-} from '@salto-io/adapter-api'
+import { Element, Values, Field, InstanceElement, ReferenceExpression } from '@salto-io/adapter-api'
 import { FilterCreator } from '../filter'
 import { apiName, isInstanceOfCustomObject } from '../transformers/transformer'
 import { FIELD_ANNOTATIONS, CUSTOM_OBJECT_ID_FIELD } from '../constants'
 import { isLookupField, isMasterDetailField } from './utils'
 
 const { makeArray } = collections.array
+const { isDefined } = lowerdashValues
+const { DefaultMap } = collections.map
 
 const log = logger(module)
 
-const replaceReferenceValues = (
-  values: Values,
-  type: ObjectType,
-  instancesByType: Record<string, Record<string, InstanceElement>>
-): Values => {
-  const shouldReplace = (field: Field): boolean => (
-    isLookupField(field) || isMasterDetailField(field)
-  )
+const serializeInternalID = (typeName: string, id: string): string =>
+  (`${typeName}&${id}`)
 
-  const transformFunc: TransformFunc = ({ value, field }) => {
-    if (_.isUndefined(field) || !shouldReplace(field)) {
-      return value
+const serializeInstanceInternalID = (instance: InstanceElement): string =>
+  serializeInternalID(apiName(instance.type, true), instance.value[CUSTOM_OBJECT_ID_FIELD])
+
+const createInternalToInstance = (instances: InstanceElement[]): Record<string, InstanceElement> =>
+  (_.keyBy(instances, serializeInstanceInternalID))
+
+const shouldReplaceFieldVal = (field: Field): boolean => (
+  isLookupField(field) || isMasterDetailField(field)
+)
+
+const replaceLookupsWithReferencesAndCreateRefMap = (
+  instances: InstanceElement[],
+  internalToInstance: Record<string, InstanceElement>,
+): {
+  reverseRefsMap: collections.map.DefaultMap<string, Set<string>>
+  referenceToNothing: Set<InstanceElement>
+} => {
+  const internalToReferencedFrom = new DefaultMap<string, Set<string>>(() => new Set())
+  const referenceToNothing = new Set<InstanceElement>()
+  const replaceLookups = (
+    instance: InstanceElement
+  ): Values => {
+    const transformFunc: TransformFunc = ({ value, field }) => {
+      if (_.isUndefined(field) || !shouldReplaceFieldVal(field)) {
+        return value
+      }
+      const refTo = makeArray(field?.annotations?.[FIELD_ANNOTATIONS.REFERENCE_TO])
+      const refTarget = refTo
+        .map(typeName => internalToInstance[serializeInternalID(typeName, value)])
+        .filter(isDefined)
+        .pop()
+      if (refTarget === undefined) {
+        referenceToNothing.add(instance)
+        return value
+      }
+      internalToReferencedFrom.get(serializeInstanceInternalID(refTarget))
+        .add(serializeInstanceInternalID(instance))
+      return new ReferenceExpression(refTarget.elemID)
     }
-    const refTo = makeArray(field?.annotations?.[FIELD_ANNOTATIONS.REFERENCE_TO])
-    const refTarget = refTo
-      .map(typeName => instancesByType[typeName]?.[value])
-      .filter(lowerdashValues.isDefined)
-      .pop()
-    return refTarget === undefined ? value : new ReferenceExpression(refTarget.elemID)
+
+    return transformValues(
+      {
+        values: instance.value,
+        type: instance.type,
+        transformFunc,
+        strict: false,
+      }
+    ) ?? instance.value
   }
 
-  return transformValues(
-    {
-      values,
-      type,
-      transformFunc,
-      strict: false,
-    }
-  ) ?? values
-}
-
-const replaceLookupsWithReferences = (elements: Element[]): void => {
-  const customObjectInstances = elements.filter(isInstanceOfCustomObject)
-  const instancesByType = _.mapValues(
-    _.groupBy(
-      customObjectInstances,
-      instance => apiName(instance.type, true)
-    ),
-    typeInstances =>
-      _.keyBy(
-        typeInstances,
-        inst => inst.value[CUSTOM_OBJECT_ID_FIELD]
-      )
-  ) as Record<string, Record<string, InstanceElement>>
-  customObjectInstances.forEach((instance, index) => {
-    instance.value = replaceReferenceValues(
-      instance.value,
-      instance.type,
-      instancesByType,
-    )
-    if (index % 500 === 0) {
+  instances.forEach((instance, index) => {
+    instance.value = replaceLookups(instance)
+    if (index > 0 && index % 500 === 0) {
       log.debug(`Replaced lookup with references for ${index} instances`)
     }
   })
+  return {
+    reverseRefsMap: internalToReferencedFrom, referenceToNothing,
+  }
+}
+
+const getIllegalRefsFrom = (
+  referencesToNothing: Set<InstanceElement>,
+  duplicates: InstanceElement[],
+  reverseRefsMap: collections.map.DefaultMap<string, Set<string>>,
+): Set<string> => {
+  const illegalRefsFromSet = new Set<string>()
+  const illegalRefsTargets = [
+    ...[...referencesToNothing].flatMap(serializeInstanceInternalID),
+    ...duplicates.flatMap(serializeInstanceInternalID),
+  ]
+  while (illegalRefsTargets.length > 0) {
+    const currentBrokenRef = illegalRefsTargets.pop()
+    if (currentBrokenRef === undefined) {
+      break
+    }
+    const refsToCurrentIllegal = [...reverseRefsMap.get(currentBrokenRef)]
+    refsToCurrentIllegal.filter(r => !illegalRefsFromSet.has(r))
+      .forEach(newIllegalRefFrom => {
+        illegalRefsTargets.push(newIllegalRefFrom)
+        illegalRefsFromSet.add(newIllegalRefFrom)
+      })
+  }
+  return illegalRefsFromSet
 }
 
 const filter: FilterCreator = () => ({
   onFetch: async (elements: Element[]) => {
-    replaceLookupsWithReferences(elements)
+    const customObjectInstances = elements.filter(isInstanceOfCustomObject)
+    const internalToInstance = createInternalToInstance(customObjectInstances)
+    const { reverseRefsMap, referenceToNothing } = replaceLookupsWithReferencesAndCreateRefMap(
+      customObjectInstances,
+      internalToInstance,
+    )
+    const instancesWithDuplicateElemID = Object
+      .values(_.groupBy(customObjectInstances, instance => instance.elemID.getFullName()))
+      .filter(instances => instances.length > 1)
+      .flat()
+    const illegalRefsFrom = getIllegalRefsFrom(
+      referenceToNothing, instancesWithDuplicateElemID, reverseRefsMap,
+    )
+    const invalidInstances = new Set(
+      [
+        ...illegalRefsFrom,
+        ...[...referenceToNothing].flatMap(serializeInstanceInternalID),
+        ...instancesWithDuplicateElemID.flatMap(serializeInstanceInternalID),
+      ]
+    )
+    _.remove(
+      elements,
+      element =>
+        (isInstanceOfCustomObject(element)
+        && !invalidInstances.has(serializeInstanceInternalID(element))),
+    )
   },
 })
 

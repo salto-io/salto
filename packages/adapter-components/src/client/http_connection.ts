3a24906842b44320e54d149a80e3c77bb08d43b9
Neta Marcus <netam.code@gmail.com>
2021-04-13 07:50:43 -0700

    cr change - different template param for post data

--- packages/adapter-components/src/client/http_connection.ts
+++ packages/adapter-components/src/client/http_connection.ts
@@ -37,10 +37,10 @@ export type Response<T> = {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type APIConnection<T = any> = {
+export type APIConnection<T = any, S = any> = {
   // based on https://github.com/axios/axios/blob/f472e5da5fe76c72db703d6a0f5190e4ad31e642/index.d.ts#L140
   get: (url: string, config?: { params: Record<string, unknown> }) => Promise<Response<T>>
-  post: (url: string, data: T, config?: { params: Record<string, unknown> }) => Promise<Response<T>>
+  post: (url: string, data: S, config?: { params: Record<string, unknown> }) => Promise<Response<T>>
 }
 
 type AuthenticatedAPIConnection = APIConnection & {

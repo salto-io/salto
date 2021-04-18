5a6bc7b72588e4d89fdf8bf744dfc444901ffc67
Nitzan Haim <61940253+nitzan-haim@users.noreply.github.com>
2021-04-18 17:02:51 +0300

    SALTO-1163: Change validator for multiple default values (#1945)
    * added a change validator to check for multiple 'default = true' in picklists & in instances' valueSet fields

---
+++ packages/salesforce-adapter/src/change_validators/multiple_deafults.ts
@@ -0,0 +1,124 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import {
+  ChangeError, getChangeElement, ChangeValidator, isAdditionOrModificationChange,
+  isInstanceChange, Field, InstanceElement,
+  isFieldChange, isListType, TypeElement, isMapType, Value, Values, isReferenceExpression,
+} from '@salto-io/adapter-api'
+import { safeJsonStringify } from '@salto-io/adapter-utils'
+import _ from 'lodash'
+import { FIELD_ANNOTATIONS, LABEL } from '../constants'
+import { isFieldOfCustomObject } from '../transformers/transformer'
+
+const fieldNameToInnerContextField: Record<string, string> = {
+  applicationVisibilities: 'application',
+  recordTypeVisibilities: 'recordType',
+  standardValue: 'label',
+  customValue: 'label',
+}
+
+type ValueSetInnerObject = {
+  default: boolean
+  label: string
+} & Values[]
+
+type FieldWithValueSet = Field & {
+  annotations: {
+    valueSet: Array<ValueSetInnerObject>
+  }
+}
+
+const isFieldWithValueSet = (field: Field): field is FieldWithValueSet => (
+  _.isArray(field.annotations[FIELD_ANNOTATIONS.VALUE_SET])
+)
+
+const formatContext = (context: Value): string => {
+  if (isReferenceExpression(context)) {
+    return context.elemId.getFullName()
+  }
+  if (_.isString(context)) {
+    return context
+  }
+  return safeJsonStringify(context)
+}
+
+const createChangeError = (field: Field, contexts: string[], instanceName?: string):
+  ChangeError => ({
+  elemID: field.elemID,
+  severity: 'Warning',
+  message: `There cannot be more than one 'default' field set to 'true'. Field name: ${field.name} in${instanceName ? ` instance: ${instanceName}` : ''} type ${field.parent.elemID.name}.`,
+  detailedMessage: `There cannot be more than one 'default' ${field.name} in${instanceName ? ` instance: ${instanceName}` : ''} type ${field.parent.elemID.name}. The following ${fieldNameToInnerContextField[field.name] ?? LABEL}s are set to default: ${contexts}`,
+})
+
+const getPicklistMultipleDefaultsErrors = (field: FieldWithValueSet): ChangeError[] => {
+  const contexts = field.annotations.valueSet
+    .filter(obj => obj.default)
+    .map(obj => obj[LABEL])
+    .map(formatContext)
+  return contexts.length > 1 ? [createChangeError(field, contexts)] : []
+}
+
+const getInstancesMultipleDefaultsErrors = (after: InstanceElement): ChangeError[] => {
+  const getDefaultObjectsList = (val: Value, type: TypeElement): Value[] => {
+    if (isMapType(type)) {
+      return Object.values(val).flatMap(inner => getDefaultObjectsList(inner, type.innerType))
+    }
+    if (isListType(type) && _.isArray(val)) {
+      return val.flatMap(inner => getDefaultObjectsList(inner, type.innerType))
+    }
+    return val
+  }
+
+  const errors: ChangeError[] = Object.entries(after.value)
+    .filter(([fieldName]) => Object.keys(fieldNameToInnerContextField).includes(fieldName))
+    .flatMap(([fieldName, value]) => {
+      const defaultObjects = getDefaultObjectsList(value, after.type.fields[fieldName].type)
+      const contexts = defaultObjects
+        .filter(val => val.default)
+        .map(obj => obj[fieldNameToInnerContextField[fieldName]])
+        .map(formatContext)
+      return contexts.length > 1
+        ? [createChangeError(after.type.fields[fieldName], contexts, after.elemID.name)]
+        : []
+    })
+
+  return errors
+}
+
+/**
+ * It is forbidden to set more than one 'default' field as 'true' for some types.
+ */
+const changeValidator: ChangeValidator = async changes => {
+  const instanceChangesErrors = changes
+    .filter(isAdditionOrModificationChange)
+    .filter(isInstanceChange)
+    .map(getChangeElement)
+    .flatMap(getInstancesMultipleDefaultsErrors)
+
+  // special treatment for picklist & multipicklist valueSets
+  const picklistChangesErrors = changes
+    .filter(isAdditionOrModificationChange)
+    .filter(isFieldChange)
+    .map(getChangeElement)
+    .filter(isFieldOfCustomObject)
+    .filter(isFieldWithValueSet)
+    .flatMap(getPicklistMultipleDefaultsErrors)
+
+  return [...instanceChangesErrors, ...picklistChangesErrors]
+}
+
+export default changeValidator
---
+++ packages/salesforce-adapter/test/change_validators/multiple_defaults.test.ts
@@ -0,0 +1,391 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import {
+  ChangeError, ElemID, Field, InstanceElement, ListType, ObjectType, toChange,
+  BuiltinTypes, MapType,
+} from '@salto-io/adapter-api'
+import { safeJsonStringify } from '@salto-io/adapter-utils'
+import { createInstanceElement, Types } from '../../src/transformers/transformer'
+import multipleDefaultsValidator from '../../src/change_validators/multiple_deafults'
+import { createField } from '../utils'
+import { API_NAME, CUSTOM_OBJECT, METADATA_TYPE, SALESFORCE } from '../../src/constants'
+
+
+describe('multiple defaults change validator', () => {
+  const runChangeValidatorOnUpdate = (before: Field | InstanceElement,
+    after: Field | InstanceElement):
+      Promise<ReadonlyArray<ChangeError>> =>
+    multipleDefaultsValidator([toChange({ before, after })])
+  const createChangeField = (name: string, type: ObjectType): Field => (
+    new Field(type, name, new ListType(new ObjectType(
+      { elemID: new ElemID(SALESFORCE, 'valuesList'),
+        fields: {
+          fullName: { type: BuiltinTypes.STRING },
+          default: { type: BuiltinTypes.BOOLEAN },
+          label: { type: BuiltinTypes.STRING },
+        } }
+    )))
+  )
+  describe('in custom fields', () => {
+    let obj: ObjectType
+    beforeEach(() => {
+      obj = new ObjectType({
+        elemID: new ElemID('salesforce', 'obj'),
+        annotations: {
+          [METADATA_TYPE]: CUSTOM_OBJECT,
+          [API_NAME]: 'obj',
+        },
+      })
+    })
+
+    const createAfterField = (beforeField: Field): Field => {
+      const afterField = beforeField.clone()
+      afterField.annotations.valueSet[1].default = true
+      return afterField
+    }
+
+    describe('picklist', () => {
+      it('should have warning for a picklist field', async () => {
+        const beforeField = createField(obj, Types.primitiveDataTypes.Picklist, 'PicklistField', {
+          valueSet: [{
+            fullName: 'Gold',
+            default: true,
+            label: 'Gold',
+          },
+          {
+            fullName: 'Silver',
+            default: false,
+            label: 'Silver',
+          },
+          ],
+        })
+        const afterField = createAfterField(beforeField)
+        const changeErrors = await runChangeValidatorOnUpdate(beforeField, afterField)
+        expect(changeErrors).toHaveLength(1)
+        const [changeError] = changeErrors
+        expect(changeError.elemID).toEqual(beforeField.elemID)
+        expect(changeError.severity).toEqual('Warning')
+      })
+      it('should not have error for <= 1 default values in picklist', async () => {
+        const beforeField = createField(obj, Types.primitiveDataTypes.Picklist, 'PicklistField', {
+          valueSet: [{
+            fullName: 'Gold',
+            default: false,
+            label: 'Gold',
+          },
+          {
+            fullName: 'Silver',
+            default: false,
+            label: 'Silver',
+          },
+          ],
+        })
+        const afterField = createAfterField(beforeField)
+        const changeErrors = await runChangeValidatorOnUpdate(beforeField, afterField)
+        expect(changeErrors).toHaveLength(0)
+      })
+    })
+
+    describe('multi-select picklist', () => {
+      it('should have warning for a MultiselectPicklist field', async () => {
+        const beforeField = createField(obj, Types.primitiveDataTypes.MultiselectPicklist, 'PicklistField', {
+          valueSet: [{
+            fullName: 'Gold',
+            default: true,
+            label: 'Gold',
+          },
+          {
+            fullName: 'Silver',
+            default: false,
+            label: 'Silver',
+          },
+          ],
+        })
+        const afterField = createAfterField(beforeField)
+        const changeErrors = await runChangeValidatorOnUpdate(beforeField, afterField)
+        expect(changeErrors).toHaveLength(1)
+        const [changeError] = changeErrors
+        expect(changeError.elemID).toEqual(beforeField.elemID)
+        expect(changeError.severity).toEqual('Warning')
+      })
+      it('should not have error for <= 1 default values in picklist', async () => {
+        const beforeField = createField(obj, Types.primitiveDataTypes.MultiselectPicklist, 'PicklistField', {
+          valueSet: [{
+            fullName: 'Gold',
+            default: false,
+            label: 'Gold',
+          },
+          {
+            fullName: 'Silver',
+            default: false,
+            label: 'Silver',
+          },
+          ],
+        })
+        const afterField = createAfterField(beforeField)
+        const changeErrors = await runChangeValidatorOnUpdate(beforeField, afterField)
+        expect(changeErrors).toHaveLength(0)
+      })
+    })
+  })
+  describe('In metadata instances', () => {
+    describe('GlobalValueSet', () => {
+      let type: ObjectType
+      beforeEach(() => {
+        type = new ObjectType({ elemID: new ElemID(SALESFORCE, 'GlobalValueSet'),
+          fields: { customValue: { type: new ListType(new ObjectType({
+            elemID: new ElemID(SALESFORCE, 'CustomValue'),
+            fields: {
+              fullName: { type: BuiltinTypes.STRING },
+              default: { type: BuiltinTypes.BOOLEAN },
+              label: { type: BuiltinTypes.STRING },
+            },
+            annotations: {
+              [METADATA_TYPE]: 'CustomValue',
+            },
+          })) } } })
+      })
+
+      const createAfterInstance = (beforeInstance: InstanceElement): InstanceElement => {
+        const afterInstance = beforeInstance.clone()
+        afterInstance.value.customValue[1].default = true
+        return afterInstance
+      }
+
+      it('should have warning for a GlobalValueSet instance', async () => {
+        const beforeInstance = createInstanceElement({ fullName: 'globalValueSetIntance',
+          customValue: [
+            {
+              fullName: 'lolo',
+              default: true,
+              label: 'lolo',
+            },
+            {
+              fullName: 'lala',
+              default: false,
+              label: 'lala',
+            },
+          ] }, type)
+        const changeField = createChangeField('customValue', type)
+        const afterInstance = createAfterInstance(beforeInstance)
+        const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+        expect(changeErrors).toHaveLength(1)
+        const [changeError] = changeErrors
+        expect(changeError.elemID).toEqual(changeField.elemID)
+        expect(changeError.severity).toEqual('Warning')
+      })
+      it('should not have error for <= 1 default values GlobalValueSet', async () => {
+        const beforeInstance = createInstanceElement({ fullName: 'globalValueSetIntance',
+          customValue: [
+            {
+              fullName: 'lolo',
+              default: false,
+              label: 'lolo',
+            },
+            {
+              fullName: 'lala',
+              default: false,
+              label: 'lala',
+            },
+          ] }, type)
+        const afterInstance = createAfterInstance(beforeInstance)
+        const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+        expect(changeErrors).toHaveLength(0)
+      })
+    })
+
+    describe('Profile', () => {
+      let type: ObjectType
+      beforeEach(() => {
+        type = new ObjectType({
+          elemID: new ElemID(SALESFORCE, 'Profile'),
+          fields: {
+            applicationVisibilities: { type: new MapType(new ObjectType({
+              elemID: new ElemID(SALESFORCE, 'ProfileApplicationVisibility'),
+              fields: { default: { type: BuiltinTypes.BOOLEAN } },
+              annotations: {
+                [METADATA_TYPE]: 'ProfileApplicationVisibility',
+              },
+            })) },
+            recordTypeVisibilities: { type: new MapType(new MapType(new ObjectType({
+              elemID: new ElemID(SALESFORCE, 'ProfileRecordTypeVisibility'),
+              fields: { default: { type: BuiltinTypes.BOOLEAN } },
+              annotations: {
+                [METADATA_TYPE]: 'ProfileRecordTypeVisibility',
+              },
+            }))) },
+          },
+          annotations: {
+            [METADATA_TYPE]: 'Profile',
+          },
+        })
+      })
+
+      describe('ProfileApplicationVisibility', () => {
+        const createAfterInstance = (beforeInstance: InstanceElement): InstanceElement => {
+          const afterInstance = beforeInstance.clone()
+          afterInstance.value.applicationVisibilities.app.default = true
+          return afterInstance
+        }
+
+        it('should have warning for ProfileApplicationVisibility', async () => {
+          const beforeInstance = createInstanceElement({
+            fullName: 'ProfileInstance',
+            applicationVisibilities: {
+              app: {
+                default: false,
+              },
+              anotherApp: {
+                default: true,
+              },
+            },
+          }, type)
+
+          const afterInstance = createAfterInstance(beforeInstance)
+          const changeField = createChangeField('applicationVisibilities', type)
+          const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+          expect(changeErrors).toHaveLength(1)
+          const [changeError] = changeErrors
+          expect(changeError.elemID).toEqual(changeField.elemID)
+          expect(changeError.severity).toEqual('Warning')
+        })
+
+        it('should not have error for <= 1 default values in ProfileApplicationVisibility', async () => {
+          const beforeInstance = createInstanceElement({
+            fullName: 'ProfileInstance',
+            applicationVisibilities: {
+              app: {
+                default: false,
+              },
+              anotherApp: {
+                default: false,
+              },
+            },
+          }, type)
+
+          const afterInstance = createAfterInstance(beforeInstance)
+          const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+          expect(changeErrors).toHaveLength(0)
+        })
+      })
+
+      describe('ProfileRecordTypeVisibility', () => {
+        const createAfterInstance = (beforeInstance: InstanceElement): InstanceElement => {
+          const afterInstance = beforeInstance.clone()
+          afterInstance.value.recordTypeVisibilities.test1.testRecordType1.default = true
+          return afterInstance
+        }
+        it('should have error for ProfileRecordTypeVisibility', async () => {
+          const beforeInstance = createInstanceElement({
+            fullName: 'ProfileInstance',
+            recordTypeVisibilities: {
+              test1: {
+                testRecordType1: {
+                  default: false,
+                },
+              },
+              test2: {
+                testRecordType2: {
+                  default: true,
+                },
+              },
+
+            },
+          }, type)
+
+          const afterInstance = createAfterInstance(beforeInstance)
+          const changeField = createChangeField('recordTypeVisibilities', type)
+          const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+          expect(changeErrors).toHaveLength(1)
+          const [changeError] = changeErrors
+          expect(changeError.elemID).toEqual(changeField.elemID)
+          expect(changeError.severity).toEqual('Warning')
+        })
+
+        it('should not have error for <= 1 default values in ProfileRecordTypeVisibility', async () => {
+          const beforeInstance = createInstanceElement({
+            fullName: 'ProfileInstance',
+            recordTypeVisibilities: {
+              test1: {
+                testRecordType1: {
+                  default: false,
+                },
+              },
+              test2: {
+                testRecordType2: {
+                  default: false,
+                },
+              },
+
+            },
+          }, type)
+
+          const afterInstance = createAfterInstance(beforeInstance)
+          const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+          expect(changeErrors).toHaveLength(0)
+        })
+      })
+
+      describe('several warnings in one instance', () => {
+        const createAfterInstance = (beforeInstance: InstanceElement): InstanceElement => {
+          const afterInstance = beforeInstance.clone()
+          afterInstance.value.recordTypeVisibilities.test1.testRecordType1.default = true
+          afterInstance.value.applicationVisibilities.app.default = true
+          return afterInstance
+        }
+        it('should have 2 errors', async () => {
+          const beforeInstance = createInstanceElement({
+            fullName: 'ProfileInstance',
+            recordTypeVisibilities: {
+              test1: {
+                testRecordType1: {
+                  default: false,
+                },
+              },
+              test2: {
+                testRecordType2: {
+                  default: true,
+                },
+              },
+            },
+            applicationVisibilities: {
+              app: {
+                default: false,
+              },
+              anotherApp: {
+                default: true,
+              },
+            },
+          }, type)
+
+          const afterInstance = createAfterInstance(beforeInstance)
+          const changeFieldRecordType = createChangeField('recordTypeVisibilities', type)
+          const changeFieldApplication = createChangeField('applicationVisibilities', type)
+          const changeErrors = await runChangeValidatorOnUpdate(beforeInstance, afterInstance)
+          const changeErrorsIds = changeErrors.map(error => safeJsonStringify(error.elemID))
+          expect(changeErrors).toHaveLength(2)
+
+          // doesn't work without the JsonStringify
+          expect(changeErrorsIds).toContain(safeJsonStringify(changeFieldRecordType.elemID))
+          expect(changeErrorsIds).toContain(safeJsonStringify(changeFieldApplication.elemID))
+          changeErrors.forEach(error => {
+            expect(error.severity).toEqual('Warning')
+          })
+        })
+      })
+    })
+  })
+})

3b2449d7800d104890fb1be5daf18264da0d96bf
Roi Ronn <roi.ronn@salto.io>
2021-04-27 14:59:10 +0300

    rebase fix

--- packages/netsuite-adapter/e2e_test/adapter.test.ts
+++ packages/netsuite-adapter/e2e_test/adapter.test.ts
@@ -289,7 +289,7 @@ describe('Netsuite adapter E2E with real account', () => {
         const permissions = makeArray(fetchedCustomRecordType.value.permissions?.permission)
         const createdRolePermission = permissions
           .find(permission => isReferenceExpression(permission.permittedrole)
-          && permission.permittedrole.elemId
+          && permission.permittedrole.elemID
             .isEqual(roleToCreateThatDependsOnCustomRecord.elemID.createNestedID(SCRIPT_ID)))
         expect(createdRolePermission).toBeDefined()
       })
@@ -303,7 +303,7 @@ describe('Netsuite adapter E2E with real account', () => {
         const permissions = makeArray(fetchedRole.value.permissions?.permission)
         const customRecordTypePermission = permissions
           .find(permission => isReferenceExpression(permission.permkey)
-          && permission.permkey.elemId
+          && permission.permkey.elemID
             .isEqual(customRecordTypeToCreate.elemID.createNestedID(SCRIPT_ID)))
         expect(customRecordTypePermission).toBeDefined()
       })

2dd1002b5601cf051744b888e137b875356cb12c
netama <netam.code@gmail.com>
2021-05-11 16:15:29 -0700

    Fix dummy adapter failure on 2nd unittest run (#2064)

--- packages/dummy-adapter/generate.sh
+++ packages/dummy-adapter/generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p dist/src/data/
 cp src/data/strings.dat dist/src/data/strings.dat
-cp -R src/data/fixtures dist/src/data/fixtures
+cp -R src/data/fixtures dist/src/data/

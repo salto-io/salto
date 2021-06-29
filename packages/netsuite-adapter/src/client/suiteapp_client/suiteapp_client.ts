4cdd6bb41ace6ce85d3357c03c68e1e6aaab8192
Alon Stern <alon.stern@salto.io>
2021-06-29 12:42:52 +0300

    fixed missing results from suiteql

--- packages/netsuite-adapter/src/client/suiteapp_client/suiteapp_client.ts
+++ packages/netsuite-adapter/src/client/suiteapp_client/suiteapp_client.ts
@@ -22,6 +22,7 @@ import { logger } from '@salto-io/logging'
 import _ from 'lodash'
 import { safeJsonStringify } from '@salto-io/adapter-utils'
 import { WSDL } from 'soap'
+import { values } from '@salto-io/lowerdash'
 import { CallsLimiter, ExistingFileCabinetInstanceDetails, FileCabinetInstanceDetails,
   FILES_READ_SCHEMA, HttpMethod, isError, ReadResults, RestletOperation, RestletResults,
   RESTLET_RESULTS_SCHEMA, SavedSearchQuery, SavedSearchResults, SAVED_SEARCH_RESULTS_SCHEMA,
@@ -79,12 +80,29 @@ export default class SuiteAppClient {
 
   public async runSuiteQL(query: string):
     Promise<Record<string, unknown>[] | undefined> {
+    // There seems to be a bug in Netsuite SuiteQL in which
+    // for each page size we have some results missing.
+    // To handle this we run the query twice with two page sizes and merge the results
+    const results = (await Promise.all([
+      this.runSingleSuiteQL(query, PAGE_SIZE),
+      this.runSingleSuiteQL(query, PAGE_SIZE - 1),
+    ])).flat()
+
+    if (results.some(res => res === undefined)) {
+      return undefined
+    }
+
+    return _.uniqBy(results.filter(values.isDefined), result => safeJsonStringify(result))
+  }
+
+  private async runSingleSuiteQL(query: string, pageSize: number):
+    Promise<Record<string, unknown>[] | undefined> {
     let hasMore = true
     const items: Record<string, unknown>[] = []
-    for (let offset = 0; hasMore; offset += PAGE_SIZE) {
+    for (let offset = 0; hasMore; offset += pageSize) {
       try {
-        // eslint-disable-next-line no-await-in-loop
-        const results = await this.sendSuiteQLRequest(query, offset, PAGE_SIZE)
+      // eslint-disable-next-line no-await-in-loop
+        const results = await this.sendSuiteQLRequest(query, offset, pageSize)
         // For some reason, a "links" field with empty array is returned regardless
         // to the SELECT values in the query.
         items.push(...results.items.map(item => _.omit(item, ['links'])))

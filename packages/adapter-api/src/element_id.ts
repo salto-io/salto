e651b9461d75d24e895f775aa603dd868f63ffbf
Neta Marcus <netam.code@gmail.com>
2021-07-05 23:03:48 +0300

    use isAnnotationTypeID everywhere

--- packages/adapter-api/src/element_id.ts
+++ packages/adapter-api/src/element_id.ts
@@ -109,7 +109,7 @@ export class ElemID {
       // First name part is the instance name which is top level
       return this.nameParts.length - 1
     }
-    if (this.idType === 'annotation') {
+    if (this.isAnnotationTypeID()) {
       // annotation is already 1 level nested
       return this.nameParts.length + 1
     }
@@ -186,7 +186,7 @@ export class ElemID {
       // The parent of top level elements is the adapter
       return new ElemID(this.adapter)
     }
-    if (this.idType === 'annotation' && this.nameParts.length === 1) {
+    if (this.isAnnotationTypeID() && this.nameParts.length === 1) {
       // The parent of an annotationType is annotationTypes
       return new ElemID(this.adapter, this.typeName, this.idType)
     }

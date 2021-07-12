444441526ba9bc1ec5cd1846b5b58b86605d16ed
guy hamo <guyhamo1@gmail.com>
2021-07-12 10:35:33 +0300

    finished static file changes

--- packages/netsuite-adapter/src/change_validators/saved_search_move_environment.ts
+++ packages/netsuite-adapter/src/change_validators/saved_search_move_environment.ts
@@ -22,8 +22,9 @@ import { parseDefinition } from '../saved_search_parser'
 
 const { awu } = collections.asynciterable
 const wasModified = async (instance:InstanceElement):Promise<boolean> => {
-  const p = await parseDefinition(instance.value.definition)
-  return Object.keys(p).some((i:string) => !_.isEqual(p[i], instance.value[i]))
+  const parsedDefinition = await parseDefinition(instance.value.definition.content)
+  return Object.keys(parsedDefinition)
+    .some((i:string) => !_.isEqual(parsedDefinition[i], instance.value[i]))
 }
 
 

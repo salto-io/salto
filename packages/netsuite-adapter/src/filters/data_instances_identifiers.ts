e8fd84a45965ea710630fc15cbee1ffcdf5db6cc
alonstern <alon.stern@salto.io>
2021-08-02 13:26:31 +0300

    SALTO-825: open data instances (#2297)

---
+++ packages/netsuite-adapter/src/filters/data_instances_identifiers.ts
@@ -0,0 +1,44 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { Change, getChangeElement, InstanceElement, isInstanceChange } from '@salto-io/adapter-api'
+import { applyFunctionToChangeData } from '@salto-io/adapter-utils'
+import { collections } from '@salto-io/lowerdash'
+import { IDENTIFIER_FIELD } from '../data_elements/types'
+import { FilterWith } from '../filter'
+import { isDataObjectType } from '../types'
+
+const { awu } = collections.asynciterable
+
+const filterCreator = (): FilterWith<'preDeploy'> => ({
+  preDeploy: async changes => {
+    await awu(changes)
+      .filter(isInstanceChange)
+      .filter(async change => isDataObjectType(
+        await getChangeElement<InstanceElement>(change).getType()
+      ))
+      .forEach(async change => {
+        applyFunctionToChangeData<Change<InstanceElement>>(
+          change,
+          element => {
+            delete element.value[IDENTIFIER_FIELD]
+            return element
+          }
+        )
+      })
+  },
+})
+
+export default filterCreator
---
+++ packages/netsuite-adapter/test/filters/data_instances_identifiers.test.ts
@@ -0,0 +1,30 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ElemID, InstanceElement, ObjectType, toChange } from '@salto-io/adapter-api'
+import filterCreator from '../../src/filters/data_instances_identifiers'
+import { NETSUITE } from '../../src/constants'
+import { IDENTIFIER_FIELD } from '../../src/data_elements/types'
+
+describe('data_instances_identifiers', () => {
+  it('should remove identifier field', async () => {
+    const accountType = new ObjectType({ elemID: new ElemID(NETSUITE, 'Account'), annotations: { source: 'soap' } })
+    const accountInstance = new InstanceElement('instance', accountType, { [IDENTIFIER_FIELD]: 'someValue' })
+    await filterCreator().preDeploy?.([
+      toChange({ after: accountInstance }),
+    ])
+    expect(accountInstance.value[IDENTIFIER_FIELD]).toBeUndefined()
+  })
+})

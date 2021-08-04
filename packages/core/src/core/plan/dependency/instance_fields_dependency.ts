6ff829f2810aac69af5633a1c58b877301971235
Adam Rosenthal <adamro87@gmail.com>
2021-08-04 13:52:51 +0300

    Add dependency between Instance and it's Type's fields

---
+++ packages/core/src/core/plan/dependency/instance_fields_dependency.ts
@@ -0,0 +1,45 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import wu from 'wu'
+import { collections } from '@salto-io/lowerdash'
+import { DependencyChange, isInstanceChangeEntry, InstanceElement, ChangeEntry, DependencyChanger, getChangeElement, isField, isDependentAction, addReferenceDependency } from '@salto-io/adapter-api'
+
+const { awu } = collections.asynciterable
+
+export const addInstanceToFieldsDependency: DependencyChanger = async changes => {
+  const fieldChanges = collections.iterable.groupBy(
+    wu(changes).filter(([_id, change]) => isField(getChangeElement(change))),
+    ([_id, change]) => getChangeElement(change).elemID.getFullName(),
+  )
+
+  const addChangeFieldDependency = async (
+    [id, change]: ChangeEntry<InstanceElement>,
+  ): Promise<DependencyChange[]> => {
+    const fieldsElemIDs = Object.values((await getChangeElement(change).getType()).fields)
+      .map(field => field.elemID.getFullName())
+    return fieldsElemIDs.flatMap(fieldName => fieldChanges.get(fieldName) ?? [])
+      .filter(([_id, fieldChange]) => isDependentAction(change.action, fieldChange.action))
+      .map(
+        ([fieldChangeId, fieldChange]) =>
+          addReferenceDependency(fieldChange.action, id, fieldChangeId)
+      )
+  }
+
+  return awu(changes)
+    .filter(isInstanceChangeEntry)
+    .flatMap(addChangeFieldDependency)
+    .toArray()
+}

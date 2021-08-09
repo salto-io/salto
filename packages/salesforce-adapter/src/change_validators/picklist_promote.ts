04a239f9e3920e9f78a87f6508eea1353a42d2d0
Roy Segev <emaohi@gmail.com>
2021-08-09 18:33:42 +0300

    add tests

--- packages/salesforce-adapter/src/change_validators/picklist_promote.ts
+++ packages/salesforce-adapter/src/change_validators/picklist_promote.ts
@@ -14,11 +14,13 @@
 * limitations under the License.
 */
 import { Change, ChangeError, Field, getAllChangeElements, isModificationChange, ChangeValidator,
-  getChangeElement, ReferenceExpression} from '@salto-io/adapter-api'
+  getChangeElement, ReferenceExpression, SaltoErrorSeverity, ChangeDataType } from '@salto-io/adapter-api'
 import { collections } from '@salto-io/lowerdash'
 import { apiName, isCustom } from '../transformers/transformer'
 import { isPicklistField, isGlobalValueSetPicklistField } from '../filters/value_set'
 import { VALUE_SET_FIELDS } from '../constants'
+import { isInstanceOfType } from '../filters/utils'
+import { GLOBAL_VALUE_SET } from '../filters/global_value_sets'
 
 const { awu } = collections.asynciterable
 
@@ -28,21 +30,42 @@ const isGlobalPicklistChange = async (change: Change): Promise<boolean> => {
   && !isGlobalValueSetPicklistField(before) && isGlobalValueSetPicklistField(after)
 }
 
-const createChangeErrors = (pickListField: Field): ChangeError[] => {
-  const gvsElemID = pickListField.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME].elemID
-
-  return [{
+const createChangeErrors = (res: {pickListField: ChangeDataType, globalValueSetFound: boolean}): ChangeError[] => {
+  const {pickListField, globalValueSetFound} = res
+  const picklistErr = {
     elemID: pickListField.elemID,
-    severity: 'Error',
-    message: `You cannot promote a picklist value set to global one. Field: ${pickListField.name}`,
+    severity: 'Error' as SaltoErrorSeverity,
+    message: `You cannot promote a picklist value set to global one. Field: ${(pickListField as Field).name}`,
     detailedMessage: 'You cannot promote a picklist value set to global one. Please promote via the service.',
-  },
-  {
-    elemID: gvsElemID,
-    severity: 'Error',
-    message: `You cannot create a global value set as a result of a picklist promote. Field: ${gvsElemID.name}`,
-    detailedMessage: 'You cannot promote a picklist value set to global one. Please promote via the service.',
-  }]
+  }
+  // picklistField valueSetName annotation points to a valid GVS
+  if (globalValueSetFound) {
+    const gvsElemID = pickListField.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME].value.elemID
+    const gvsErr = {
+      elemID: gvsElemID,
+      severity: 'Error' as SaltoErrorSeverity,
+      message: `You cannot create a global value set as a result of a picklist promote. Value list name: ${gvsElemID.name}`,
+      detailedMessage: 'You cannot create a global value set as a result of a picklist promote. Please promote via the service.',
+    }
+    return [picklistErr, gvsErr]
+  } else {
+    return [picklistErr]
+  }
+}
+
+// validate that the changed picklist field has corresponding new global value set addition change
+const valdiateCreatedGvsCreated = (changes: readonly Change[]) => {
+  return (change: Change): boolean => {
+    const referencedGvs = getChangeElement(change).annotations[VALUE_SET_FIELDS.VALUE_SET_NAME]
+    if (referencedGvs instanceof ReferenceExpression) {
+      const referencedValue = referencedGvs.value
+      if (isInstanceOfType(GLOBAL_VALUE_SET)(referencedValue)) {
+        return changes.filter(c => c.action === 'add'
+          && getChangeElement(c).elemID.getFullName() === referencedValue.elemID.getFullName()).length !== 0
+      }
+    }
+    return false
+  }
 }
 
 /**
@@ -50,12 +73,18 @@ const createChangeErrors = (pickListField: Field): ChangeError[] => {
  */
 const changeValidator: ChangeValidator = async changes => {
 
+  const gvsCreatedResolver = valdiateCreatedGvsCreated(changes)
+
   return awu(changes)
     .filter(isModificationChange)
     .filter(isGlobalPicklistChange)
-    .map(getChangeElement)
-    .flatMap(field => createChangeErrors(field as Field))
+    .map((change) => {
+      return { pickListField: getChangeElement(change), globalValueSetFound: gvsCreatedResolver(change) }
+    })
+    .flatMap(createChangeErrors)
     .toArray()
 }
+
+
 
 export default changeValidator
---
+++ packages/salesforce-adapter/test/change_validators/picklist_promote.test.ts
@@ -0,0 +1,103 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import {
+  Change,
+  ElemID, Field, InstanceElement, ObjectType, ReferenceExpression, toChange,
+} from '@salto-io/adapter-api'
+import { Types } from '../../src/transformers/transformer'
+import picklistPromoteValidator from '../../src/change_validators/picklist_promote'
+import { API_NAME, VALUE_SET_FIELDS } from '../../src/constants'
+import { createField } from '../utils'
+import * as constants from '../../src/constants'
+import { GLOBAL_VALUE_SET } from '../../src/filters/global_value_sets'
+
+
+describe('picklist promote change validator', () => {
+  describe('onUpdate', () => {
+
+    const createGlobalValueSetInstanceElement = (): InstanceElement =>
+      new InstanceElement('global_value_set_test', new ObjectType({
+        elemID: new ElemID(constants.SALESFORCE, 'global_value_set'),
+        annotationRefsOrTypes: {},
+        annotations: { [constants.METADATA_TYPE]: GLOBAL_VALUE_SET },
+      }))
+
+    const pickListFieldName = 'pick__c'
+    let obj: ObjectType
+    let beforeField: Field
+    let afterField: Field
+    let gvs: InstanceElement
+    let pickListChange: Change
+    let gvsChange: Change
+    beforeEach(() => {
+      obj = new ObjectType({
+        elemID: new ElemID('salesforce', 'obj'),
+      })
+      beforeField = createField(obj, Types.primitiveDataTypes.Picklist, pickListFieldName)
+      beforeField.name = pickListFieldName
+      gvs = createGlobalValueSetInstanceElement()
+      afterField = createAfterField(beforeField)
+      pickListChange = toChange({ before: beforeField, after: afterField })
+      gvsChange = toChange({after: gvs})
+    })
+
+    const createAfterField = (beforeField: Field): Field => {
+      const afterField = beforeField.clone()
+      afterField.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME] = new ReferenceExpression(new ElemID(''), gvs)
+      return afterField
+    }
+    
+    it('should have created 2 errors (for picklist & value-set)', async () => {
+      const changeErrors = await picklistPromoteValidator([pickListChange, gvsChange])
+      expect(changeErrors).toHaveLength(2)
+      const errorElemsNamesSet = new Set(changeErrors.map((changeErr) => changeErr.elemID.name))
+      expect(errorElemsNamesSet).toEqual(new Set(['pick__c', 'global_value_set_test']))
+    })
+
+    it('should have created just picklist error when value-set change doesn\'t exist', async () => {
+      const changeErrors = await picklistPromoteValidator([pickListChange])
+      expect(changeErrors).toHaveLength(1)
+      const errorElemsNamesSet = new Set(changeErrors.map((changeErr) => changeErr.elemID.name))
+      expect(new Set(['pick__c'])).toEqual(errorElemsNamesSet)
+    })
+
+    it('should have created just picklist error when extra change isn\'t reference', async () => {
+      afterField.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME] = "some string"
+      const changeErrors = await picklistPromoteValidator([pickListChange, gvsChange])
+      expect(changeErrors).toHaveLength(1)
+      const errorElemsNamesSet = new Set(changeErrors.map((changeErr) => changeErr.elemID.name))
+      expect(new Set(['pick__c'])).toEqual(errorElemsNamesSet)
+    })
+
+    it('should not have errors for non-custom picklist', async () => {
+      beforeField.annotations[API_NAME] = 'pick'
+      const changeErrors = await picklistPromoteValidator([pickListChange, gvsChange])
+      expect(changeErrors).toHaveLength(0)
+    })
+
+    it('should not have errors for already global value-set picklist field', async () => {
+      beforeField.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME] = 'something'
+      const changeErrors = await picklistPromoteValidator([pickListChange, gvsChange])
+      expect(changeErrors).toHaveLength(0)
+    })
+    
+    it('should not have errors for moving to a non-global value-set change', async () => {
+      delete afterField.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME]
+      const changeErrors = await picklistPromoteValidator([pickListChange])
+      expect(changeErrors).toHaveLength(0)
+    })
+  })
+})

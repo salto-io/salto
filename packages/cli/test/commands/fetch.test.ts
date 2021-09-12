e84e2878a9dbc959416e5e56c8567d78cd1ba597
guy hamo <guyhamo1@gmail.com>
2021-09-12 19:38:33 +0300

    Revert "Salto 1224/nacl detailed change (#2372)"
    This reverts commit 9f6de667ccf26f8ebf6e90a340bd668c0618c864.

--- packages/cli/test/commands/fetch.test.ts
+++ packages/cli/test/commands/fetch.test.ts
@@ -243,7 +243,7 @@ describe('fetch command', () => {
       })
       describe('with upstream changes', () => {
         const changes = mocks.dummyChanges.map(
-          (change: DetailedChange): FetchChange => ({ change, serviceChange: change, audit: {} })
+          (change: DetailedChange): FetchChange => ({ change, serviceChange: change })
         )
         const mockFetchWithChanges = jest.fn().mockResolvedValue(
           {

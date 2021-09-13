6de24be2937858dc492b20d4033d081132c8bc72
guy hamo <guyhamo1@gmail.com>
2021-09-13 17:54:17 +0300

    export metadata type

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 export { Plan, PlanItem } from './src/core/plan'
-export { FetchChange, FetchProgressEvents, StepEmitter } from './src/core/fetch'
+export { FetchChange, FetchProgressEvents, StepEmitter, FetchChangeMetadata } from './src/core/fetch'
 export * from './src/api'
 export { ItemStatus } from './src/core/deploy'
 export { getAdaptersCredentialsTypes, getDefaultAdapterConfig } from './src/core/adapters/adapters'

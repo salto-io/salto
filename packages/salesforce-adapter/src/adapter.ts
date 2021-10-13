41a2bc0c7a9af550911612903740bcab299d4df0
Nitzan Haim <nitzanhaim@gmail.com>
2021-10-13 13:31:13 +0300

    PR comments

--- packages/salesforce-adapter/src/adapter.ts
+++ packages/salesforce-adapter/src/adapter.ts
@@ -459,9 +459,9 @@ export default class SalesforceAdapter implements AdapterOperations {
           .includes(await apiName(type)))
         .toArray()
 
-      const result = await awu(objectTypesToRead)
-        .map(async type => this.createMetadataInstances(type))
-        .toArray()
+      const result = await Promise.all(
+        objectTypesToRead.map(type => this.createMetadataInstances(type))
+      )
 
       return {
         elements: _.flatten(result.map(r => r.elements)),

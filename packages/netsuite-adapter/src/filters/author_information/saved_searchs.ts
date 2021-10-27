8520ba72a869a5dea671ae0cc4813b35ba57f012
guy hamo <guyhamo1@gmail.com>
2021-10-27 10:35:01 +0300

    add new filter for saved searches author information

---
+++ packages/netsuite-adapter/src/filters/author_information/saved_searchs.ts
@@ -0,0 +1,113 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { isInstanceElement, Element, CORE_ANNOTATIONS, InstanceElement } from '@salto-io/adapter-api'
+import { logger } from '@salto-io/logging'
+import Ajv from 'ajv'
+import _ from 'lodash'
+import { SAVED_SEARCH } from '../../constants'
+import { FilterCreator, FilterWith } from '../../filter'
+import NetsuiteClient from '../../client/client'
+
+const log = logger(module)
+
+const SAVED_SEARCH_RESULT_SCHEMA = {
+  items: {
+    properties: {
+      id: {
+        type: 'string',
+      },
+      modifiedby: {
+        items: {
+          properties: {
+            text: {
+              type: 'string',
+            },
+            value: {
+              type: 'string',
+            },
+          },
+          required: [
+            'text',
+            'value',
+          ],
+          type: 'object',
+        },
+        type: 'array',
+      },
+    },
+    required: [
+      'id',
+      'modifiedby',
+    ],
+    type: 'object',
+  },
+  type: 'array',
+}
+
+type modifiedbyField = {
+  value: string
+  text: string
+}
+
+type SavedSearchesResult = {
+  id: string
+  modifiedby: modifiedbyField[]
+}
+
+const isSavedSearchInstance = (element: Element): element is InstanceElement =>
+  isInstanceElement(element) && element.elemID.typeName === SAVED_SEARCH
+
+const fetchSavedSearches = async (client: NetsuiteClient): Promise<SavedSearchesResult[]> => {
+  const savedSearches = await client.runSavedSearchQuery({
+    type: 'savedsearch',
+    columns: ['modifiedby', 'id'],
+    filters: ['datemodified', 'noton', '7/11/2022 9:33 pm'],
+  })
+  if (savedSearches === undefined) {
+    return []
+  }
+  const ajv = new Ajv({ allErrors: true, strict: false })
+  if (!ajv.validate<SavedSearchesResult[]>(SAVED_SEARCH_RESULT_SCHEMA, savedSearches)) {
+    log.error(`Got invalid results from listing employees table: ${ajv.errorsText()}`)
+    throw new Error('Failed to list employees')
+  }
+  return savedSearches
+}
+
+const getSavedSearchesModifiersMap = async (
+  client: NetsuiteClient,
+): Promise<Record<string, string>> => {
+  const savedSearches = await fetchSavedSearches(client)
+  return Object.fromEntries(savedSearches.map(savedSearch =>
+    [savedSearch.id, savedSearch.modifiedby[0].text]))
+}
+
+const filterCreator: FilterCreator = ({ client }): FilterWith<'onFetch'> => ({
+  onFetch: async elements => {
+    if (!client.isSuiteAppConfigured()) {
+      return
+    }
+    const savedSearchesMap = await getSavedSearchesModifiersMap(client)
+    if (_.isEmpty(savedSearchesMap)) {
+      return
+    }
+    const savedSearchesInstances = elements.filter(isSavedSearchInstance)
+    savedSearchesInstances.forEach(search =>
+      search.annotate({ [CORE_ANNOTATIONS.CHANGED_BY]: savedSearchesMap[search.value.scriptid] }))
+  },
+})
+
+export default filterCreator

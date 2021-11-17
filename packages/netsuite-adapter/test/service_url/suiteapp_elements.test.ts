9a4858b9cd039daabd28d68c3ecf8cd8557c4ca3
Shir Reifenberg <shir.reifenberg@salto.io>
2021-11-17 14:15:17 +0200

    update tests to raise coverage

--- packages/netsuite-adapter/test/service_url/suiteapp_elements.test.ts
+++ packages/netsuite-adapter/test/service_url/suiteapp_elements.test.ts
@@ -28,9 +28,19 @@ describe('setSuiteAppElementsUrl', () => {
     elements = [
       new InstanceElement('A', new ObjectType({ elemID: new ElemID(NETSUITE, 'account'), annotations: { source: 'soap' } }), { internalId: '123' }),
       new InstanceElement('B', new ObjectType({ elemID: new ElemID(NETSUITE, 'subsidiary'), annotations: { source: 'soap' } }), { internalId: '124' }),
-      new InstanceElement('C', new ObjectType({ elemID: new ElemID(NETSUITE, 'manufacturingCostTemplate'), annotations: { source: 'soap' } }), { internalId: '125' }),
-      new InstanceElement('D', new ObjectType({ elemID: new ElemID(NETSUITE, 'inventoryItem'), annotations: { source: 'soap' } }), { internalId: '126' }),
-      new InstanceElement('E', new ObjectType({ elemID: new ElemID(NETSUITE, 'assemblyItem'), annotations: { source: 'soap' } }), { internalId: '127' }),
+      new InstanceElement('C', new ObjectType({ elemID: new ElemID(NETSUITE, 'department'), annotations: { source: 'soap' } }), { internalId: '125' }),
+      new InstanceElement('D', new ObjectType({ elemID: new ElemID(NETSUITE, 'classification'), annotations: { source: 'soap' } }), { internalId: '126' }),
+      new InstanceElement('E', new ObjectType({ elemID: new ElemID(NETSUITE, 'location'), annotations: { source: 'soap' } }), { internalId: '127' }),
+      new InstanceElement('F', new ObjectType({ elemID: new ElemID(NETSUITE, 'currency'), annotations: { source: 'soap' } }), { internalId: '128' }),
+      new InstanceElement('G', new ObjectType({ elemID: new ElemID(NETSUITE, 'customer'), annotations: { source: 'soap' } }), { internalId: '129' }),
+      new InstanceElement('H', new ObjectType({ elemID: new ElemID(NETSUITE, 'accountingPeriod'), annotations: { source: 'soap' } }), { internalId: '130' }),
+      new InstanceElement('I', new ObjectType({ elemID: new ElemID(NETSUITE, 'employee'), annotations: { source: 'soap' } }), { internalId: '131' }),
+      new InstanceElement('J', new ObjectType({ elemID: new ElemID(NETSUITE, 'job'), annotations: { source: 'soap' } }), { internalId: '132' }),
+      new InstanceElement('K', new ObjectType({ elemID: new ElemID(NETSUITE, 'manufacturingCostTemplate'), annotations: { source: 'soap' } }), { internalId: '133' }),
+      new InstanceElement('L', new ObjectType({ elemID: new ElemID(NETSUITE, 'partner'), annotations: { source: 'soap' } }), { internalId: '134' }),
+      new InstanceElement('M', new ObjectType({ elemID: new ElemID(NETSUITE, 'solution'), annotations: { source: 'soap' } }), { internalId: '135' }),
+      new InstanceElement('N', new ObjectType({ elemID: new ElemID(NETSUITE, 'inventoryItem'), annotations: { source: 'soap' } }), { internalId: '136' }),
+      new InstanceElement('O', new ObjectType({ elemID: new ElemID(NETSUITE, 'assemblyItem'), annotations: { source: 'soap' } }), { internalId: '137' }),
     ]
   })
 
@@ -38,9 +48,19 @@ describe('setSuiteAppElementsUrl', () => {
     await setSuiteAppUrls(elements, client)
     expect(elements[0].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/accounting/account/account.nl?id=123')
     expect(elements[1].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/otherlists/subsidiarytype.nl?id=124')
-    expect(elements[2].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/accounting/manufacturing/mfgcosttemplate.nl?id=125')
-    expect(elements[3].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/item/item.nl?id=126')
-    expect(elements[4].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/item/item.nl?id=127')
+    expect(elements[2].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/otherlists/departmenttype.nl?id=125')
+    expect(elements[3].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/otherlists/classtype.nl?id=126')
+    expect(elements[4].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/otherlists/locationtype.nl?id=127')
+    expect(elements[5].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/multicurrency/currency.nl?e=T&id=128')
+    expect(elements[6].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/entity/custjob.nl?id=129')
+    expect(elements[7].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/setup/period/fiscalperiod.nl?e=T&id=130')
+    expect(elements[8].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/entity/employee.nl?id=131')
+    expect(elements[9].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/accounting/project/project.nl?id=132')
+    expect(elements[10].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/accounting/manufacturing/mfgcosttemplate.nl?id=133')
+    expect(elements[11].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/entity/partner.nl?id=134')
+    expect(elements[12].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/crm/support/kb/solution.nl?id=135')
+    expect(elements[13].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/item/item.nl?id=136')
+    expect(elements[14].annotations[CORE_ANNOTATIONS.SERVICE_URL]).toEqual('https://tstdrv2259448.app.netsuite.com/app/common/item/item.nl?id=137')
   })
 
   it('should not set url if the type name is not in list', async () => {

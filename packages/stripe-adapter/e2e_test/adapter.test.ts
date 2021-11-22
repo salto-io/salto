8734b028ec82cac3274e09c1f3dde2a5dca17464
tamtamirr <tamir.ohayon@salto.io>
2021-11-22 19:32:46 +0200

    Stripe E2E: - Turned test into parametrized tests for more specific errors. - Removed plans related types from the test.

--- packages/stripe-adapter/e2e_test/adapter.test.ts
+++ packages/stripe-adapter/e2e_test/adapter.test.ts
@@ -13,73 +13,69 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { isObjectType } from '@salto-io/adapter-api'
-import { CredsLease } from '@salto-io/e2e-credentials-store'
-import { AccessTokenCredentials } from '../src/auth'
-import { credsLease, realAdapter } from './adapter'
-import StripeAdapter from '../src/adapter'
+import {isObjectType, ObjectType} from '@salto-io/adapter-api'
+import {CredsLease} from '@salto-io/e2e-credentials-store'
+import {AccessTokenCredentials} from '../src/auth'
+import {credsLease, realAdapter} from './adapter'
 
+
+jest.setTimeout(10 * 1000)
+
+// assumes the mapping: v1__<plural-typeName> -> <plural-typeName> is declared for all the supported types.
 describe('Stripe adapter E2E with real swagger and mock replies', () => {
-  let adapter: StripeAdapter
-  let credLease: CredsLease<AccessTokenCredentials>
+    let swaggerObjectTypes: ObjectType[]
+    let credLease: CredsLease<AccessTokenCredentials>
 
-  jest.setTimeout(10 * 1000)
-
-  beforeAll(async () => {
-    credLease = await credsLease()
-    const adapterAttr = realAdapter({ credentials: credLease.value })
-    adapter = adapterAttr.adapter
-  })
-
-  afterAll(async () => {
-    if (credLease.return) {
-      await credLease.return()
-    }
-  })
-
-  describe('fetch', () => {
-    describe('swagger types only', () => {
-      it('should generate the right type elements on fetch', async () => {
-        const { elements } = await adapter.fetch({ progressReporter:
-          { reportProgress: () => null } })
-
-        expect(elements.every(isObjectType)).toBeTruthy()
-        expect(elements.map(e => e.elemID.getFullName())).toEqual(expect.arrayContaining([
-          // assuming the mapping: v1__<plural-typeName> -> <plural-typeName>
-          // is defined in typeNameOverrides:
-          'stripe.country_specs',
-          'stripe.coupons',
-          'stripe.plans',
-          'stripe.prices',
-          'stripe.products',
-          'stripe.reporting__report_types',
-          'stripe.tax_rates',
-          'stripe.webhook_endpoints',
-
-          'stripe.price',
-          'stripe.product',
-          'stripe.reporting_report_type',
-          'stripe.country_spec',
-          'stripe.coupon',
-          'stripe.plan',
-          'stripe.tax_rate',
-          'stripe.webhook_endpoint',
-          'stripe.product_metadata',
-          'stripe.package_dimensions',
-          'stripe.recurring',
-          'stripe.price_tier',
-          'stripe.transform_quantity',
-          'stripe.country_spec_supported_bank_account_currencies',
-          'stripe.country_spec_verification_fields',
-          'stripe.coupon_applies_to',
-          'stripe.coupon_metadata',
-          'stripe.plan_metadata',
-          'stripe.plan_tier',
-          'stripe.transform_usage',
-          'stripe.tax_rate_metadata',
-          'stripe.webhook_endpoint_metadata',
-        ]))
-      })
+    beforeAll(async () => {
+        credLease = await credsLease()
+        const adapterAttr = realAdapter({credentials: credLease.value})
+        const {elements} = await adapterAttr.adapter.fetch({
+            progressReporter:
+                {reportProgress: () => null}
+        })
+        swaggerObjectTypes = elements.filter(isObjectType)
     })
-  })
+
+    afterAll(async () => {
+        if (credLease.return) {
+            await credLease.return()
+        }
+    })
+
+    const TYPE_NAMES = [
+        'stripe.country_specs',
+        'stripe.coupons',
+        'stripe.prices',
+        'stripe.products',
+        'stripe.reporting__report_types',
+        'stripe.tax_rates',
+        'stripe.webhook_endpoints',
+        'stripe.price',
+        'stripe.product',
+        'stripe.reporting_report_type',
+        'stripe.country_spec',
+        'stripe.coupon',
+        'stripe.tax_rate',
+        'stripe.webhook_endpoint',
+        'stripe.product_metadata',
+        'stripe.package_dimensions',
+        'stripe.recurring',
+        'stripe.price_tier',
+        'stripe.transform_quantity',
+        'stripe.country_spec_supported_bank_account_currencies',
+        'stripe.country_spec_verification_fields',
+        'stripe.coupon_applies_to',
+        'stripe.coupon_metadata',
+        'stripe.transform_usage',
+        'stripe.tax_rate_metadata',
+        'stripe.webhook_endpoint_metadata',
+    ]
+
+    describe('fetches swagger types', () => {
+        test.each(TYPE_NAMES)(
+            '%s',
+            async typeName => {
+                expect(swaggerObjectTypes.some(e => e.elemID.getFullName() === typeName))
+            })
+    })
 })

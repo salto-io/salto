77500b178b78310012a12fb524021c39848388ef
tamtamirr <tamir.ohayon@salto.io>
2021-12-08 15:25:25 +0200

    Added tests to custom_object_split.test.ts: - Should not split non-custom object - should filter out empty objects from the filter result

--- packages/salesforce-adapter/test/filters/custom_object_split.test.ts
+++ packages/salesforce-adapter/test/filters/custom_object_split.test.ts
@@ -13,8 +13,9 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+import 'jest-extended'
 import _ from 'lodash'
-import { ObjectType, BuiltinTypes, Element } from '@salto-io/adapter-api'
+import { ObjectType, BuiltinTypes, Element, ElemID } from '@salto-io/adapter-api'
 import filterCreator from '../../src/filters/custom_object_split'
 import { CUSTOM_OBJECT_TYPE_ID } from '../../src/filters/custom_objects'
 import { FilterWith } from '../../src/filter'
@@ -120,4 +121,80 @@ describe('Custom Object Split filter', () => {
       .isEqual(customObject.fields.custom_namespace__c)).toBeTruthy()
   })
 })
+  describe('when split elements contains empty objects', () => {
+    const getSplitElementsPaths = async (...customObjects: ObjectType[]): Promise<string[][]> => {
+      const splitElements = await runFilter(...customObjects)
+      return splitElements.map(customObject => customObject.path as string[] ?? [])
+    }
+
+    it('should filter out empty standard fields object', async () => {
+      const objectWithNoStandardFields = new ObjectType({
+        elemID: CUSTOM_OBJECT_TYPE_ID,
+        fields: {
+          custom__c: {
+            refType: BuiltinTypes.STRING,
+          },
+          custom_namespace__c: {
+            refType: BuiltinTypes.STRING,
+            annotations: {
+              [API_NAME]: 'objectRandom__c.namespace__random__c',
+            },
+          },
+        },
+        annotations: {
+          [METADATA_TYPE]: CUSTOM_OBJECT,
+          [API_NAME]: 'objectRandom__c',
+        },
+      })
+      const splitElementsPaths = await getSplitElementsPaths(objectWithNoStandardFields)
+      expect(splitElementsPaths).toIncludeSameMembers([
+        [SALESFORCE, OBJECTS_PATH, 'CustomObject', 'CustomObjectCustomFields'],
+        [SALESFORCE, OBJECTS_PATH, 'CustomObject', 'CustomObjectAnnotations'],
+      ])
+    })
+    it('should filter out empty custom fields object', async () => {
+      const objectWithNoCustomFields = new ObjectType({
+        elemID: CUSTOM_OBJECT_TYPE_ID,
+        fields: {
+          standard: {
+            refType: BuiltinTypes.STRING,
+          },
+        },
+        annotations: {
+          [METADATA_TYPE]: CUSTOM_OBJECT,
+          [API_NAME]: 'objectRandom__c',
+        },
+      })
+      const splitElementsPaths = await getSplitElementsPaths(objectWithNoCustomFields)
+      expect(splitElementsPaths).toIncludeSameMembers([
+        [SALESFORCE, OBJECTS_PATH, 'CustomObject', 'CustomObjectStandardFields'],
+        [SALESFORCE, OBJECTS_PATH, 'CustomObject', 'CustomObjectAnnotations'],
+      ])
+    })
+  })
+  it('should not split non-custom object', async () => {
+    const nonCustomObject = new ObjectType({
+      elemID: new ElemID(SALESFORCE, 'NonCustomObject'),
+      fields: {
+        standard: {
+          refType: BuiltinTypes.STRING,
+        },
+        custom__c: {
+          refType: BuiltinTypes.STRING,
+        },
+        custom_namespace__c: {
+          refType: BuiltinTypes.STRING,
+          annotations: {
+            [API_NAME]: 'objectRandom__c.namespace__random__c',
+          },
+        },
+      },
+      annotations: {
+        [METADATA_TYPE]: 'NonCustomObject',
+        [API_NAME]: 'objectRandom__c',
+      },
+    })
+    const splitElements = await runFilter(nonCustomObject)
+    expect(splitElements).toEqual([nonCustomObject])
+  })
 })

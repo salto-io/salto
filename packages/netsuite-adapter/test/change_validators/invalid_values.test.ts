542b2cea5530efc21d5781b1c8bd55624f6d2042
Daniel Tal <dantal4@gmail.com>
2021-12-21 22:56:24 +0200

    tests

---
+++ packages/netsuite-adapter/test/change_validators/invalid_values.test.ts
@@ -0,0 +1,87 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { InstanceElement, toChange } from '@salto-io/adapter-api'
+import invalidValuesValidator from '../../src/change_validators/invalid_values'
+import { customTypes } from '../../src/types'
+import { SCRIPT_ID, ROLE, ADDRESS_FORM } from '../../src/constants'
+
+
+describe('invalid values validator', () => {
+  const origInstance = new InstanceElement(
+    'instance',
+    customTypes[ROLE],
+    {
+      isinactive: false,
+      [SCRIPT_ID]: 'customrole1009',
+      name: 'workato_intg',
+      subsidiaryoption: 'ALL',
+    }
+  )
+  let instance: InstanceElement
+
+  beforeEach(() => {
+    instance = origInstance.clone()
+  })
+
+  it('should not have ChangeError when deployoong a type that doesn\'t have invalid value', async () => {
+    const anotherInstance = new InstanceElement(
+      'instance',
+      customTypes[ADDRESS_FORM],
+      {
+        [SCRIPT_ID]: 'custform_2_t1440050_248',
+        name: 'My Address Form',
+      }
+    )
+
+    const after = anotherInstance.clone()
+    after.value.name = 'Changed'
+    const changeErrors = await invalidValuesValidator(
+      [toChange({ before: instance, after })]
+    )
+    expect(changeErrors).toHaveLength(0)
+  })
+  it('should not have ChangeError when deploying an instance without invalid value', async () => {
+    const after = instance.clone()
+    after.value.subsidiaryoption = 'OWN'
+    const changeErrors = await invalidValuesValidator(
+      [toChange({ before: instance, after })]
+    )
+    expect(changeErrors).toHaveLength(0)
+  })
+
+  it('should have error ChangeError when modifying an invalid value', async () => {
+    const after = instance.clone()
+    after.value.subsidiaryoption = 'SELECTED'
+    const changeErrors = await invalidValuesValidator(
+      [toChange({ before: instance, after })]
+    )
+    expect(changeErrors).toHaveLength(1)
+    expect(changeErrors[0].severity).toEqual('Error')
+    expect(changeErrors[0].elemID).toEqual(instance.elemID)
+  })
+
+  it('should not have error ChangeError when modifying an instance with unchanged invalid value', async () => {
+    const before = instance.clone()
+    const after = instance.clone()
+    before.value.subsidiaryoption = 'SELECTED'
+    after.value.subsidiaryoption = 'SELECTED'
+    after.value.name = 'changed'
+    const changeErrors = await invalidValuesValidator(
+      [toChange({ before, after })]
+    )
+    expect(changeErrors).toHaveLength(0)
+  })
+})

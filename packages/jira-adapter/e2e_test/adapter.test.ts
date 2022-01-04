afad4727e7412423e394b0ea0b2ae12dd8b52a54
tamtamirr <tamir.ohayon@salto.io>
2022-01-04 19:05:27 +0200

    Added basic sanity fetch E2E to Jira Adapter

---
+++ packages/jira-adapter/e2e_test/adapter.test.ts
@@ -0,0 +1,47 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { Element } from '@salto-io/adapter-api'
+import 'jest-extended'
+import { CredsLease } from '@salto-io/e2e-credentials-store'
+import { Credentials } from '../src/auth'
+import { credsLease, realAdapter } from './adapter'
+
+jest.setTimeout(10 * 1000)
+
+describe('Jira E2E', () => {
+  let fetchedElements: Element[]
+  let credLease: CredsLease<Credentials>
+
+  beforeAll(async () => {
+    credLease = await credsLease()
+    const adapterAttr = realAdapter({ credentials: credLease.value })
+    const { elements } = await adapterAttr.adapter.fetch({
+      progressReporter:
+        { reportProgress: () => null },
+    })
+    fetchedElements = elements
+  })
+
+  afterAll(async () => {
+    if (credLease.return) {
+      await credLease.return()
+    }
+  })
+
+  it('should fetch elements', () => {
+    expect(fetchedElements).not.toBeEmpty()
+  })
+})

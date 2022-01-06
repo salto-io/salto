3c1824bc38489bcbf1b16471b9705c7378a66c3e
dantal4 <70883080+dantal4@users.noreply.github.com>
2022-01-06 14:21:14 +0200

    SALTO-1752 netsuite - add change validator for invalid values (#2562)

---
+++ packages/netsuite-adapter/src/change_validators/custom_types_invalid_values.ts
@@ -0,0 +1,94 @@
+/*
+*                      Copyright 2021 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import _ from 'lodash'
+import { values } from '@salto-io/lowerdash'
+import {
+  Change,
+  ChangeError,
+  ChangeValidator,
+  getChangeElement,
+  InstanceElement,
+  isAdditionOrModificationChange,
+  isInstanceChange,
+  isModificationChange,
+  SaltoErrorSeverity,
+  Value,
+} from '@salto-io/adapter-api'
+import { isCustomType } from '../types'
+
+const { isDefined } = values
+
+type InvalidValue = {
+  typeName: string
+  path: string[]
+  value: Value
+  error: {
+    severity: SaltoErrorSeverity
+    detailedMessage: string
+  }
+}
+
+const invalidValues: InvalidValue[] = [
+  {
+    typeName: 'role',
+    path: ['subsidiaryoption'],
+    value: 'SELECTED',
+    error: {
+      severity: 'Error',
+      detailedMessage: 'role.subsidiaryoption cannot be deployed with value "SELECTED", please set a different value and manually edit it via the NetSuite UI',
+    },
+  },
+]
+
+const getInvalidValuesChangeErrors = (
+  { typeName, path, value, error }: InvalidValue,
+  instanceChanges: Record<string, Change<InstanceElement>[]>
+): ChangeError[] => {
+  if (!(typeName in instanceChanges)) {
+    return []
+  }
+
+  return instanceChanges[typeName].map(change => {
+    const instance = getChangeElement(change)
+    if (_.get(instance.value, path) !== value
+      || (isModificationChange(change) && _.get(change.data.before.value, path) === value)) {
+      return undefined
+    }
+
+    const elemID = instance.elemID.createNestedID(...path)
+    return {
+      ...error,
+      elemID,
+      message: `Invalid value in ${elemID.getFullName()}`,
+    }
+  }).filter(isDefined)
+}
+
+const changeValidator: ChangeValidator = async changes => {
+  const instanceChanges = _.groupBy(
+    changes
+      .filter(isAdditionOrModificationChange)
+      .filter(isInstanceChange)
+      .filter(change => isCustomType(getChangeElement(change).refType.elemID)),
+    change => getChangeElement(change).elemID.typeName
+  )
+
+  return invalidValues.flatMap(
+    invalidValue => getInvalidValuesChangeErrors(invalidValue, instanceChanges)
+  )
+}
+
+export default changeValidator

b1dcdf5544a133db7f6905a9bd7d76c41eb8bd40
tamtamirr <tamir.ohayon@salto.io>
2022-01-10 12:56:28 +0200

    fixed typo in test

--- packages/logging/test/internal/namespace.test.ts
+++ packages/logging/test/internal/namespace.test.ts
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import {namespaceNormalizer, toHexColor,} from '../../src/internal/namespace'
+import { namespaceNormalizer, toHexColor } from '../../src/internal/namespace'
 
 describe('namespace', () => {
   describe('toHexColor', () => {

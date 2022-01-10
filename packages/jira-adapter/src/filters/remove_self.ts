2833974c5b669a3707d83f64e854b5678a6d584e
alonstern <alon.stern@salto.io>
2022-01-10 20:21:07 +0200

    SALTO-1780: removed redundant values from the Jira instances (#2615)

---
+++ packages/jira-adapter/src/filters/remove_self.ts
@@ -0,0 +1,54 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { Element, isInstanceElement, isObjectType } from '@salto-io/adapter-api'
+import { transformValues } from '@salto-io/adapter-utils'
+import { collections } from '@salto-io/lowerdash'
+import { FilterCreator } from '../filter'
+
+const { awu } = collections.asynciterable
+
+/**
+ * Removes 'self' values from types and instances
+ */
+const filter: FilterCreator = () => ({
+  onFetch: async (elements: Element[]) => {
+    await awu(elements)
+      .filter(isInstanceElement)
+      .forEach(async instance => {
+        instance.value = await transformValues({
+          values: instance.value,
+          type: await instance.getType(),
+          pathID: instance.elemID,
+          strict: false,
+          allowEmpty: true,
+          transformFunc: ({ value, path }) => {
+            if (path?.name === 'self') {
+              return undefined
+            }
+            return value
+          },
+        }) ?? {}
+      })
+
+    elements
+      .filter(isObjectType)
+      .forEach(async type => {
+        delete type.fields.self
+      })
+  },
+})
+
+export default filter
---
+++ packages/jira-adapter/test/filters/remove_self.test.ts
@@ -0,0 +1,69 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { BuiltinTypes, ElemID, InstanceElement, ObjectType } from '@salto-io/adapter-api'
+import { filterUtils } from '@salto-io/adapter-components'
+import { DEFAULT_CONFIG } from '../../src/config'
+import { JIRA } from '../../src/constants'
+import removeSelfFilter from '../../src/filters/remove_self'
+import { mockClient } from '../utils'
+
+describe('removeSelfFilter', () => {
+  let filter: filterUtils.FilterWith<'onFetch'>
+  let instance: InstanceElement
+  let type: ObjectType
+  beforeEach(async () => {
+    const { client, paginator } = mockClient()
+    filter = removeSelfFilter({
+      client,
+      paginator,
+      config: DEFAULT_CONFIG,
+    }) as typeof filter
+
+    type = new ObjectType({
+      elemID: new ElemID(JIRA, 'someType'),
+      fields: {
+        self: { refType: BuiltinTypes.STRING },
+      },
+    })
+
+    instance = new InstanceElement(
+      'instance',
+      type,
+      {
+        self: 'someSelf',
+        obj: {
+          self: 'someSelf2',
+          other: 'other',
+        },
+        other: 'other',
+      }
+    )
+  })
+
+  it('should remove self from types', async () => {
+    await filter.onFetch([type])
+    expect(type.fields.self).toBeUndefined()
+  })
+  it('should remove self from instanecs', async () => {
+    await filter.onFetch([instance])
+    expect(instance.value).toEqual({
+      other: 'other',
+      obj: {
+        other: 'other',
+      },
+    })
+  })
+})

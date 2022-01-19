3aa5ba50701aa570cac9bbec482b27da1dcdaf71
Neta Marcus <netam.code@gmail.com>
2022-01-19 17:42:32 +0200

    SALTO-1791 Remove restrictions impacted by custom objects and fields

---
+++ packages/salesforce-adapter/src/filters/remove_restriction_annotations.ts
@@ -0,0 +1,60 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { isObjectType, Element, ObjectType, CORE_ANNOTATIONS } from '@salto-io/adapter-api'
+import { collections } from '@salto-io/lowerdash'
+import { FilterCreator } from '../filter'
+import { metadataType } from '../transformers/transformer'
+
+const { awu } = collections.asynciterable
+
+const FIELDS_TO_REMOVE_RESTRICTION_FROM_BY_TYPE: Record<string, string[]> = {
+  AccessControlPolicy: ['targetEntity'],
+  AnimationRule: ['sobjectType', 'targetField'],
+  PlatformEventChannelMember: ['selectedEntity'],
+  RestrictionRule: ['targetEntity'],
+  AppProfileActionOverride: ['pageOrSobjectType'],
+  QueueSobject: ['sobjectType'],
+}
+
+/**
+ * Remove _restriction annotation values from types that are impacted by custom objects.
+ * Note: This is a short-term solution - once SALTO-1381 is implemented, we should
+ * convert the builtin _restriction annotation to hidden (with _hidden_value=true),
+ * and hide all the values on upgrade.
+ */
+export const makeFilter = (
+  typeNameToFieldMapping: Record<string, string[]>,
+): FilterCreator => () => ({
+  onFetch: async (elements: Element[]) => {
+    const removeRestrictionsFromTypeFields = async (type: ObjectType): Promise<void> => {
+      const relevantFields = typeNameToFieldMapping[await metadataType(type)]
+      relevantFields.forEach(fieldName => {
+        const field = type.fields[fieldName]
+        if (field !== undefined && field.annotations[CORE_ANNOTATIONS.RESTRICTION] !== undefined) {
+          field.annotations[CORE_ANNOTATIONS.RESTRICTION] = undefined
+        }
+      })
+    }
+
+    await awu(elements).filter(isObjectType).filter(
+      async type => typeNameToFieldMapping[
+        await metadataType(type)
+      ] !== undefined
+    ).forEach(removeRestrictionsFromTypeFields)
+  },
+})
+
+export default makeFilter(FIELDS_TO_REMOVE_RESTRICTION_FROM_BY_TYPE)

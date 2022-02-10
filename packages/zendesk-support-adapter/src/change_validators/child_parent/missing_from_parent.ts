edfc3ed477c11348ed1ad32f697f0fb74862d985
Noam Hammer <noam9995@gmail.com>
2022-02-10 17:05:01 +0200

    SALTO-1984: add child parent change validators (#2775)

---
+++ packages/zendesk-support-adapter/src/change_validators/child_parent/missing_from_parent.ts
@@ -0,0 +1,67 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import _ from 'lodash'
+import { ChangeValidator, CORE_ANNOTATIONS, getChangeData, InstanceElement, isInstanceElement,
+  isReferenceExpression, isInstanceChange, AdditionChange, ChangeError, isAdditionChange,
+  isAdditionOrModificationChange } from '@salto-io/adapter-api'
+import { ZendeskApiConfig } from '../../config'
+import { getChildAndParentTypeNames, getRemovedAndAddedChildren } from './utils'
+
+export const createParentReferencesError = (
+  change: AdditionChange<InstanceElement>,
+  parentFullName: string,
+): ChangeError => {
+  const instance = getChangeData(change)
+  return {
+    elemID: instance.elemID,
+    severity: 'Error',
+    message: `Can not add ${instance.elemID.typeName} because there is no reference to it from its parent`,
+    detailedMessage: `Can not add ${instance.elemID.getFullName()} because there is no reference to it from ${parentFullName}`,
+  }
+}
+
+export const missingFromParentValidatorCreator = (
+  apiConfig: ZendeskApiConfig,
+): ChangeValidator => async changes => {
+  const relationships = getChildAndParentTypeNames(apiConfig)
+  const childrenTypes = new Set(relationships.map(r => r.child))
+  const instanceChanges = changes.filter(isInstanceChange)
+  const relevantChanges = instanceChanges
+    .filter(isAdditionChange)
+    .filter(change => childrenTypes.has(getChangeData(change).elemID.typeName))
+  const changeByID = _.keyBy(instanceChanges, change => getChangeData(change).elemID.getFullName())
+  return relevantChanges.flatMap(change => {
+    const instance = getChangeData(change)
+    const { typeName } = instance.elemID
+    const parentRef = instance.annotations[CORE_ANNOTATIONS.PARENT]?.[0]
+    if (!(isReferenceExpression(parentRef) && isInstanceElement(parentRef.value))) {
+      return []
+    }
+    const parentFullName = parentRef.value.elemID.getFullName()
+    const relevantRelations = relationships.filter(r => r.child === typeName)
+    return relevantRelations.flatMap(relation => {
+      const parentChange = changeByID[parentFullName]
+      if (parentChange === undefined || !isAdditionOrModificationChange(parentChange)) {
+        return [createParentReferencesError(change, parentFullName)]
+      }
+      const { added } = getRemovedAndAddedChildren(parentChange, relation.fieldName)
+      if (isAdditionChange(change) && !added.includes(instance.elemID.getFullName())) {
+        return [createParentReferencesError(change, parentFullName)]
+      }
+      return []
+    })
+  })
+}

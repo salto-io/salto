8691fcc2798ff2665bea9e7e224db809a9193996
Noam Hammer <noam9995@gmail.com>
2022-02-21 17:23:31 +0200

    SALTO-1938: change validator for custom role names (#2809)

---
+++ packages/zendesk-support-adapter/src/change_validators/custom_role.ts
@@ -0,0 +1,81 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import _ from 'lodash'
+import { Change, ChangeValidator, getChangeData, InstanceElement, isAdditionChange,
+  isAdditionOrModificationChange, isInstanceChange, isInstanceElement, isModificationChange } from '@salto-io/adapter-api'
+import { collections } from '@salto-io/lowerdash'
+import { logger } from '@salto-io/logging'
+
+const { awu } = collections.asynciterable
+const log = logger(module)
+const SYSTEM_ROLE_NAMES = [
+  'agen', 'agent', 'administrator', 'admin', 'billing admin', 'light agent',
+]
+export const CUSTOM_ROLE_TYPE_NAME = 'custom_role'
+
+const isRelevantChange = (change: Change<InstanceElement>): boolean =>
+  (getChangeData(change).elemID.typeName === CUSTOM_ROLE_TYPE_NAME)
+  && (isAdditionChange(change)
+    || (isModificationChange(change)
+      && (change.data.before.value.name !== change.data.after.value.name)))
+
+export const customRoleNameValidator: ChangeValidator = async (
+  changes, elementSource
+) => {
+  const relevantInstances = changes
+    .filter(isAdditionOrModificationChange)
+    .filter(isInstanceChange)
+    .filter(isRelevantChange)
+    .map(getChangeData)
+  if (elementSource === undefined) {
+    log.error('Failed to run customRoleNameValidator because no element source was provided')
+    return []
+  }
+  if (_.isEmpty(relevantInstances)) {
+    return []
+  }
+  const allCustomRoles = await awu(await elementSource.list())
+    .filter(id => id.typeName === CUSTOM_ROLE_TYPE_NAME)
+    .filter(id => id.idType === 'instance')
+    .map(id => elementSource.get(id))
+    .filter(isInstanceElement)
+    .toArray()
+  return relevantInstances
+    .flatMap(instance => {
+      if (SYSTEM_ROLE_NAMES.includes(instance.value.name?.toLowerCase())) {
+        return [{
+          elemID: instance.elemID,
+          severity: 'Error',
+          message: 'Can not change custom_role because the name is reserved for a system role',
+          detailedMessage: `Can not change ${instance.elemID.getFullName()} because the name (${instance.value.name}) is reserved for a system role`,
+        }]
+      }
+      const otherCustomRoleWithTheSameName = allCustomRoles
+        .filter(customRole =>
+          customRole.value.name?.toLowerCase() === instance.value.name?.toLowerCase())
+        .filter(customRole => !customRole.elemID.isEqual(instance.elemID))
+      if (_.isEmpty(otherCustomRoleWithTheSameName)) {
+        return []
+      }
+      return [{
+        elemID: instance.elemID,
+        severity: 'Error',
+        message: 'Can not change custom_role because the name is already in use',
+        detailedMessage: `Can not change ${instance.elemID.getFullName()} because the name is already in use by ${
+          otherCustomRoleWithTheSameName.map(customRole => customRole.elemID.getFullName()).join(', ')}`,
+      }]
+    })
+}

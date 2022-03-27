aa2e48bdbe02462eb35421a4612fa812ac14aba1
Guy Kremnitzer <guy.kremnitzer@salto.io>
2022-03-27 16:46:05 +0300

    SALTO-2165 - Add invertNaclCase function

--- packages/adapter-utils/src/nacl_case_utils.ts
+++ packages/adapter-utils/src/nacl_case_utils.ts
@@ -13,8 +13,10 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+import invert from 'lodash/invert'
 
 const NACL_ESCAPING_SUFFIX_SEPARATOR = '@'
+const NACL_CUSTOM_MAPPING_PREFIX = '_'
 // Windows has the lowest known limit, of 255
 // This can have an effect at a time we add a ~15 chars suffix
 // So we are taking an extra buffer and limit it to 200
@@ -27,7 +29,8 @@ export const pathNaclCase = (name?: string): string =>
 // Current values in this mapping should not be changed
 // Values in the map should be unique
 // Adding more values should be with a leading z as an indication the value has more than one letter
-const defaultNaclCaseMapping = {
+// The code in this file assumes that the replacements are of maximum length of 2
+const defaultNaclCaseMapping: Record<string, string> = {
   '?': 'a',
   '-': 'b',
   '\\': 'c',
@@ -58,7 +61,9 @@ const defaultNaclCaseMapping = {
   '$': 'zc',
   ',': 'zd',
   '+': 'ze',
-} as Record<string, string>
+}
+
+const invertedDefaultNaclCaseMapping = invert(defaultNaclCaseMapping)
 
 const suffixFromList = (specialCharsMappingList: string[]): string => {
   if (specialCharsMappingList.length === 0
@@ -74,6 +79,31 @@ const suffixFromList = (specialCharsMappingList: string[]): string => {
   return `${NACL_ESCAPING_SUFFIX_SEPARATOR}${specialCharsMappingList.join('')}`
 }
 
+const listFromSuffix = (suffix: string, startIdx = 0): string[] => {
+  if (suffix.length === 0 || startIdx >= suffix.length) {
+    return []
+  }
+  if (suffix[startIdx] === NACL_CUSTOM_MAPPING_PREFIX) {
+    return [
+      String.fromCharCode(
+        Number(suffix.slice(startIdx + 1, startIdx + 6))
+      ),
+      ...listFromSuffix(suffix, startIdx + 6),
+    ]
+  }
+  if (suffix[startIdx] === 'z') {
+    return [
+      invertedDefaultNaclCaseMapping[suffix.slice(startIdx, startIdx + 2)],
+      ...listFromSuffix(suffix, startIdx + 2),
+    ]
+  }
+  return [
+    invertedDefaultNaclCaseMapping[suffix.slice(startIdx, startIdx + 1)],
+    ...listFromSuffix(suffix, startIdx + 1),
+  ]
+}
+
+
 export const naclCase = (name?: string): string => {
   // replace all special chars with _
   // then add a special chars mapping after the separator for uniqueness
@@ -86,9 +116,26 @@ export const naclCase = (name?: string): string => {
   }
   const specialCharsMappingList: string[] = []
   const replaceChar = (char: string): string => {
-    specialCharsMappingList.push(defaultNaclCaseMapping[char] ?? `_${char.charCodeAt(0).toString().padStart(5, '0')}`)
+    specialCharsMappingList.push(
+      defaultNaclCaseMapping[char] ?? `${NACL_CUSTOM_MAPPING_PREFIX}${char.charCodeAt(0).toString().padStart(5, '0')}`
+    )
     return '_'
   }
   const cleanName = name.replace(/[^a-zA-Z0-9]/g, replaceChar)
   return `${cleanName}${suffixFromList(specialCharsMappingList)}`
+}
+
+export const invertNaclCase = (name: string): string => {
+  if (name === '') {
+    return ''
+  }
+  const [prefix, suffix] = name.split(NACL_ESCAPING_SUFFIX_SEPARATOR)
+  if (!suffix) {
+    return prefix
+  }
+  const specialCharsMappingList = listFromSuffix(suffix)
+  return prefix.replace(/_/g, () =>
+    (specialCharsMappingList.length === 1
+      ? specialCharsMappingList[0]
+      : specialCharsMappingList.shift() ?? ''))
 }

f8ce34a52ec0140729b1399d37592017a16a1342
Omri Farkash <omrifarkash@gmail.com>
2022-03-31 10:53:11 +0300

    fix UTs

--- packages/cli/test/commands/workspace.test.ts
+++ packages/cli/test/commands/workspace.test.ts
@@ -18,6 +18,7 @@ import * as callbacks from '../../src/callbacks'
 import * as mocks from '../mocks'
 import { cleanAction, cacheUpdateAction } from '../../src/commands/workspace'
 import { CliExitCode } from '../../src/types'
+import Prompts from '../../src/prompts'
 
 jest.mock('@salto-io/core', () => ({
   ...jest.requireActual<{}>('@salto-io/core'),
@@ -84,7 +85,7 @@ describe('workspace command group', () => {
           },
           workspace: mocks.mockWorkspace({}),
         })).toBe(CliExitCode.Success)
-        expect(callbacks.getUserBooleanInput).toHaveBeenCalledWith('Do you want to deploy?')
+        expect(callbacks.getUserBooleanInput).toHaveBeenCalledWith(Prompts.SHOULD_EXECUTE_PLAN)
         expect(output.stdout.content.search('Canceling...')).toBeGreaterThan(0)
       })
       it('should prompt user and exit if no (regenerate-cache)', async () => {
@@ -103,7 +104,7 @@ describe('workspace command group', () => {
           },
           workspace: mocks.mockWorkspace({}),
         })).toBe(CliExitCode.Success)
-        expect(callbacks.getUserBooleanInput).toHaveBeenCalledWith('Do you want to deploy?')
+        expect(callbacks.getUserBooleanInput).toHaveBeenCalledWith(Prompts.SHOULD_EXECUTE_PLAN)
         expect(output.stdout.content.search('Canceling...')).toBeGreaterThan(0)
       })
       it('should fail if trying to clean static resources without all dependent components', async () => {
@@ -140,7 +141,7 @@ describe('workspace command group', () => {
           },
           workspace,
         })).toBe(CliExitCode.Success)
-        expect(callbacks.getUserBooleanInput).toHaveBeenCalledWith('Do you want to deploy?')
+        expect(callbacks.getUserBooleanInput).toHaveBeenCalledWith(Prompts.SHOULD_EXECUTE_PLAN)
         expect(core.cleanWorkspace).toHaveBeenCalledWith(workspace, {
           nacl: true,
           state: true,

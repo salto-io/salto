675fbaa6164d1743992e667810ae2e15fd2228bb
alonstern <alon.stern@salto.io>
2022-04-13 19:39:15 +0300

    SALTO-2085: Deleting an active workflow fails (#2939)

---
+++ packages/jira-adapter/src/dependency_changers/removals.ts
@@ -0,0 +1,54 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { dependencyChange, DependencyChanger, getChangeData, InstanceElement, isAdditionChange, isInstanceChange, isRemovalChange, RemovalChange } from '@salto-io/adapter-api'
+import { references } from '@salto-io/adapter-utils'
+import { collections } from '@salto-io/lowerdash'
+import _ from 'lodash'
+
+
+export const removalsDependencyChanger: DependencyChanger = async changes => {
+  const removalsChanges = _(Array.from(changes.entries()))
+    .map(([key, change]) => ({ key, change }))
+    .filter(
+      (change): change is { key: collections.set.SetId; change: RemovalChange<InstanceElement> } =>
+        isInstanceChange(change.change)
+        && isRemovalChange(change.change)
+    )
+    .keyBy(({ change }) => getChangeData(change).elemID.getFullName())
+    .value()
+
+  return Array.from(changes.entries())
+    .flatMap(([destKey, change]) => {
+      if (isAdditionChange(change)) {
+        return []
+      }
+
+      const referencedKeys = references.getReferences(change.data.before)
+        .map(({ value }) => value.elemID.createTopLevelParentID().parent.getFullName())
+        .filter(id => id in removalsChanges)
+        .map(id => removalsChanges[id])
+        .map(({ key }) => key)
+        .filter(key => key !== destKey)
+
+      return referencedKeys.map(
+        sourceKey => dependencyChange(
+          'add',
+          sourceKey,
+          destKey,
+        )
+      )
+    })
+}

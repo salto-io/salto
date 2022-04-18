3a3c8d1f0e690bbcb6b4a0a83f3ce3f4a138b992
Eldar Solomon <eldar.solomon@salto.io>
2022-04-18 13:19:22 +0300

    SALTO-2187 added brand creation warning change validator

---
+++ packages/zendesk-support-adapter/src/change_validators/brand_creation.ts
@@ -0,0 +1,35 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ChangeValidator, getChangeData,
+  isAdditionChange, isInstanceElement } from '@salto-io/adapter-api'
+
+export const BRAND_NAME = 'brand'
+
+export const brandCreationValidator: ChangeValidator = async changes => (
+  changes
+    .filter(isAdditionChange)
+    .map(getChangeData)
+    .filter(isInstanceElement)
+    .filter(instance => instance.elemID.typeName === BRAND_NAME)
+    .flatMap(instance => (
+      [{
+        elemID: instance.elemID,
+        severity: 'Warning',
+        message: `Brand subdomains are globally unique, please make sure to set an available subdomain for brand ${instance.value.name} before attempting to create it from Salto`,
+        detailedMessage: `Brand subdomains are globally unique, please make sure to set an available subdomain for brand ${instance.value.name} before attempting to create it from Salto`,
+      }]
+    ))
+)

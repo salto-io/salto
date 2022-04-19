15af09e6c40bfbbf3784287f0da53476bbf6e82e
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2022-04-19 09:56:08 +0300

    SALTO-1688 remove ids from Zendesk's dynamic_content_item__variants name (#2954)
    * remove locale ids from dynamic_content_item_variants instances

--- packages/zendesk-support-adapter/src/adapter.ts
+++ packages/zendesk-support-adapter/src/adapter.ts
@@ -67,6 +67,7 @@ import omitInactiveFilter from './filters/omit_inactive'
 import tagsFilter from './filters/tag'
 import defaultDeployFilter from './filters/default_deploy'
 import ducktypeCommonFilters from './filters/ducktype_common'
+import referencedIdFieldsFilter from './filters/referenced_id_fields'
 import { getConfigFromConfigChanges } from './config_change'
 
 const log = logger(module)
@@ -117,6 +118,7 @@ export const DEFAULT_FILTERS = [
   // unorderedListsFilter should run after fieldReferencesFilter
   unorderedListsFilter,
   dynamicContentReferencesFilter,
+  referencedIdFieldsFilter,
   serviceUrlFilter,
   ...ducktypeCommonFilters,
   // defaultDeployFilter should be last!
@@ -170,6 +172,7 @@ export default class ZendeskAdapter implements AdapterOperations {
             fetch: config.fetch,
             apiDefinitions: config.apiDefinitions,
           },
+          getElemIdFunc,
         },
         filterCreators,
         concatObjects,
---
+++ packages/zendesk-support-adapter/test/filters/referenced_id_fields.test.ts
@@ -0,0 +1,89 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ObjectType, ElemID, InstanceElement, ReferenceExpression } from '@salto-io/adapter-api'
+import { client as clientUtils, filterUtils } from '@salto-io/adapter-components'
+import { DEFAULT_CONFIG } from '../../src/config'
+import ZendeskClient from '../../src/client/client'
+import { ZENDESK_SUPPORT } from '../../src/constants'
+import { paginate } from '../../src/client/pagination'
+import filterCreator from '../../src/filters/referenced_id_fields'
+
+describe('referenced id fields filter', () => {
+  let client: ZendeskClient
+  type FilterType = filterUtils.FilterWith<'onFetch'>
+  let filter: FilterType
+  const localeObj = new ObjectType({ elemID: new ElemID(ZENDESK_SUPPORT, 'locales') })
+  const localeIns = new InstanceElement('es', localeObj, { id: 123, locale: 'es-US', name: 'English' })
+  const dynamicContentItemVarObj = new ObjectType({ elemID: new ElemID(ZENDESK_SUPPORT, 'dynamic_content_item__variants') })
+  const dynamicContentItemVarIns = new InstanceElement(
+    '123',
+    dynamicContentItemVarObj,
+    { locale_id: new ReferenceExpression(localeIns.elemID, localeIns), content: 'abc' },
+  )
+  beforeEach(async () => {
+    jest.clearAllMocks()
+    client = new ZendeskClient({
+      credentials: { username: 'a', password: 'b', subdomain: 'ignore' },
+    })
+  })
+
+  it('should resolve ids in instances names if & exist in the config', async () => {
+    const elements = [dynamicContentItemVarIns].map(e => e.clone())
+    filter = filterCreator({
+      client,
+      paginator: clientUtils.createPaginator({
+        client,
+        paginationFuncCreator: paginate,
+      }),
+      config: DEFAULT_CONFIG,
+    }) as FilterType
+    await filter.onFetch(elements)
+    expect(elements.map(e => e.elemID.getFullName()).sort())
+      .toEqual(['zendesk_support.dynamic_content_item__variants.instance.es'])
+  })
+  it('should not add referenced id fields if & is not in the config', async () => {
+    const elements = [dynamicContentItemVarIns].map(e => e.clone())
+    filter = filterCreator({
+      client,
+      paginator: clientUtils.createPaginator({
+        client,
+        paginationFuncCreator: paginate,
+      }),
+      config: {
+        fetch: {
+          includeTypes: ['dynamic_content_item__variants', 'locales'],
+        },
+        apiDefinitions: {
+          typeDefaults: {
+            transformation: {
+              idFields: ['name'],
+            },
+          },
+          types: {
+            dynamic_content_item__variants: {
+              transformation: {
+                idFields: ['locale_id'],
+              },
+            },
+          },
+        },
+      },
+    }) as FilterType
+    await filter.onFetch(elements)
+    expect(elements.map(e => e.elemID.getFullName()).sort())
+      .toEqual(['zendesk_support.dynamic_content_item__variants.instance.123'])
+  })
+})

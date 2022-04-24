8a581607a66987d17d33ea6fb96629ebd499aff6
guy hamo <guyhamo1@gmail.com>
2022-04-24 16:35:55 +0300

    add deploy tests

--- packages/jira-adapter/test/filters/service_url/service_url_information.test.ts
+++ packages/jira-adapter/test/filters/service_url/service_url_information.test.ts
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { ObjectType, ElemID, InstanceElement, CORE_ANNOTATIONS } from '@salto-io/adapter-api'
+import { ObjectType, ElemID, InstanceElement, CORE_ANNOTATIONS, toChange } from '@salto-io/adapter-api'
 import { client as clientUtils, filterUtils } from '@salto-io/adapter-components'
 import { buildElementsSourceFromElements } from '@salto-io/adapter-utils'
 import { mockClient } from '../../utils'
@@ -167,6 +167,140 @@ describe('service url information filter', () => {
         const objType = new ObjectType({ elemID: new ElemID(JIRA, 'IssueEvent') })
         const elements = [new InstanceElement('IssueEvent', objType, { id: 11 })]
         await filter.onFetch(elements)
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.IssueEvent.instance.IssueEvent'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/secure/admin/ListEventTypes.jspa',
+        })
+      })
+    })
+  })
+  describe('onDeploy', () => {
+    describe('Board type', () => {
+      it('should add service url annotation', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'Board') })
+        const elements = [new InstanceElement('Board', objType, { id: 11, name: 'wow board' })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.Board.instance.Board'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/jira/software/projects/wow/boards/11',
+        })
+      })
+    })
+    describe('ProjectComponent type', () => {
+      it('should add service url annotation', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'ProjectComponent') })
+        const elements = [new InstanceElement('ProjectComponent', objType, { name: 'test' }, undefined, { _parent: testParent })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.ProjectComponent.instance.ProjectComponent'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual(expect.objectContaining({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/plugins/servlet/project-config/test/administer-components?filter=test&orderDirection=DESC&orderField=NAME&page=1',
+        }))
+      })
+    })
+    describe('CustomFieldContext type', () => {
+      it('should add service url annotation for context of custom field', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'CustomFieldContext') })
+        const elements = [new InstanceElement('CustomFieldContext', objType, { id: 11 }, undefined, { _parent: testParent })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.CustomFieldContext.instance.CustomFieldContext'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual(expect.objectContaining({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/secure/admin/ManageConfigurationScheme!default.jspa?=&customFieldId=test&fieldConfigSchemeId=11',
+        }))
+      })
+      it('should not add service url annotation for non custom field context', async () => {
+        const nonCustomParent = { resValue: { value: { key: 'test', id: 'test' } } }
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'CustomFieldContext') })
+        const elements = [new InstanceElement('CustomFieldContext', objType, { id: '11' }, undefined, { _parent: nonCustomParent })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.CustomFieldContext.instance.CustomFieldContext'])
+        const [instance] = elements
+        expect(instance.annotations[CORE_ANNOTATIONS.SERVICE_URL]).not.toBeDefined()
+      })
+    })
+    describe('Field type', () => {
+      it('should add service url annotation for custom field', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'Field') })
+        const elements = [new InstanceElement('Field', objType, { id: 'customfield_11' })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.Field.instance.Field'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/secure/admin/EditCustomField!default.jspa?id=11',
+        })
+      })
+      it('should not add service url annotation for non custom field', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'Field') })
+        const elements = [new InstanceElement('Field', objType, { id: '11' })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.Field.instance.Field'])
+        const [instance] = elements
+        expect(instance.annotations[CORE_ANNOTATIONS.SERVICE_URL]).not.toBeDefined()
+      })
+    })
+    describe('DashboardGadget type', () => {
+      it('should add service url annotation', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'DashboardGadget') })
+        const elements = [new InstanceElement('DashboardGadget', objType, { id: 11 }, undefined, { _parent: testParent })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.DashboardGadget.instance.DashboardGadget'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual(expect.objectContaining({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/jira/dashboards/customfield_test?maximized=11',
+        }))
+      })
+    })
+    describe('Automation type', () => {
+      it('should add service url annotation for automation with project key', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'Automation') })
+        const elements = [new InstanceElement('Automation', objType, { id: 11, projects: [{ projectId: { resValue: { value: { key: 'test' } } } }] })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.Automation.instance.Automation'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/jira/software/projects/test/settings/automate#/rule/11',
+        })
+      })
+      it('should not add service url annotation for automation without project key', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'Automation') })
+        const elements = [new InstanceElement('Automation', objType, { id: '11' })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.Automation.instance.Automation'])
+        const [instance] = elements
+        expect(instance.annotations[CORE_ANNOTATIONS.SERVICE_URL]).not.toBeDefined()
+      })
+    })
+    describe('Webhook type', () => {
+      it('should add service url annotation', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'Webhook') })
+        const elements = [new InstanceElement('Webhook', objType, { id: 11 })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
+        expect(elements.map(e => e.elemID.getFullName()).sort())
+          .toEqual(['jira.Webhook.instance.Webhook'])
+        const [instance] = elements
+        expect(instance.annotations).toEqual({
+          [CORE_ANNOTATIONS.SERVICE_URL]: 'https://ori-salto-test.atlassian.net/plugins/servlet/webhooks#',
+        })
+      })
+    })
+    describe('IssueEvent type', () => {
+      it('should add service url annotation', async () => {
+        const objType = new ObjectType({ elemID: new ElemID(JIRA, 'IssueEvent') })
+        const elements = [new InstanceElement('IssueEvent', objType, { id: 11 })]
+        await filter.onDeploy(elements.map(inst => toChange({ after: inst })))
         expect(elements.map(e => e.elemID.getFullName()).sort())
           .toEqual(['jira.IssueEvent.instance.IssueEvent'])
         const [instance] = elements

cfde1bd8cdd0548ba60f07aebcc9aa2ef844d37e
Nitzan Haim <nitzanhaim@gmail.com>
2022-05-03 16:36:17 +0300

    remove unused import

--- packages/salesforce-adapter/test/filters/layouts.test.ts
+++ packages/salesforce-adapter/test/filters/layouts.test.ts
@@ -13,7 +13,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import _ from 'lodash'
 import { InstanceElement, CORE_ANNOTATIONS, ReferenceExpression, BuiltinTypes, ObjectType, ElemID, isInstanceElement } from '@salto-io/adapter-api'
 import { naclCase, pathNaclCase } from '@salto-io/adapter-utils'
 import { createCustomObjectType, createMetadataTypeElement, defaultFilterContext } from '../utils'

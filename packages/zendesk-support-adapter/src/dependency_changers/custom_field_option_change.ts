71b6cecf5d043460712348253f1d6b216b827197
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2022-05-03 18:31:11 +0300

    first version of dc

---
+++ packages/zendesk-support-adapter/src/dependency_changers/custom_field_option_change.ts
@@ -0,0 +1,66 @@
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { Change, dependencyChange, DependencyChanger, getChangeData, InstanceElement,
+  isInstanceChange, isAdditionOrModificationChange, isRemovalOrModificationChange,
+  DependencyChange } from '@salto-io/adapter-api'
+import { collections, values } from '@salto-io/lowerdash'
+import { isRelevantChange, RELEVANT_PARENT_AND_CHILD_TYPES } from '../change_validators/duplicate_option_values'
+
+export const customFieldOptionDependencyChanger: DependencyChanger = async changes => {
+  const getRelevantValue = (instance: InstanceElement) : string => {
+    const instanceType = instance.elemID.typeName
+    if (RELEVANT_PARENT_AND_CHILD_TYPES.some(types => types.parent === instanceType)) {
+      return instance.value.tag
+    }
+    return instance.value.value
+  }
+
+  const getDependencies = (
+    customFieldOptionChanges: {key: collections.set.SetId; change: Change<InstanceElement>}[],
+  )
+  : DependencyChange[] => customFieldOptionChanges.map(({ change, key }) => {
+    if (isRemovalOrModificationChange(change)) {
+      const beforeValue = getRelevantValue(change.data.before)
+      const instanceWithSameValue = customFieldOptionChanges
+        .find(otherChange => isAdditionOrModificationChange(otherChange.change)
+        && getRelevantValue(getChangeData(otherChange.change)) === beforeValue)
+      return instanceWithSameValue !== undefined ? dependencyChange(
+        'add',
+        instanceWithSameValue.key,
+        key,
+      )
+        : undefined
+    }
+    return undefined
+  }).filter(values.isDefined)
+
+  // We don't support the case the change in value A was x->y and the change value B was y->x,
+  // therfore we remove the dependency to avoid a circular dependency
+  const removeCyclicDependencies = (dependencies: DependencyChange[])
+   : DependencyChange[] => dependencies.filter(dependency =>
+    !dependencies.some(d => d.dependency.source === dependency.dependency.target
+        && d.dependency.target === dependency.dependency.source))
+
+  const relevantChanges = Array.from(changes.entries())
+    .map(([key, change]) => ({ key, change }))
+    .filter(
+      (change): change is { key: collections.set.SetId; change: Change<InstanceElement> } =>
+        isInstanceChange(change.change)
+    ).filter(({ change }) => isRelevantChange(change))
+
+  const dependencies = getDependencies(relevantChanges)
+  return removeCyclicDependencies(dependencies)
+}

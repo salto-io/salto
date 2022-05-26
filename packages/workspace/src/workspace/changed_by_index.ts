89540693b67512fec0b620e0c7c78e6f2ded8b19
guy hamo <guyhamo1@gmail.com>
2022-05-26 17:56:18 +0300

    unexport unneeded stuff

--- packages/workspace/src/workspace/changed_by_index.ts
+++ packages/workspace/src/workspace/changed_by_index.ts
@@ -24,8 +24,8 @@ const { awu } = collections.asynciterable
 
 const log = logger(module)
 export const CHANGED_BY_INDEX_VERSION = 1
-export const CHANGED_BY_INDEX_KEY = 'changed_by_index'
-export const UNKNOWN_USER_NAME = 'Unknown'
+const CHANGED_BY_INDEX_KEY = 'changed_by_index'
+const UNKNOWN_USER_NAME = 'Unknown'
 const CHANGED_BY_KEY_DELIMITER = '@@'
 
 const getAllElementsChanges = async (

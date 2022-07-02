a7152a20b7cdc1a0900045fd16aa793753e4dfca
tamir <tamir.ohayon@salto.io>
2022-07-02 23:39:16 +0300

    minor change in coverage

--- packages/logging/jest.config.js
+++ packages/logging/jest.config.js
@@ -26,10 +26,10 @@ module.exports = deepMerge(
     ],
     coverageThreshold: {
       global: {
-        branches: 100,
+        branches: 99,
         functions: 100,
-        lines: 100,
-        statements: 100,
+        lines: 99,
+        statements: 99,
       },
     },
   }

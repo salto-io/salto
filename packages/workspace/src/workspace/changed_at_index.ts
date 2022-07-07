56065b083196d93cbbaba5591848715683b4ce51
guy hamo <guyhamo1@gmail.com>
2022-07-07 16:38:42 +0300

    add field element id when it has author information and not his top level object

--- packages/workspace/src/workspace/changed_at_index.ts
+++ packages/workspace/src/workspace/changed_at_index.ts
@@ -30,7 +30,7 @@ import {
 } from '@salto-io/adapter-api'
 import { logger } from '@salto-io/logging'
 import { collections, values } from '@salto-io/lowerdash'
-import _, { isEmpty } from 'lodash'
+import _, { entries, isEmpty } from 'lodash'
 import { ElementsSource } from './elements_source'
 import { RemoteMap } from './remote_map'
 
@@ -51,29 +51,39 @@ const getAllElementsChanges = async (
     .concat(currentChanges)
     .toArray()
 
-const getChangedAtDates = (change: Change<Element>): string[] => {
-  const element = getChangeData(change)
-  const dates = []
-  if (isObjectType(element)) {
-    dates.push(...Object.values(element.fields)
-      .map(field => field.annotations[CORE_ANNOTATIONS.CHANGED_AT])
-      .filter(isDefined))
+const getChangedAtDates = (change: Change<Element>): Record<string, ElemID[]> => {
+  const changeElement = getChangeData(change)
+  const datesMap: Record<string, ElemID[]> = {}
+  const addElementToDatesMap = (element: Element): void => {
+    const date = element.annotations[CORE_ANNOTATIONS.CHANGED_AT]
+    if (isDefined(date)) {
+      if (!datesMap[date]) {
+        datesMap[date] = []
+      }
+      datesMap[date].push(element.elemID)
+    }
   }
-  if (element.annotations[CORE_ANNOTATIONS.CHANGED_AT]) {
-    dates.push(element.annotations[CORE_ANNOTATIONS.CHANGED_AT])
+  if (isObjectType(changeElement)) {
+    Object.values(changeElement.fields)
+      .filter(field => field.annotations[CORE_ANNOTATIONS.CHANGED_AT])
+      .forEach(addElementToDatesMap)
   }
-  return dates
+  if (changeElement.annotations[CORE_ANNOTATIONS.CHANGED_AT]) {
+    addElementToDatesMap(changeElement)
+  }
+  return datesMap
 }
 
 const updateAdditionChange = (
   change: AdditionChange<Element>,
   datesMap: Record<string, Set<string>>,
 ): void => {
-  getChangedAtDates(change).forEach(modificationDate => {
-    if (!datesMap[modificationDate]) {
-      datesMap[modificationDate] = new Set()
+  Object.entries(getChangedAtDates(change)).forEach(entry => {
+    const [date, elemIds] = entry
+    if (!datesMap[date]) {
+      datesMap[date] = new Set()
     }
-    datesMap[modificationDate].add(change.data.after.elemID.getFullName())
+    elemIds.forEach(elemId => datesMap[date].add(elemId.getFullName()))
   })
 }
 
@@ -81,10 +91,12 @@ const updateRemovalChange = (
   change: RemovalChange<Element>,
   datesMap: Record<string, Set<string>>,
 ): void => {
-  getChangedAtDates(change).forEach(modificationDate => {
-    if (datesMap[modificationDate]) {
-      datesMap[modificationDate].delete(change.data.before.elemID.getFullName())
+  Object.entries(getChangedAtDates(change)).forEach(entry => {
+    const [date, elemIds] = entry
+    if (!datesMap[date]) {
+      datesMap[date] = new Set()
     }
+    elemIds.forEach(elemId => datesMap[date].delete(elemId.getFullName()))
   })
 }
 
@@ -122,9 +134,9 @@ const getUniqueDates = (changes: Change<Element>[]): Set<string> => {
   const dateSet = new Set<string>()
   changes.flatMap(change => (
     isModificationChange(change)
-      ? [...getChangedAtDates(toChange({ before: change.data.before })),
-        ...getChangedAtDates(toChange({ after: change.data.after }))]
-      : [...getChangedAtDates(change)])
+      ? [...Object.keys(getChangedAtDates(toChange({ before: change.data.before }))),
+        ...Object.keys(getChangedAtDates(toChange({ after: change.data.after })))]
+      : [...Object.keys(getChangedAtDates(change))])
     .forEach(date => dateSet.add(date)))
   return dateSet
 }

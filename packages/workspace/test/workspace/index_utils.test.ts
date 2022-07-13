c1e3f4fded6180593b6754db16d0fbaee1df527c
guy hamo <guyhamo1@gmail.com>
2022-07-13 14:25:14 +0300

    fixed lint

--- packages/workspace/test/workspace/index_utils.test.ts
+++ packages/workspace/test/workspace/index_utils.test.ts
@@ -13,7 +13,6 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import _ from 'lodash'
 import { ElemID, ObjectType, toChange } from '@salto-io/adapter-api'
 import { buildElementsSourceFromElements } from '@salto-io/adapter-utils'
 import { getAllElementsChanges } from '../../src/workspace/index_utils'

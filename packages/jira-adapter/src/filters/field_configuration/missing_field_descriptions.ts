b744b039b1406c7ca3a7357eafbb0315b340c9eb
alonstern <alon.stern@salto.io>
2022-08-14 10:44:10 +0300

    SALTO-2581: Fixed field configuration descriptions (#3252)

--- packages/jira-adapter/src/filters/field_configuration/missing_field_descriptions.ts
+++ packages/jira-adapter/src/filters/field_configuration/missing_field_descriptions.ts
@@ -13,25 +13,22 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { Element, isInstanceElement } from '@salto-io/adapter-api'
-import { FilterCreator } from '../filter'
-import { PROJECT_ROLE_TYPE } from '../constants'
-import { FIELD_TYPE_NAME } from './fields/constants'
+import { Element, isInstanceElement, isReferenceExpression } from '@salto-io/adapter-api'
+import _ from 'lodash'
+import { FIELD_CONFIGURATION_ITEM_TYPE_NAME } from '../../constants'
+import { FilterCreator } from '../../filter'
 
-
-const RELEVANT_TYPES = [
-  PROJECT_ROLE_TYPE,
-  FIELD_TYPE_NAME,
-]
 
 const filter: FilterCreator = () => ({
   onFetch: async (elements: Element[]) => {
     elements
       .filter(isInstanceElement)
-      .filter(instance => RELEVANT_TYPES.includes(instance.elemID.typeName))
-      .filter(instance => instance.value.description === undefined)
+      .filter(instance => instance.elemID.typeName === FIELD_CONFIGURATION_ITEM_TYPE_NAME)
+      .filter(instance => _.isEmpty(instance.value.description))
+      .filter(instance => isReferenceExpression(instance.value.id)
+        && isInstanceElement(instance.value.id.value))
       .forEach(instance => {
-        instance.value.description = ''
+        instance.value.description = instance.value.id.value.value.description ?? ''
       })
   },
 })

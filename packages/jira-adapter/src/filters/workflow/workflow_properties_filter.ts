bddc73d0bd682f91b5eabfb6a14ee9532b47518a
alonstern <alon.stern@salto.io>
2022-09-01 14:56:15 +0300

    added workflow status properties references

--- packages/jira-adapter/src/filters/workflow/workflow_properties_filter.ts
+++ packages/jira-adapter/src/filters/workflow/workflow_properties_filter.ts
@@ -13,14 +13,16 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { BuiltinTypes, Change, CORE_ANNOTATIONS, Element, ElemID, Field, getChangeData, isInstanceChange, isInstanceElement, ListType, ObjectType, Values } from '@salto-io/adapter-api'
-import { applyFunctionToChangeData } from '@salto-io/adapter-utils'
+import { BuiltinTypes, Change, CORE_ANNOTATIONS, Element, ElemID, Field, getChangeData, InstanceElement, isInstanceChange, isInstanceElement, ListType, ObjectType, Values } from '@salto-io/adapter-api'
+import { applyFunctionToChangeData, resolveValues, restoreChangeElement } from '@salto-io/adapter-utils'
 import { collections } from '@salto-io/lowerdash'
 import { elements as elementUtils } from '@salto-io/adapter-components'
+import _ from 'lodash'
 import { findObject } from '../../utils'
 import { FilterCreator } from '../../filter'
 import { isWorkflowInstance, WorkflowInstance } from './types'
 import { JIRA, WORKFLOW_TYPE_NAME } from '../../constants'
+import { getLookUpName } from '../../reference_mapping'
 
 const STATUS_PROPERTY_TYPE_NAME = 'StatusProperty'
 
@@ -46,68 +48,90 @@ const convertStatusesPropertiesToMap = (instance: WorkflowInstance): void => {
 }
 
 
-const filter: FilterCreator = () => ({
-  onFetch: async (elements: Element[]) => {
-    const propertyType = new ObjectType({
-      elemID: new ElemID(JIRA, STATUS_PROPERTY_TYPE_NAME),
-      fields: {
-        key: {
-          refType: BuiltinTypes.STRING,
-          annotations: { [CORE_ANNOTATIONS.CREATABLE]: true },
+const filter: FilterCreator = () => {
+  const originalChanges: Record<string, Change<InstanceElement>> = {}
+
+  return {
+    onFetch: async (elements: Element[]) => {
+      const propertyType = new ObjectType({
+        elemID: new ElemID(JIRA, STATUS_PROPERTY_TYPE_NAME),
+        fields: {
+          key: {
+            refType: BuiltinTypes.STRING,
+            annotations: { [CORE_ANNOTATIONS.CREATABLE]: true },
+          },
+          value: {
+            refType: BuiltinTypes.STRING,
+            annotations: { [CORE_ANNOTATIONS.CREATABLE]: true },
+          },
         },
-        value: {
-          refType: BuiltinTypes.STRING,
-          annotations: { [CORE_ANNOTATIONS.CREATABLE]: true },
-        },
-      },
-      path: [JIRA, elementUtils.TYPES_PATH, STATUS_PROPERTY_TYPE_NAME],
-    })
-
-    elements.push(propertyType)
-
-    const workflowStatusType = findObject(elements, 'WorkflowStatus')
-    if (workflowStatusType !== undefined) {
-      workflowStatusType.fields.properties = new Field(workflowStatusType, 'properties', new ListType(propertyType), { [CORE_ANNOTATIONS.REQUIRED]: true })
-    }
-
-    elements
-      .filter(isInstanceElement)
-      .filter(instance => instance.elemID.typeName === WORKFLOW_TYPE_NAME)
-      .filter(isWorkflowInstance)
-      .forEach(convertStatusesPropertiesToList)
-  },
-
-  preDeploy: async changes => {
-    await awu(changes)
-      .filter(isInstanceChange)
-      .filter(change => getChangeData(change).elemID.typeName === WORKFLOW_TYPE_NAME)
-      .filter(change => isWorkflowInstance(getChangeData(change)))
-      .forEach(async change => {
-        await applyFunctionToChangeData<Change<WorkflowInstance>>(
-          change,
-          instance => {
-            convertStatusesPropertiesToMap(instance)
-            return instance
-          }
-        )
+        path: [JIRA, elementUtils.TYPES_PATH, STATUS_PROPERTY_TYPE_NAME],
       })
-  },
 
-  onDeploy: async changes => {
-    await awu(changes)
-      .filter(isInstanceChange)
-      .filter(change => getChangeData(change).elemID.typeName === WORKFLOW_TYPE_NAME)
-      .filter(change => isWorkflowInstance(getChangeData(change)))
-      .forEach(async change => {
-        await applyFunctionToChangeData<Change<WorkflowInstance>>(
-          change,
-          instance => {
-            convertStatusesPropertiesToList(instance)
-            return instance
-          }
-        )
-      })
-  },
-})
+      elements.push(propertyType)
+
+      const workflowStatusType = findObject(elements, 'WorkflowStatus')
+      if (workflowStatusType !== undefined) {
+        workflowStatusType.fields.properties = new Field(workflowStatusType, 'properties', new ListType(propertyType), { [CORE_ANNOTATIONS.REQUIRED]: true })
+      }
+
+      elements
+        .filter(isInstanceElement)
+        .filter(instance => instance.elemID.typeName === WORKFLOW_TYPE_NAME)
+        .filter(isWorkflowInstance)
+        .forEach(convertStatusesPropertiesToList)
+    },
+
+    preDeploy: async changes => {
+      const relevantChanges = changes
+        .filter(isInstanceChange)
+        .filter(change => getChangeData(change).elemID.typeName === WORKFLOW_TYPE_NAME)
+        .filter(change => isWorkflowInstance(getChangeData(change)))
+
+      const changesToReturn = await awu(relevantChanges)
+        .map(async change => {
+          originalChanges[getChangeData(change).elemID.getFullName()] = change
+          return applyFunctionToChangeData<Change<WorkflowInstance>>(
+            change,
+            async instance => {
+              // I have to call resolveValues here because after I change the status
+              // properties structure the getLookUpName won't know how to resolve
+              // the references inside correctly
+              const resolvedInstance = (await resolveValues(instance, getLookUpName))
+              convertStatusesPropertiesToMap(resolvedInstance)
+              return resolvedInstance
+            }
+          )
+        })
+        .toArray()
+
+      _.pullAll(changes, relevantChanges)
+      changesToReturn.forEach(change => changes.push(change))
+    },
+
+    onDeploy: async changes => {
+      const relevantChanges = changes
+        .filter(isInstanceChange)
+        .filter(change => getChangeData(change).elemID.typeName === WORKFLOW_TYPE_NAME)
+        .filter(change => isWorkflowInstance(getChangeData(change)))
+
+      const changesToReturn = await awu(relevantChanges)
+        .map(async change => {
+          await applyFunctionToChangeData<Change<WorkflowInstance>>(
+            change,
+            instance => {
+              convertStatusesPropertiesToList(instance)
+              return instance
+            }
+          )
+          return restoreChangeElement(change, originalChanges, getLookUpName)
+        })
+        .toArray()
+
+      _.pullAll(changes, relevantChanges)
+      changesToReturn.forEach(change => changes.push(change))
+    },
+  }
+}
 
 export default filter

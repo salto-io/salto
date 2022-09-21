4df120dcb2f29dbcc7386efb7f8e1e98cdc72453
Daniel Tal <dantal4@gmail.com>
2022-09-21 11:04:56 +0300

    SALTO-820 workato: fix netsuite customrecordtype references

--- packages/workato-adapter/src/filters/cross_service/netsuite/element_index.ts
+++ packages/workato-adapter/src/filters/cross_service/netsuite/element_index.ts
@@ -13,28 +13,25 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { Element, ElemID, InstanceElement, isInstanceElement, isObjectType, ObjectType } from '@salto-io/adapter-api'
-import { values } from '@salto-io/lowerdash'
-import { logger } from '@salto-io/logging'
 import _ from 'lodash'
-
-const log = logger(module)
-const { isPlainRecord, isDefined } = values
+import { Element, ElemID, isInstanceElement, isObjectType, ObjectType } from '@salto-io/adapter-api'
 
 export type NetsuiteIndex = {
   scriptId: Record<string, ElemID>
   type: Record<string, Readonly<ObjectType>>
 }
 
+const METADATA_TYPE_ANNOTATION = 'metadataType'
 const CUSTOM_RECORD_TYPE = 'customrecordtype'
-const CUSTOM_RECORD_CUSTOM_FIELDS = 'customrecordcustomfields'
-const CUSTOM_RECORD_CUSTOM_FIELD = 'customrecordcustomfield'
+const CUSTOM_FIELD_PREFIX = 'custom_'
 
 export const indexNetsuiteByTypeAndScriptId = (
   elements: ReadonlyArray<Readonly<Element>>
 ): NetsuiteIndex => {
   const indexInstancesByScriptId = (): Record<string, ElemID> => {
-    const toScriptId = (inst: Readonly<InstanceElement>): string | undefined => inst.value.scriptid
+    const toScriptId = (element: Readonly<Element>): string | undefined => (
+      isInstanceElement(element) ? element.value.scriptid : element.annotations.scriptid
+    )
     const instances = elements.filter(isInstanceElement)
 
     const instanceIndex = _.mapValues(
@@ -45,41 +42,21 @@ export const indexNetsuiteByTypeAndScriptId = (
       e => e.elemID,
     )
 
-    const customRecordTypeInstances = instances.filter(
-      inst => inst.elemID.typeName === CUSTOM_RECORD_TYPE
-    )
-    const nestedFields = (
-      customRecordTypeInstances
-        .flatMap(inst => (
-          Object.entries(
-            inst.value[CUSTOM_RECORD_CUSTOM_FIELDS]?.[CUSTOM_RECORD_CUSTOM_FIELD] ?? {}
-          ).map(([key, item]) => {
-            if (!isPlainRecord(item)) {
-              log.warn(
-                '%s is not a plain object as expected: %o',
-                inst.elemID.createNestedID(
-                  CUSTOM_RECORD_CUSTOM_FIELDS, CUSTOM_RECORD_CUSTOM_FIELD, key
-                ).getFullName(),
-                item
-              )
-              return undefined
-            }
-            return {
-              scriptId: item.scriptid,
-              nestedPath: inst.elemID.createNestedID(
-                CUSTOM_RECORD_CUSTOM_FIELDS,
-                CUSTOM_RECORD_CUSTOM_FIELD,
-                key,
-              ),
-            }
-          }).filter(isDefined)))
-    )
-    // TODO these should be replaced by maps or nested fields under custom objects - see SALTO-1078
-    const customRecordTypeNestedFieldIndex = Object.fromEntries(
-      nestedFields.map(f => [f.scriptId, f.nestedPath])
-    )
+    const customRecordTypes = elements
+      .filter(isObjectType)
+      .filter(element => element.annotations[METADATA_TYPE_ANNOTATION] === CUSTOM_RECORD_TYPE)
+    const customRecordTypeIndex = Object.fromEntries(customRecordTypes
+      .filter(e => toScriptId(e) !== undefined)
+      .map(type => [toScriptId(type), type.elemID]))
+    const customRecordTypeNestedFieldIndex = Object.fromEntries(customRecordTypes
+      .flatMap(type => Object.values(type.fields)
+        .filter(field => field.name.startsWith(CUSTOM_FIELD_PREFIX)
+          && toScriptId(field) !== undefined)
+        .map(field => [toScriptId(field), field.elemID])))
+
     return {
       ...instanceIndex,
+      ...customRecordTypeIndex,
       ...customRecordTypeNestedFieldIndex,
     }
   }

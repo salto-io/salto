a3cb334721a9e55176da937a0308f05f71e93642
guy hamo <guyhamo1@gmail.com>
2022-10-19 13:53:08 +0300

    refactored filter

--- packages/jira-adapter/src/filters/permission_scheme/allowed_permission_schemes.ts
+++ packages/jira-adapter/src/filters/permission_scheme/allowed_permission_schemes.ts
@@ -13,61 +13,29 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { AdditionChange, getChangeData, InstanceElement, isAdditionOrModificationChange, isInstanceChange, ModificationChange } from '@salto-io/adapter-api'
-import _ from 'lodash'
 import { logger } from '@salto-io/logging'
-import { JiraConfig } from '../../config/config'
 import { FilterCreator } from '../../filter'
-import { deployChanges, defaultDeployChange } from '../../deployment/standard_deployment'
 import { getAllowedPermissionTypes } from '../../change_validators/permission_type'
-import { PERMISSION_SCHEME_TYPE_NAME } from '../../constants'
-import JiraClient from '../../client/client'
+import { PermissionHolder, omitChanges, addBackPermissions } from './omit_permissions_common'
 
 const log = logger(module)
 
-const deployPermissionScheme = async (
-  permissionScheme: AdditionChange<InstanceElement> | ModificationChange<InstanceElement>,
-  client: JiraClient,
-  config: JiraConfig,
-  allowedPermissions: string[],
-):Promise<void> => {
-  const clonedPermissionSChemeChange = _.clone(permissionScheme)
-  clonedPermissionSChemeChange.data.after.value.permissions = getChangeData(
-    clonedPermissionSChemeChange
-  ).value.permissions.filter((permission: { permission: string }) =>
-    allowedPermissions.includes(permission.permission))
-  await defaultDeployChange({
-    change: clonedPermissionSChemeChange,
-    client,
-    apiDefinitions: config.apiDefinitions,
+const filter: FilterCreator = ({ elementsSource }) => {
+  let unsupportedPermissionSchemes: Record<string, PermissionHolder[]> = {}
+  return ({
+    preDeploy: async changes => {
+      const allowedPermissions = await getAllowedPermissionTypes(elementsSource)
+      if (allowedPermissions.length === 0) {
+        log.warn('Could not find allowed permission types for permission Scheme filter. skipping pre deploy permission scheme validations')
+      }
+      unsupportedPermissionSchemes = omitChanges(
+        changes,
+        (holder: PermissionHolder) => !allowedPermissions.includes(holder.permission)
+      )
+    },
+    onDeploy: async changes => {
+      addBackPermissions(changes, unsupportedPermissionSchemes)
+    },
   })
 }
-
-const filter: FilterCreator = ({ client, elementsSource, config }) => ({
-  deploy: async changes => {
-    const allowedPermissions = await getAllowedPermissionTypes(elementsSource)
-    if (allowedPermissions.length === 0) {
-      log.warn('Could not find allowed permission types for permission Scheme filter. Skipping all permission schemes deploy')
-    }
-    const [relevantChanges, leftoverChanges] = _.partition(
-      changes,
-      change => isInstanceChange(change)
-        && isAdditionOrModificationChange(change)
-        && getChangeData(change).elemID.typeName === PERMISSION_SCHEME_TYPE_NAME
-    )
-
-    const deployResult = await deployChanges(
-      relevantChanges
-        .filter(isInstanceChange)
-        .filter(isAdditionOrModificationChange),
-      change => deployPermissionScheme(change, client, config, allowedPermissions)
-    )
-
-    return {
-      leftoverChanges,
-      deployResult,
-    }
-  },
-})
-
 export default filter

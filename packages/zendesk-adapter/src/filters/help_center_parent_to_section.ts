51b8d5de03ac987aed41a4e199475a8a5317703c
Eden Hassid <edenhassid@gmail.com>
2022-10-24 12:06:50 +0300

    comment

--- packages/zendesk-adapter/src/filters/help_center_parent_to_section.ts
+++ packages/zendesk-adapter/src/filters/help_center_parent_to_section.ts
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { isInstanceElement } from '@salto-io/adapter-api'
+// import { isInstanceElement } from '@salto-io/adapter-api'
 // import { FilterCreator } from '../filter'
 //
 // const SECTION_TYPE_NAME = 'section'

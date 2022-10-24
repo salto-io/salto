c9e384baa5e6dbe62ac93da4912fb9065cb2a4da
Uri Bar <uri.bar@salto.io>
2022-10-24 17:37:06 +0300

    remove default hardcoded annotation

--- packages/salesforce-adapter/test/filters/utils.test.ts
+++ packages/salesforce-adapter/test/filters/utils.test.ts
@@ -15,7 +15,7 @@
 */
 import { ObjectType, ElemID, BuiltinTypes, Field, InstanceElement, createRefToElmWithValue } from '@salto-io/adapter-api'
 import { addDefaults } from '../../src/filters/utils'
-import { SALESFORCE, LABEL, API_NAME, CUSTOM_FIELD, INSTANCE_FULL_NAME_FIELD, METADATA_TYPE, CUSTOM_OBJECT, CUSTOM_SETTINGS_TYPE } from '../../src/constants'
+import { SALESFORCE, LABEL, API_NAME, INSTANCE_FULL_NAME_FIELD, METADATA_TYPE, CUSTOM_OBJECT, CUSTOM_SETTINGS_TYPE } from '../../src/constants'
 import { Types } from '../../src/transformers/transformer'
 import { CustomObject } from '../../src/client/types'
 import { mockTypes } from '../mock_elements'
@@ -83,10 +83,6 @@ describe('addDefaults', () => {
           [API_NAME]: 'test__c',
           [METADATA_TYPE]: CUSTOM_OBJECT,
           [LABEL]: 'test',
-          deploymentStatus: 'Deployed',
-          pluralLabel: 'tests',
-          nameField: { type: 'Text', label: 'Name' },
-          sharingModel: 'ReadWrite',
         } as Partial<CustomObject>)
       })
       it('should add annotation types', () => {
@@ -94,13 +90,7 @@ describe('addDefaults', () => {
           [API_NAME]: createRefToElmWithValue(BuiltinTypes.SERVICE_ID),
           [METADATA_TYPE]: createRefToElmWithValue(BuiltinTypes.SERVICE_ID),
           [LABEL]: createRefToElmWithValue(BuiltinTypes.STRING),
-          deploymentStatus: createRefToElmWithValue(BuiltinTypes.STRING),
-          pluralLabel: createRefToElmWithValue(BuiltinTypes.STRING),
-          sharingModel: createRefToElmWithValue(BuiltinTypes.STRING),
         })
-        expect(object.annotationRefTypes.nameField?.elemID).toEqual(
-          new ElemID(SALESFORCE, CUSTOM_FIELD)
-        )
       })
       it('should add defaults to fields', () => {
         expect(object.fields.a.annotations).toMatchObject({
@@ -127,7 +117,6 @@ describe('addDefaults', () => {
       it('should add missing annotations', () => {
         expect(object.annotations).toMatchObject({
           [API_NAME]: 'test__c',
-          sharingModel: 'ReadWrite',
         } as Partial<CustomObject>)
       })
       it('should not override existing annotations', () => {
@@ -136,37 +125,16 @@ describe('addDefaults', () => {
           nameField: { type: 'AutoNumber', label: 'Name' },
         } as Partial<CustomObject>)
       })
-      it('should add plural label according to the label', () => {
-        expect(object.annotations).toHaveProperty('pluralLabel', 'myLabels')
-      })
       it('should add missing annotation types', () => {
         expect(object.annotationRefTypes).toMatchObject({
           [API_NAME]: createRefToElmWithValue(BuiltinTypes.SERVICE_ID),
           [LABEL]: createRefToElmWithValue(BuiltinTypes.STRING),
         })
-        expect(object.annotationRefTypes.nameField?.elemID).toEqual(
-          new ElemID(SALESFORCE, CUSTOM_FIELD)
-        )
       })
       it('should not override existing annotation types', () => {
         expect(object.annotationRefTypes).toMatchObject({
           sharingModel: createRefToElmWithValue(BuiltinTypes.HIDDEN_STRING),
         })
-      })
-    })
-    describe('when object has a master detail field', () => {
-      let object: ObjectType
-      beforeEach(async () => {
-        object = new ObjectType({
-          elemID: new ElemID(SALESFORCE, 'test'),
-          fields: {
-            a: { refType: Types.primitiveDataTypes.MasterDetail },
-          },
-        })
-        await addDefaults(object)
-      })
-      it('should set sharing model to controlled by parent', () => {
-        expect(object.annotations).toHaveProperty('sharingModel', 'ControlledByParent')
       })
     })
   })

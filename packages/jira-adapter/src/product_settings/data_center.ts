38560d314c1a5108561f3cc4909475542c5f2dad
shayc331 <77064001+shayc331@users.noreply.github.com>
2022-10-26 15:43:40 +0300

    add screens post, put and delete DC urls (#3461)

--- packages/jira-adapter/src/product_settings/data_center.ts
+++ packages/jira-adapter/src/product_settings/data_center.ts
@@ -47,7 +47,7 @@ const PLUGIN_URL_PATTERNS: UrlPattern[] = [
     url: '/rest/api/3/statuses',
   },
   {
-    httpMethods: ['get'],
+    httpMethods: ['get', 'post'],
     url: '/rest/api/3/screens',
   },
   {
@@ -61,6 +61,26 @@ const PLUGIN_URL_PATTERNS: UrlPattern[] = [
   {
     httpMethods: ['delete'],
     url: '/rest/api/3/workflow/\\d+',
+  },
+  {
+    httpMethods: ['put', 'delete'],
+    url: '/rest/api/3/screens/\\d+',
+  },
+  {
+    httpMethods: ['get'],
+    url: '/rest/api/3/workflow/search',
+  },
+  {
+    httpMethods: ['post'],
+    url: '/rest/api/3/workflow',
+  },
+  {
+    httpMethods: ['delete'],
+    url: '/rest/api/3/workflow/\\d+',
+  },
+  {
+    httpMethods: ['put', 'delete'],
+    url: '/rest/api/3/screens/\\d+',
   },
 ]
 

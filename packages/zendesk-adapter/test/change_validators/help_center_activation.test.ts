c914289f693d05084ee9a02b2f3fbc98422d176f
Eden Hassid <edenhassid@gmail.com>
2022-11-08 11:01:05 +0200

    fix build

--- packages/zendesk-adapter/test/change_validators/help_center_activation.test.ts
+++ packages/zendesk-adapter/test/change_validators/help_center_activation.test.ts
@@ -1,10 +1,24 @@
-import {ElemID, InstanceElement, ObjectType, toChange} from '@salto-io/adapter-api'
-import {BRAND_TYPE_NAME, ZENDESK} from '../../src/constants'
-import {helpCenterActivationValidator} from "../../src/change_validators";
+/*
+*                      Copyright 2022 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ElemID, InstanceElement, ObjectType, toChange } from '@salto-io/adapter-api'
+import { BRAND_TYPE_NAME, ZENDESK } from '../../src/constants'
+import { helpCenterActivationValidator } from '../../src/change_validators'
 
 
 describe('helpCenterActivationValidator', () => {
-
   const BrandType = new ObjectType({
     elemID: new ElemID(ZENDESK, BRAND_TYPE_NAME),
   })
@@ -69,6 +83,4 @@ describe('helpCenterActivationValidator', () => {
     )
     expect(errors).toHaveLength(0)
   })
-
-
 })

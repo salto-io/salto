f4c5254ab9780ebcef2e38caf13b12530a06a6ca
shayc331 <77064001+shayc331@users.noreply.github.com>
2022-11-08 13:53:52 +0200

    SALTO-2910: add plugin urls for IssueTypeScreenScheme - Jira DC (#3504)
    * add plugin urls for IssueTypeScreenScheme

--- packages/jira-adapter/src/product_settings/data_center.ts
+++ packages/jira-adapter/src/product_settings/data_center.ts
@@ -160,6 +160,30 @@ const PLUGIN_URL_PATTERNS: UrlPattern[] = [
     httpMethods: ['get'],
     url: '/rest/api/3/field/.*/context/.*/option',
   },
+  {
+    httpMethods: ['get', 'post'],
+    url: '/rest/api/3/issuetypescreenscheme',
+  },
+  {
+    httpMethods: ['put', 'delete'],
+    url: '/rest/api/3/issuetypescreenscheme/\\d+',
+  },
+  {
+    httpMethods: ['put'],
+    url: '/rest/api/3/issuetypescreenscheme/\\d+/mapping',
+  },
+  {
+    httpMethods: ['put'],
+    url: '/rest/api/3/issuetypescreenscheme/\\d+/mapping/default',
+  },
+  {
+    httpMethods: ['post'],
+    url: '/rest/api/3/issuetypescreenscheme/\\d+/mapping/remove',
+  },
+  {
+    httpMethods: ['get'],
+    url: '/rest/api/3/issuetypescreenscheme/mapping.+',
+  },
 ]
 
 const DC_DEFAULT_TYPE_CUSTOMIZATIONS: JiraApiConfig['types'] = {

70b1ea6d67fe1cc11854a5fc19e5986ce8175b88
guy hamo <guyhamo1@gmail.com>
2022-11-21 17:01:37 +0200

    Revert "temp"
    This reverts commit d948e2ecf818d49c7a0cdd59d103ef0ddaea99f5.

--- packages/jira-adapter/src/filters/group_name.ts
+++ packages/jira-adapter/src/filters/group_name.ts
@@ -22,7 +22,7 @@ import { GROUP_TYPE_NAME, JIRA } from '../constants'
 import { JiraConfig } from '../config/config'
 
 const UUID_REGEX = '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}'
-export const GROUP_NAME_REGEX = new RegExp(`^(.*)-${UUID_REGEX}$`)
+const GROUP_NAME_REGEX = new RegExp(`^(.*)-${UUID_REGEX}$`)
 
 const isGroupElement = (element: Element): boolean =>
   element.elemID.typeName === GROUP_TYPE_NAME

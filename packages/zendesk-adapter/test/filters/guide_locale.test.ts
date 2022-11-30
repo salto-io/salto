724b4cdfa4d3b6565b5969b110f4eb20d971fefb
idoser <idoser@gmail.com>
2022-11-30 14:51:56 +0200

    Test comment

--- packages/zendesk-adapter/test/filters/guide_locale.test.ts
+++ packages/zendesk-adapter/test/filters/guide_locale.test.ts
@@ -38,6 +38,7 @@ const languageSetting1 = new InstanceElement('settings1', languageSettingsType, 
 const languageSetting2 = new InstanceElement('settings2', languageSettingsType, { brand: 2, locale: 'he' })
 
 const category1 = new InstanceElement('category1', categoryType, { brand: 1, locale: 'en-us', source_locale: 'en-us' })
+// Notice that the settings for this 'source' does not exist - so it should stay the same
 const category2 = new InstanceElement('category2', categoryType, { brand: 2, locale: 'en-us', source_locale: 'he' })
 
 const categoryTranslation1 = new InstanceElement('translation1', categoryTranslationType, { brand: 1, locale: 'en-us' })
@@ -56,75 +57,3 @@ describe('onFetch', () => {
     console.log(brand1)
   })
 })
-
-// describe('guide locale filter', () => {
-//   let mockClient: MockInterface<ZendeskClient>
-//   type FilterType = filterUtils.FilterWith<'onFetch'>
-//   let filter: FilterType
-//   const mockGetSinglePage = jest.fn()
-//
-//   beforeEach(async () => {
-//     jest.clearAllMocks()
-//     mockClient = {
-//       getSinglePage: mockGetSinglePage,
-//     } as unknown as MockInterface<ZendeskClient>
-//     filter = filterCreator(createFilterCreatorParams({
-//       client: mockClient as unknown as ZendeskClient,
-//       config: {
-//         ...DEFAULT_CONFIG,
-//         [FETCH_CONFIG]: {
-//           ...DEFAULT_CONFIG[FETCH_CONFIG],
-//           enableGuide: true,
-//         },
-//       },
-//     })) as FilterType
-//   })
-//
-//   describe('onFetch', () => {
-//     it('should add locales instances and type', async () => {
-//       const elements: Element[] = []
-//       mockGetSinglePage.mockResolvedValue({
-//         data: {
-//           locales: ['en-us', 'es'],
-//           default_locale: 'en-us',
-//         },
-//       })
-//       await filter.onFetch(elements)
-//       expect(elements.map(e => e.elemID.getFullName()).sort())
-//         .toEqual([
-//           'zendesk.guide_locale',
-//           'zendesk.guide_locale.instance.en_us@b',
-//           'zendesk.guide_locale.instance.es',
-//         ])
-//       const locales = elements.filter(isInstanceElement)
-//       const enLocale = locales.find(locale => locale.value.id === 'en-us')
-//       expect(enLocale).toBeDefined()
-//       expect(enLocale?.value).toEqual({ id: 'en-us', default: true })
-//       const esLocale = locales.find(locale => locale.value.id === 'es')
-//       expect(esLocale).toBeDefined()
-//       expect(esLocale?.value).toEqual({ id: 'es', default: false })
-//     })
-//     it('should not add locales instances and type if enableGuide is false', async () => {
-//       const elements: Element[] = []
-//       mockGetSinglePage.mockResolvedValue({
-//         data: {
-//           locales: ['en-us', 'es'],
-//           default_locale: 'en-us',
-//         },
-//       })
-//       const filterWithNoGuide = filterCreator(createFilterCreatorParams({})) as FilterType
-//       await filterWithNoGuide.onFetch(elements)
-//       expect(elements.map(e => e.elemID.getFullName()).sort()).toEqual([])
-//     })
-//     it('should not add locales if the response is invalid', async () => {
-//       const elements: Element[] = []
-//       mockGetSinglePage.mockResolvedValue({
-//         data: {
-//           invalid: ['en-us', 'es'],
-//         },
-//       })
-//       await filter.onFetch(elements)
-//       expect(elements.map(e => e.elemID.getFullName()).sort()).toEqual([])
-//     })
-//   })
-// })

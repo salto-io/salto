2186fde550bc56e75cb6aa615f304dc3a0c4041c
Eden Hassid <edenhassid@gmail.com>
2022-12-15 13:46:18 +0200

    rebase

--- packages/zendesk-adapter/src/config.ts
+++ packages/zendesk-adapter/src/config.ts
@@ -1740,8 +1740,6 @@ export const DEFAULT_TYPES: ZendeskApiConfig['types'] = {
       fieldTypeOverrides: [
         { fieldName: 'id', fieldType: 'number' },
         { fieldName: 'brand', fieldType: 'number' },
-        { fieldName: 'created_by_id', fieldType: 'unknown' },
-        { fieldName: 'updated_by_id', fieldType: 'unknown' },
       ],
       fieldsToOmit: FIELDS_TO_OMIT.concat(
         { fieldName: 'html_url', fieldType: 'string' },

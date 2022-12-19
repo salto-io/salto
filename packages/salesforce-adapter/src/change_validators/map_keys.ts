4801e36ed19b9fbfc366fef834628b2a56629a24
Uri Bar <109457445+Uri-Bar@users.noreply.github.com>
2022-12-19 12:11:26 +0300

    SALTO-3081 Map_keys CV - add unresolved reference validation (#3677)

--- packages/salesforce-adapter/src/change_validators/map_keys.ts
+++ packages/salesforce-adapter/src/change_validators/map_keys.ts
@@ -26,7 +26,6 @@ import {
 } from '@salto-io/adapter-api'
 import { TransformFunc, transformValues, resolveValues } from '@salto-io/adapter-utils'
 import { collections } from '@salto-io/lowerdash'
-import { logger } from '@salto-io/logging'
 import { defaultMapper, metadataTypeToFieldToMapDef } from '../filters/convert_maps'
 import { API_NAME_SEPARATOR, PERMISSION_SET_METADATA_TYPE, PROFILE_METADATA_TYPE } from '../constants'
 import { getLookUpName } from '../transformers/reference_mapping'
@@ -34,8 +33,6 @@ import { isInstanceOfTypeChange } from '../filters/utils'
 import { apiName } from '../transformers/transformer'
 
 const { awu } = collections.asynciterable
-
-const log = logger(module)
 
 const metadataTypesToValidate = [
   PROFILE_METADATA_TYPE,
@@ -70,8 +67,9 @@ const getMapKeyErrors = async (
           })
           return undefined
         }
+        // this validation intend to catch unresolved reference, and should be removed after the general fix
         if (typeof value[mapDef.key] !== 'string') {
-          log.error(`found a non string value in field ${typeName}.${fieldName}.${mapDef.key}, value: ${value[mapDef.key]}`)
+          return undefined
         }
         // we reached the map's inner value
         const expectedPath = defaultMapper(value[mapDef.key]).slice(0, mapDef.nested ? 2 : 1)

bc5b7bc0924ca809d9fc2a6b7586ca3de89a2b25
Eden Hassid <edenhassid@gmail.com>
2022-12-25 16:44:42 +0200

    remove character

--- packages/zendesk-adapter/src/filters/handle_template_expressions.ts
+++ packages/zendesk-adapter/src/filters/handle_template_expressions.ts
@@ -51,7 +51,7 @@ BRACKETS.forEach(([opener, closer]) => {
   // dynamic content references look different, but can still be part of template
   typeSearchRegexes.push(new RegExp(`(${opener})([^\\$}]*dc\\.[\\w]+[^}]*)(${closer})`, 'g'))
 })
-const potentialReferenceTypeRegex = new RegExp(`((?:${POTENTIAL_REFERENCE_TYPES.join('|')})_\u200B?[\\d]+)`, 'g')
+const potentialReferenceTypeRegex = new RegExp(`((?:${POTENTIAL_REFERENCE_TYPES.join('|')})_?[\\d]+)`, 'g')
 const potentialMacroFields = [
   'comment_value', 'comment_value_html', 'side_conversation', 'side_conversation_ticket', 'subject', 'side_conversation_slack',
 ]
@@ -145,7 +145,7 @@ const formulaToTemplate = (
 ): TemplateExpression | string => {
   const handleZendeskReference = (expression: string, ref: RegExpMatchArray): TemplatePart[] => {
     const reference = ref.pop() ?? ''
-    const splitReference = reference.split(/_\u200B?([\d]+)/).filter(v => !_.isEmpty(v))
+    const splitReference = reference.split(/_?([\d]+)/).filter(v => !_.isEmpty(v))
     // should be exactly of the form TYPE_INNERID, so should contain exactly two parts
     if (splitReference.length !== 2) {
       return [expression]

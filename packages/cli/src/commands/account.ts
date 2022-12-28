98efbe9d3491875279219e2285a7634b9ef2653f
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2022-12-28 18:11:35 +0200

    add domain as optional login param in zendesk

--- packages/cli/src/commands/account.ts
+++ packages/cli/src/commands/account.ts
@@ -20,6 +20,7 @@ import {
   InstanceElement,
   OAuthMethod,
   ObjectType,
+  CORE_ANNOTATIONS,
 } from '@salto-io/adapter-api'
 import { EOL } from 'os'
 import {
@@ -143,7 +144,9 @@ const getLoginConfig = async (
 const createConfigFromLoginParameters = (loginParameters: string[]) => (
   async (credentialsType: ObjectType): Promise<InstanceElement> => {
     const configValues = Object.fromEntries(loginParameters.map(entryFromRawLoginParameter))
-    const missingLoginParameters = Object.keys(credentialsType.fields)
+    const requiredFields = Object.fromEntries(Object.entries(credentialsType.fields)
+      .filter(([_fieldName, field]) => field.annotations[CORE_ANNOTATIONS.REQUIRED] !== false))
+    const missingLoginParameters = Object.keys(requiredFields)
       .filter(key => _.isUndefined(configValues[key]))
     if (!_.isEmpty(missingLoginParameters)) {
       throw new Error(`Missing the following login parameters: ${missingLoginParameters}`)

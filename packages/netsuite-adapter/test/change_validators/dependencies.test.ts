e1b32e19fdf33b47f08a6c773e3d1fedb75d3bbe
Batel Delaroz <66998687+kami-delaroz@users.noreply.github.com>
2023-01-17 18:03:22 +0200

    return change errors only for changes that depends on required references (#3825)
    (additional elements and required references)

--- packages/netsuite-adapter/test/change_validators/dependencies.test.ts
+++ packages/netsuite-adapter/test/change_validators/dependencies.test.ts
@@ -13,84 +13,103 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { BuiltinTypes, ElemID, InstanceElement, ObjectType, ReferenceExpression, toChange } from '@salto-io/adapter-api'
+import { Change, ElemID, InstanceElement, ObjectType, ReferenceExpression, toChange } from '@salto-io/adapter-api'
 import { fileType } from '../../src/types/file_cabinet_types'
-import { entitycustomfieldType } from '../../src/autogen/types/standard_types/entitycustomfield'
+import { customsegmentType } from '../../src/autogen/types/standard_types/customsegment'
 import { validateDependsOnInvalidElement } from '../../src/change_validators/dependencies'
-import { PATH, SCRIPT_ID } from '../../src/constants'
+import { CUSTOM_RECORD_TYPE, METADATA_TYPE, NETSUITE, PATH, SCRIPT_ID } from '../../src/constants'
 
-describe('Change Validator', () => {
-  const entitycustomfield = entitycustomfieldType().type
-  const customFieldInstance = new InstanceElement('elementName',
-    entitycustomfield, {
-      label: 'elementName',
-      [SCRIPT_ID]: 'custentity_my_script_id',
+describe('Required Dependencies Validator', () => {
+  const customsegment = customsegmentType().type
+  let fileInstance: InstanceElement
+  let customRecordType: ObjectType
+  let customSegmentInstance: InstanceElement
+  let dependsOn2Instances: InstanceElement
+  let changes: ReadonlyArray<Change>
+  beforeEach(() => {
+    fileInstance = new InstanceElement('file_instance', fileType(), {
+      [PATH]: 'Templates/E-mail Templates/Inner EmailTemplates Folder/content.html',
     })
 
-  const fileInstance = new InstanceElement('fileInstance', fileType(), {
-    [PATH]: 'Templates/E-mail Templates/Inner EmailTemplates Folder/content.html',
+    customRecordType = new ObjectType({
+      elemID: new ElemID(NETSUITE, 'custom_record_type'),
+      annotations: {
+        [METADATA_TYPE]: CUSTOM_RECORD_TYPE,
+        [SCRIPT_ID]: 'custom_record_type_script_id',
+      },
+    })
+
+    customSegmentInstance = new InstanceElement('custom_segment_instance', customsegment, {
+      [SCRIPT_ID]: 'custom_segment_instance_script_id',
+    })
+
+    customSegmentInstance.value.recordtype = new ReferenceExpression(
+      customRecordType.elemID.createNestedID('attr', SCRIPT_ID), 'val', customRecordType
+    )
+
+    customRecordType.annotations.customsegment = new ReferenceExpression(
+      customSegmentInstance.elemID.createNestedID(SCRIPT_ID),
+      customSegmentInstance.value[SCRIPT_ID],
+      customSegmentInstance
+    )
+
+    dependsOn2Instances = new InstanceElement('depends_on_2_instances', customsegment, {
+      [SCRIPT_ID]: 'depends_on_2_instances_script_id',
+      label: new ReferenceExpression(
+        customSegmentInstance.elemID.createNestedID(SCRIPT_ID),
+        customSegmentInstance.value[SCRIPT_ID],
+        customSegmentInstance
+      ),
+      description: new ReferenceExpression(
+        fileInstance.elemID.createNestedID(PATH),
+        fileInstance.value[PATH],
+        fileInstance
+      ),
+    })
+
+    // dependsOn2Instances ---> customSegmentInstance <---> customRecordType
+    //        |
+    //        '--> fileInstance
+
+    changes = [
+      toChange({ before: dependsOn2Instances, after: dependsOn2Instances }),
+      toChange({ before: customRecordType, after: customRecordType }),
+      toChange({ after: customSegmentInstance }),
+      toChange({ after: fileInstance }),
+    ]
   })
-
-  const dependsOn1Instance = new InstanceElement('dependsOn1Instance', entitycustomfield, {
-    [SCRIPT_ID]: 'custentity_depends_on_1_instance',
-    label: new ReferenceExpression(fileInstance.elemID.createNestedID(PATH),
-      fileInstance.value[PATH], fileInstance),
-  })
-
-  const anotherAdapterInstance = new InstanceElement(
-    'anotherAdapterInstance',
-    new ObjectType({ elemID: new ElemID('another', 'type'),
-      fields: {
-        id: { refType: BuiltinTypes.SERVICE_ID },
-      } }),
-    { id: 'serviceIdValue' },
-  )
-
-  const instanceWithManyRefs = new InstanceElement('dependsOn2Instances', entitycustomfield, {
-    [SCRIPT_ID]: 'custentity_depends_on_2',
-    label: new ReferenceExpression(dependsOn1Instance.elemID.createNestedID(SCRIPT_ID),
-      dependsOn1Instance.value[SCRIPT_ID], dependsOn1Instance),
-    description: new ReferenceExpression(customFieldInstance.elemID.createNestedID('label'),
-      customFieldInstance.value.label, customFieldInstance),
-    help: new ReferenceExpression(anotherAdapterInstance.elemID.createNestedID('id'),
-      anotherAdapterInstance.value.id, anotherAdapterInstance),
-  })
-
-  const changes = [toChange({ after: instanceWithManyRefs }),
-    toChange({ after: dependsOn1Instance }),
-    toChange({ after: fileInstance }),
-    toChange({ after: customFieldInstance })]
 
   it('should return no change errors when there are no invalid elements from other change validators', async () => {
     expect(await validateDependsOnInvalidElement([], changes)).toEqual([])
   })
 
   it('should return no change errors if no other change depends on an invalid element', async () => {
-    expect(
-      await validateDependsOnInvalidElement(
-        [instanceWithManyRefs.elemID.getFullName(), customFieldInstance.elemID.getFullName()],
-        changes
-      )
-    ).toEqual([])
+    expect(await validateDependsOnInvalidElement(
+      [dependsOn2Instances.elemID.getFullName(), fileInstance.elemID.getFullName()],
+      changes
+    )).toEqual([])
   })
 
-  it('should return change errors for all changes that depend on an invalid element including deep dependency', async () => {
-    expect(await validateDependsOnInvalidElement([fileInstance.elemID.getFullName()], changes))
+  it('should return change errors for all changes that have dependency on an invalid element that was added', async () => {
+    const changeErrors = await validateDependsOnInvalidElement([fileInstance.elemID.getFullName()], changes)
+    expect(changeErrors)
       .toEqual(expect.arrayContaining([
         expect.objectContaining({
-          elemID: dependsOn1Instance.elemID,
-        }),
-        expect.objectContaining({
-          elemID: instanceWithManyRefs.elemID,
+          elemID: dependsOn2Instances.elemID,
         })]))
+    expect(changeErrors).toHaveLength(1)
   })
 
-  it('should return change error for change that depends on an invalid element', async () => {
-    expect(
-      await validateDependsOnInvalidElement([dependsOn1Instance.elemID.getFullName()], changes)
-    ).toEqual(expect.arrayContaining([
-      expect.objectContaining({
-        elemID: instanceWithManyRefs.elemID,
-      })]))
+  it('should return change errors for all changes that have required dependency on an invalid element (either added or required references), including deep dependency', async () => {
+    const changeErrors = await validateDependsOnInvalidElement([customRecordType.elemID.getFullName()], changes)
+    expect(changeErrors)
+      .toEqual(expect.arrayContaining([
+        expect.objectContaining({
+          elemID: customSegmentInstance.elemID,
+        }),
+        expect.objectContaining({
+          elemID: dependsOn2Instances.elemID,
+        })]))
+    expect(changeErrors).toHaveLength(2)
   })
 })

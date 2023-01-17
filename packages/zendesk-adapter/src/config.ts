ded946f4b55e13a35b5556aed851f0dc9d84abe8
Eden Hassid <edenhassid@gmail.com>
2023-01-17 15:22:07 +0200

    SALTO-3411: special characters throw warnings in some zendesk types

--- packages/zendesk-adapter/src/config.ts
+++ packages/zendesk-adapter/src/config.ts
@@ -832,7 +832,17 @@ export const DEFAULT_TYPES: ZendeskApiConfig['types'] = {
     transformation: {
       idFields: ['value'],
       fieldsToHide: FIELDS_TO_HIDE.concat({ fieldName: 'id', fieldType: 'number' }),
-      fieldTypeOverrides: [{ fieldName: 'id', fieldType: 'number' }],
+      fieldTypeOverrides: [
+        { fieldName: 'id', fieldType: 'number' },
+        {
+          fieldName: 'value',
+          fieldType: 'string',
+          restrictions: {
+            enforce_value: true,
+            regex: '^[^&%$#@\\! \\{\\}\\[\\]=\\+\\(\\)\\*\\?<>,"\'`;\\\\]+$',
+          },
+        },
+      ],
     },
   },
   user_field: {

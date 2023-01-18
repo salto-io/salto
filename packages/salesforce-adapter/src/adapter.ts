7cd2f09b245a7addb3a811c73deba4bfabde74b8
Tom Sellek <tom.sellek@salto.io>
2023-01-18 16:01:47 -0500

    Move filter up in the chain

--- packages/salesforce-adapter/src/adapter.ts
+++ packages/salesforce-adapter/src/adapter.ts
@@ -109,6 +109,7 @@ export const allFilters: Array<LocalFilterCreatorDefinition | RemoteFilterCreato
   { creator: customMetadataToObjectTypeFilter },
   // customObjectsFilter depends on missingFieldsFilter and settingsFilter
   { creator: customObjectsFromDescribeFilter, addsNewInformation: true },
+  { creator: organizationWideDefaults, addsNewInformation: true },
   // customSettingsFilter depends on customObjectsFilter
   { creator: customSettingsFilter, addsNewInformation: true },
   { creator: customObjectsToObjectTypeFilter },
@@ -171,7 +172,6 @@ export const allFilters: Array<LocalFilterCreatorDefinition | RemoteFilterCreato
   { creator: profileInstanceSplitFilter },
   // Any filter that relies on _created_at or _changed_at should run after removeUnixTimeZero
   { creator: removeUnixTimeZeroFilter },
-  { creator: organizationWideDefaults, addsNewInformation: true },
 ]
 
 // By default we run all filters and provide a client

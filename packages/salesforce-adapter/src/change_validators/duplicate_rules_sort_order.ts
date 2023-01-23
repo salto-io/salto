f7d324ab0c2c1eb145019f1d22e146bebee16ff7
tamir <tamir.ohayon@salto.io>
2023-01-23 17:04:22 +0200

    Added `duplicateRulesSortOrder` change validator

---
+++ packages/salesforce-adapter/src/change_validators/duplicate_rules_sort_order.ts
@@ -0,0 +1,124 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import {
+  Change, ChangeError,
+  ChangeValidator, getChangeData, InstanceElement,
+  isAdditionOrModificationChange, isInstanceChange,
+  isInstanceElement,
+} from '@salto-io/adapter-api'
+import { collections, values } from '@salto-io/lowerdash'
+import _ from 'lodash'
+import Joi from 'joi'
+import { createSchemeGuard } from '@salto-io/adapter-utils'
+import { DUPLICATE_RULE_METADATA_TYPE, INSTANCE_FULL_NAME_FIELD } from '../constants'
+import { apiName } from '../transformers/transformer'
+
+const { awu, groupByAsync } = collections.asynciterable
+const { isDefined } = values
+
+export const SORT_ORDER = 'sortOrder'
+
+type DuplicateRuleInstance = InstanceElement & {
+  value: InstanceElement['value'] & {
+    [INSTANCE_FULL_NAME_FIELD]: string
+    [SORT_ORDER]: number
+  }
+}
+
+const DUPLICATE_RULE_INSTANCE_SCHEMA = Joi.object({
+  value: Joi.object({
+    [INSTANCE_FULL_NAME_FIELD]: Joi.string().required(),
+    [SORT_ORDER]: Joi.number().required(),
+  }).unknown(true).required(),
+}).unknown(true).required()
+
+
+const isDuplicateRuleInstance = createSchemeGuard<DuplicateRuleInstance>(DUPLICATE_RULE_INSTANCE_SCHEMA)
+
+const getRelatedObjectName = (instance: DuplicateRuleInstance): string => (
+  instance.value[INSTANCE_FULL_NAME_FIELD].split('.')[0]
+)
+
+const createSortOrderError = (
+  instance: DuplicateRuleInstance,
+  objectName: string,
+  order: number[]
+): ChangeError => ({
+  elemID: instance.elemID,
+  severity: 'Error',
+  message: 'DuplicateRule instances must be in sequential order',
+  detailedMessage: `Could not deploy DuplicateRule instance ${instance.elemID.name} since the DuplicateRule instances
+  on the type ${objectName} are not in sequential order. Order: ${order}`,
+})
+
+const isInvalidSortOrder = (sortOrder: number[]): boolean => {
+  const maxSortOrder = _.last(sortOrder)
+  return isDefined(maxSortOrder)
+    && !_.isEqual(sortOrder.sort(), _.range(1, maxSortOrder + 1))
+}
+
+const changeValidator: ChangeValidator = async (changes, elementsSource) => {
+  const isInstanceOfTypeDuplicateRule = async (instance: InstanceElement): Promise<boolean> => (
+    await apiName(await instance.getType(elementsSource)) === DUPLICATE_RULE_METADATA_TYPE
+  )
+  const relatedChangesByObjectName = await groupByAsync(
+    await awu(changes)
+      .filter(isInstanceChange)
+      .filter(isAdditionOrModificationChange)
+      .filter(change => isInstanceOfTypeDuplicateRule(getChangeData(change)))
+      .filter(change => isDuplicateRuleInstance(getChangeData(change)))
+      .toArray() as Change<DuplicateRuleInstance>[],
+    change => getRelatedObjectName(getChangeData(change))
+  )
+  if (_.isEmpty(relatedChangesByObjectName) || elementsSource === undefined) {
+    return []
+  }
+  const duplicateRuleInstances = await awu(await elementsSource.getAll())
+    .filter(isInstanceElement)
+    .filter(isInstanceOfTypeDuplicateRule)
+    .filter(isDuplicateRuleInstance)
+    .toArray()
+
+  const relevantDuplicateRuleInstancesByObjectName = _.pick(
+    _.groupBy(duplicateRuleInstances, getRelatedObjectName),
+    Object.keys(relatedChangesByObjectName),
+  )
+
+  const invalidSortOrderByObjectName = _.pickBy(
+    _.mapValues(
+      relevantDuplicateRuleInstancesByObjectName,
+      instances => instances.map(instance => instance.value.sortOrder).sort()
+    ),
+    isInvalidSortOrder,
+  )
+
+  const invalidChangesByObjectName = _.pick(
+    relatedChangesByObjectName,
+    Object.keys(invalidSortOrderByObjectName)
+  )
+
+  return Object.entries(invalidChangesByObjectName)
+    .flatMap(([objectName, invalidChanges]) =>
+      invalidChanges
+        .map(getChangeData)
+        .map(instance => createSortOrderError(
+          instance,
+          objectName,
+          invalidSortOrderByObjectName[objectName]
+        )))
+}
+
+export default changeValidator
--- packages/salesforce-adapter/test/mock_elements.ts
+++ packages/salesforce-adapter/test/mock_elements.ts
@@ -26,13 +26,14 @@ import {
   API_NAME,
   METADATA_TYPE,
   CUSTOM_OBJECT,
-  CPQ_QUOTE,
+  CPQ_QUOTE, DUPLICATE_RULE_METADATA_TYPE,
 } from '../src/constants'
 import { createInstanceElement, createMetadataObjectType } from '../src/transformers/transformer'
 import { allMissingSubTypes } from '../src/transformers/salesforce_types'
 import { API_VERSION } from '../src/client/client'
 import { WORKFLOW_FIELD_TO_TYPE } from '../src/filters/workflow'
 import { createCustomObjectType } from './utils'
+import { SORT_ORDER } from '../src/change_validators/duplicate_rules_sort_order'
 
 
 export const mockTypes = {
@@ -309,6 +310,17 @@ export const mockTypes = {
     },
     fields: {
       filter: { refType: BuiltinTypes.STRING },
+    },
+  }),
+  [DUPLICATE_RULE_METADATA_TYPE]: createMetadataObjectType({
+    annotations: {
+      metadataType: DUPLICATE_RULE_METADATA_TYPE,
+      suffix: 'rule',
+      dirName: 'rules',
+    },
+    fields: {
+      [INSTANCE_FULL_NAME_FIELD]: { refType: BuiltinTypes.STRING },
+      [SORT_ORDER]: { refType: BuiltinTypes.NUMBER },
     },
   }),
   // CustomMetadataRecordType with name MDType__mdt

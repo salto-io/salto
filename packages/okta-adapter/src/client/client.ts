e6acafe8f709b09a6870e89cfa8043a487d22169
idoser <idoser@gmail.com>
2023-01-31 12:20:13 +0200

    Added the field to okta

--- packages/okta-adapter/src/client/client.ts
+++ packages/okta-adapter/src/client/client.ts
@@ -19,7 +19,7 @@ import { OKTA } from '../constants'
 import { Credentials } from '../auth'
 
 const {
-  RATE_LIMIT_UNLIMITED_MAX_CONCURRENT_REQUESTS,
+  RATE_LIMIT_UNLIMITED_MAX_CONCURRENT_REQUESTS, DEFAULT_RETRY_OPTS,
 } = clientUtils
 
 const DEFAULT_MAX_CONCURRENT_API_REQUESTS: Required<clientUtils.ClientRateLimitConfig> = {
@@ -52,6 +52,7 @@ export default class OktaClient extends clientUtils.AdapterHTTPClient<
         retry: {
           maxAttempts: 5, // try 5 times
           retryDelay: 10000, // wait for 10s before trying again, change after SALTO-2649
+          additionalStatusCodesToRetry: DEFAULT_RETRY_OPTS.additionalStatusCodesToRetry,
         },
       }
     )

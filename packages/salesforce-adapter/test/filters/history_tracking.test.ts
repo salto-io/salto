8d7ff4b207c0fa3694c7ab1235e1360a3dbe76c9
Tom Sellek <tom.sellek@salto.io>
2023-02-01 11:09:32 -0500

    Add a unit test

---
+++ packages/salesforce-adapter/test/filters/history_tracking.test.ts
@@ -0,0 +1,67 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import filterCreator from '../../src/filters/history_tracking'
+import { createCustomObjectType, defaultFilterContext } from '../utils'
+import { mockTypes } from '../mock_elements'
+import { FilterWith } from '../../src/filter'
+import { Types } from '../../src/transformers/transformer'
+import { HISTORY_TRACKED_FIELDS } from '../../src/constants'
+
+describe('History tracking', () => {
+  const filter = filterCreator({ config: defaultFilterContext }) as FilterWith<'onFetch'|'preDeploy'>
+  describe('When fetching an object with history tracking disabled', () => {
+    it('Should not modify the object', async () => {
+      const inputType = mockTypes.Account.clone()
+      inputType.annotations.enableHistory = false
+      const expectedOutput = inputType.clone()
+      const elements = [inputType]
+      await filter.onFetch(elements)
+      expect(elements).toEqual([expectedOutput])
+    })
+  })
+  describe('When fetching an object with history tracking enabled', () => {
+    const typeWithHistoryTrackedFields = createCustomObjectType('TypeWithHistoryTracking', {
+      // annotations: {
+      //   enableHistory: true,
+      // },
+      fields: {
+        fieldWithHistoryTracking: {
+          refType: Types.primitiveDataTypes.Text,
+          annotations: {
+            name: 'fieldWithHistoryTracking',
+            trackHistory: true,
+          },
+        },
+        fieldWithoutHistoryTracking: {
+          refType: Types.primitiveDataTypes.Text,
+          annotations: {
+            name: 'fieldWithoutHistoryTracking',
+            trackHistory: false,
+          },
+        },
+      },
+    })
+    it('Should update the object and field annotations correctly', async () => {
+      typeWithHistoryTrackedFields.annotations.enableHistory = true
+      await filter.onFetch([typeWithHistoryTrackedFields])
+      const trackedFieldNames = typeWithHistoryTrackedFields.annotations[HISTORY_TRACKED_FIELDS]
+      expect(trackedFieldNames).toBeDefined()
+      expect(trackedFieldNames).toEqual(['fieldWithHistoryTracking'])
+      expect(typeWithHistoryTrackedFields.fields.fieldWithHistoryTracking.annotations.trackHistory).not.toBeDefined()
+      expect(typeWithHistoryTrackedFields.fields.fieldWithoutHistoryTracking.annotations.trackHistory).not.toBeDefined()
+    })
+  })
+})

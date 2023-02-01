c2823d0ae74345e2b134bd662c58828eff2f9291
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2023-02-01 15:36:17 +0200

    temp code

---
+++ packages/okta-adapter/src/filters/expression_language.ts
@@ -0,0 +1,147 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import _ from 'lodash'
+import { Element, InstanceElement, isInstanceElement, ReferenceExpression, TemplateExpression, TemplatePart } from '@salto-io/adapter-api'
+import { extractTemplate, resolvePath, setPath } from '@salto-io/adapter-utils'
+import { logger } from '@salto-io/logging'
+import { FilterCreator } from '../filter'
+import { GROUP_RULE_TYPE_NAME, GROUP_TYPE_NAME, POLICY_RULE_TYPE_NAME, USER_SCHEMA_TYPE_NAME } from '../constants'
+
+const log = logger(module)
+
+const USER_SCHEMA_REGEX = /(user\.[a-zA-Z]+)/g // pattern: user.someString
+const ID_REEGX = /(\(["'].+?['"]\))/g // pattern: ("someId", "anotherId")
+const GROUP_ID_IE_REGEX = /(['"]group\.id['"]:{['"\s,a-zA-Z\d]+})/g // pattern: 'group.id':{'id1', 'id2'}
+const USER_SCHEMA_IE_REGEX = /(user\.profile\.[a-zA-Z]+)/g // pattern: user.profile.someString
+
+type expressionDetails = {
+  path: string[]
+  patterns: RegExp[]
+}
+
+const TYPE_TO_EXPRESSIONS: Record<string, expressionDetails> = {
+  [GROUP_RULE_TYPE_NAME]: {
+    path: ['conditions', 'expression', 'value'],
+    patterns: [USER_SCHEMA_REGEX, ID_REEGX],
+  },
+  [POLICY_RULE_TYPE_NAME]: {
+    path: ['conditions', 'additionalProperties', 'elCondition', 'condition'],
+    patterns: [GROUP_ID_IE_REGEX, USER_SCHEMA_IE_REGEX],
+  },
+}
+
+/**
+ * Gets a string with ids and returns the matching template
+ */
+const getTemplatePartsFromIds = (
+  expression: string,
+  instancesByType: Record<string, InstanceElement[]>,
+): TemplatePart | TemplatePart[] => {
+  const ids = expression.split(', ').map(id => id.replace(/['"]/g, ''))
+  const templateParts = ids.map(id => {
+    const matchingInstance = instancesByType[GROUP_TYPE_NAME]?.find(instance => instance.value.id === id)
+    return matchingInstance !== undefined ? new ReferenceExpression(matchingInstance.elemID, matchingInstance) : `"${id}"`
+  })
+  return templateParts
+}
+
+const getUserSchemaReferences = (
+  userAttribute: string,
+  userSchemaInstance: InstanceElement,
+): TemplatePart | TemplatePart[] | undefined => {
+  const customPath = ['definitions', 'custom', 'properties', 'additionalProperties', userAttribute]
+  const basePath = ['definitions', 'base', 'properties', userAttribute]
+  const customValue = resolvePath(userSchemaInstance, userSchemaInstance.elemID.createNestedID(...customPath))
+  if (customValue !== undefined) {
+    return new ReferenceExpression(
+      userSchemaInstance.elemID.createNestedID(...customPath),
+      userSchemaInstance.value.definitions?.custom?.properties?.additionalProperties?.userFieldName
+    )
+  }
+  const baseValue = resolvePath(userSchemaInstance, userSchemaInstance.elemID.createNestedID(...basePath))
+  if (baseValue) {
+    return new ReferenceExpression(
+      userSchemaInstance.elemID.createNestedID(...basePath),
+      userSchemaInstance.value.definitions?.base?.properties?.userFieldName
+    )
+  }
+  return undefined
+}
+
+/**
+ * This function receives a string in okta expression language and replaces it with salto style templates.
+ */
+const stringToTemplate = (
+  expressionValue: string,
+  patterns: RegExp[],
+  instancesByType: Record<string, InstanceElement[]>
+): string | TemplateExpression => {
+  const template = extractTemplate(
+    expressionValue,
+    patterns,
+    expression => {
+      if (expression.match(ID_REEGX)) {
+        const ids = expression.slice(1, -1) // remove brackets to get ids
+        return ['(', getTemplatePartsFromIds(ids, instancesByType), ')'].flat()
+      }
+      if (expression.match(GROUP_ID_IE_REGEX)) {
+        const ids = expression.replace(/['"]group.id["']:/, '').slice(1, -1) // remove group.id prefix and curly braces to get ids
+        return ['"group.id":{', getTemplatePartsFromIds(ids, instancesByType), '}'].flat()
+      }
+      if (expression.match(USER_SCHEMA_REGEX)) {
+        const userSchemaInstance = instancesByType[USER_SCHEMA_TYPE_NAME]?.find(instance => instance.elemID.name === 'user')
+        if (userSchemaInstance === undefined) {
+          return expression
+        }
+        if (expression.match(USER_SCHEMA_IE_REGEX)) {
+          const userAttribute = expression.replace('user.profile.', '')
+          return getUserSchemaReferences(userAttribute, userSchemaInstance) ?? expression
+        }
+        const userAttribute = expression.replace('user.', '')
+        return getUserSchemaReferences(userAttribute, userSchemaInstance) ?? expression
+      }
+      return expression
+    },
+  )
+  return template
+}
+
+const replaceFormulasWithTemplates = async (instances: InstanceElement[]): Promise<void> => {
+  const instancesByType = _.groupBy(instances, i => i.elemID.typeName)
+  instances
+    .filter(instance => Object.keys(TYPE_TO_EXPRESSIONS).includes(instance.elemID.typeName))
+    .forEach(instance => {
+      const { path, patterns } = TYPE_TO_EXPRESSIONS[instance.elemID.typeName]
+      const expressionFieldPath = instance.elemID.createNestedID(...path)
+      const expressionValue = resolvePath(instance, expressionFieldPath)
+      if (_.isString(expressionValue)) {
+        const template = stringToTemplate(expressionValue, patterns, instancesByType)
+        setPath(instance, expressionFieldPath, template)
+      }
+    })
+}
+
+
+/**
+ * Create template expressions for okta expression language references
+ */
+const filter: FilterCreator = () => ({
+  onFetch: async (elements: Element[]) => log.time(async () => {
+    await replaceFormulasWithTemplates(elements.filter(isInstanceElement))
+  }, 'Expression language filter'),
+})
+
+export default filter
---
+++ packages/okta-adapter/test/filters/expression_language.test.ts
@@ -0,0 +1,155 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ElemID, InstanceElement, ObjectType, ReferenceExpression, TemplateExpression, isInstanceElement } from '@salto-io/adapter-api'
+import { filterUtils } from '@salto-io/adapter-components'
+import { getFilterParams } from '../utils'
+import oktaExpressionLanguageFilter from '../../src/filters/expression_language'
+import { GROUP_RULE_TYPE_NAME, GROUP_TYPE_NAME, OKTA, POLICY_RULE_TYPE_NAME, USER_SCHEMA_TYPE_NAME } from '../../src/constants'
+
+
+describe('expression language filter', () => {
+      type FilterType = filterUtils.FilterWith<'onFetch' | 'onDeploy' | 'preDeploy'>
+      let filter: FilterType
+      const userSchemaType = new ObjectType({ elemID: new ElemID(OKTA, USER_SCHEMA_TYPE_NAME) })
+      const groupType = new ObjectType({ elemID: new ElemID(OKTA, GROUP_TYPE_NAME) })
+      const groupRuleType = new ObjectType({ elemID: new ElemID(OKTA, GROUP_RULE_TYPE_NAME) })
+      const policyRuleType = new ObjectType({ elemID: new ElemID(OKTA, POLICY_RULE_TYPE_NAME) })
+      const groupRuleWithTemplate = new InstanceElement(
+        'groupRuleTest',
+        groupRuleType,
+        {
+          conditions: {
+            expression: {
+              value: '(String.stringContains(user.department, "salto") OR isMemberOfGroupNameRegex("/.*admin.*")) AND isMemberOfAnyGroup("123") AND !isMemberOfAnyGroup("234", \'345\')',
+            },
+          },
+        }
+      )
+      const groupInstances = [
+        new InstanceElement('group1', groupType, { id: 123 },),
+        new InstanceElement('group2', groupType, { id: 234 },),
+        new InstanceElement('group3', groupType, { id: 345 },),
+      ]
+      const userSchemaInstance = new InstanceElement(
+        'user',
+        userSchemaType,
+        {
+          name: 'user',
+          definitions: {
+            custom: {
+              properties: {
+                additionalProperties: {
+                  saltoDepartment: {
+                    title: 'salto',
+                    type: 'string',
+                  },
+                },
+              },
+            },
+            base: {
+              properties: {
+                department: {
+                  title: 'Department',
+                  type: 'string',
+                },
+              },
+            },
+          },
+        }
+      )
+      const policyRuleInstance = new InstanceElement(
+        'policyRuleTest',
+        policyRuleType,
+        {
+          name: 'policy',
+          conditions: {
+            additionalProperties: {
+              elCondition: {
+                condition: 'user.profile.department == \'salto\' AND user.isMemberOf({\'group.id\': {"345", \'123\'}})',
+              },
+            },
+          },
+        }
+      )
+
+      beforeEach(() => {
+        filter = oktaExpressionLanguageFilter(getFilterParams()) as typeof filter
+      })
+
+      describe('onFetch', () => {
+        let elements: (InstanceElement | ObjectType)[]
+
+        it('should resolve templates in instances', async () => {
+          elements = [userSchemaType, groupType, groupRuleType, policyRuleInstance, policyRuleType,
+            groupRuleWithTemplate, ...groupInstances, userSchemaInstance]
+          await filter.onFetch(elements)
+          const groupRule = elements.filter(isInstanceElement).find(i => i.elemID.name === 'groupRuleTest')
+          expect(groupRule).toBeDefined()
+          expect(groupRule?.value?.conditions?.expression?.value).toEqual(new TemplateExpression({
+            parts: [
+              '(String.stringContains(',
+              new ReferenceExpression(userSchemaInstance.elemID, userSchemaInstance),
+              ', "salto")',
+            ],
+          }))
+          const policyRule = elements.filter(isInstanceElement).find(i => i.elemID.name === 'policyRuleTest')
+          expect(policyRule).toBeDefined()
+          expect(policyRule?.value?.conditions?.additionalProperties?.elCondition?.condition)
+            .toEqual(new TemplateExpression({
+              parts: [
+                new ReferenceExpression(userSchemaInstance.elemID, userSchemaInstance),
+                ' == "salto" AND user.isMemberOf({',
+                '"group.id": {"',
+                new ReferenceExpression(groupInstances[2].elemID, groupInstances[2]),
+                new ReferenceExpression(groupInstances[0].elemID, groupInstances[0]),
+                '})',
+              ],
+            }))
+        })
+
+        it('should not create references if there is no match', async () => {
+          const groupRuleWithMissingId = new InstanceElement(
+            'groupRuleWithMissingId',
+            groupRuleType,
+            {
+              conditions: {
+                expression: {
+                  value: 'isMemberOfAnyGroup("123", "555")',
+                },
+              },
+            }
+          )
+          elements = [groupRuleType, groupType, groupRuleWithMissingId, ...groupInstances]
+          await filter.onFetch(elements)
+          const groupRule = elements.filter(isInstanceElement).find(i => i.elemID.name === 'groupRuleWithMissingId')
+          expect(groupRule).toBeDefined()
+          expect(groupRule?.value?.conditions?.expression?.value).toEqual(
+            new TemplateExpression({
+              parts: [
+                'isMemberOfAnyGroup',
+                '("',
+                new ReferenceExpression(groupInstances[0].elemID, groupInstances),
+                ', "555")',
+              ],
+            })
+          )
+        })
+
+        // it('should not create template expression if expression path does not exist', () => {
+
+        // })
+      })
+})

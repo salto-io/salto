5527f9a1a7ca9b04a9cb6ed40a0016de94f08c65
idoser <idoser@gmail.com>
2023-02-01 12:27:45 +0200

    added comment

--- packages/okta-adapter/src/client/client.ts
+++ packages/okta-adapter/src/client/client.ts
@@ -49,6 +49,7 @@ export default class OktaClient extends clientUtils.AdapterHTTPClient<
         rateLimit: DEFAULT_MAX_CONCURRENT_API_REQUESTS,
         // TODO SALTO-2649: add better handling for rate limits
         maxRequestsPerMinute: DEFAULT_MAX_REQUESTS_PER_MINUTE,
+        // wait for 10s before trying again, change after SALTO-2649
         retry: { ...DEFAULT_RETRY_OPTS, retryDelay: 10000 },
       }
     )

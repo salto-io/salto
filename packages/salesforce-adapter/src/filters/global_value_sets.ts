b1fd3cb1b5030708b6a2ae6067fdde250d79e6ac
tamtamirr <92912659+tamtamirr@users.noreply.github.com>
2023-02-02 10:07:28 +0200

    SALTO-3479: Added restrictions on Picklist field values upon fetch (#3843)

--- packages/salesforce-adapter/src/filters/global_value_sets.ts
+++ packages/salesforce-adapter/src/filters/global_value_sets.ts
@@ -13,20 +13,51 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { Element, ObjectType, Field, ReferenceExpression, ElemID, isObjectType } from '@salto-io/adapter-api'
-import { multiIndex } from '@salto-io/lowerdash'
+import {
+  Element,
+  ObjectType,
+  Field,
+  ReferenceExpression,
+  isObjectType,
+  CORE_ANNOTATIONS,
+  createRestriction,
+  InstanceElement,
+  isInstanceElement, Values,
+} from '@salto-io/adapter-api'
+import { collections, multiIndex } from '@salto-io/lowerdash'
+import { logger } from '@salto-io/logging'
+import _ from 'lodash'
 import { FilterWith, LocalFilterCreator } from '../filter'
-import { VALUE_SET_FIELDS } from '../constants'
+import {
+  FIELD_ANNOTATIONS,
+  GLOBAL_VALUE_SET_METADATA_TYPE,
+  INSTANCE_FULL_NAME_FIELD,
+  VALUE_SET_FIELDS,
+} from '../constants'
 import { isCustomObject, apiName } from '../transformers/transformer'
 import { isInstanceOfType, buildElementsSourceForFetch } from './utils'
 
-export const GLOBAL_VALUE_SET = 'GlobalValueSet'
-export const CUSTOM_VALUE = 'customValue'
+const log = logger(module)
+const { awu } = collections.asynciterable
+const { makeArray } = collections.array
+
 export const MASTER_LABEL = 'master_label'
 
-const addGlobalValueSetRefToObject = (
+
+type GlobalValueSetValue = InstanceElement['value'] & {
+  [FIELD_ANNOTATIONS.CUSTOM_VALUE]: {
+    [INSTANCE_FULL_NAME_FIELD]: string
+  }[]
+}
+
+const isGlobalValueSetValue = (value: Values): value is GlobalValueSetValue => (
+  makeArray(value[FIELD_ANNOTATIONS.CUSTOM_VALUE])
+    .every(entry => _.isString(_.get(entry, INSTANCE_FULL_NAME_FIELD)))
+)
+
+const addRefAndRestrict = (
   object: ObjectType,
-  gvsToRef: multiIndex.Index<[string], ElemID>
+  globalValueSetInstanceByName: multiIndex.Index<[string], InstanceElement>
 ): void => {
   const getValueSetName = (field: Field): string | undefined =>
     field.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME]
@@ -37,10 +68,21 @@ const addGlobalValueSetRefToObject = (
       if (valueSetName === undefined) {
         return
       }
-      const valueSetId = gvsToRef.get(valueSetName)
-      if (valueSetId) {
-        f.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME] = new ReferenceExpression(valueSetId)
+      const globalValueSetInstance = globalValueSetInstanceByName.get(valueSetName)
+      if (globalValueSetInstance === undefined) {
+        log.warn('Could not find GlobalValueSet instance with name: %s', valueSetName)
+        return
       }
+      f.annotations[VALUE_SET_FIELDS.VALUE_SET_NAME] = new ReferenceExpression(globalValueSetInstance.elemID)
+      const { value: globalValueSetValue } = globalValueSetInstance
+      if (!isGlobalValueSetValue(globalValueSetValue)) {
+        log.warn('Could not create restriction for GlobalValueSet %s, due to unknown value format: %o', valueSetName, globalValueSetValue)
+        return
+      }
+      f.annotations[CORE_ANNOTATIONS.RESTRICTION] = createRestriction({
+        enforce_value: true,
+        values: globalValueSetValue.customValue.map(entry => entry[INSTANCE_FULL_NAME_FIELD]),
+      })
     })
 }
 
@@ -54,13 +96,14 @@ const filterCreator: LocalFilterCreator = ({ config }): FilterWith<'onFetch'> =>
   onFetch: async (elements: Element[]): Promise<void> => {
     const referenceElements = buildElementsSourceForFetch(elements, config)
     const valueSetNameToRef = await multiIndex.keyByAsync({
-      iter: await referenceElements.getAll(),
-      filter: isInstanceOfType(GLOBAL_VALUE_SET),
+      iter: awu(await referenceElements.getAll())
+        .filter(isInstanceElement)
+        .filter(isInstanceOfType(GLOBAL_VALUE_SET_METADATA_TYPE)),
       key: async inst => [await apiName(inst)],
-      map: inst => inst.elemID,
+      map: inst => inst,
     })
     const customObjects = elements.filter(isObjectType).filter(isCustomObject)
-    customObjects.forEach(object => addGlobalValueSetRefToObject(object, valueSetNameToRef))
+    customObjects.forEach(object => addRefAndRestrict(object, valueSetNameToRef))
   },
 })
 

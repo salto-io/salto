8550f5d6bea78c2254366cf1f55ef8f9adb9281d
tamir <tamir.ohayon@salto.io>
2023-02-07 15:57:18 +0200

    deleted unrelated files

--- packages/salesforce-adapter/src/filter.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.isLocalFilterCreator = void 0;
-var isLocalFilterCreator = function (filterDef) { return (filterDef.addsNewInformation !== true); };
-exports.isLocalFilterCreator = isLocalFilterCreator;

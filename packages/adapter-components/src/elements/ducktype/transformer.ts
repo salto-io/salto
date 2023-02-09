1a2362c84ddc75410a52777db95664fe74535eb0
Lior Neudorfer <liorus@gmail.com>
2023-02-09 12:18:34 +0200

    SALTO-3618 Change CV text to include disabled features (#3943)
    * Change 403 CV text to include disabled features
    
    * Fixing string characters
    
    * Changing string structure to "

--- packages/adapter-components/src/elements/ducktype/transformer.ts
+++ packages/adapter-components/src/elements/ducktype/transformer.ts
@@ -389,7 +389,7 @@ export const getAllElements = async ({
         }
         if (e.response?.status === 403) {
           const newError: SaltoError = {
-            message: `Salto was forbidden from accessing the ${args.typeName} resource. Elements from that type were not fetched. Please make sure that the supplied user credentials have sufficient permissions to access this data, and try again. Learn more at https://help.salto.io/en/articles/6947061-salto-was-forbidden-from-accessing-the-resource`,
+            message: `Salto could not access the ${args.typeName} resource. Elements from that type were not fetched. Please make sure that this type is enabled in your service, and that the supplied user credentials have sufficient permissions to access this data. You can also exclude this data from Salto's fetches by changing the environment configuration. Learn more at https://help.salto.io/en/articles/6947061-salto-could-not-access-the-resource`,
             severity: 'Warning',
           }
           return { elements: [], errors: [newError] }

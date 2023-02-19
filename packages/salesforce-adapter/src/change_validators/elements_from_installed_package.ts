dc6cb6ee9b13e4534bba6e63ca088dbc44c8cc9d
tamir <tamir.ohayon@salto.io>
2023-02-19 16:47:28 +0200

    Implemented the change validator

--- packages/salesforce-adapter/src/change_validators/elements_from_installed_package.ts
+++ packages/salesforce-adapter/src/change_validators/elements_from_installed_package.ts
@@ -13,11 +13,70 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { ChangeValidator } from '@salto-io/adapter-api'
+import {
+  ChangeError,
+  ChangeValidator,
+  CORE_ANNOTATIONS,
+  Element,
+  getChangeData,
+  isAdditionOrModificationChange,
+  isInstanceElement,
+} from '@salto-io/adapter-api'
+import { collections, types, values } from '@salto-io/lowerdash'
+import _ from 'lodash'
+import { isSubscriberPackageInstance, isToolingInstance, ToolingInstance } from '../tooling/types'
 
-const changeValidator: ChangeValidator = async changes => {
-  console.log(changes)
-  return []
+const { awu } = collections.asynciterable
+const { makeArray } = collections.array
+const { isNonEmptyArray } = types
+const { isDefined } = values
+
+const getDependentPackages = (element: Element): ToolingInstance['SubscriberPackage'][] => (
+  makeArray(element.annotations[CORE_ANNOTATIONS.GENERATED_DEPENDENCIES])
+    .map(dependency => dependency.reference)
+    .filter(isInstanceElement)
+    .filter(isToolingInstance)
+    .filter(isSubscriberPackageInstance)
+)
+
+const createMissingNamespacesChangeError = (
+  { elemID }: Element,
+  missingPackageNamespaces: types.NonEmptyArray<string>
+): ChangeError => ({
+  elemID,
+  severity: 'Error',
+  message: 'Element is dependent on non installed package',
+  detailedMessage: `Missing dependent package namespaces: ${missingPackageNamespaces.join(', ')}`,
+})
+
+const changeValidator: ChangeValidator = async (changes, elementsSource) => {
+  if (elementsSource === undefined) {
+    return []
+  }
+  const relevantChanges = changes
+    .filter(isAdditionOrModificationChange)
+    .filter(change => getDependentPackages(getChangeData(change)).length > 0)
+  const dependentPackages = relevantChanges
+    .flatMap(change => getDependentPackages(getChangeData(change)))
+  const missingPackages = await awu(dependentPackages)
+    .filter(async dependentPackage => !(await elementsSource.get(dependentPackage.elemID)))
+    .toArray()
+
+  if (_.isEmpty(missingPackages)) {
+    return []
+  }
+
+  return relevantChanges
+    .map(getChangeData)
+    .map(element => {
+      const missingPackageNamespaces = getDependentPackages(element)
+        .filter(dependentPackage => missingPackages.includes(dependentPackage))
+        .map(dependentPackage => dependentPackage.value.NamespacePrefix)
+      return isNonEmptyArray(missingPackageNamespaces)
+        ? createMissingNamespacesChangeError(element, missingPackageNamespaces)
+        : undefined
+    })
+    .filter(isDefined)
 }
 
 export default changeValidator

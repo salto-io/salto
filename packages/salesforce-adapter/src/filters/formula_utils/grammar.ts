89d54c9c530eb0899ff9e92f1a015906b5dec1ed
Tom Sellek <tom.sellek@salto.io>
2023-02-21 15:30:22 -0500

    Use existing constants where possible

--- packages/salesforce-adapter/src/filters/formula_utils/grammar.ts
+++ packages/salesforce-adapter/src/filters/formula_utils/grammar.ts
@@ -14,24 +14,28 @@
 * limitations under the License.
 */
 
-import { $, getField, getObject } from './utils'
+import { $, getField, getObject, parts } from './utils'
+import { CPQ_NAMESPACE, CUSTOM_METADATA_SUFFIX, NAMESPACE_SEPARATOR, SALESFORCE_CUSTOM_SUFFIX } from '../../constants'
 
-export const RELATIONSHIP_SUFFIX = '__R'
-export const USER_FIELDS = ['OWNER', 'MANAGER', 'CREATEDBY', 'LASTMODIFIEDBY']
-export const CPQ_NAMESPACE = 'SBQQ__'
-export const CUSTOM_METADATA_PREFIX = '__MDT'
-export const CUSTOM_LABEL_PREFIX = '$LABEL.'
-export const CUSTOM_SETTING_PREFIX = '$SETUP.'
-export const CUSTOM_ENTITY_SUFFIX = '__C'
-export const OBJECT_TYPE_PREFIX = '$OBJECTTYPE.'
-export const SELF_REFERENTIAL_PARENT_FIELD = 'PARENTID'
-export const SELF_REFERENTIAL_PARENT_OBJECT = 'PARENT'
-export const PROCESS_BUILDER_BRACKET_START = '['
-export const PROCESS_BUILDER_BRACKET_END = ']'
-export const SPECIAL_PREFIXES = ['$USER', '$PROFILE', '$ORGANIZATION', '$USERROLE', '$SYSTEM']
+const RELATIONSHIP_SUFFIX = '__R'
+const USER_FIELDS = ['OWNER', 'MANAGER', 'CREATEDBY', 'LASTMODIFIEDBY']
+const CUSTOM_LABEL_PREFIX = '$LABEL.'
+const CUSTOM_SETTING_PREFIX = '$SETUP.'
+const OBJECT_TYPE_PREFIX = '$OBJECTTYPE.'
+const SELF_REFERENTIAL_PARENT_FIELD = 'PARENTID'
+const SELF_REFERENTIAL_PARENT_OBJECT = 'PARENT'
+const PROCESS_BUILDER_BRACKET_START = '['
+const PROCESS_BUILDER_BRACKET_END = ']'
+const SPECIAL_PREFIXES = ['$USER', '$PROFILE', '$ORGANIZATION', '$USERROLE', '$SYSTEM']
+
+
 export const isUserField = (value: string): boolean => USER_FIELDS.includes($(getObject(value)))
-export const isCustom = (value: string): boolean => $(value).endsWith(CUSTOM_ENTITY_SUFFIX)
-export const isCustomMetadata = (value: string): boolean => $(value).includes(CUSTOM_METADATA_PREFIX)
+
+export const isCustom = (value: string): boolean => value.toLocaleLowerCase().endsWith(SALESFORCE_CUSTOM_SUFFIX)
+
+export const isCustomMetadata = (value: string): boolean => parts(value.toLocaleLowerCase())
+  .some(part => part.includes(CUSTOM_METADATA_SUFFIX))
+
 export const isCustomLabel = (value: string): boolean => $(value).startsWith(CUSTOM_LABEL_PREFIX)
 export const isCustomSetting = (value: string): boolean => $(value).startsWith(CUSTOM_SETTING_PREFIX)
 export const isObjectType = (value: string): boolean => $(value).startsWith(OBJECT_TYPE_PREFIX)
@@ -46,5 +50,5 @@ export const isProcessBuilderPrefix = (value: string): boolean => (
 export const isCPQRelationship = (value: string): boolean => {
   const obj = $(getObject(value))
 
-  return obj.startsWith(CPQ_NAMESPACE) && obj.endsWith(RELATIONSHIP_SUFFIX)
+  return obj.startsWith(`${CPQ_NAMESPACE}${NAMESPACE_SEPARATOR}`) && obj.endsWith(RELATIONSHIP_SUFFIX)
 }

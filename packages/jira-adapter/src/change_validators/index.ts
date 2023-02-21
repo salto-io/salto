360cd2715c89f4c8563b92274df42de034036cc5
guyhamo <56266307+guyhamo@users.noreply.github.com>
2023-02-21 17:37:10 +0200

    SALTO-3602/issue type scheme migration (#3987)

--- packages/jira-adapter/src/change_validators/index.ts
+++ packages/jira-adapter/src/change_validators/index.ts
@@ -51,6 +51,7 @@ import { activeSchemeDeletionValidator } from './active_scheme_deletion'
 import { automationProjectUnresolvedReferenceValidator } from './automation_unresolved_references'
 import { unresolvedReferenceValidator } from './unresolved_references'
 import { sameIssueTypeNameChangeValidator } from './same_issue_type_name'
+import { issueTypeSchemeMigrationValidator } from './issue_type_scheme_migration'
 import { issueTypeDeletionValidator } from './issue_type_deletion'
 
 const {
@@ -85,6 +86,7 @@ export default (
     statusMigrationChangeValidator,
     // Must run after statusMigrationChangeValidator
     workflowSchemeMigrationValidator(client, config, paginator),
+    issueTypeSchemeMigrationValidator(client),
     activeSchemeChangeValidator(client),
     maskingValidator(client),
     issueTypeDeletionValidator(client),

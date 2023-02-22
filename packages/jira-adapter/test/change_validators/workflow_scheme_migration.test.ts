353ce7942ecdb2dd853f2af56a27b52fdebb3b73
guy hamo <guyhamo1@gmail.com>
2023-02-22 22:47:32 +0200

    fixed conflict

--- packages/jira-adapter/test/change_validators/workflow_scheme_migration.test.ts
+++ packages/jira-adapter/test/change_validators/workflow_scheme_migration.test.ts
@@ -214,7 +214,6 @@ describe('workflow scheme migration', () => {
     const errors = await validator([toChange({ before: workflowInstance, after: modifiedInstance })], elementSource)
     expect(errors).toHaveLength(0)
   })
-<<<<<<< Updated upstream
   it('should not throw on unresolved reference', async () => {
     modifiedInstance.value.items.push(
       {
@@ -222,11 +221,12 @@ describe('workflow scheme migration', () => {
         issueType: new ReferenceExpression(new ElemID(JIRA, 'IssueType', 'instance', 'issueType5')),
       },
     )
-=======
+    const errorsPromise = validator([toChange({ before: workflowInstance, after: modifiedInstance })], elementSource)
+    await expect(errorsPromise).resolves.not.toThrow()
+  })
   it('should not throw if there are no items at all', async () => {
     workflowInstance.value.items = undefined
     modifiedInstance.value.items = undefined
->>>>>>> Stashed changes
     const errorsPromise = validator([toChange({ before: workflowInstance, after: modifiedInstance })], elementSource)
     await expect(errorsPromise).resolves.not.toThrow()
   })

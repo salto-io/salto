ec7b152f98351b883f2189d1e0ee1f026f7c07cd
idoser <idoser@gmail.com>
2023-02-23 11:03:55 +0200

    Changed the filter to a validator that warns the user, until we have more information on the source of the issue

--- packages/zendesk-adapter/src/change_validators/bad_format_webhook_action.ts
+++ packages/zendesk-adapter/src/change_validators/bad_format_webhook_action.ts
@@ -13,6 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+
 import _ from 'lodash'
 import {
   ChangeError,
@@ -22,42 +23,33 @@ import {
   isInstanceChange,
 } from '@salto-io/adapter-api'
 import { values as lowerDashValues } from '@salto-io/lowerdash'
-import joi from 'joi'
-import { AUTOMATION_TYPE_NAME, TRIGGER_TYPE_NAME } from '../constants'
 
 const { isDefined } = lowerDashValues
 
-type NotificationWebhookAction = {
+type Action = {
     field: string
     value: unknown
 }
 
-const notificationWebhookAction = joi.object({
-  field: joi.string().valid('notification_webhook').required(),
-  value: joi.any().required(),
-}).unknown(true)
+const isAction = (value: unknown): value is Action => (
+  _.isObject(value) && 'field' in value && 'value' in value
+)
 
-const isNotificationWebhookAction = (value: unknown): value is NotificationWebhookAction =>
-  notificationWebhookAction.validate(value).error === undefined
+const POTENTIAL_BAD_FORMAT_TYPES = ['automation', 'trigger']
 
-const POTENTIAL_BAD_FORMAT_TYPES = [AUTOMATION_TYPE_NAME, TRIGGER_TYPE_NAME]
-
-/**
- * Validate that automation and trigger with notification_webhook actions, have the correct value type of an array.
- */
 export const badFormatWebhookActionValidator : ChangeValidator = async changes =>
   changes.filter(isInstanceChange).filter(isAdditionOrModificationChange)
     .map(getChangeData).filter(instance => POTENTIAL_BAD_FORMAT_TYPES.includes(instance.elemID.typeName))
     .filter(instance => _.isArray(instance.value.actions))
     .map((instance) : ChangeError | undefined => {
-      const webhookActions = instance.value.actions.filter(isNotificationWebhookAction)
-      if (webhookActions.some((action: NotificationWebhookAction) => !_.isArray(action.value))) {
+      const webhookActions = instance.value.actions.filter(isAction).filter((action: Action) => action.field === 'notification_webhook')
+      if (webhookActions.some((action: Action) => !_.isArray(action.value))) {
         const { typeName } = instance.elemID
         return {
           elemID: instance.elemID,
           severity: 'Warning',
-          message: `${typeName} instance has unexpected structure and might not work properly`,
-          detailedMessage: `The instance have an action of notification_webhook with a value in a bad format (should be an array). This might cause the ${typeName} to not work properly.`,
+          message: `${typeName} instance might not work properly`,
+          detailedMessage: `The element have an action with of notification_webhook with a value in a bad format. This might cause the ${typeName} to not work properly.`,
         }
       }
       return undefined

b90ce37ba0ef99f491d1d430e1f95388e97f1b2f
idoser <idoser@gmail.com>
2023-02-23 11:05:49 +0200

    revert merge

--- packages/zendesk-adapter/src/adapter.ts
+++ packages/zendesk-adapter/src/adapter.ts
@@ -116,7 +116,6 @@ import supportAddress from './filters/support_address'
 import customStatus from './filters/custom_statuses'
 import organizationsFilter from './filters/organizations'
 import hideAccountFeatures from './filters/hide_account_features'
-import auditTimeFilter from './filters/audit_logs'
 
 const { makeArray } = collections.array
 const log = logger(module)
@@ -155,7 +154,6 @@ export const DEFAULT_FILTERS = [
   restrictionFilter,
   organizationFieldFilter,
   hardcodedChannelFilter,
-  auditTimeFilter, // needs to be before userFilter as it uses the ids of the users
   // removeDefinitionInstancesFilter should be after hardcodedChannelFilter
   removeDefinitionInstancesFilter,
   usersFilter,

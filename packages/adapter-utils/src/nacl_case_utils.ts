e2c6fe75994ff6bd62b4b0d4217ca224f28c8433
Eden Hassid <edenhassid@gmail.com>
2023-02-23 12:35:58 +0200

    add prettify

--- packages/adapter-utils/src/nacl_case_utils.ts
+++ packages/adapter-utils/src/nacl_case_utils.ts
@@ -159,13 +159,55 @@ export const invertNaclCase = (name: string): string => {
   if (!suffix) {
     return prefix
   }
-  const [suffixCode, ...rest] = suffix.split('.')
-  const specialCharsMappingList = listFromSuffix(suffixCode)
-  const updatedPrefix = prefix.replace(/_/g, () =>
+  const specialCharsMappingList = listFromSuffix(suffix)
+  return prefix.replace(/_/g, () =>
     (specialCharsMappingList.length === 1
       ? specialCharsMappingList[0]
-      : specialCharsMappingList.shift() ?? ''),)
-  return [updatedPrefix, ...rest].filter(Boolean).join('.')
+      : specialCharsMappingList.shift() ?? ''))
+}
+
+const prettifyWord = (str: string): string[] => {
+  if (allCapsRegex.test(str)) {
+    return [str]
+  }
+  let result = str
+  if (camelCaseRegex.test(str)) {
+    result = str.replace(camelCaseRegex, ([lower, upper]) => [lower, upper].join(' '))
+  }
+  if (allCapsCamelCaseRegex.test(result)) {
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    result = result.replace(allCapsCamelCaseRegex, (match, subMatch) => `${match[0]} ${subMatch}`)
+  }
+
+  return result.split(' ')
+}
+
+
+const recapitalize = (str: string): string => str.slice(0, 1).toUpperCase() + str.slice(1)
+
+
+/**
+ * name is a single namePart from the elemId
+ */
+export const prettifyName = (name: string): string => {
+  const cleanNaclCase = invertNaclCase(name)
+
+  // if ([ZUORA, JIRA].includes(adapter)) {
+  //   cleanNaclCase = cleanCustomSuffix(cleanNaclCase)
+  // }
+
+  if (cleanNaclCase.includes(' ')) {
+    return cleanNaclCase
+  }
+  // if ([NETSUITE].includes(adapter)) {
+  //   return cleanNaclCase
+  // }
+  // if (SALESFORCE === adapter && name.includes('__')) {
+  //   return cleanNaclCase
+  // }
+
+  const words = cleanNaclCase.split('_').flatMap(prettifyWord).map(recapitalize)
+  return words.join(' ')
 }
 
 const prettifyWord = (str: string): string[] => {

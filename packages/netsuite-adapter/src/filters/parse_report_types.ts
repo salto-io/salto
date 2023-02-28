e250c19153e86cc01eb838ad4233394387248d2b
dantal4 <70883080+dantal4@users.noreply.github.com>
2023-02-28 20:16:12 +0200

    SALTO-3725 improve parse-report-types filter (#4034)

--- packages/netsuite-adapter/src/filters/parse_report_types.ts
+++ packages/netsuite-adapter/src/filters/parse_report_types.ts
@@ -73,8 +73,9 @@ const filterCreator: FilterCreator = ({ elementsSource }) => ({
       const parsedInstance = await parser(instance.value[layoutOrDefinition])
       Object.assign(instance.value, parsedInstance)
       if (oldInstance?.value[layoutOrDefinition] !== undefined) {
-        if (_.isEqual(await parser(oldInstance.value[layoutOrDefinition]),
-          parsedInstance)) {
+        const oldType = typeNameToOldType[instance.elemID.typeName]
+        const oldParsedInstance = _.omitBy(oldInstance.value, (_val, key) => oldType.fields[key] !== undefined)
+        if (_.isEqual(oldParsedInstance, parsedInstance)) {
           // In case the parsed definitions are equal that mean there is no reason
           // to change the definition string and create a change in the file.
           instance.value[layoutOrDefinition] = oldInstance.value[layoutOrDefinition]

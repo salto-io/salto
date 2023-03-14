349e06f0e54a92483107fc3dcdce4a17486c3f90
tamtamirr <92912659+tamtamirr@users.noreply.github.com>
2023-03-14 11:12:44 +0200

    SALTO-3794: CustomMetadata type deployment bugfix (#4080)

--- packages/salesforce-adapter/src/filters/custom_metadata_to_object_type.ts
+++ packages/salesforce-adapter/src/filters/custom_metadata_to_object_type.ts
@@ -27,7 +27,8 @@ import { logger } from '@salto-io/logging'
 import { FilterWith, LocalFilterCreator } from '../filter'
 import {
   CUSTOM_METADATA,
-  CUSTOM_METADATA_SUFFIX, CUSTOM_OBJECT,
+  CUSTOM_METADATA_SUFFIX,
+  CUSTOM_OBJECT,
 } from '../constants'
 import { createCustomObjectChange, createCustomTypeFromCustomObjectInstance } from './custom_objects_to_object_type'
 import { apiName } from '../transformers/transformer'

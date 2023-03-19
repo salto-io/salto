fb3bad816a5c1716c5c078bdc338d2d4bd77f769
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2023-03-19 18:47:23 +0200

    SALTO-3743: Hide name for custom applications in Okta  (#4104)
    * hideNameForCustomApp

--- packages/okta-adapter/src/filters/app_deployment.ts
+++ packages/okta-adapter/src/filters/app_deployment.ts
@@ -14,15 +14,21 @@
 * limitations under the License.
 */
 import _ from 'lodash'
-import { Change, InstanceElement, isInstanceChange, getChangeData, isAdditionOrModificationChange, isAdditionChange, AdditionChange } from '@salto-io/adapter-api'
-import { config as configUtils, deployment } from '@salto-io/adapter-components'
-import { APPLICATION_TYPE_NAME, INACTIVE_STATUS } from '../constants'
+import Joi from 'joi'
+import { Change, InstanceElement, Element, isInstanceChange, getChangeData, isAdditionOrModificationChange, isAdditionChange, AdditionChange, isInstanceElement, ElemID, ReadOnlyElementsSource, Values, isModificationChange } from '@salto-io/adapter-api'
+import { config as configUtils } from '@salto-io/adapter-components'
+import { logger } from '@salto-io/logging'
+import { createSchemeGuard } from '@salto-io/adapter-utils'
+import { APPLICATION_TYPE_NAME, INACTIVE_STATUS, OKTA, ORG_SETTING_TYPE_NAME, CUSTOM_NAME_FIELD } from '../constants'
 import OktaClient from '../client/client'
 import { OktaConfig, API_DEFINITIONS_CONFIG } from '../config'
 import { FilterCreator } from '../filter'
 import { deployChanges, defaultDeployChange, deployEdges } from '../deployment'
 
-export const AUTO_LOGIN_APP = 'AUTO_LOGIN'
+const log = logger(module)
+
+const AUTO_LOGIN_APP = 'AUTO_LOGIN'
+const SAML_2_0_APP = 'SAML_2_0'
 const APP_ASSIGNMENT_FIELDS: Record<string, configUtils.DeploymentRequestsByAction> = {
   assignedGroups: {
     add: {
@@ -48,59 +54,87 @@ const APP_ASSIGNMENT_FIELDS: Record<string, configUtils.DeploymentRequestsByActi
   },
 }
 
+type Application = {
+  id: string
+  name: string
+  signOnMode: string
+}
+
+const EXPECTED_APP_SCHEMA = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+  signOnMode: Joi.string().required(),
+}).unknown(true)
+
+export const isAppResponse = createSchemeGuard<Application>(EXPECTED_APP_SCHEMA, 'Received an invalid application response')
+
+const isCustomApp = (value: Values, subdomain: string): boolean => (
+  [AUTO_LOGIN_APP, SAML_2_0_APP].includes(value.signOnMode)
+  && value.name !== undefined
+  // custom app names starts with subdomain and '_'
+  && _.startsWith(value.name, `${subdomain}_`)
+)
+
 // Set fields that are created by the service to the returned app instance
 const assignCreatedFieldsToApp = (
   change: AdditionChange<InstanceElement>,
-  response: deployment.ResponseResult,
+  appResponse: Application,
+  subdomain?: string,
 ): void => {
   const instance = getChangeData(change)
-  const signing = _.get(response, ['credentials', 'signing'])
+  const signing = _.get(appResponse, ['credentials', 'signing'])
   if (_.isPlainObject(signing)) {
     _.set(instance, ['value', 'credentials', 'signing'], signing)
   }
-  if (instance.value.signOnMode === AUTO_LOGIN_APP) {
-    const createdAppName = _.get(response, ['name'])
-    if (_.isString(createdAppName)) {
-      _.set(instance, ['value', 'name'], createdAppName)
-    }
+  // Assign created name field which is not multi-env in custom apps to customName field, which is hidden
+  if (subdomain !== undefined && isCustomApp(appResponse, subdomain)) {
+    const createdAppName = appResponse.name
+    _.set(instance, ['value', CUSTOM_NAME_FIELD], createdAppName)
   }
   if (instance.value.licensing !== undefined) {
-    const licensing = _.get(response, ['licensing'])
+    const licensing = _.get(appResponse, ['licensing'])
     if (_.isPlainObject(licensing)) {
       _.set(instance, ['value', 'licensing'], licensing)
     }
   }
 }
 
+const getSubdomainFromElementsSource = async (elementsSource: ReadOnlyElementsSource): Promise<string | undefined> => {
+  const orgSettingInstance = await elementsSource.get(
+    new ElemID(OKTA, ORG_SETTING_TYPE_NAME, 'instance', ElemID.CONFIG_NAME)
+  )
+  if (!isInstanceElement(orgSettingInstance)) {
+    log.error(`Failed to get ${ORG_SETTING_TYPE_NAME} instance, can not find subdomain`)
+    return undefined
+  }
+  return orgSettingInstance.value.subdomain
+}
 
 // TODO SALTO-2736 : adjust to support addition of more application types
 const deployApp = async (
   change: Change<InstanceElement>,
   client: OktaClient,
   config: OktaConfig,
+  subdomain?: string,
 ): Promise<void> => {
-  const getAdditionalFieldsToIgnore = (instance: InstanceElement): string[] =>
-    (instance.value.signOnMode === AUTO_LOGIN_APP ? ['name'] : [])
-
   const fieldsToIgnore = [
     ...Object.keys(APP_ASSIGNMENT_FIELDS),
     // TODO SALTO-2690: remove this once completed
-    'id', 'created', 'lastUpdated', 'licensing', '_links', '_embedded',
+    'id', 'created', 'lastUpdated', 'licensing', '_links', '_embedded', CUSTOM_NAME_FIELD,
   ]
 
   const response = await defaultDeployChange(
     change,
     client,
     config[API_DEFINITIONS_CONFIG],
-    isAdditionChange(change)
-      ? fieldsToIgnore.concat(getAdditionalFieldsToIgnore(getChangeData(change))) : fieldsToIgnore,
+    fieldsToIgnore,
     // Application is created with status 'ACTIVE' by default, unless we provide activate='false' as a query param
-    isAdditionChange(change) && getChangeData(change).value.status === INACTIVE_STATUS ? { activate: 'false' } : undefined
+    isAdditionChange(change) && getChangeData(change).value?.status === INACTIVE_STATUS ? { activate: 'false' } : undefined
   )
 
   if (isAdditionOrModificationChange(change)) {
-    if (isAdditionChange(change)) {
-      assignCreatedFieldsToApp(change, response)
+    if (isAdditionChange(change) && isAppResponse(response)) {
+      assignCreatedFieldsToApp(change, response, subdomain)
     }
     await deployEdges(change, APP_ASSIGNMENT_FIELDS, client)
   }
@@ -110,24 +144,72 @@ const deployApp = async (
  * Application type is deployed separately to update application's status,
  * application's assigned group and application's policies
  */
-const filterCreator: FilterCreator = ({ client, config }) => ({
+const filterCreator: FilterCreator = ({ elementsSource, client, config }) => ({
   name: 'appDeploymentFilter',
+  onFetch: async (elements: Element[]) => {
+    const instances = elements.filter(isInstanceElement)
+    const appInstances = instances
+      .filter(instance => instance.elemID.typeName === APPLICATION_TYPE_NAME)
+    // OrgSetting is settings type
+    const orgInstance = instances.find(instance => instance.elemID.typeName === ORG_SETTING_TYPE_NAME)
+    const subdomain = orgInstance?.value?.subdomain
+    if (!_.isString(subdomain)) {
+      log.error('Could not create customName field for custom apps because subdomain was missing')
+      return
+    }
+    // create customName field for non custom apps and delete name field as its value is not multienv
+    appInstances.forEach(app => {
+      if (isCustomApp(app.value, subdomain)) {
+        app.value.customName = app.value.name
+        delete app.value.name
+      }
+    })
+  },
+  preDeploy: async (changes: Change<InstanceElement>[]) => {
+    changes
+      .filter(isModificationChange)
+      .map(getChangeData)
+      .filter(isInstanceElement)
+      .filter(instance => instance.elemID.typeName === APPLICATION_TYPE_NAME)
+      .forEach(
+        instance => {
+          const { customName } = instance.value
+          if (customName !== undefined) {
+            instance.value.name = customName
+          }
+        }
+      )
+  },
   deploy: async changes => {
     const [relevantChanges, leftoverChanges] = _.partition(
       changes,
       change => isInstanceChange(change)
             && getChangeData(change).elemID.typeName === APPLICATION_TYPE_NAME
     )
-
+    const subdomain = await getSubdomainFromElementsSource(elementsSource)
     const deployResult = await deployChanges(
       relevantChanges.filter(isInstanceChange),
-      async change => deployApp(change, client, config)
+      async change => deployApp(change, client, config, subdomain)
     )
 
     return {
       leftoverChanges,
       deployResult,
     }
+  },
+  onDeploy: async (changes: Change<InstanceElement>[]) => {
+    changes
+      .map(getChangeData)
+      .filter(isInstanceElement)
+      .filter(instance => instance.elemID.typeName === APPLICATION_TYPE_NAME)
+      .forEach(
+        instance => {
+          const { customName } = instance.value
+          if (customName !== undefined) {
+            delete instance.value.name
+          }
+        }
+      )
   },
 })
 

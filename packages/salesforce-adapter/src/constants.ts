eb1fed9940f46ee179a696168f3350dd4d2a76f7
tamir <tamir.ohayon@salto.io>
2023-03-26 19:22:44 +0300

    Removed default RSS filter

--- packages/salesforce-adapter/src/constants.ts
+++ packages/salesforce-adapter/src/constants.ts
@@ -373,7 +373,6 @@ export const CUSTOM_METADATA = 'CustomMetadata'
 export const FLOW_DEFINITION_METADATA_TYPE = 'FlowDefinition'
 export const INSTALLED_PACKAGE_METADATA = 'InstalledPackage'
 export const ACCOUNT_SETTINGS_METADATA_TYPE = 'AccountSettings'
-export const ACTIVATE_RSS = 'activateRSS'
 export const GLOBAL_VALUE_SET_METADATA_TYPE = 'GlobalValueSet'
 
 // Artifitial Types

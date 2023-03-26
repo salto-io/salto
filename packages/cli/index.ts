fa7792bcfc3c01d2ed1873677482fbc6c6d8d11b
Yuri Bronshtein <yuri.bronshtein@salto.io>
2023-03-26 17:02:31 +0300

    fix package.json, coverage

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -13,5 +13,5 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-export { formatWordsSeries } from './src/formatter'
+export * as formatter from './src/formatter'
 export { default as Prompts } from './src/prompts'
--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -35,8 +35,8 @@ module.exports = deepMerge(
       global: {
         branches: 81.09,
         functions: 89.75,
-        lines: 95.01,
-        statements: 94.85,
+        lines: 94.95,
+        statements: 94.75,
       },
     },
   }

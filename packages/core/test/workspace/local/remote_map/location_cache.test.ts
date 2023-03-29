200311c1405a5b73a595e8f2321d5a3ab2c429ee
Tom Sellek <tom.sellek@salto.io>
2023-03-29 11:28:47 -0400

    Allow the user to pass the pool contents.

--- packages/core/test/workspace/local/remote_map/location_cache.test.ts
+++ packages/core/test/workspace/local/remote_map/location_cache.test.ts
@@ -15,35 +15,35 @@
 */
 
 import _ from 'lodash'
-import { LocationCachePool, createLocationCachePool } from '../../../../src/local-workspace/remote_map/location_cache'
-import { counters } from '../../../../src/local-workspace/remote_map/counters'
+import {
+  LocationCachePool,
+  createLocationCachePool,
+  LocationCachePoolContents, LocationCache,
+} from '../../../../src/local-workspace/remote_map/location_cache'
 
 describe('remote map location cache pool', () => {
+  let poolContents: LocationCachePoolContents
   let pool: LocationCachePool
   const LOCATION1 = 'SomeLocation'
   const LOCATION2 = 'SomeOtherLocation'
 
   beforeEach(() => {
-    pool = createLocationCachePool()
-  })
-
-  afterEach(() => {
-    [LOCATION1, LOCATION2].forEach(loc => counters.deleteLocation(loc))
+    poolContents = new Map<string, { cache: LocationCache; refcnt: number }>()
+    pool = createLocationCachePool(poolContents)
   })
 
   it('should create a location cache for the right location', () => {
     const cache = pool.get(LOCATION1, 5000)
     expect(cache.location).toEqual(LOCATION1)
-    expect(counters.locationCounters(LOCATION1).LocationCacheCreated.value()).toEqual(1)
-    expect(counters.locationCounters(LOCATION1).LocationCacheReuse.value()).toEqual(0)
-    pool.release(cache)
+    expect(poolContents.size).toEqual(1)
+    expect(poolContents.get(LOCATION1)).toBeDefined()
+    expect(poolContents.get(LOCATION1)?.refcnt).toEqual(1)
   })
 
   it('should reuse caches where possible', () => {
-    const caches = _.times(10, () => pool.get(LOCATION1, 5000))
-    expect(counters.locationCounters(LOCATION1).LocationCacheCreated.value()).toEqual(1)
-    expect(counters.locationCounters(LOCATION1).LocationCacheReuse.value()).toEqual(caches.length - 1)
-    caches.forEach(cache => pool.release(cache))
+    _.times(10, () => pool.get(LOCATION1, 5000))
+    expect(poolContents.size).toEqual(1)
+    expect(poolContents.get(LOCATION1)?.refcnt).toEqual(10)
   })
 
   it('should not reuse caches of a different location', () => {
@@ -51,19 +51,17 @@ describe('remote map location cache pool', () => {
     const anotherCache = pool.get(LOCATION2, 5000)
     expect(cache.location).toEqual(LOCATION1)
     expect(anotherCache.location).toEqual(LOCATION2)
-    expect(counters.locationCounters(LOCATION1).LocationCacheCreated.value()).toEqual(1)
-    expect(counters.locationCounters(LOCATION2).LocationCacheCreated.value()).toEqual(1)
-    expect(counters.locationCounters(LOCATION1).LocationCacheReuse.value()).toEqual(0)
-    pool.release(cache)
-    pool.release(anotherCache)
+    expect(poolContents.size).toEqual(2)
+    expect(poolContents.get(LOCATION1)?.refcnt).toEqual(1)
+    expect(poolContents.get(LOCATION2)?.refcnt).toEqual(1)
   })
 
   it('should destroy cache when the last reference to it is returned', () => {
     const cache = pool.get(LOCATION1, 5000)
     pool.release(cache)
-    const anotherCache = pool.get(LOCATION1, 5000)
-    expect(counters.locationCounters(LOCATION1).LocationCacheCreated.value()).toEqual(2)
-    expect(counters.locationCounters(LOCATION1).LocationCacheReuse.value()).toEqual(0)
-    pool.release(anotherCache)
+    expect(poolContents.size).toEqual(0)
+    pool.get(LOCATION1, 5000)
+    expect(poolContents.size).toEqual(1)
+    expect(poolContents.get(LOCATION1)?.refcnt).toEqual(1)
   })
 })

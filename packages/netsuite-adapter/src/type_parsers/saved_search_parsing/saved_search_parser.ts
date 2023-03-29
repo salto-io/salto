942ae517edc97ea9ce9e81dbd2344edca828016f
dantal4 <70883080+dantal4@users.noreply.github.com>
2023-03-29 10:41:47 +0300

    Handle undefined values in report types (#4176)

--- packages/netsuite-adapter/src/type_parsers/saved_search_parsing/saved_search_parser.ts
+++ packages/netsuite-adapter/src/type_parsers/saved_search_parsing/saved_search_parser.ts
@@ -26,57 +26,64 @@ import { ElementParts,
   getObjectFromValues,
   getFlags,
   extractRecordsValues,
-  getDefinitionOrLayout } from '../../report_types_parser_utils'
+  getDefinitionOrLayout } from '../report_types_parser_utils'
 import { ParsedSavedSearchType } from './parsed_saved_search'
 
-type FilterObject = { descriptor: { values: { Value: AttributeObject[] }}
- values: { values: {Record: RecordObject | RecordObject[]} }}
-
-const getFilterRecords = (filter: FilterObject): Values[] =>
-  collections.array.makeArray(filter.values.values?.Record)
-    .map(record => getObjectFromValues(record.values.Value))
-
-const getFilter = (filter: FilterObject): Values => {
-  const parsedFilter = getObjectFromValues(filter.descriptor.values.Value)
-  const records = getFilterRecords(filter)
-  if (!_.isEmpty(records)) {
-    Object.assign(parsedFilter, { RECORDS: records })
+type FilterObject = {
+  descriptor?: {
+    values?: {
+      Value?: AttributeObject[]
+    }
   }
-  return parsedFilter
+  values?: {
+    values?: {
+      Record?: RecordObject | RecordObject[]
+    }
+  }
 }
 
-const extractSearchDefinitionValues = (search: ElementCompact): Values[] =>
-  collections.array.makeArray(search.values?.SearchFilter).map(getFilter)
+const getFilterRecords = (filter: FilterObject | undefined): Values[] =>
+  collections.array.makeArray(filter?.values?.values?.Record)
+    .map(record => getObjectFromValues(record?.values?.Value))
+
+const getFilter = (filter: FilterObject | undefined): Values => {
+  const parsedFilter = getObjectFromValues(filter?.descriptor?.values?.Value)
+  const records = getFilterRecords(filter)
+  return {
+    ...parsedFilter,
+    ...!_.isEmpty(records) ? { RECORDS: records } : {},
+  }
+}
+
+const extractSearchDefinitionValues = (search: ElementCompact | undefined): Values[] =>
+  collections.array.makeArray(search?.values?.SearchFilter).map(getFilter)
 
 const getAudience = (search: ElementCompact[]): Values => {
   const record = collections.array.makeArray(search).filter(i => Object.keys(i).includes('Record'))[0]
-  return record === undefined ? [] : getObjectFromValues(record.Record.values.Value)
+  return record === undefined ? [] : getObjectFromValues(record.Record?.values?.Value)
 }
 
-const getAlertRecipients = (search: ElementCompact): Values[] => {
-  if (search.alertRecipientFields === undefined
-    || search.alertRecipientFields.values === undefined) {
-    return []
-  }
-  return collections.array.makeArray(search.alertRecipientFields.values.Record)
-    .map((record:RecordObject) => getObjectFromValues(record.values.Value))
-}
+const getAlertRecipients = (search: ElementCompact | undefined): Values[] =>
+  collections.array.makeArray(search?.alertRecipientFields?.values?.Record)
+    .map((record: RecordObject) => getObjectFromValues(record?.values?.Value))
 
 const getSearchPartsFromDefinition = async (definition:string): Promise<ElementParts> => {
   const parsedXml = await getJson(definition)
-  return { definition: getDefinitionOrLayout(parsedXml, SAVED_SEARCH),
-    dependency: getElementDependency(parsedXml) }
+  return {
+    definition: getDefinitionOrLayout(parsedXml, SAVED_SEARCH),
+    dependency: getElementDependency(parsedXml),
+  }
 }
 
 export const parseDefinition = async (definition:string): Promise<ParsedSavedSearchType> => {
   const searchParts = await getSearchPartsFromDefinition(definition)
   const returnInstance = {
-    search_filter: extractSearchDefinitionValues(searchParts.definition.filters),
-    search_summary_filters: extractSearchDefinitionValues(searchParts.definition.summaryFilters),
-    available_filters: extractRecordsValues(searchParts.definition.availableFilterFields),
-    return_fields: extractRecordsValues(searchParts.definition.returnFields),
-    detail_fields: extractRecordsValues(searchParts.definition.detailFields),
-    sort_columns: extractRecordsValues(searchParts.definition.sortColumns),
+    search_filter: extractSearchDefinitionValues(searchParts.definition?.filters),
+    search_summary_filters: extractSearchDefinitionValues(searchParts.definition?.summaryFilters),
+    available_filters: extractRecordsValues(searchParts.definition?.availableFilterFields),
+    return_fields: extractRecordsValues(searchParts.definition?.returnFields),
+    detail_fields: extractRecordsValues(searchParts.definition?.detailFields),
+    sort_columns: extractRecordsValues(searchParts.definition?.sortColumns),
     audience: getAudience(searchParts.dependency),
     alert_recipients: getAlertRecipients(searchParts.definition),
   }

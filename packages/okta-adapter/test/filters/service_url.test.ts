b3ca1cb197241b02d3918d77db9974ca74ddb5c3
idoamit8 <63644199+idoamit8@users.noreply.github.com>
2023-04-10 10:21:01 +0300

    added more tests

--- packages/okta-adapter/test/filters/service_url.test.ts
+++ packages/okta-adapter/test/filters/service_url.test.ts
@@ -15,7 +15,7 @@
 */
 
 import { filterUtils } from '@salto-io/adapter-components'
-import { CORE_ANNOTATIONS, ElemID, InstanceElement, ObjectType, ReferenceExpression } from '@salto-io/adapter-api'
+import { CORE_ANNOTATIONS, ElemID, InstanceElement, ObjectType, ReferenceExpression, getChangeData, toChange } from '@salto-io/adapter-api'
 import { getFilterParams, mockClient } from '../utils'
 import OktaClient from '../../src/client/client'
 import serviceUrlFilterCreator from '../../src/filters/service_url'
@@ -25,50 +25,91 @@ describe('serviceUrlFilterCreator', () => {
   let client: OktaClient
     type FilterType = filterUtils.FilterWith<'onFetch' | 'onDeploy'>
     let filter: FilterType
+
     beforeEach(async () => {
       jest.clearAllMocks()
       const mockCli = mockClient()
       client = mockCli.client
       filter = serviceUrlFilterCreator(getFilterParams({ client })) as typeof filter
     })
-    describe('onFetch', () => {
-      it('should add service url annotation for application if it is exist in the config', async () => {
-        const appType = new ObjectType({ elemID: new ElemID(OKTA, APPLICATION_TYPE_NAME) })
-        const appInstance = new InstanceElement(
-          'app1',
-          appType,
-          { id: 11, name: 'app1' },
-        )
 
-        const elements = [appInstance].map(e => e.clone())
-        await filter.onFetch(elements)
-        const [instance] = elements
-        expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toEqual(
-          `${client.baseUrlWithAdmin}/admin/app/${appInstance.value.name}/instance/${appInstance.value.id}/#tab-assignments`,
-        )
+    describe('cheking object types that use addUrlToInstance() and can be filtered using the config', () => {
+      const appType = new ObjectType({ elemID: new ElemID(OKTA, APPLICATION_TYPE_NAME) })
+      const appInstance = new InstanceElement(
+        'app1',
+        appType,
+        { id: 11, name: 'app1' },
+      )
+      describe('onFetch', () => {
+        it('should add service url annotation for application if it is exist in the config', async () => {
+          const elements = [appInstance].map(e => e.clone())
+          await filter.onFetch(elements)
+          const [instance] = elements
+          expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toEqual(
+            `${client.baseUrlWithAdmin}/admin/app/${appInstance.value.name}/instance/${appInstance.value.id}/#tab-assignments`,
+          )
+        })
       })
-      it('should add service url annotation for userSchema if it has userType as reference expression', async () => {
-        const userSchemaType = new ObjectType({ elemID: new ElemID(OKTA, USER_SCHEMA_TYPE_NAME) })
-        const userTypeType = new ObjectType({ elemID: new ElemID(OKTA, USERTYPE_TYPE_NAME) })
-        const userTypeInstance = new InstanceElement(
-          'user1',
-          userTypeType,
-          { id: 11, name: 'user1' }
-        )
-        const userSchemaInstace = new InstanceElement(
-          'schema',
-          userSchemaType,
-          { id: 12 },
-          undefined,
-          { [CORE_ANNOTATIONS.PARENT]: [
-            new ReferenceExpression(userTypeInstance.elemID, userTypeInstance)] },
-        )
-        const elements = [userSchemaInstace].map(e => e.clone())
-        await filter.onFetch(elements)
-        const [instance] = elements
-        expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toEqual(
-          `${client.baseUrlWithAdmin}/admin/universaldirectory#okta/${userTypeInstance.value.id}`,
-        )
+      describe('onDeploy', () => {
+        it('should add service url annotation for application if it is exist in the config', async () => {
+          const changes = [appInstance].map(e => e.clone()).map(inst => toChange({ after: inst }))
+          await filter.onDeploy(changes)
+          const instance = getChangeData(changes[0])
+          expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toEqual(
+            `${client.baseUrlWithAdmin}/admin/app/${appInstance.value.name}/instance/${appInstance.value.id}/#tab-assignments`,
+          )
+        })
+      })
+    })
+    describe('cheking user schame object type', () => {
+      const userSchemaType = new ObjectType({ elemID: new ElemID(OKTA, USER_SCHEMA_TYPE_NAME) })
+      const userTypeType = new ObjectType({ elemID: new ElemID(OKTA, USERTYPE_TYPE_NAME) })
+      const userTypeInstance = new InstanceElement(
+        'user1',
+        userTypeType,
+        { id: 11, name: 'user1' }
+      )
+      const userSchemaInstace = new InstanceElement(
+        'schema',
+        userSchemaType,
+        { id: 12 },
+        undefined,
+        { [CORE_ANNOTATIONS.PARENT]: [
+          new ReferenceExpression(userTypeInstance.elemID, userTypeInstance)] },
+      )
+      const userSchemaInstaceWithoutParents = new InstanceElement(
+        'schema',
+        userSchemaType,
+        { id: 12 },
+        undefined,
+        {},
+      )
+      describe('onFetch', () => {
+        it('should add service url annotation for userSchema if it has userType as reference expression', async () => {
+          const elements = [userSchemaInstace].map(e => e.clone())
+          await filter.onFetch(elements)
+          const [instance] = elements
+          expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toEqual(
+            `${client.baseUrlWithAdmin}/admin/universaldirectory#okta/${userTypeInstance.value.id}`,
+          )
+        })
+        it('should not add service url annotation for userSchema if it has no parents annotation', async () => {
+          const elements = [userSchemaInstaceWithoutParents.clone()]
+          delete elements[0].annotations[CORE_ANNOTATIONS.PARENT]
+          await filter.onFetch(elements)
+          const [instance] = elements
+          expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toBeUndefined()
+        })
+      })
+      describe('onDeploy', () => {
+        it('should add service url annotation for userSchema if it has userType as reference expression', async () => {
+          const changes = [userSchemaInstace].map(e => e.clone()).map(inst => toChange({ after: inst }))
+          await filter.onDeploy(changes)
+          const instance = getChangeData(changes[0])
+          expect(instance.annotations?.[CORE_ANNOTATIONS.SERVICE_URL]).toEqual(
+            `${client.baseUrlWithAdmin}/admin/universaldirectory#okta/${userTypeInstance.value.id}`,
+          )
+        })
       })
     })
 })

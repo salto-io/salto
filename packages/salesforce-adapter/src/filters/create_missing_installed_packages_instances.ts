ee0e2d9890dc7fc30051a4ce7c9953d0cba5bcbb
tamir <tamir.ohayon@salto.io>
2023-04-16 18:50:31 +0300

    Added possible bugfix in `create_missing_installed_packages_instances` filter

--- packages/salesforce-adapter/src/filters/create_missing_installed_packages_instances.ts
+++ packages/salesforce-adapter/src/filters/create_missing_installed_packages_instances.ts
@@ -37,7 +37,7 @@ const createMissingInstalledPackageInstance = (file: FileProperties, installedPa
 const filterCreator: RemoteFilterCreator = ({ client, config }) => ({
   name: 'createMissingInstalledPackagesInstancesFilter',
   onFetch: async (elements: Element[]): Promise<FilterResult | undefined> => {
-    if (!config.fetchProfile.metadataQuery.isPartialFetch()) {
+    if (config.fetchProfile.metadataQuery.isPartialFetch()) {
       return
     }
     const installedPackageType = await awu(elements)

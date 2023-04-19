f5b86d60e1ead366587081b311e0c7b25b4def2c
Shulik95 <51851511+Shulik95@users.noreply.github.com>
2023-04-19 15:20:16 +0300

    Add fallback in case error message doesn't match
    Co-authored-by: dantal4 <70883080+dantal4@users.noreply.github.com>

--- packages/netsuite-adapter/src/change_validators/client_validation.ts
+++ packages/netsuite-adapter/src/change_validators/client_validation.ts
@@ -44,7 +44,7 @@ const mapObjectDeployErrorToInstance = (error: Error):
   } = multiLanguageErrorDetectors[detectedLanguage]
   const scriptIdToErrorRecord: Record<string, string> = {}
   const splitterRegex = validationFailed.test(error.message) ? validationFailed : settingsValidationErrorRegex
-  const errorMessageChunks = error.message.split(splitterRegex)[1]?.split('\n\n') ?? []
+  const errorMessageChunks = error.message.split(splitterRegex)[1]?.split('\n\n') ?? [error.message]
   errorMessageChunks.forEach(chunk => {
     const objectErrorScriptId = getGroupItemFromRegex(
       chunk, objectValidationErrorRegex, OBJECT_ID

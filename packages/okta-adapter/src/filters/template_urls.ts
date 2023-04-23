0f1d330435b967266171e9899bebe023508c81ee
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2023-04-23 17:37:45 +0300

    template urls filter

---
+++ packages/okta-adapter/src/filters/template_urls.ts
@@ -0,0 +1,60 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import _ from 'lodash'
+import { Element, isInstanceElement, ReferenceExpression } from '@salto-io/adapter-api'
+import { extractTemplate } from '@salto-io/adapter-utils'
+import { logger } from '@salto-io/logging'
+import { FilterCreator } from '../filter'
+import { ORG_SETTING_TYPE_NAME } from '../constants'
+
+const log = logger(module)
+
+/**
+ * Create template expressions for url in BrandTheme type
+ */
+const filter: FilterCreator = () => ({
+  name: 'templateUrlsFilter',
+  onFetch: async (elements: Element[]) => {
+    const instances = elements.filter(isInstanceElement)
+    const orgSettingsInstance = instances.find(instance => instance.elemID.typeName === ORG_SETTING_TYPE_NAME)
+    if (orgSettingsInstance === undefined) {
+      log.warn(`Could not find ${ORG_SETTING_TYPE_NAME} instance, skipping templateUrlsFilter`)
+      return
+    }
+    const subdomain = orgSettingsInstance?.value?.subdomain
+    const subdomainReg = new RegExp(`(${subdomain})`)
+
+    const brandThemeInstance = instances.find(instance => instance.elemID.typeName === 'BrandTheme')
+    const faviconUrl = brandThemeInstance?.value?.favicon
+    if (brandThemeInstance === undefined || !_.isString(faviconUrl)) {
+      log.warn('Could not change favicon field in BrandTheme with TemplateExpression')
+      return
+    }
+    const template = extractTemplate(
+      faviconUrl,
+      [subdomainReg],
+      expression => {
+        if (expression === subdomain) {
+          return new ReferenceExpression(orgSettingsInstance.elemID.createNestedID('subdomain'), subdomain)
+        }
+        return expression
+      },
+    )
+    brandThemeInstance.value.favicon = template
+  },
+})
+
+export default filter
---
+++ packages/okta-adapter/test/filters/template_urls.test.ts
@@ -0,0 +1,66 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import { ElemID, InstanceElement, ObjectType, ReferenceExpression, TemplateExpression, isInstanceElement } from '@salto-io/adapter-api'
+import { filterUtils } from '@salto-io/adapter-components'
+import { getFilterParams } from '../utils'
+import templateUrlsFilter from '../../src/filters/template_urls'
+import { OKTA, ORG_SETTING_TYPE_NAME } from '../../src/constants'
+
+describe('templateUrlsFilter', () => {
+      type FilterType = filterUtils.FilterWith<'onFetch'>
+      let filter: FilterType
+      const orgType = new ObjectType({ elemID: new ElemID(OKTA, ORG_SETTING_TYPE_NAME) })
+      const themeType = new ObjectType({ elemID: new ElemID(OKTA, 'BrandTheme') })
+      const orgInst = new InstanceElement(
+        ElemID.CONFIG_NAME,
+        orgType,
+        { subdomain: 'myOkta', companyName: 'comp', status: 'ACTIVE' },
+      )
+      const themeInstance = new InstanceElement(
+        ElemID.CONFIG_NAME,
+        themeType,
+        { id: '111', favicon: 'https://myOkta.oktapreview.com/favicon.ico' },
+      )
+
+      const faviconTemplate = new TemplateExpression({
+        parts: [
+          'https://',
+          new ReferenceExpression(orgInst.elemID.createNestedID('subdomain'), orgInst.value.subdomain),
+          '.oktapreview.com/favicon.ico',
+        ],
+      })
+
+      beforeEach(() => {
+        filter = templateUrlsFilter(getFilterParams()) as typeof filter
+      })
+
+      describe('onFetch', () => {
+        it('should resolve templates in urls', async () => {
+          const elements = [orgType, orgInst, themeInstance.clone(), themeType]
+          await filter.onFetch(elements)
+          const theme = elements.filter(isInstanceElement).find(i => i.elemID.typeName === 'BrandTheme')
+          expect(theme?.value?.favicon).toEqual(faviconTemplate)
+        })
+
+        it('should not create reference for org settings if org settings instance does not exist', async () => {
+          const elements = [orgType, themeInstance.clone(), themeType]
+          await filter.onFetch(elements)
+          const theme = elements.filter(isInstanceElement).find(i => i.elemID.typeName === 'BrandTheme')
+          expect(theme?.value?.favicon).toEqual('https://myOkta.oktapreview.com/favicon.ico')
+        })
+      })
+})

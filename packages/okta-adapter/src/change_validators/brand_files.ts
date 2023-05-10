eed5b3ee1154f648b341f0bb33415e3230bd263c
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2023-05-10 16:34:27 +0300

    SALTO-4043: Deploy Brand and BrandTheme (#4304)
    * SALTO-4043: Deploy Brand and BrandTheme

---
+++ packages/okta-adapter/src/change_validators/brand_files.ts
@@ -0,0 +1,44 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ChangeValidator, InstanceElement, ModificationChange, getChangeData, isEqualValues, isInstanceChange, isModificationChange } from '@salto-io/adapter-api'
+
+const isFaviconOrLogoChange = (change: ModificationChange<InstanceElement>): boolean => {
+  const { before, after } = change.data
+  return !isEqualValues(before.value.logo, after.value.logo)
+    || !isEqualValues(before.value.favicon, after.value.favicon)
+}
+/**
+ * Deployment of brand logo and brand favicon is not supported yet
+ * TODO: remove this validator after SALTO-4058
+ */
+export const brandThemeFilesValidator: ChangeValidator = async changes => {
+  const brandThemeChange = changes
+    .filter(isInstanceChange)
+    .filter(isModificationChange)
+    .find(change => getChangeData(change).elemID.typeName === 'BrandTheme')
+
+  if (brandThemeChange === undefined || !isFaviconOrLogoChange(brandThemeChange)) {
+    return []
+  }
+
+  const brandThemeInstance = getChangeData(brandThemeChange)
+  return [{
+    elemID: brandThemeInstance.elemID,
+    severity: 'Info',
+    message: 'Changes to brand logo and brand favicon are not supported',
+    detailedMessage: 'Changes to brand logo and brand favicon will not be deployed, please use the admin console',
+  }]
+}

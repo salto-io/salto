3b1749dfe6acaea2702c26e45b68d06a308f564e
Eden Hassid <edenhassid@gmail.com>
2023-05-11 14:29:32 +0300

    SALTO-3145: add warning when default locale is not english

---
+++ packages/zendesk-adapter/src/filters/locale.ts
@@ -0,0 +1,53 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { isInstanceElement, Element, SaltoError } from '@salto-io/adapter-api'
+import { logger } from '@salto-io/logging'
+import { FilterCreator } from '../filter'
+import { LOCALE_TYPE_NAME } from '../constants'
+
+const log = logger(module)
+
+
+const getWarningsForLocale = (
+): SaltoError[] => [{
+  message: 'Please be aware that your Zendesk account\'s default locale is not set to English, which may impact your ability to compare environments with different default locales',
+  severity: 'Warning',
+}]
+
+/**
+ * This filter checks that the default locale is set to en-US, if not it will raise a warning. We have seen that the
+ * default local determines the language of different values and therefore may affect the elemId.
+ */
+const filterCreator: FilterCreator = () => ({
+  name: 'locale',
+  onFetch: async (elements: Element[]) => {
+    const defaultLocale = elements
+      .filter(isInstanceElement)
+      .filter(obj => obj.elemID.typeName === LOCALE_TYPE_NAME)
+      .find(localeInstance => localeInstance.value.default === true)
+
+    if (defaultLocale === undefined) {
+      log.warn('could not find a default locale')
+      return { errors: [] }
+    }
+
+    const warnings = defaultLocale.value.locale === 'en-US'
+      ? []
+      : getWarningsForLocale()
+    return { errors: warnings }
+  },
+})
+export default filterCreator
---
+++ packages/zendesk-adapter/test/filters/locale.test.ts
@@ -0,0 +1,87 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { filterUtils } from '@salto-io/adapter-components'
+import {
+  ElemID,
+  InstanceElement,
+  ObjectType,
+} from '@salto-io/adapter-api'
+import filterCreator from '../../src/filters/locale'
+import { LOCALE_TYPE_NAME, ZENDESK } from '../../src/constants'
+import { createFilterCreatorParams } from '../utils'
+import { FilterResult } from '../../src/filter'
+
+describe('locale filter', () => {
+  type FilterType = filterUtils.FilterWith<'onFetch', FilterResult>
+  let filter: FilterType
+
+  const localeType = new ObjectType({ elemID: new ElemID(ZENDESK, LOCALE_TYPE_NAME) })
+  const warningMsg = 'Please be aware that your Zendesk account\'s default locale is not set to English, which may impact your ability to compare environments with different default locales'
+
+  beforeEach(async () => {
+    filter = filterCreator(createFilterCreatorParams({})) as FilterType
+  })
+
+  describe('onFetch', () => {
+    it('should add warning when default locale is not english', async () => {
+      const enUsLocaleInstance = new InstanceElement(
+        'en US',
+        localeType,
+        {
+          locale: 'en-US',
+          default: false,
+        }
+      )
+      const heLocaleInstance = new InstanceElement(
+        'he',
+        localeType,
+        {
+          locale: 'he',
+          default: true,
+        }
+      )
+
+      const res = await filter.onFetch([enUsLocaleInstance, heLocaleInstance]) as FilterResult
+      expect(res.errors).toEqual([
+        {
+          message: warningMsg,
+          severity: 'Warning',
+        },
+      ])
+    })
+    it('should not add warning when default locale is english', async () => {
+      const enUsLocaleInstance = new InstanceElement(
+        'en US',
+        localeType,
+        {
+          locale: 'en-US',
+          default: true,
+        }
+      )
+      const heLocaleInstance = new InstanceElement(
+        'he',
+        localeType,
+        {
+          locale: 'he',
+          default: false,
+        }
+      )
+
+      const res = await filter.onFetch([enUsLocaleInstance, heLocaleInstance]) as FilterResult
+      expect(res.errors).toEqual([])
+    })
+  })
+})

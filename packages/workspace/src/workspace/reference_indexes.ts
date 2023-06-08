88d932a47046188133e165667b27cf75582f425f
Ido Seroussi <idoser@gmail.com>
2023-06-08 15:27:54 +0300

    Update reference_indexes.ts

--- packages/workspace/src/workspace/reference_indexes.ts
+++ packages/workspace/src/workspace/reference_indexes.ts
@@ -34,7 +34,7 @@ import { getAllElementsChanges } from './index_utils'
 import { RemoteMap, RemoteMapEntry } from './remote_map'
 
 const log = logger(module)
-export const REFERENCE_INDEXES_VERSION = 2
+export const REFERENCE_INDEXES_VERSION = 3
 export const REFERENCE_INDEXES_KEY = 'reference_indexes'
 
 type ReferenceDetails = {

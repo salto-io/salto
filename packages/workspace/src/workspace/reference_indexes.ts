98f3b554d143dd4a3d5715ee5c665725d4750f91
Ido Seroussi <idoser@gmail.com>
2023-06-11 15:30:59 +0200

    SAAS-7820 - Targets reference index as a TreeMap (#4434)
    * Added serialize and deserialize for the tree
    
    * Big changes after better understanding of the reference index
    
    * Added workspace tests
    
    * Tuning before CR
    
    * Added tests for getElementOutgoingReferencesByTree
    
    * Removed unnecessary step
    
    * Removed the old index and replaced it with the new one
    
    * Update tests
    
    * Use the previous calculation of the references to prevent two different calculations
    
    * Removed old code
    
    * Move code to previous location to make PR diff easier
    
    * Move code to previous location to make PR diff easier, again
    
    * Tuning
    
    * No need Elem.fromFullName
    
    * CR, need to change saving the tree to relative path instead of full path
    
    * Save the path in the tree without the baseID
    
    * updated mock
    
    * update tests
    
    * CR
    
    * CR
    
    * Removed testing console.log
    
    * Update reference_indexes.ts
    
    * fix from conflicts

--- packages/workspace/src/workspace/reference_indexes.ts
+++ packages/workspace/src/workspace/reference_indexes.ts
@@ -13,16 +13,29 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { Change, ElemID, getChangeData, isReferenceExpression, Element, isModificationChange, isObjectTypeChange, isRemovalOrModificationChange, isAdditionOrModificationChange, isTemplateExpression, ReferenceMapping } from '@salto-io/adapter-api'
+import {
+  Change,
+  ElemID,
+  getChangeData,
+  isReferenceExpression,
+  Element,
+  isModificationChange,
+  isRemovalOrModificationChange,
+  isAdditionOrModificationChange,
+  isTemplateExpression,
+  ReferenceMapping,
+  isObjectTypeChange,
+} from '@salto-io/adapter-api'
 import { walkOnElement, WALK_NEXT_STEP } from '@salto-io/adapter-utils'
 import { logger } from '@salto-io/logging'
 import _ from 'lodash'
+import { collections } from '@salto-io/lowerdash'
 import { ElementsSource } from './elements_source'
 import { getAllElementsChanges } from './index_utils'
 import { RemoteMap, RemoteMapEntry } from './remote_map'
 
 const log = logger(module)
-export const REFERENCE_INDEXES_VERSION = 2
+export const REFERENCE_INDEXES_VERSION = 3
 export const REFERENCE_INDEXES_KEY = 'reference_indexes'
 
 type ChangeReferences = {
@@ -66,57 +79,55 @@ const getReferencesFromChange = (change: Change<Element>): ChangeReferences => {
   }
 }
 
-const updateUniqueIndex = async (
-  index: RemoteMap<ElemID[]>,
-  updates: RemoteMapEntry<ElemID[]>[]
-): Promise<void> => {
-  const [deletions, modifications] = _.partition(updates, update => update.value.length === 0)
-  const uniqueModification = modifications.map(modification => ({
-    ...modification,
-    value: _.uniqBy(modification.value, id => id.getFullName()),
-  }))
-  await Promise.all([
-    modifications.length !== 0 ? index.setAll(uniqueModification) : undefined,
-    deletions.length !== 0 ? index.deleteAll(deletions.map(deletion => deletion.key)) : undefined,
-  ])
-}
+const createReferenceTree = (references: ReferenceMapping[], rootFields = false): collections.treeMap.TreeMap<ElemID> =>
+  new collections.treeMap.TreeMap<ElemID>(
+    references.map(
+      ref => {
+        // In case we are creating a reference tree for a type object, the fields path is not relevant
+        // This is because a request for the field's references will be to the field itself, and not through the object
+        const key = rootFields && ref.source.idType === 'field'
+          ? ''
+          : ref.source.createBaseID().path.join(ElemID.NAMESPACE_SEPARATOR)
+        return [key, [ref.target]]
+      }
+    ),
+    ElemID.NAMESPACE_SEPARATOR
+  )
 
 const getReferenceTargetIndexUpdates = (
   change: Change<Element>,
   changeToReferences: Record<string, ChangeReferences>,
-): RemoteMapEntry<ElemID[]>[] => {
-  const indexUpdates: RemoteMapEntry<ElemID[]>[] = []
-
-  const references = changeToReferences[getChangeData(change).elemID.getFullName()]
-    .currentAndNew
-  const baseIdToReferences = _(references)
-    .groupBy(reference => reference.source.createBaseID().parent.getFullName())
-    .mapValues(referencesGroup => referencesGroup.map(ref => ref.target))
-    .value()
+): RemoteMapEntry<collections.treeMap.TreeMap<ElemID>>[] => {
+  const indexUpdates: RemoteMapEntry<collections.treeMap.TreeMap<ElemID>>[] = []
 
   if (isObjectTypeChange(change)) {
-    const type = getChangeData(change)
+    const objectType = getChangeData(change)
+    const elemId = objectType.elemID.getFullName()
+
+    const baseIdToReferences = _.groupBy(
+      changeToReferences[elemId].currentAndNew,
+      reference => reference.source.createBaseID().parent.getFullName()
+    )
 
     const allFields = isModificationChange(change)
       ? {
         ...change.data.before.fields,
-        ...type.fields,
+        ...objectType.fields,
       }
-      : type.fields
+      : objectType.fields
 
     indexUpdates.push(
       ...Object.values(allFields)
         .map(field => ({
           key: field.elemID.getFullName(),
-          value: baseIdToReferences[field.elemID.getFullName()] ?? [],
+          value: createReferenceTree(baseIdToReferences[field.elemID.getFullName()] ?? []),
         }))
     )
   }
   const elemId = getChangeData(change).elemID.getFullName()
   indexUpdates.push({
     key: elemId,
-    value: changeToReferences[elemId].currentAndNew
-      .map(ref => ref.target),
+    value: createReferenceTree(changeToReferences[elemId].currentAndNew, true),
   })
 
   return indexUpdates
@@ -124,14 +135,16 @@ const getReferenceTargetIndexUpdates = (
 
 const updateReferenceTargetsIndex = async (
   changes: Change<Element>[],
-  index: RemoteMap<ElemID[]>,
-  changeToReferences: Record<string, ChangeReferences>
+  referenceTargetsIndex: RemoteMap<collections.treeMap.TreeMap<ElemID>>,
+  changeToReferences: Record<string, ChangeReferences>,
 ): Promise<void> => {
-  const updates = changes.flatMap(
-    change => getReferenceTargetIndexUpdates(change, changeToReferences)
-  )
+  const changesTrees = changes.flatMap(change => getReferenceTargetIndexUpdates(change, changeToReferences))
+  const [toAdd, toDelete] = _.partition(changesTrees, change => change.value.size > 0)
 
-  await updateUniqueIndex(index, updates)
+  await Promise.all([
+    referenceTargetsIndex.setAll(toAdd),
+    referenceTargetsIndex.deleteAll(toDelete.map(change => change.key)),
+  ])
 }
 
 const updateIdOfReferenceSourcesIndex = (
@@ -210,12 +223,20 @@ const updateReferenceSourcesIndex = async (
         changedReferenceSources,
       ))
 
-  await updateUniqueIndex(index, updates)
+  const [deletions, modifications] = _.partition(updates, update => update.value.length === 0)
+  const uniqueModification = modifications.map(modification => ({
+    ...modification,
+    value: _.uniqBy(modification.value, id => id.getFullName()),
+  }))
+  await Promise.all([
+    modifications.length !== 0 ? index.setAll(uniqueModification) : undefined,
+    deletions.length !== 0 ? index.deleteAll(deletions.map(deletion => deletion.key)) : undefined,
+  ])
 }
 
 export const updateReferenceIndexes = async (
   changes: Change<Element>[],
-  referenceTargetsIndex: RemoteMap<ElemID[]>,
+  referenceTargetsIndex: RemoteMap<collections.treeMap.TreeMap<ElemID>>,
   referenceSourcesIndex: RemoteMap<ElemID[]>,
   mapVersions: RemoteMap<number>,
   elementsSource: ElementsSource,
@@ -247,11 +268,13 @@ export const updateReferenceIndexes = async (
       getReferencesFromChange(change),
     ]))
 
+  // Outgoing references
   await updateReferenceTargetsIndex(
     relevantChanges,
     referenceTargetsIndex,
     changeToReferences,
   )
+  // Incoming references
   await updateReferenceSourcesIndex(
     relevantChanges,
     referenceSourcesIndex,

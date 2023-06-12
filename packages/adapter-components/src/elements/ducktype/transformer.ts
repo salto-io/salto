1e49a785dc03eb925ff0f968dcff3325a791605f
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2023-06-12 10:47:58 +0300

    FetchErrorFor401

--- packages/adapter-components/src/elements/ducktype/transformer.ts
+++ packages/adapter-components/src/elements/ducktype/transformer.ts
@@ -403,7 +403,7 @@ export const getAllElements = async ({
           })
           return { elements: [], errors: [] }
         }
-        if (e.response?.status === 403) {
+        if (e.response?.status === 403 || e.response?.status === 401) {
           const newError: SaltoError = {
             message: `Salto could not access the ${args.typeName} resource. Elements from that type were not fetched. Please make sure that this type is enabled in your service, and that the supplied user credentials have sufficient permissions to access this data. You can also exclude this data from Salto's fetches by changing the environment configuration. Learn more at https://help.salto.io/en/articles/6947061-salto-could-not-access-the-resource`,
             severity: 'Warning',

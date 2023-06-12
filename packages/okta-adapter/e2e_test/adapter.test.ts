c4e52226e01c090c1d4013abed4fe686234341c9
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2023-06-12 19:28:02 +0300

    update e2e

--- packages/okta-adapter/e2e_test/adapter.test.ts
+++ packages/okta-adapter/e2e_test/adapter.test.ts
@@ -274,7 +274,13 @@ describe('Okta adapter E2E', () => {
           { reportProgress: () => null },
       })
       elements = fetchResult.elements
-      expect(fetchResult.errors).toHaveLength(0)
+      expect(fetchResult.errors).toHaveLength(1)
+      expect(fetchResult.errors).toEqual([
+        {
+          severity: 'Warning',
+          message: "Salto could not access the api__v1__mappings resource. Elements from that type were not fetched. Please make sure that this type is enabled in your service, and that the supplied user credentials have sufficient permissions to access this data. You can also exclude this data from Salto's fetches by changing the environment configuration. Learn more at https://help.salto.io/en/articles/6947061-salto-could-not-access-the-resource",
+        },
+      ])
       adapterAttr = realAdapter(
         { credentials: credLease.value,
           elementsSource: buildElementsSourceFromElements(elements) },

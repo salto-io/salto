c7aa7d3936d65bb25870d7fca1699b4d2c237ff1
edenhassid <110904940+edenhassid@users.noreply.github.com>
2023-06-13 16:02:28 +0300

    SALTO-4301: update static file path (#4484)

--- packages/workspace/src/workspace/nacl_files/nacl_files_source.ts
+++ packages/workspace/src/workspace/nacl_files/nacl_files_source.ts
@@ -828,6 +828,18 @@ const buildNaclFilesSource = (
         .map(getNestedStaticFiles)
         .flat()
         .forEach(file => staticFilesSource.delete(file))
+
+      fileChanges
+        .filter(isModificationChange)
+        .filter(change => isStaticFile(change.data.before))
+        .filter(change => (
+          (isStaticFile(change.data.after) && change.data.before.filepath !== change.data.after.filepath)
+          || !isStaticFile(change.data.after)
+        ))
+        .map(change => change.data.before)
+        .map(getNestedStaticFiles)
+        .flat()
+        .forEach(file => staticFilesSource.delete(file))
     }
 
     const changesByFileName = await groupChangesByFilename(changes)

409cdaa7fb03b081d289f8460d80096cea4ff76a
idoamit8 <63644199+idoamit8@users.noreply.github.com>
2023-06-18 15:34:26 +0300

    SALTO-3984 App Logo, brand Logo and Fav Icon (#4346)
    * added fetch to appLogo
    
    * added fetch and deploy for brand logo
    
    * changed hard coded png
    
    * fixed app_logo sugestions in okta
    
    * created okta getSource and add appLogo test
    
    * adde fetch and deploy for appLogo brandLogo and favIcon
    
    * added small doc and rebased
    
    * fixed spelling mistake
    
    * added test for unsupported file type
    
    * added test for logo and resolved shir's suggestions
    
    * changed getResource to use oktaClient
    
    * merge to one filter
    
    * changed filter name
    
    * samll change
    
    * changed path, added tests and returning saltoErrors
    
    * fixed shir suggesitons

--- packages/okta-adapter/src/client/client.ts
+++ packages/okta-adapter/src/client/client.ts
@@ -17,6 +17,8 @@ import _ from 'lodash'
 import { client as clientUtils } from '@salto-io/adapter-components'
 import { Values } from '@salto-io/adapter-api'
 import { logger } from '@salto-io/logging'
+import axios from 'axios'
+import { safeJsonStringify } from '@salto-io/adapter-utils'
 import { createConnection } from './connection'
 import { OKTA } from '../constants'
 import { Credentials } from '../auth'
@@ -24,6 +26,7 @@ import { LINK_HEADER_NAME } from './pagination'
 
 const {
   RATE_LIMIT_UNLIMITED_MAX_CONCURRENT_REQUESTS, DEFAULT_RETRY_OPTS,
+  throttle, logDecorator,
 } = clientUtils
 const log = logger(module)
 
@@ -118,5 +121,32 @@ export default class OktaClient extends clientUtils.AdapterHTTPClient<
         ..._.pickBy(headers, (_val, key) => key.toLowerCase() === LINK_HEADER_NAME),
       }
       : undefined
+  }
+
+  @throttle<clientUtils.ClientRateLimitConfig>({ bucketName: 'get', keys: ['url'] })
+  @logDecorator(['url'])
+  // We use this function without client instance because we don't need it
+  // but we want to take advantage of the client's capabilities.
+  // eslint-disable-next-line class-methods-use-this
+  public async getResource(
+    args: clientUtils.ClientBaseParams,
+  ): Promise<clientUtils.Response<clientUtils.ResponseValue | clientUtils.ResponseValue[]>> {
+    try {
+      const { url, responseType } = args
+      const httpClient = axios.create({ url })
+      const response = await httpClient.get(url, { responseType })
+      const { data, status } = response
+      log.debug('Received response for resource request %s with status %d', url, status)
+      log.trace('Full HTTP response for resource %s: %s', url, safeJsonStringify({
+        url, response: data,
+      }))
+      return {
+        data,
+        status,
+      }
+    } catch (e) {
+      log.warn('Failed to get response from resource: %s. error %o', args.url, e)
+      throw e
+    }
   }
 }

3243f4488ee2c8613fa00b2d9e00173cbb8f49ca
tomsellek <117099820+tomsellek@users.noreply.github.com>
2023-06-19 10:18:40 -0400

    SALTO-1730 - Salesforce: Enable formula parsing by default (#4190)

--- packages/salesforce-adapter/src/types.ts
+++ packages/salesforce-adapter/src/types.ts
@@ -76,9 +76,8 @@ export type OptionalFeatures = {
   addMissingIds?: boolean
   authorInformation?: boolean
   describeSObjects?: boolean
-  skipParsingFormulas?: boolean // Negative flag because we want it disabled by default and optional features are
-                                // enabled by default
   skipAliases?: boolean
+  formulaDeps?: boolean
 }
 
 export type ChangeValidatorName = (
@@ -571,8 +570,8 @@ const optionalFeaturesType = createMatchingObjectType<OptionalFeatures>({
     addMissingIds: { refType: BuiltinTypes.BOOLEAN },
     authorInformation: { refType: BuiltinTypes.BOOLEAN },
     describeSObjects: { refType: BuiltinTypes.BOOLEAN },
-    skipParsingFormulas: { refType: BuiltinTypes.BOOLEAN },
     skipAliases: { refType: BuiltinTypes.BOOLEAN },
+    formulaDeps: { refType: BuiltinTypes.BOOLEAN },
   },
   annotations: {
     [CORE_ANNOTATIONS.ADDITIONAL_PROPERTIES]: false,

8107e46f76169595102a161d3bd82cd991e2cbb5
Tom Sellek <tom.sellek@salto.io>
2023-06-21 15:31:46 -0400

    Use isInstanceOfType in tests. remove redundant clearing of mocks

--- packages/salesforce-adapter/src/fetch.ts
+++ packages/salesforce-adapter/src/fetch.ts
@@ -332,6 +332,11 @@ type RetrieveMetadataInstancesArgs = {
   maxItemsInRetrieveRequest: number
   metadataQuery: MetadataQuery
   addNamespacePrefixToFullName: boolean
+
+  // Some types are retrieved via filters and should not be fetched in the normal fetch flow. However, we need these
+  // types as context for profiles - when fetching profiles using retrieve we only get information about the types that
+  // are included in the same retrieve request as the profile. Thus typesToSkip - a list of types that will be retrieved
+  // along with the profiles, but discarded.
   typesToSkip: ReadonlySet<string>
 }
 

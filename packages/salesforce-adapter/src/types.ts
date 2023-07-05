c507c286bad61258a2544057e051cee876ca138c
tomsellek <117099820+tomsellek@users.noreply.github.com>
2023-07-05 12:42:12 -0400

    SALTO-4402 - undo revert and disable by default (#4573)
    This reverts commit 0e6a18bde7cabf230527f246a82c8e59189a2a1a.

--- packages/salesforce-adapter/src/types.ts
+++ packages/salesforce-adapter/src/types.ts
@@ -79,6 +79,7 @@ export type OptionalFeatures = {
   skipAliases?: boolean
   formulaDeps?: boolean
   fetchCustomObjectUsingRetrieveApi?: boolean
+  generateRefsInProfiles?: boolean
 }
 
 export type ChangeValidatorName = (
@@ -574,6 +575,7 @@ const optionalFeaturesType = createMatchingObjectType<OptionalFeatures>({
     skipAliases: { refType: BuiltinTypes.BOOLEAN },
     formulaDeps: { refType: BuiltinTypes.BOOLEAN },
     fetchCustomObjectUsingRetrieveApi: { refType: BuiltinTypes.BOOLEAN },
+    generateRefsInProfiles: { refType: BuiltinTypes.BOOLEAN },
   },
   annotations: {
     [CORE_ANNOTATIONS.ADDITIONAL_PROPERTIES]: false,

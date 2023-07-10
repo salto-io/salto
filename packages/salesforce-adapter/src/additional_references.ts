39363faee9fbd43c4859c6fec2baee2862d3c975
tomsellek <117099820+tomsellek@users.noreply.github.com>
2023-07-10 11:23:54 -0400

    SALTO-4402 - Salesforce: Recreate profile refs as additional refs (#4577)

--- packages/salesforce-adapter/src/additional_references.ts
+++ packages/salesforce-adapter/src/additional_references.ts
@@ -13,15 +13,65 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { GetAdditionalReferencesFunc, getChangeData, isAdditionChange, isFieldChange, isInstanceChange, isRemovalOrModificationChange } from '@salto-io/adapter-api'
+import {
+  Element,
+  GetAdditionalReferencesFunc,
+  getChangeData,
+  InstanceElement,
+  isAdditionChange,
+  isFieldChange,
+  isInstanceChange,
+  isRemovalOrModificationChange,
+  ReferenceMapping,
+} from '@salto-io/adapter-api'
 import _ from 'lodash'
 import { collections } from '@salto-io/lowerdash'
-import { isFieldOfCustomObject } from './transformers/transformer'
+import {
+  isCustomObject,
+  isFieldOfCustomObject,
+} from './transformers/transformer'
 import { isInstanceOfType, safeApiName } from './filters/utils'
-import { API_NAME_SEPARATOR, FIELD_PERMISSIONS, PERMISSION_SET_METADATA_TYPE, PROFILE_METADATA_TYPE } from './constants'
+import {
+  APEX_CLASS_METADATA_TYPE,
+  APEX_PAGE_METADATA_TYPE,
+  API_NAME_SEPARATOR,
+  CUSTOM_APPLICATION_METADATA_TYPE,
+  FIELD_PERMISSIONS,
+  FLOW_METADATA_TYPE,
+  LAYOUT_TYPE_ID_METADATA_TYPE,
+  PERMISSION_SET_METADATA_TYPE,
+  PROFILE_METADATA_TYPE,
+  RECORD_TYPE_METADATA_TYPE,
+} from './constants'
 
 const { awu } = collections.asynciterable
 
+const CUSTOM_APP_SECTION = 'applicationVisibilities'
+const APEX_CLASS_SECTION = 'classAccesses'
+const FLOW_SECTION = 'flowAccesses'
+const LAYOUTS_SECTION = 'layoutAssignments'
+const OBJECT_SECTION = 'objectPermissions'
+const APEX_PAGE_SECTION = 'pageAccesses'
+const RECORD_TYPE_SECTION = 'recordTypeVisibilities'
+
+const refsFromProfileOrPermissionSet = async (
+  profilesAndPermissionSets: InstanceElement[],
+  potentialTarget: Readonly<Element>,
+  profileSection: string,
+): Promise<ReferenceMapping[]> => {
+  const apiName = await safeApiName(potentialTarget)
+  if (apiName === undefined) {
+    return []
+  }
+  return profilesAndPermissionSets
+    .filter(profileOrPermissionSet => _.get(profileOrPermissionSet.value[profileSection], apiName))
+    .flatMap(profileOrPermissionSet => (
+      {
+        source: profileOrPermissionSet.elemID.createNestedID(profileSection, ...apiName.split(API_NAME_SEPARATOR)),
+        target: potentialTarget.elemID,
+      }
+    ))
+}
 
 export const getAdditionalReferences: GetAdditionalReferencesFunc = async changes => {
   const relevantFields = await awu(changes)
@@ -31,29 +81,58 @@ export const getAdditionalReferences: GetAdditionalReferencesFunc = async change
     .filter(isFieldOfCustomObject)
     .toArray()
 
-  if (relevantFields.length === 0) {
-    return []
-  }
+  const customObjects = changes
+    .filter(isAdditionChange)
+    .map(getChangeData)
+    .filter(isCustomObject)
 
-  const profilesAndPermissionSets = await awu(changes)
+
+  const addedInstances = changes
+    .filter(isInstanceChange)
+    .filter(isAdditionChange)
+    .map(getChangeData)
+
+  const addedInstancesByType = await awu(addedInstances)
+    .groupBy(async instance => (await instance.getType()).elemID.typeName)
+
+  const profilesAndPermSets = await awu(changes)
     .filter(isRemovalOrModificationChange)
     .filter(isInstanceChange)
     .map(getChangeData)
     .filter(instance => isInstanceOfType(PROFILE_METADATA_TYPE, PERMISSION_SET_METADATA_TYPE)(instance))
     .toArray()
 
-  return awu(relevantFields)
-    .flatMap(async field => {
-      const fieldApiName = await safeApiName(field)
-      if (fieldApiName === undefined) {
-        return []
-      }
-      return profilesAndPermissionSets
-        .filter(instance => _.get(instance.value.fieldPermissions, fieldApiName) !== undefined)
-        .map(instance => ({
-          source: instance.elemID.createNestedID(FIELD_PERMISSIONS, ...fieldApiName.split(API_NAME_SEPARATOR)),
-          target: field.elemID,
-        }))
-    })
+  const fieldPermissionsRefs = awu(relevantFields)
+    .flatMap(async field => refsFromProfileOrPermissionSet(profilesAndPermSets, field, FIELD_PERMISSIONS))
+
+  const customAppsRefs = awu(addedInstancesByType[CUSTOM_APPLICATION_METADATA_TYPE] ?? [])
+    .flatMap(async customApp => refsFromProfileOrPermissionSet(profilesAndPermSets, customApp, CUSTOM_APP_SECTION))
+
+  const apexClassRefs = awu(addedInstancesByType[APEX_CLASS_METADATA_TYPE] ?? [])
+    .flatMap(async apexClass => refsFromProfileOrPermissionSet(profilesAndPermSets, apexClass, APEX_CLASS_SECTION))
+
+  const flowRefs = awu(addedInstancesByType[FLOW_METADATA_TYPE] ?? [])
+    .flatMap(async flow => refsFromProfileOrPermissionSet(profilesAndPermSets, flow, FLOW_SECTION))
+
+  const layoutRefs = awu(addedInstancesByType[LAYOUT_TYPE_ID_METADATA_TYPE] ?? [])
+    .flatMap(async layout => refsFromProfileOrPermissionSet(profilesAndPermSets, layout, LAYOUTS_SECTION))
+
+  const apexPageRefs = awu(addedInstancesByType[APEX_PAGE_METADATA_TYPE] ?? [])
+    .flatMap(async apexPage => refsFromProfileOrPermissionSet(profilesAndPermSets, apexPage, APEX_PAGE_SECTION))
+
+  const recordTypeRefs = awu(addedInstancesByType[RECORD_TYPE_METADATA_TYPE] ?? [])
+    .flatMap(async recordType => refsFromProfileOrPermissionSet(profilesAndPermSets, recordType, RECORD_TYPE_SECTION))
+
+  const objectRefs = awu(customObjects)
+    .flatMap(async object => refsFromProfileOrPermissionSet(profilesAndPermSets, object, OBJECT_SECTION))
+
+  return fieldPermissionsRefs
+    .concat(customAppsRefs)
+    .concat(apexClassRefs)
+    .concat(flowRefs)
+    .concat(layoutRefs)
+    .concat(objectRefs)
+    .concat(apexPageRefs)
+    .concat(recordTypeRefs)
     .toArray()
 }
--- packages/salesforce-adapter/test/additional_references.test.ts
+++ packages/salesforce-adapter/test/additional_references.test.ts
@@ -13,112 +13,364 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { BuiltinTypes, ElemID, Field, InstanceElement, ObjectType, toChange } from '@salto-io/adapter-api'
-import { API_NAME, CUSTOM_OBJECT, METADATA_TYPE, SALESFORCE } from '../src/constants'
+import {
+  BuiltinTypes,
+  Change,
+  Field,
+  InstanceElement,
+  ObjectType,
+  toChange,
+  Values,
+} from '@salto-io/adapter-api'
+import {
+  API_NAME,
+  INSTANCE_FULL_NAME_FIELD,
+} from '../src/constants'
 import { getAdditionalReferences } from '../src/additional_references'
+import { mockTypes } from './mock_elements'
+import {
+  createCustomObjectType,
+  createMetadataTypeElement,
+} from './utils'
 
 describe('getAdditionalReferences', () => {
-  let permissionSetInstance: InstanceElement
-  let profileInstance: InstanceElement
-  let field: Field
+  const createTestInstances = (fields: Values): [InstanceElement, InstanceElement] => [
+    new InstanceElement('test', mockTypes.Profile, fields),
+    new InstanceElement('test', mockTypes.PermissionSet, fields),
+  ]
 
-  beforeEach(() => {
-    permissionSetInstance = new InstanceElement(
-      'test',
-      new ObjectType({
-        elemID: new ElemID(SALESFORCE, 'PermissionSet'),
-        annotations: { [METADATA_TYPE]: 'PermissionSet' },
-      }),
-      {
+  describe('fields', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let field: Field
+
+    beforeEach(() => {
+      [permissionSetInstance, profileInstance] = createTestInstances({
         fieldPermissions: {
           Account: {
             testField__c: 'ReadWrite',
           },
         },
-      }
-    )
+      })
 
-    profileInstance = new InstanceElement(
-      'test',
-      new ObjectType({
-        elemID: new ElemID(SALESFORCE, 'Profile'),
-        annotations: { [METADATA_TYPE]: 'Profile' },
-      }),
-      {
-        fieldPermissions: {
-          Account: {
-            testField__c: 'ReadWrite',
+      field = new Field(
+        mockTypes.Account,
+        'testField__c',
+        BuiltinTypes.STRING,
+        {
+          [API_NAME]: 'Account.testField__c',
+        }
+      )
+    })
+
+    it('should create references from field to profile and permission sets', async () => {
+      const refs = await getAdditionalReferences([
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ after: field }),
+      ])
+
+      expect(refs).toEqual([
+        { source: permissionSetInstance.elemID.createNestedID('fieldPermissions', 'Account', 'testField__c'), target: field.elemID },
+        { source: profileInstance.elemID.createNestedID('fieldPermissions', 'Account', 'testField__c'), target: field.elemID },
+      ])
+    })
+
+    it('should add references only for addition fields', async () => {
+      const refs = await getAdditionalReferences([
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: field, after: field }),
+      ])
+
+      expect(refs).toEqual([])
+    })
+
+    it('should not add references for addition profiles and permission sets', async () => {
+      const refs = await getAdditionalReferences([
+        toChange({ after: permissionSetInstance }),
+        toChange({ after: profileInstance }),
+        toChange({ after: field }),
+      ])
+
+      expect(refs).toEqual([])
+    })
+
+    it('should not add references if not contained the field section', async () => {
+      delete permissionSetInstance.value.fieldPermissions.Account.testField__c
+      delete profileInstance.value.fieldPermissions.Account.testField__c
+      const refs = await getAdditionalReferences([
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ after: field }),
+      ])
+
+      expect(refs).toEqual([])
+    })
+
+    it('should not add references if field does not have an api name', async () => {
+      delete field.annotations[API_NAME]
+      const refs = await getAdditionalReferences([
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ after: field }),
+      ])
+
+      expect(refs).toEqual([])
+    })
+  })
+  describe('custom apps', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let customApp: InstanceElement
+    let changes: Change[]
+
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        applicationVisibilities: {
+          SomeApplication: {
+            application: 'SomeApplication',
+            default: false,
+            visible: false,
           },
         },
-      }
-    )
+      })
+      customApp = new InstanceElement(
+        'SomeApplication',
+        createMetadataTypeElement('CustomApplication', {}),
+        { [INSTANCE_FULL_NAME_FIELD]: 'SomeApplication' },
+      )
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: customApp }),
+      ]
+    })
 
-    field = new Field(
-      new ObjectType({
-        elemID: new ElemID(SALESFORCE, 'Account'),
-        annotations: { [METADATA_TYPE]: CUSTOM_OBJECT, [API_NAME]: 'Account' },
-      }),
-      'testField__c',
-      BuiltinTypes.STRING,
-      {
-        [API_NAME]: 'Account.testField__c',
-      }
-    )
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('applicationVisibilities', 'SomeApplication'), target: customApp.elemID },
+        { source: profileInstance.elemID.createNestedID('applicationVisibilities', 'SomeApplication'), target: customApp.elemID },
+      ])
+    })
+  })
+  describe('apex classes', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let apexClass: InstanceElement
+    let changes: Change[]
+
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        classAccesses: {
+          SomeApexClass: {
+            apexClass: 'SomeApexClass',
+            enabled: false,
+          },
+        },
+      })
+      apexClass = new InstanceElement(
+        'SomeApexClass',
+        mockTypes.ApexClass,
+        { [INSTANCE_FULL_NAME_FIELD]: 'SomeApexClass' },
+      )
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: apexClass }),
+      ]
+    })
+
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('classAccesses', 'SomeApexClass'), target: apexClass.elemID },
+        { source: profileInstance.elemID.createNestedID('classAccesses', 'SomeApexClass'), target: apexClass.elemID },
+      ])
+    })
+  })
+  describe('flows', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let flow: InstanceElement
+    let changes: Change[]
+
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        flowAccesses: {
+          SomeFlow: {
+            enabled: false,
+            flow: 'SomeFlow',
+          },
+        },
+      })
+      flow = new InstanceElement(
+        'SomeFlow',
+        mockTypes.Flow,
+        { [INSTANCE_FULL_NAME_FIELD]: 'SomeFlow' },
+      )
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: flow }),
+      ]
+    })
+
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('flowAccesses', 'SomeFlow'), target: flow.elemID },
+        { source: profileInstance.elemID.createNestedID('flowAccesses', 'SomeFlow'), target: flow.elemID },
+      ])
+    })
+  })
+  describe('layouts', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let layout: InstanceElement
+    let changes: Change[]
+
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        layoutAssignments: {
+          Account_Account_Layout: [
+            {
+              layout: 'Account_Account_Layout',
+            },
+          ],
+        },
+      })
+      layout = new InstanceElement(
+        'Account_Account_Layout',
+        mockTypes.Layout,
+        { [INSTANCE_FULL_NAME_FIELD]: 'Account_Account_Layout' },
+      )
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: layout }),
+      ]
+    })
+
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('layoutAssignments', 'Account_Account_Layout'), target: layout.elemID },
+        { source: profileInstance.elemID.createNestedID('layoutAssignments', 'Account_Account_Layout'), target: layout.elemID },
+      ])
+    })
   })
 
-  it('should create references from field to profile and permission sets', async () => {
-    const refs = await getAdditionalReferences([
-      toChange({ before: permissionSetInstance, after: permissionSetInstance }),
-      toChange({ before: profileInstance, after: profileInstance }),
-      toChange({ after: field }),
-    ])
+  describe('objects', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let customObject: ObjectType
+    let changes: Change[]
 
-    expect(refs).toEqual([
-      { source: permissionSetInstance.elemID.createNestedID('fieldPermissions', 'Account', 'testField__c'), target: field.elemID },
-      { source: profileInstance.elemID.createNestedID('fieldPermissions', 'Account', 'testField__c'), target: field.elemID },
-    ])
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        objectPermissions: {
+          Account: {
+            allowCreate: true,
+            allowDelete: true,
+            allowEdit: true,
+            allowRead: true,
+            modifyAllRecords: false,
+            object: 'Account',
+            viewAllRecords: false,
+          },
+        },
+      })
+      customObject = createCustomObjectType('Account', {})
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: customObject }),
+      ]
+    })
+
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('objectPermissions', 'Account'), target: customObject.elemID },
+        { source: profileInstance.elemID.createNestedID('objectPermissions', 'Account'), target: customObject.elemID },
+      ])
+    })
   })
 
-  it('should add references only for addition fields', async () => {
-    const refs = await getAdditionalReferences([
-      toChange({ before: permissionSetInstance, after: permissionSetInstance }),
-      toChange({ before: profileInstance, after: profileInstance }),
-      toChange({ before: field, after: field }),
-    ])
+  describe('Apex pages', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let apexPage: InstanceElement
+    let changes: Change[]
 
-    expect(refs).toEqual([])
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        pageAccesses: {
+          SomeApexPage: {
+            apexPage: 'SomeApexPage',
+            enabled: false,
+          },
+        },
+      })
+      apexPage = new InstanceElement(
+        'SomeApexPage',
+        mockTypes.ApexPage,
+        { [INSTANCE_FULL_NAME_FIELD]: 'SomeApexPage' },
+      )
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: apexPage }),
+      ]
+    })
+
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('pageAccesses', 'SomeApexPage'), target: apexPage.elemID },
+        { source: profileInstance.elemID.createNestedID('pageAccesses', 'SomeApexPage'), target: apexPage.elemID },
+      ])
+    })
   })
 
-  it('should not add references for addition profiles and permission sets', async () => {
-    const refs = await getAdditionalReferences([
-      toChange({ after: permissionSetInstance }),
-      toChange({ after: profileInstance }),
-      toChange({ after: field }),
-    ])
+  describe('record types', () => {
+    let permissionSetInstance: InstanceElement
+    let profileInstance: InstanceElement
+    let recordType: InstanceElement
+    let changes: Change[]
 
-    expect(refs).toEqual([])
-  })
+    beforeEach(() => {
+      [profileInstance, permissionSetInstance] = createTestInstances({
+        recordTypeVisibilities: {
+          Case: {
+            SomeCaseRecordType: {
+              default: true,
+              recordType: 'Case.SomeCaseRecordType',
+              visible: true,
+            },
+          },
+        },
+      })
+      recordType = new InstanceElement(
+        'Case_SomeCaseRecordType',
+        mockTypes.RecordType,
+        { [INSTANCE_FULL_NAME_FIELD]: 'Case.SomeCaseRecordType' },
+      )
+      changes = [
+        toChange({ before: profileInstance, after: profileInstance }),
+        toChange({ before: permissionSetInstance, after: permissionSetInstance }),
+        toChange({ after: recordType }),
+      ]
+    })
 
-  it('should not add references if not contained the field section', async () => {
-    delete permissionSetInstance.value.fieldPermissions.Account.testField__c
-    delete profileInstance.value.fieldPermissions.Account.testField__c
-    const refs = await getAdditionalReferences([
-      toChange({ before: permissionSetInstance, after: permissionSetInstance }),
-      toChange({ before: profileInstance, after: profileInstance }),
-      toChange({ after: field }),
-    ])
-
-    expect(refs).toEqual([])
-  })
-
-  it('should not add references if field does not have an api name', async () => {
-    delete field.annotations[API_NAME]
-    const refs = await getAdditionalReferences([
-      toChange({ before: permissionSetInstance, after: permissionSetInstance }),
-      toChange({ before: profileInstance, after: profileInstance }),
-      toChange({ after: field }),
-    ])
-
-    expect(refs).toEqual([])
+    it('should create a reference', async () => {
+      const refs = await getAdditionalReferences(changes)
+      expect(refs).toIncludeAllPartialMembers([
+        { source: permissionSetInstance.elemID.createNestedID('recordTypeVisibilities', 'Case', 'SomeCaseRecordType'), target: recordType.elemID },
+        { source: profileInstance.elemID.createNestedID('recordTypeVisibilities', 'Case', 'SomeCaseRecordType'), target: recordType.elemID },
+      ])
+    })
   })
 })

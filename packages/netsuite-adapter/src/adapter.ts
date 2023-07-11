239e17ae786d6eb7a3df1267595f39e9753f2f25
dantal4 <70883080+dantal4@users.noreply.github.com>
2023-07-11 17:44:21 +0300

    SALTO-4216 Return Salto Errors on NS deploy (#4589)

--- packages/netsuite-adapter/src/adapter.ts
+++ packages/netsuite-adapter/src/adapter.ts
@@ -14,16 +14,16 @@
 * limitations under the License.
 */
 import {
-  FetchResult, AdapterOperations, DeployResult, DeployOptions,
+  FetchResult, AdapterOperations, DeployOptions,
   ElemIdGetter, ReadOnlyElementsSource, ProgressReporter,
-  FetchOptions, DeployModifiers, getChangeData, isObjectType, isInstanceElement, ElemID,
+  FetchOptions, DeployModifiers, getChangeData, isObjectType, isInstanceElement, ElemID, isSaltoElementError,
 } from '@salto-io/adapter-api'
 import _ from 'lodash'
 import { collections, values } from '@salto-io/lowerdash'
 import { logger } from '@salto-io/logging'
 import { filter } from '@salto-io/adapter-utils'
 import { createElements } from './transformer'
-import { isCustomRecordType } from './types'
+import { DeployResult, isCustomRecordType } from './types'
 import { INTEGRATION } from './constants'
 import convertListsToMaps from './filters/convert_lists_to_maps'
 import replaceElementReferences from './filters/element_references'
@@ -472,6 +472,34 @@ export default class NetsuiteAdapter implements AdapterOperations {
     return { changedObjectsQuery, serverTime: sysInfo.time, timeZoneAndFormat }
   }
 
+  private static getDeployErrors(
+    originalChanges: DeployOptions['changeGroup']['changes'],
+    deployResult: DeployResult
+  ): DeployResult['errors'] {
+    const originalChangesElemIds = new Set(originalChanges.map(change =>
+      getChangeData(change).elemID.getFullName()))
+
+    const [saltoElementErrors, saltoErrors] = _.partition(deployResult.errors, isSaltoElementError)
+    const [originalChangesErrors, additionalChangesErrors] = _.partition(
+      saltoElementErrors,
+      error => originalChangesElemIds.has(error.elemID.createBaseID().parent.getFullName())
+    )
+
+    const errorsOnCustomFieldsByParents = _(originalChangesErrors)
+      .filter(error => error.elemID.idType === 'field')
+      .groupBy(error => error.elemID.createTopLevelParentID().parent.getFullName())
+      .mapValues(errors => new Set(errors.map(error => error.message)))
+      .value()
+
+    additionalChangesErrors.forEach(error => {
+      const errorsOnFields = errorsOnCustomFieldsByParents[error.elemID.createBaseID().parent.getFullName()]
+      if (!errorsOnFields?.has(error.message)) {
+        saltoErrors.push({ message: error.message, severity: error.severity })
+      }
+    })
+
+    return [...originalChangesErrors, ...saltoErrors]
+  }
 
   public async deploy({ changeGroup: { changes, groupID } }: DeployOptions): Promise<DeployResult> {
     const changesToDeploy = changes.map(cloneChange)
@@ -496,7 +524,7 @@ export default class NetsuiteAdapter implements AdapterOperations {
     await filtersRunner.onDeploy(appliedChanges, deployResult)
 
     return {
-      errors: deployResult.errors,
+      errors: NetsuiteAdapter.getDeployErrors(changes, deployResult),
       appliedChanges,
     }
   }

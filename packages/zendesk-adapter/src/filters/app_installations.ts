db2bfbcc9ac87165b1fbb1b5624afc74368a978e
Ido Seroussi <idoser@gmail.com>
2023-07-12 10:47:16 +0300

    import fix

--- packages/zendesk-adapter/src/filters/app_installations.ts
+++ packages/zendesk-adapter/src/filters/app_installations.ts
@@ -16,7 +16,7 @@
 import _ from 'lodash'
 import Joi from 'joi'
 import {
-  Change,
+  Change, Element,
   createSaltoElementError,
   getChangeData, InstanceElement, isAdditionChange,
   isAdditionOrModificationChange, isInstanceElement, ReferenceExpression,

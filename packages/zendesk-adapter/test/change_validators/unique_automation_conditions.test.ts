e2b22a97916ea885417d2e36bf6970840a6ae8a9
Ido Seroussi <idoser@gmail.com>
2023-07-17 10:06:09 +0300

    CR

--- packages/zendesk-adapter/test/change_validators/unique_automation_conditions.test.ts
+++ packages/zendesk-adapter/test/change_validators/unique_automation_conditions.test.ts
@@ -54,9 +54,13 @@ const createAutomationInstance = (name: string, conds: Value): InstanceElement =
       conditions: _.cloneDeep(conds),
     },
   )
-const automationInstance = createAutomationInstance('automation', conditions)
 
 describe('duplicateAutomationConditionValidator', () => {
+  let automationInstance: InstanceElement
+  beforeEach(() => {
+    automationInstance = createAutomationInstance('automation', conditions)
+  })
+
   it('should not return errors for automations with unique conditions', async () => {
     const uniqueAutomation1 = createAutomationInstance('unique1', 'unique1')
     const uniqueAutomation2 = createAutomationInstance('unique2', 'unique2')

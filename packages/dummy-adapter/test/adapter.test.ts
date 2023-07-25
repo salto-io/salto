8ac693f6292ff721e2484d0d502409bdc96e8c0c
edenhassid <110904940+edenhassid@users.noreply.github.com>
2023-07-25 14:42:49 +0300

    SALTO-4525: add alias to dummy adapter (#4660)

--- packages/dummy-adapter/test/adapter.test.ts
+++ packages/dummy-adapter/test/adapter.test.ts
@@ -13,7 +13,16 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-import { ElemID, ObjectType, toChange, InstanceElement, getChangeData } from '@salto-io/adapter-api'
+import {
+  ElemID,
+  ObjectType,
+  toChange,
+  InstanceElement,
+  getChangeData,
+  isInstanceElement,
+  isObjectType, CORE_ANNOTATIONS,
+} from '@salto-io/adapter-api'
+import _ from 'lodash'
 import DummyAdapter from '../src/adapter'
 import * as generator from '../src/generator'
 import testParams from './test_params'
@@ -83,14 +92,32 @@ describe('dummy adapter', () => {
     it('should return the result of the generateElement command withuot modifications', async () => {
       const mockReporter = { reportProgress: jest.fn() }
       const fetchResult = await adapter.fetch({ progressReporter: mockReporter })
-      expect(fetchResult)
-        .toEqual({ elements: await generator.generateElements(testParams, mockReporter) })
+      expect(fetchResult).toEqual({ elements: await generator.generateElements(testParams, mockReporter) })
     })
     it('should report fetch progress', async () => {
       await adapter.fetch({ progressReporter: progressReportMock })
       expect(progressReportMock.reportProgress).toHaveBeenCalledTimes(7)
       expect(progressReportMock.reportProgress).toHaveBeenLastCalledWith({
         message: 'Generation done',
+      })
+    })
+    it('should add alias to instances and custom objects', async () => {
+      const mockReporter = { reportProgress: jest.fn() }
+      const fetchResult = await adapter.fetch({ progressReporter: mockReporter })
+      fetchResult.elements.forEach(elem => {
+        if (isInstanceElement(elem)
+          && elem.elemID.typeName !== 'Profile'
+          && elem.path
+          && elem.path[1] === 'Records') {
+          expect(elem.annotations[CORE_ANNOTATIONS.ALIAS]).toBeDefined()
+        } else if (isObjectType(elem)
+          && elem.path
+          && elem.path[1] === 'Objects'
+          && _.last(elem.path)?.endsWith('Annotations')) {
+          expect(elem.annotations[CORE_ANNOTATIONS.ALIAS]).toBeDefined()
+        } else {
+          expect(elem.annotations[CORE_ANNOTATIONS.ALIAS]).not.toBeDefined()
+        }
       })
     })
   })

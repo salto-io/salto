0b42224fdc6ae9a2953ed3df134b9dacd436605e
Shay Cohen <shay.cohen@salto.io>
2023-08-02 10:27:47 +0300

    fix defaultFieldConfiguration validator to not fail items changes

--- packages/jira-adapter/src/change_validators/default_field_configuration.ts
+++ packages/jira-adapter/src/change_validators/default_field_configuration.ts
@@ -14,6 +14,7 @@
 * limitations under the License.
 */
 import { ChangeValidator, getChangeData, isInstanceChange, isModificationChange, SeverityLevel } from '@salto-io/adapter-api'
+// import { getDetailedChanges } from '@salto-io/adapter-utils'
 
 export const defaultFieldConfigurationValidator: ChangeValidator = async changes => (
   changes
@@ -21,6 +22,8 @@ export const defaultFieldConfigurationValidator: ChangeValidator = async changes
     .filter(isModificationChange)
     .filter(change => getChangeData(change).elemID.typeName === 'FieldConfiguration')
     .filter(change => getChangeData(change).value.isDefault)
+    .filter(change => !(change.data.before.value.name === change.data.after.value.name
+      && change.data.before.value.description === change.data.after.value.description))
     .map(change => ({
       elemID: getChangeData(change).elemID,
       severity: 'Error' as SeverityLevel,

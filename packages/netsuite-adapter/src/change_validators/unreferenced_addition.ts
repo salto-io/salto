606ccbf7b88e1ab6e7a61ec639ee023a3edeb7b9
Seggev Haimovich <seggev.haimovich@salto.io>
2023-08-06 13:59:07 +0300

    first edition, still not usefull

---
+++ packages/netsuite-adapter/src/change_validators/unreferenced_addition.ts
@@ -0,0 +1,60 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import { InstanceElement, getChangeData, isAdditionChange, isAdditionOrModificationChange, isInstanceElement, ChangeError } from '@salto-io/adapter-api'
+import { WALK_NEXT_STEP, WalkOnFunc, walkOnElement } from '@salto-io/adapter-utils'
+import { NetsuiteChangeValidator } from './types'
+
+const fileIsReferenced = (
+  changes: InstanceElement[],
+  fileChange: string
+): boolean => {
+  let answer = false
+
+  // how to do it whitout for
+  const func: WalkOnFunc = ({ value }) => {
+    if (value === fileChange) {
+      answer = true
+      return WALK_NEXT_STEP.EXIT
+    }
+    return WALK_NEXT_STEP.RECURSE
+  }
+
+  changes.forEach(element => walkOnElement({ element, func }))
+
+  return answer
+}
+const changeValidator: NetsuiteChangeValidator = async (changes, _deployReferencedElements, _elementsSource) => {
+  const instanceElementChanges = changes
+    .filter(isAdditionOrModificationChange)
+    .map(change => getChangeData(change))
+    .filter(isInstanceElement)
+  const fileAdditions = changes
+    .filter(isAdditionChange)
+    .map(getChangeData).filter(change => change.elemID.typeName === 'file')
+  const unreferencedFiles = fileAdditions
+    .filter(file => !fileIsReferenced(instanceElementChanges, file.elemID.getFullName()))
+  const res = unreferencedFiles
+    .map(({ elemID }): ChangeError => ({
+      elemID,
+      severity: 'Info',
+      message: 'This file is not referenced by any element',
+      detailedMessage: 'Usually files are referenced by an element, it is possible that you forgot to deploy the relevant element',
+    }))
+  return res
+}
+
+export default changeValidator

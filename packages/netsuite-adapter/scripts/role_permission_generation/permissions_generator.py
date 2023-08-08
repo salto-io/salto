3bfed1cb5c905a5b00eef2a632aa92d0e3fbc9bf
Shalom Kachko <shalom.kachko@salto.io>
2023-08-08 16:44:30 +0300

    update premission record and simplify python script

--- packages/netsuite-adapter/scripts/role_permission_generation/permissions_generator.py
+++ packages/netsuite-adapter/scripts/role_permission_generation/permissions_generator.py
@@ -1,9 +1,8 @@
  #!/usr/bin/python3
-from selenium import webdriver
-from bs4 import BeautifulSoup
 import os
 from pathlib import Path
-from constants import LICENSE_HEADER, LINE_SEPERATOR, TABLE_DATA, TAB
+from constants import LICENSE_HEADER, LINE_SEPERATOR, TAB
+import pandas as pd
 
 SCRIPT_DIR = os.path.dirname(__file__)
 SRC_DIR = os.path.join(SCRIPT_DIR, '../../src/autogen/')
@@ -21,40 +20,32 @@ export const ID_TO_PERMISSION_INFO: Readonly<Record<string, ReadonlySet<Permissi
 
 def is_valid_row(row_cells, permissions, permission_id):
   return (len(row_cells) == PERMISSION_TABLE_ROW_LENGTH
-  and permission_id not in permissions
-  and permission_id != '')
+  and permission_id not in permissions)
 
 def parse_table_row(row, permissions):
-  cells = row.find_all(TABLE_DATA)
-  if len(cells) == 0: return
-  permission_id = cells[0].text.strip()
-  if is_valid_row(cells, permissions, permission_id):
-    permissions[permission_id] = cells[3].text.strip().split(', ')
+  if len(row) == 0: return
+  permission_id = row[0]
+  if is_valid_row(row, permissions, permission_id):
+    permissions[permission_id] = row[3].split(', ')
 
-def parse_permissions_table(driver):
-  try:	
-    driver.get(permissions_table_link)
-    html_content = driver.page_source
-    soup_parser = BeautifulSoup(html_content, 'html.parser')
-    table = soup_parser.select('table tr')
+def parse_permissions_table():
+    table = pd.read_html(permissions_table_link)[0].dropna(how='all')
     permissions = {}
-    for row in table:
-        parse_table_row(row, permissions)
+    for row in table.values.tolist():
+      print(row)
+      parse_table_row(row, permissions)
     return permissions
-  finally:
-    driver.quit()
 
 def create_permissions_file(permissions):
-  formatted_permissions = ["{key}: new Set({value}),".format(key = key,value = [level.upper() for level in permissions[key]]) for key in permissions]
+  formatted_permissions = [" {key}: new Set({value}),".format(key = key,value = [level.upper() for level in permissions[key]]) for key in permissions]
   file_content = role_permissions_file_template.format(permission_id_to_valid_levels = (LINE_SEPERATOR + TAB).join(formatted_permissions))
   Path(PERMISSIONS_DIR).mkdir(parents=True, exist_ok=True)
   with open(PERMISSIONS_DIR + 'role_permissions.ts', 'w') as file:
     file.write(file_content)
 
-def main(driver):
-  permissions = parse_permissions_table(driver)
+def main():
+  permissions = parse_permissions_table()
   create_permissions_file(permissions)
 
 if __name__=='__main__':
-  driver = webdriver.Chrome()
-  main(driver)
+  main()

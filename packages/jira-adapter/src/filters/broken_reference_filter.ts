1c314d74e1ced1cfb1b175a5680e9e029da29a42
IdoZakk <109526049+IdoZakk@users.noreply.github.com>
2023-08-16 11:54:12 +0300

    SALTO-3958: generalize broken reference filter and CV (#4730)
    * SALTO-3958: generalize broken reference filter and CV

---
+++ packages/jira-adapter/src/filters/broken_reference_filter.ts
@@ -0,0 +1,97 @@
+/*
+*                      Copyright 2023 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import { Change, ChangeDataType, ReferenceExpression, Value, getChangeData, isAdditionOrModificationChange, isInstanceChange, isReferenceExpression } from '@salto-io/adapter-api'
+import { isResolvedReferenceExpression } from '@salto-io/adapter-utils'
+import _ from 'lodash'
+import { AUTOMATION_TYPE } from '../constants'
+import { FilterCreator } from '../filter'
+
+export type ProjectType = { projectId: ReferenceExpression }
+
+export const isProjectType = (element: unknown): element is ProjectType => {
+  const projectId = _.get(element, 'projectId')
+  return projectId !== undefined && isReferenceExpression(projectId)
+}
+
+export const isProjectReferenceBroken = (project: ProjectType): boolean =>
+  !isResolvedReferenceExpression(project.projectId)
+
+
+export type BrokenReferenceInfo = {
+  location: string
+  filter: (project: unknown) => boolean
+  namePath: string
+  referencesTypeName: string
+  singleReferenceTypeName: string
+  mustHaveReference: boolean
+}
+export const BROKEN_REFERENCE_TYPE_MAP: Record<string, BrokenReferenceInfo[]> = {
+  [AUTOMATION_TYPE]: [{
+    location: 'projects',
+    filter: (project: unknown): boolean => isProjectType(project) && isProjectReferenceBroken(project),
+    namePath: 'projectId.value.target.name',
+    referencesTypeName: 'projects',
+    singleReferenceTypeName: 'project',
+    mustHaveReference: true,
+  }],
+}
+
+// we allow broken references in some cases, for instance between Automation to Project,
+// so in this filter we remove those broken references in preDeploy and add them back in onDeploy
+export const filter: FilterCreator = () => {
+  const preDeployReferences: Record<string, Value> = {}
+  return {
+    name: 'BrokenReferenceFilter',
+    preDeploy: async (changes: Change<ChangeDataType>[]) => {
+      Object.entries(BROKEN_REFERENCE_TYPE_MAP)
+        .forEach(([typeName, typeInfos]) => {
+          typeInfos.forEach(typeInfo => {
+            changes.filter(isInstanceChange)
+              .filter(isAdditionOrModificationChange)
+              .filter(change => getChangeData(change).elemID.typeName === typeName)
+              .filter(change => getChangeData(change).value[typeInfo.location] !== undefined)
+              .forEach(change => {
+                preDeployReferences[getChangeData(change).elemID.getFullName()] = change.data.after
+                  .value[typeInfo.location]
+                change.data.after.value[typeInfo.location] = change.data.after.value[typeInfo.location]
+                  .filter((value: Value) => !typeInfo.filter(value))
+              })
+          })
+        })
+    },
+
+    onDeploy: async (changes: Change<ChangeDataType>[]) => {
+      Object.entries(BROKEN_REFERENCE_TYPE_MAP)
+        .forEach(([typeName, typeInfos]) => {
+          typeInfos.forEach(typeInfo => {
+            changes.filter(isInstanceChange)
+              .filter(isAdditionOrModificationChange)
+              .filter(change => getChangeData(change).elemID.typeName === typeName)
+              .filter(change => getChangeData(change).value[typeInfo.location] !== undefined)
+              .forEach(change => {
+                if (preDeployReferences[getChangeData(change).elemID.getFullName()] !== undefined) {
+                  change.data.after.value[typeInfo.location] = preDeployReferences[getChangeData(change)
+                    .elemID.getFullName()]
+                }
+              })
+          })
+        })
+    },
+  }
+}
+
+export default filter

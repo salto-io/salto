3a11f0285c823099ae3740e3916f7dac46dfa0d2
Ido Seroussi <idoser@gmail.com>
2023-08-17 14:22:07 +0300

    Added the filter to the filters list

--- packages/zendesk-adapter/src/adapter.ts
+++ packages/zendesk-adapter/src/adapter.ts
@@ -144,6 +144,7 @@ import customRoleDeployFilter from './filters/custom_role_deploy'
 import routingAttributeValueDeployFilter from './filters/routing_attribute_value'
 import localeFilter from './filters/locale'
 import ticketStatusCustomStatusDeployFilter from './filters/ticket_status_custom_status'
+import articleAttachmentsFilter from './filters/article/article_attachment'
 
 const { makeArray } = collections.array
 const log = logger(module)
@@ -212,6 +213,7 @@ export const DEFAULT_FILTERS = [
   // everyoneUserSegmentFilter needs to be before articleFilter
   everyoneUserSegmentFilter,
   articleFilter,
+  articleAttachmentsFilter,
   guideSectionCategoryFilter,
   guideTranslationFilter,
   guideGuideSettings,

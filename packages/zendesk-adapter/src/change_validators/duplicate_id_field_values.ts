904fa1ed6a18c560b9f5f7d36f063cdf5ef7fdee
Ido Seroussi <idoser@gmail.com>
2023-08-29 15:26:26 +0300

    Update message

--- packages/zendesk-adapter/src/change_validators/duplicate_id_field_values.ts
+++ packages/zendesk-adapter/src/change_validators/duplicate_id_field_values.ts
@@ -91,7 +91,7 @@ export const duplicateIdFieldValuesValidator = (
           elemID: instance.elemID,
           severity: 'Error',
           message: `${typeName} duplication detected`,
-          detailedMessage: `This ${typeName} cannot be deployed as it is a duplicate of '${instancesWithSameName.map(i => i.elemID.name).join(', ')}'. This likely indicates a misalignment of Salto IDs. To address this, please execute a fetch on both the source and target environments. Ensure you select the 'Regenerate Salto IDs' option in the advanced settings.`,
+          detailedMessage: `This ${typeName} cannot be deployed as it is a duplicate of '${instancesWithSameName.map(i => i.elemID.name).join(', ')}'. This likely indicates a misalignment of Salto IDs. To address this, please execute a fetch on both the source and target environments. Ensure you select the 'Regenerate Salto IDs' option in the advanced settings. More details can be found here: https://help.salto.io/en/articles/8290892-misalignment-of-salto-element-ids`,
         }
       // This is probably a duplication created by the user, we prevent it from being deployed
         : {

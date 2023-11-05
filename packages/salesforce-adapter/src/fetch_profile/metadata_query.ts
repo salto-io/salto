fd1e2d598eb7b2c7f3292e5c38bb5a5c0a69c066
tamir <tamir.ohayon@salto.io>
2023-11-05 20:04:07 +0200

    created baseMetadataQuery as a required step for fetchWithChangesDetection on CustomObjects

--- packages/salesforce-adapter/src/fetch_profile/metadata_query.ts
+++ packages/salesforce-adapter/src/fetch_profile/metadata_query.ts
@@ -30,6 +30,7 @@ import {
 } from '../constants'
 import { ConfigValidationError, validateRegularExpressions } from '../config_validation'
 import {
+  BaseMetadataQuery,
   METADATA_EXCLUDE_LIST,
   METADATA_INCLUDE_LIST,
   METADATA_SEPARATE_FIELD_LIST,
@@ -94,19 +95,79 @@ const isFolderMetadataTypeNameMatch = ({ name: instanceName }: MetadataInstance,
   || regex.isFullRegexMatch(instanceName, name)
 )
 
-type BuildMetadataQueryParams = {
+type BaseMetadataQueryParams = {
   metadataParams: MetadataParams
   target?: string[]
   isFetchWithChangesDetection: boolean
+}
+
+type BuildMetadataQueryParams = BaseMetadataQueryParams & {
   changedAtSingleton?: InstanceElement
 }
 
-export const buildMetadataQuery = ({
-  metadataParams,
-  target,
-  isFetchWithChangesDetection,
-  changedAtSingleton,
-}: BuildMetadataQueryParams): MetadataQuery => {
+export const buildBaseMetadataQuery = (
+  {
+    metadataParams,
+    target,
+    isFetchWithChangesDetection,
+  }: BaseMetadataQueryParams
+)
+  : BaseMetadataQuery => {
+  const { include = [{}], exclude = [] } = metadataParams
+  const fullExcludeList = [...exclude, ...PERMANENT_SKIP_LIST]
+
+  const isIncludedInPartialFetch = (type: string): boolean => {
+    if (target === undefined) {
+      return true
+    }
+    if (target.includes(type)) {
+      return true
+    }
+    if (type === TOPICS_FOR_OBJECTS_METADATA_TYPE && target.includes(CUSTOM_OBJECT)) {
+      return true
+    }
+    // We should really do this only when config.preferActiveFlowVersions is true
+    // if you have another use-case to pass the config here also handle this please
+    if (type === FLOW_DEFINITION_METADATA_TYPE && target.includes(FLOW_METADATA_TYPE)) {
+      return true
+    }
+    return false
+  }
+
+  const isTypeIncluded = (type: string): boolean => (
+    include.some(({ metadataType = '.*' }) => new RegExp(`^${metadataType}$`).test(type))
+    && isIncludedInPartialFetch(type)
+  )
+  const isTypeExcluded = (type: string): boolean => (
+    fullExcludeList.some(({ metadataType = '.*', namespace = '.*', name = '.*' }) => (
+      namespace === '.*' && name === '.*' && new RegExp(`^${metadataType}$`).test(type)
+    ))
+  )
+  return {
+    isTypeMatch: type => isTypeIncluded(type) && !isTypeExcluded(type),
+    isTargetedFetch: () => target !== undefined,
+    isFetchWithChangesDetection: () => isFetchWithChangesDetection,
+    isPartialFetch: () => target !== undefined || isFetchWithChangesDetection,
+    getFolderPathsByName: (folderType: string) => {
+      const folderPaths = include
+        .filter(params => params.metadataType === folderType)
+        .flatMap(params => {
+          const { name: nameRegex } = params
+          return isDefined(nameRegex)
+            ? getPaths(nameRegex)
+            : []
+        })
+      return _.keyBy(folderPaths, path => _.last(path.split('/')) ?? path)
+    },
+  }
+}
+
+export const buildMetadataQuery = (buildMetadataQueryParams: BuildMetadataQueryParams): MetadataQuery => {
+  const {
+    metadataParams,
+    isFetchWithChangesDetection,
+    changedAtSingleton,
+  } = buildMetadataQueryParams
   const { include = [{}], exclude = [] } = metadataParams
   const fullExcludeList = [...exclude, ...PERMANENT_SKIP_LIST]
 
@@ -129,24 +190,6 @@ export const buildMetadataQuery = ({
       ? isFolderMetadataTypeNameMatch(instance, name)
       : regex.isFullRegexMatch(instance.name, name)
   }
-
-  const isIncludedInPartialFetch = (type: string): boolean => {
-    if (target === undefined) {
-      return true
-    }
-    if (target.includes(type)) {
-      return true
-    }
-    if (type === TOPICS_FOR_OBJECTS_METADATA_TYPE && target.includes(CUSTOM_OBJECT)) {
-      return true
-    }
-    // We should really do this only when config.preferActiveFlowVersions is true
-    // if you have another use-case to pass the config here also handle this please
-    if (type === FLOW_DEFINITION_METADATA_TYPE && target.includes(FLOW_METADATA_TYPE)) {
-      return true
-    }
-    return false
-  }
   const isIncludedInFetchWithChangesDetection = (instance: MetadataInstance): boolean => {
     if (UNSUPPORTED_FETCH_WITH_CHANGES_DETECTION_TYPES.includes(instance.metadataType)) {
       return false
@@ -159,42 +202,14 @@ export const buildMetadataQuery = ({
       ? new Date(lastChangedAt).getTime() < new Date(instance.changedAt).getTime()
       : true
   }
-  const isTypeIncluded = (type: string): boolean => (
-    include.some(({ metadataType = '.*' }) => new RegExp(`^${metadataType}$`).test(type))
-    && isIncludedInPartialFetch(type)
-  )
-  const isTypeExcluded = (type: string): boolean => (
-    fullExcludeList.some(({ metadataType = '.*', namespace = '.*', name = '.*' }) => (
-      namespace === '.*' && name === '.*' && new RegExp(`^${metadataType}$`).test(type)
-    ))
-  )
 
   return {
-    isTypeMatch: type => isTypeIncluded(type) && !isTypeExcluded(type),
-
+    ...buildBaseMetadataQuery(buildMetadataQueryParams),
     isInstanceMatch: instance => (
       include.some(params => isInstanceMatchQueryParams(instance, params))
       && !fullExcludeList.some(params => isInstanceMatchQueryParams(instance, params))
       && (!isFetchWithChangesDetection || isIncludedInFetchWithChangesDetection(instance))
     ),
-
-    isTargetedFetch: () => target !== undefined,
-
-    isFetchWithChangesDetection: () => isFetchWithChangesDetection,
-
-    isPartialFetch: () => target !== undefined || isFetchWithChangesDetection,
-
-    getFolderPathsByName: (folderType: string) => {
-      const folderPaths = include
-        .filter(params => params.metadataType === folderType)
-        .flatMap(params => {
-          const { name: nameRegex } = params
-          return isDefined(nameRegex)
-            ? getPaths(nameRegex)
-            : []
-        })
-      return _.keyBy(folderPaths, path => _.last(path.split('/')) ?? path)
-    },
   }
 }
 

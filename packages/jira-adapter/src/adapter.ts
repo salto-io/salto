6d8a0ea65b947c9a3588e877b38295e0950b74ab
idoamit8 <63644199+idoamit8@users.noreply.github.com>
2023-12-06 20:29:13 +0200

    Fix missing serviceDeskId (#5168)
    * fix missing serviceDeskId
    
    * added a warn log

--- packages/jira-adapter/src/adapter.ts
+++ packages/jira-adapter/src/adapter.ts
@@ -561,6 +561,13 @@ export default class JiraAdapter implements AdapterOperations {
       .filter(project => project.elemID.typeName === PROJECT_TYPE)
       .filter(isInstanceElement)
       .filter(project => project.value.projectTypeKey === SERVICE_DESK)
+      .filter(project => {
+        if (project.value.serviceDeskId?.id === undefined) {
+          log.debug(`Service desk project ${project.elemID.name} does not have a service desk id`)
+          return false
+        }
+        return true
+      })
 
     const fetchResultWithDuplicateTypes = await Promise.all(serviceDeskProjects.map(async projectInstance => {
       const serviceDeskProjRecord: Record<string, string> = {
@@ -568,7 +575,7 @@ export default class JiraAdapter implements AdapterOperations {
         serviceDeskId: projectInstance.value.serviceDeskId.id,
         projectId: projectInstance.value.id,
       }
-      log.debug(`Fetching elements for brand ${projectInstance.elemID.name}`)
+      log.debug(`Fetching elements for project ${projectInstance.elemID.name}`)
       return getAllElements({
         adapterName: JIRA,
         types: jsmApiDefinitions.types,

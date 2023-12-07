2f7412069fd13b481f7c1aeef588b9010c0f2a24
Eden Hassid <edenhassid@gmail.com>
2023-12-07 16:28:48 +0200

    add to extra elements

--- packages/dummy-adapter/src/generator.ts
+++ packages/dummy-adapter/src/generator.ts
@@ -750,8 +750,24 @@ export const generateElements = async (
       annotationRefsOrTypes: {
         SharedHidden: BuiltinTypes.HIDDEN_STRING,
         DiffHidden: BuiltinTypes.HIDDEN_STRING,
+        active: BuiltinTypes.BOOLEAN,
+        name: BuiltinTypes.STRING,
       },
       path: [DUMMY_ADAPTER, 'EnvStuff', 'PrimWithAnnos'],
+      annotations: {
+        [CORE_ANNOTATIONS.IMPORTANT_VALUES]: [
+          {
+            value: 'active',
+            indexed: true,
+            highlighted: true,
+          },
+          {
+            value: 'name',
+            indexed: false,
+            highlighted: false,
+          },
+        ],
+      },
     })
 
     const sharedObj = new ObjectType({
@@ -771,6 +787,8 @@ export const generateElements = async (
           annotations: {
             SharedHidden: 'HIDDEN!',
             DiffHidden: `${envID}-HIDDENNNN!!!!`,
+            active: true,
+            name: 'test',
           },
         },
       },
@@ -788,6 +806,35 @@ export const generateElements = async (
         SharedHidden: 'HIDDEN!',
         DiffHidden: `${envID}-HIDDENNNN!!!!`,
         [CORE_ANNOTATIONS.ALIAS]: 'EnvObj_alias',
+        [CORE_ANNOTATIONS.IMPORTANT_VALUES]: [
+          {
+            value: 'SharedButDiffField',
+            indexed: true,
+            highlighted: true,
+          },
+          {
+            value: 'SharedField',
+            indexed: false,
+            highlighted: false,
+          },
+          {
+            value: 'doesNotExist',
+            indexed: false,
+            highlighted: true,
+          },
+        ],
+        [CORE_ANNOTATIONS.SELF_IMPORTANT_VALUES]: [
+          {
+            value: 'SharedButDiffAnno',
+            indexed: true,
+            highlighted: false,
+          },
+          {
+            value: 'SharedAnno',
+            indexed: false,
+            highlighted: true,
+          },
+        ],
       },
       path: [DUMMY_ADAPTER, 'EnvStuff', 'EnvObj'],
     })
@@ -811,16 +858,34 @@ export const generateElements = async (
         Field: {
           refType: BuiltinTypes.STRING,
         },
+        active: {
+          refType: BuiltinTypes.BOOLEAN,
+        },
       },
       path: [DUMMY_ADAPTER, 'EnvStuff', `${envID}EnvObj`],
       annotations: {
         [CORE_ANNOTATIONS.ALIAS]: `${envID}EnvObj_alias`,
+        [CORE_ANNOTATIONS.IMPORTANT_VALUES]: [
+          {
+            value: 'Field',
+            indexed: false,
+            highlighted: true,
+          },
+          {
+            value: 'active',
+            indexed: true,
+            highlighted: false,
+          },
+        ],
       },
     })
     const envSpecificInst = new InstanceElement(
       `${envID}EnvInst`,
       envSpecificObj,
-      { Field: 'FieldValue' },
+      {
+        Field: 'FieldValue',
+        active: true,
+      },
       [DUMMY_ADAPTER, 'EnvStuff', `${envID}EnvInst`],
       {
         [CORE_ANNOTATIONS.ALIAS]: `${envID}EnvInst_alias`,

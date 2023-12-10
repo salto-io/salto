fe2cbed21729ce14a0abc0bc2c843c39f95ceb91
idoamit8 <63644199+idoamit8@users.noreply.github.com>
2023-12-10 12:29:22 +0200

    fix forms elemID (#5172)

--- packages/jira-adapter/src/filters/forms/forms.ts
+++ packages/jira-adapter/src/filters/forms/forms.ts
@@ -16,7 +16,7 @@
 
 import { CORE_ANNOTATIONS, Change, InstanceElement, ReferenceExpression, getChangeData, isAdditionChange, isAdditionOrModificationChange, isInstanceChange, isInstanceElement } from '@salto-io/adapter-api'
 import { values as lowerDashValues } from '@salto-io/lowerdash'
-import { getParent, pathNaclCase } from '@salto-io/adapter-utils'
+import { getParent, naclCase, pathNaclCase } from '@salto-io/adapter-utils'
 import _ from 'lodash'
 import { FilterCreator } from '../../filter'
 import { FORM_TYPE, JSM_DUCKTYPE_API_DEFINITIONS, PROJECT_TYPE, SERVICE_DESK } from '../../constants'
@@ -100,7 +100,7 @@ const filter: FilterCreator = ({ config, client, fetchQuery }) => ({
             if (!isDetailedFormsResponse(detailedRes.data)) {
               return undefined
             }
-            const name = `${project.value.name}_${formResponse.name}`
+            const name = naclCase(`${project.value.key}_${formResponse.name}`)
             const formValue = detailedRes.data
             const parentPath = project.path ?? []
             const jsmDuckTypeApiDefinitions = config[JSM_DUCKTYPE_API_DEFINITIONS]

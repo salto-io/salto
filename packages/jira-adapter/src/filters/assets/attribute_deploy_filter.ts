7e0e9605b51248ba92192646026df8a5580d006a
idoamit8 <63644199+idoamit8@users.noreply.github.com>
2024-01-04 16:41:47 +0200

    SALTO-5210: Support deploy of default attributes (#5233)
    * support deploy of defualt queues
    
    * resolved Shir's suggestions
    
    * add Element source
    
    * change element source
    
    * fixed test

--- packages/jira-adapter/src/filters/assets/attribute_deploy_filter.ts
+++ packages/jira-adapter/src/filters/assets/attribute_deploy_filter.ts
@@ -16,8 +16,10 @@
 
 import { config as configUtils } from '@salto-io/adapter-components'
 import { logger } from '@salto-io/logging'
-import { Change, DeployResult, InstanceElement, createSaltoElementError, getChangeData, isAdditionOrModificationChange, isInstanceChange } from '@salto-io/adapter-api'
+import { AdditionChange, Change, DeployResult, InstanceElement, createSaltoElementError, getChangeData, isAdditionChange, isAdditionOrModificationChange, isInstanceChange, isReferenceExpression, isRemovalChange, toChange } from '@salto-io/adapter-api'
 import _ from 'lodash'
+import { createSchemeGuard } from '@salto-io/adapter-utils'
+import Joi from 'joi'
 import { defaultDeployChange, deployChanges } from '../../deployment/standard_deployment'
 import { FilterCreator } from '../../filter'
 import { OBJECT_TYPE_ATTRIBUTE_TYPE } from '../../constants'
@@ -26,24 +28,96 @@ import JiraClient from '../../client/client'
 
 const log = logger(module)
 
+type AttributeParams = {
+  id: string
+  name: string
+  editable: boolean
+}
+
+type AttributeGetResponse = AttributeParams[]
+
+export const DEFAULT_ATTRIBUTES = ['Key', 'Created', 'Updated']
+
+const ATTRIBUTE_RESOPNSE_SCHEME = Joi.array().items(Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required(),
+}).unknown(true)).required()
+
+const isAttributeResponse = createSchemeGuard<AttributeGetResponse>(ATTRIBUTE_RESOPNSE_SCHEME)
+
+const getExsitingAttributesNamesAndIds = async (
+  changes: AdditionChange<InstanceElement>[],
+  client: JiraClient,
+  workspaceId: string,
+):Promise<string[][]> => {
+  try {
+    const objectType = changes[0].data.after.value.objectType?.value
+    const response = await client.getSinglePage({
+      url: `/gateway/api/jsm/assets/workspace/${workspaceId}/v1/objecttype/${objectType.value.id}/attributes`,
+    })
+    if (!isAttributeResponse(response.data)) {
+      return []
+    }
+    const editableExistingAttributes = response.data
+      .filter(attribute => attribute.editable)
+      .map(attribute => [attribute.name, attribute.id])
+    return editableExistingAttributes
+  } catch (e) {
+    log.error(`failed to get existing attributes due to an error ${e}`)
+    return []
+  }
+}
+
+/*
+* This function is used to handle the case where a default attribute is added
+* and we want to deploy it as a modification change since it already exists in the service.
+*/
+const attributeAdditionOrModification = (
+  change: Change<InstanceElement>,
+  objectTypeToEditableExistiningAttributes: Record<string, string[][]>,
+): Change<InstanceElement> => {
+  if (!isAdditionChange(change)) {
+    return change
+  }
+  const instance = getChangeData(change)
+  const objectType = instance.value.objectType.elemID.getFullName()
+  const existingAttributes = Object.fromEntries(objectTypeToEditableExistiningAttributes[objectType])
+  if (existingAttributes !== undefined && Object.keys(existingAttributes).includes(instance.value.name)) {
+    change.data.after.value.id = existingAttributes[change.data.after.value.name]
+    const emptyAttributeInstance = change.data.after.clone()
+    emptyAttributeInstance.value = {}
+    return toChange({ before: emptyAttributeInstance, after: change.data.after })
+  }
+  return change
+}
+
 const deployAttributeChanges = async ({
   jsmApiDefinitions,
   changes,
   client,
   workspaceId,
+  objectTypeToEditableExistiningAttributes,
 }: {
   jsmApiDefinitions: configUtils.AdapterDuckTypeApiConfig
   changes: Change<InstanceElement>[]
   client: JiraClient
   workspaceId: string
+  objectTypeToEditableExistiningAttributes: Record<string, string[][]>
 }): Promise<Omit<DeployResult, 'extraProperties'>> => {
   const additionalUrlVars = { workspaceId }
   return deployChanges(
     changes,
     async change => {
+      if (DEFAULT_ATTRIBUTES.includes(getChangeData(change).value.name)) {
+        return
+      }
+      if (isRemovalChange(change) && getChangeData(change).value.name === 'Name') {
+        return
+      }
       const instance = getChangeData(change)
+      const modifiedChange = attributeAdditionOrModification(change, objectTypeToEditableExistiningAttributes)
       await defaultDeployChange({
-        change,
+        change: modifiedChange,
         client,
         apiDefinitions: jsmApiDefinitions,
         additionalUrlVars,
@@ -65,12 +139,15 @@ const filter: FilterCreator = ({ config, client }) => ({
   name: 'deployAttributesFilter',
   deploy: async changes => {
     const { jsmApiDefinitions } = config
-    if (!config.fetch.enableJSM || !config.fetch.enableJsmExperimental || jsmApiDefinitions === undefined) {
+    if (!config.fetch.enableJSM
+      || !config.fetch.enableJsmExperimental
+      || jsmApiDefinitions === undefined) {
       return {
         deployResult: { appliedChanges: [], errors: [] },
         leftoverChanges: changes,
       }
     }
+
     const [attributesChanges, leftoverChanges] = _.partition(
       changes.filter(isInstanceChange),
       change => isInstanceChange(change)
@@ -90,11 +167,36 @@ const filter: FilterCreator = ({ config, client }) => ({
         leftoverChanges,
       }
     }
+    const attributeAdditionChanges = changes.filter(isAdditionChange)
+      .filter(change => isInstanceChange(change))
+      .filter(change => getChangeData(change).elemID.typeName === OBJECT_TYPE_ATTRIBUTE_TYPE)
+
+    const objectTypeToAttributeAdditions = _.groupBy(attributeAdditionChanges.filter(isInstanceChange),
+      change => {
+        const instance = getChangeData(change)
+        if (!isReferenceExpression(instance.value.objectType)) {
+          return ''
+        }
+        return instance.value.objectType.elemID.getFullName()
+      })
+
+    const objectTypeToEditableExistiningAttributes: Record<string, string[][]> = Object.fromEntries(
+      await Promise.all(Object.entries(objectTypeToAttributeAdditions)
+        .map(async ([objectTypeName, attributeChanges]) =>
+          [
+            objectTypeName,
+            await getExsitingAttributesNamesAndIds(
+              attributeChanges,
+              client,
+              workspaceId,
+            )]))
+    )
     const deployResult = await deployAttributeChanges({
       jsmApiDefinitions,
       changes: attributesChanges,
       client,
       workspaceId,
+      objectTypeToEditableExistiningAttributes,
     })
 
     return {

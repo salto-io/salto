372de10e14c2bef913bed7d7b35468e30df76133
Adam Fineberg <adam.fineberg@salto.io>
2024-01-07 11:28:21 +0200

    CR Fixes

--- packages/zendesk-adapter/src/filters/guide_theme.ts
+++ packages/zendesk-adapter/src/filters/guide_theme.ts
@@ -107,11 +107,15 @@ const filterCreator: FilterCreator = ({ config, client }) => ({
         const themeElements = await unzipFolderToElements(themeZip, brandName, theme.value.name)
         theme.value.files = themeElements
       } catch (e) {
-        errors.push({
-          message: `Error fetching theme id ${theme.value.id}, ${(e as Error).message}`,
-          severity: 'Warning',
-        })
-        remove(elements, element => element.elemID.isEqual(theme.elemID))
+        if (e instanceof Error) {
+          errors.push({
+            message: `Error fetching theme id ${theme.value.id}, ${e.message}`,
+            severity: 'Warning',
+          })
+          remove(elements, element => element.elemID.isEqual(theme.elemID))
+        } else {
+          log.error('Error fetching theme id %s, %o, with stack %o', theme.value.id, e, e.stack)
+        }
       }
     }))
     return { errors }

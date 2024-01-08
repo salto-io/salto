5bf84f72da50ad10834140299d6e868a80201c0e
Adam Fineberg <adam.fineberg@salto.io>
2024-01-08 11:41:03 +0200

    SALTO-3080 Fetch guide themes (#5235)

---
+++ packages/zendesk-adapter/src/filters/guide_theme.ts
@@ -0,0 +1,125 @@
+/*
+*                      Copyright 2024 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { InstanceElement, SaltoError, StaticFile, isInstanceElement, isReferenceExpression } from '@salto-io/adapter-api'
+import { logger } from '@salto-io/logging'
+import JSZip from 'jszip'
+import _, { remove } from 'lodash'
+import { FETCH_CONFIG, isGuideEnabled, isGuideThemesEnabled } from '../config'
+import {
+  GUIDE_THEME_TYPE_NAME, ZENDESK,
+} from '../constants'
+import { FilterCreator } from '../filter'
+import { download } from './guide_themes/download'
+import { getBrandsForGuideThemes } from './utils'
+
+const log = logger(module)
+
+type ThemeFile = { filename: string; content: StaticFile }
+type ThemeDirectory = { [key: string]: ThemeFile | ThemeDirectory }
+
+const unzipFolderToElements = async (buffer: Buffer, brandName: string, name: string): Promise<ThemeDirectory> => {
+  const zip = new JSZip()
+  const unzippedContents = await zip.loadAsync(buffer)
+
+  const elements: ThemeDirectory = {}
+  await Promise.all(Object.entries(unzippedContents.files).map(async ([relativePath, file]): Promise<void> => {
+    if (!file.dir) {
+      const content = await file.async('nodebuffer')
+      _.set(elements, relativePath.split('/'), {
+        filename: relativePath,
+        content: new StaticFile({ filepath: `${ZENDESK}/themes/brands/${brandName}/${name}/${relativePath}`, content }),
+      })
+    }
+  }))
+  return elements
+}
+
+const getFullName = (instance: InstanceElement): string => instance.elemID.getFullName()
+
+const addDownloadErrors = (
+  theme: InstanceElement,
+  downloadErrors: string[]
+): SaltoError[] => ((downloadErrors.length > 0)
+  ? downloadErrors.map(e => ({
+    message: `Error fetching theme id ${theme.value.id}, ${e}`,
+    severity: 'Warning',
+  }))
+  : [{
+    message: `Error fetching theme id ${theme.value.id}, no content returned from Zendesk API`,
+    severity: 'Warning',
+  }])
+
+/**
+ * Fetches guide theme content
+ */
+const filterCreator: FilterCreator = ({ config, client }) => ({
+  name: 'guideThemesFilter',
+  onFetch: async elements => {
+    if (!isGuideEnabled(config[FETCH_CONFIG]) || !isGuideThemesEnabled(config[FETCH_CONFIG])) {
+      return undefined
+    }
+
+    const instances = elements.filter(isInstanceElement)
+    const guideThemes = instances.filter(instance => instance.elemID.typeName === GUIDE_THEME_TYPE_NAME)
+    const brands = getBrandsForGuideThemes(instances, config[FETCH_CONFIG])
+    const fullNameByNameBrand = _.mapValues(_.keyBy(brands, getFullName), 'value.name')
+    const getBrandName = (theme: InstanceElement): string | undefined => {
+      if (!isReferenceExpression(theme.value.brand_id)) {
+        log.info('brand_id is not a reference expression for instance %s.', theme.elemID.getFullName())
+        return undefined
+      }
+      const brandElemId = theme.value.brand_id?.elemID.getFullName()
+      const brandName = fullNameByNameBrand[brandElemId]
+      if (brandName === undefined) {
+        log.info('brandName was not found for instance %s.', theme.elemID.getFullName())
+        return undefined
+      }
+      return brandName
+    }
+
+    const errors: SaltoError[] = []
+    await Promise.all(guideThemes.map(async theme => {
+      const brandName = getBrandName(theme)
+      if (brandName === undefined) {
+        remove(elements, element => element.elemID.isEqual(theme.elemID))
+        return
+      }
+      const { content: themeZip, errors: downloadErrors } = await download(theme.value.id, client)
+      if (themeZip === undefined) {
+        errors.push(...addDownloadErrors(theme, downloadErrors))
+        remove(elements, element => element.elemID.isEqual(theme.elemID))
+        return
+      }
+      try {
+        const themeElements = await unzipFolderToElements(themeZip, brandName, theme.value.name)
+        theme.value.files = themeElements
+      } catch (e) {
+        if (e instanceof Error) {
+          errors.push({
+            message: `Error fetching theme id ${theme.value.id}, ${e.message}`,
+            severity: 'Warning',
+          })
+          remove(elements, element => element.elemID.isEqual(theme.elemID))
+        } else {
+          log.error('Error fetching theme id %s, %o, with stack %o', theme.value.id, e, e.stack)
+        }
+      }
+    }))
+    return { errors }
+  },
+})
+
+export default filterCreator
---
+++ packages/zendesk-adapter/test/filters/guide_theme.test.ts
@@ -0,0 +1,179 @@
+/*
+*                      Copyright 2024 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import { ElemID, InstanceElement, ObjectType, ReferenceExpression, StaticFile } from '@salto-io/adapter-api'
+import { DEFAULT_CONFIG, FETCH_CONFIG } from '../../src/config'
+import { BRAND_TYPE_NAME, GUIDE_THEME_TYPE_NAME, ZENDESK } from '../../src/constants'
+import { FilterCreator } from '../../src/filter'
+import filterCreator from '../../src/filters/guide_theme'
+import * as DownloadModule from '../../src/filters/guide_themes/download'
+import { createFilterCreatorParams } from '../utils'
+
+jest.mock('jszip', () => jest.fn().mockImplementation(() => {
+  const mockFiles = {
+    // Mocked data you expect after loading the zip file
+    'file1.txt': { async: jest.fn(() => Buffer.from('file1content')), dir: false },
+    'subfolder/file2.txt': { async: jest.fn(() => Buffer.from('file2content')), dir: false },
+  }
+  const mockCorruptedFiles = {
+    'file1.txt': { async: jest.fn(() => { throw new Error('Bad zip file') }) },
+  }
+  return {
+    loadAsync: jest.fn().mockImplementation((buffer: Buffer) => {
+      if (buffer.toString() === 'corrupted') {
+        return {
+          files: mockCorruptedFiles,
+        }
+      }
+      return {
+        files: mockFiles,
+      }
+    }),
+  }
+}))
+
+const brandType = new ObjectType({ elemID: new ElemID(ZENDESK, BRAND_TYPE_NAME) })
+const themeType = new ObjectType({ elemID: new ElemID(ZENDESK, GUIDE_THEME_TYPE_NAME) })
+
+const brand1 = new InstanceElement('brand', brandType, { id: 1, name: 'oneTwo', has_help_center: true })
+const theme1 = new InstanceElement('theme', themeType, { id: 'park?', name: 'SixFlags', brand_id: new ReferenceExpression(brand1.elemID) })
+
+describe('filterCreator', () => {
+  describe('fetch', () => {
+    describe('bad config', () => {
+      it('returns undefined if guide is not enabled', async () => {
+        expect(await filterCreator(createFilterCreatorParams({})).onFetch?.([brand1, theme1])).toBeUndefined()
+      })
+      describe('guide is enabled but themes is not', () => {
+        let filter: ReturnType<FilterCreator>
+
+        beforeEach(() => {
+          const config = { ...DEFAULT_CONFIG }
+          config[FETCH_CONFIG].guide = { brands: ['.*'], themesForBrands: [] }
+          filter = filterCreator(createFilterCreatorParams({ config }))
+        })
+
+        it('returns undefined', async () => {
+          expect(await filter.onFetch?.([brand1, theme1])).toBeUndefined()
+        })
+      })
+    })
+
+    describe('good config', () => {
+      let filter: ReturnType<FilterCreator>
+      let mockDownload: jest.SpyInstance
+
+      beforeEach(() => {
+        const config = { ...DEFAULT_CONFIG }
+        config[FETCH_CONFIG].guide = { brands: ['.*'], themesForBrands: ['.*'] }
+        filter = filterCreator(createFilterCreatorParams({ config }))
+        mockDownload = jest.spyOn(DownloadModule, 'download')
+      })
+
+      describe('theme download unsuccessful', () => {
+        describe('with custom error', () => {
+          beforeEach(() => {
+            mockDownload.mockResolvedValue({ content: undefined, errors: ['download failed specific error'] })
+          })
+
+          it('removes the theme from the elements', async () => {
+            const elements = [brand1, theme1]
+            await filter.onFetch?.(elements)
+            expect(elements).toEqual([brand1])
+          })
+
+          it('returns a warning for the theme', async () => {
+            const errors = [{ message: 'Error fetching theme id park?, download failed specific error', severity: 'Warning' }]
+            expect(await filter.onFetch?.([brand1, theme1])).toEqual({ errors })
+          })
+        })
+
+        describe('with no custom error', () => {
+          beforeEach(() => {
+            mockDownload.mockResolvedValue({ content: undefined, errors: [] })
+          })
+
+          it('removes the theme from the elements', async () => {
+            const elements = [brand1, theme1]
+            await filter.onFetch?.(elements)
+            expect(elements).toEqual([brand1])
+          })
+
+          it('returns a default warning for the theme', async () => {
+            const errors = [{ message: 'Error fetching theme id park?, no content returned from Zendesk API', severity: 'Warning' }]
+            expect(await filter.onFetch?.([brand1, theme1])).toEqual({ errors })
+          })
+        })
+      })
+
+      describe('theme download successful', () => {
+        beforeEach(() => {
+          mockDownload.mockResolvedValue({ content: Buffer.from('content'), errors: [] })
+        })
+
+        it('adds the theme files to the theme', async () => {
+          const elements = [brand1, theme1]
+          await filter.onFetch?.(elements)
+          expect(elements).toEqual([brand1, theme1])
+          expect(Object.keys(theme1.value.files)).toHaveLength(2)
+          expect(theme1.value.files['file1.txt'].filename).toEqual('file1.txt')
+          expect(theme1.value.files['file1.txt'].content).toEqual(new StaticFile({
+            filepath: `${ZENDESK}/themes/brands/oneTwo/SixFlags/file1.txt`,
+            content: Buffer.from('file1content'),
+          }))
+          expect(theme1.value.files.subfolder['file2.txt'].filename).toEqual('subfolder/file2.txt')
+          expect(theme1.value.files.subfolder['file2.txt'].content).toEqual(new StaticFile({
+            filepath: `${ZENDESK}/themes/brands/oneTwo/SixFlags/subfolder/file2.txt`,
+            content: Buffer.from('file2content'),
+          }))
+        })
+
+        it('returns no errors', async () => {
+          expect(await filter.onFetch?.([brand1, theme1])).toEqual({ errors: [] })
+        })
+
+        it('removes the theme if brand name is not found', async () => {
+          const elements = [theme1]
+          await filter.onFetch?.(elements)
+          expect(elements).toEqual([])
+        })
+
+        it('removes the theme if brand_id is not a reference expression', async () => {
+          const theme2 = new InstanceElement('theme', themeType, { id: 'park?', name: 'SixFlags', brand_id: 3 })
+          const elements = [theme2, brand1]
+          await filter.onFetch?.(elements)
+          expect(elements).toEqual([brand1])
+        })
+
+        describe('theme download corrupted', () => {
+          beforeEach(() => {
+            mockDownload.mockResolvedValue({ content: Buffer.from('corrupted'), errors: [] })
+          })
+
+          it('removes the theme from the elements', async () => {
+            const elements = [brand1, theme1]
+            await filter.onFetch?.(elements)
+            expect(elements).toEqual([brand1])
+          })
+
+          it('returns a warning for the theme', async () => {
+            const errors = [{ message: 'Error fetching theme id park?, Bad zip file', severity: 'Warning' }]
+            expect(await filter.onFetch?.([brand1, theme1])).toEqual({ errors })
+          })
+        })
+      })
+    })
+  })
+})

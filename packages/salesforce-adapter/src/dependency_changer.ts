30e6ae8974b3f1e73c1471e00ff0bf8f3cca360d
Tom Sellek <tom.sellek@salto.io>
2024-01-31 11:55:20 -0500

    Changing the deps, changing the deps (you don't know what it's like)

---
+++ packages/salesforce-adapter/src/dependency_changer.ts
@@ -0,0 +1,73 @@
+/*
+*                      Copyright 2024 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+import {
+  DependencyChanger, isInstanceChange, getChangeData, ElemID, Change, InstanceElement, DependencyChange, ChangeId,
+} from '@salto-io/adapter-api'
+import { collections, values } from '@salto-io/lowerdash'
+import { isInstanceOfCustomObjectSync } from './filters/utils'
+import { isCustomObject } from './transformers/transformer'
+
+const { awu } = collections.asynciterable
+const { isDefined } = values
+
+const generateInstanceToTypeDep = (
+  changeId: ChangeId,
+  instance: InstanceElement,
+  typeToChangeIdMap: Map<ElemID, ChangeId>
+): DependencyChange | undefined => {
+  const type = instance.getTypeSync()
+  const typeChangeId = typeToChangeIdMap.get(type.elemID)
+  if (typeChangeId === undefined) {
+    return undefined
+  }
+  return {
+    action: 'add',
+    dependency: {
+      source: changeId,
+      target: typeChangeId,
+    },
+  }
+}
+
+const dataRecordToAssociatedType: DependencyChanger = async changes => {
+  const instanceChanges: [ChangeId, Change<InstanceElement>][] = Array.from(changes.entries())
+    .filter(([, change]) => isInstanceChange(change))
+    .filter(([, change]) => (
+      isInstanceOfCustomObjectSync(getChangeData(change))
+    )) as [ChangeId, Change<InstanceElement>][]
+  const typeChanges = Array.from(changes.entries())
+    .filter(([, change]) => isCustomObject(getChangeData(change)))
+  const typeToChangeIdMap = new Map<ElemID, ChangeId>(typeChanges
+    .map(([changeId, change]) => [getChangeData(change).elemID, changeId]))
+
+  return instanceChanges
+    .map(instanceChange => generateInstanceToTypeDep(
+      instanceChange[0],
+      getChangeData(instanceChange[1]),
+      typeToChangeIdMap
+    ))
+    .filter(isDefined)
+}
+
+const DEPENDENCY_CHANGERS: DependencyChanger[] = [
+  dataRecordToAssociatedType,
+]
+
+export const dependencyChanger: DependencyChanger = async (changes, deps) => (
+  awu(DEPENDENCY_CHANGERS)
+    .flatMap(changer => changer(changes, deps))
+    .toArray()
+)

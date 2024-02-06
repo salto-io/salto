bd9bb834362d151bfc7aeb349f443f9276ef0992
Tom Sellek <tom.sellek@salto.io>
2024-02-06 13:28:20 -0500

    Handle field changes

--- packages/salesforce-adapter/src/dependency_changer.ts
+++ packages/salesforce-adapter/src/dependency_changer.ts
@@ -15,13 +15,15 @@
 */
 import {
   DependencyChanger, isInstanceChange, getChangeData, ElemID, Change, InstanceElement, DependencyChange, ChangeId,
-  isAdditionOrModificationChange,
+  isAdditionOrModificationChange, isObjectType, isField, ObjectType, Field,
 } from '@salto-io/adapter-api'
+import { logger } from '@salto-io/logging'
 import { collections, values } from '@salto-io/lowerdash'
 import { isInstanceOfCustomObjectSync, isCustomObjectSync } from './filters/utils'
 
 const { awu } = collections.asynciterable
 const { isDefined } = values
+const log = logger(module)
 
 const generateInstanceToTypeDep = (
   changeId: ChangeId,
@@ -43,6 +45,19 @@ const generateInstanceToTypeDep = (
 }
 
 const dataRecordToAssociatedType: DependencyChanger = async changes => {
+  const getAffectedType = (changedElement: Field | ObjectType): ObjectType => (
+    isObjectType(changedElement)
+      ? changedElement
+      : changedElement.parent
+  )
+  const isCustomObjectChange = (change: Change): change is Change<Field> | Change<ObjectType> => {
+    const changedElement = getChangeData(change)
+    if (!isObjectType(changedElement) && !(isField(changedElement))) {
+      return false
+    }
+    return isCustomObjectSync(getAffectedType(changedElement))
+  }
+
   // Note that we don't handle removal yet. We should probably create a reverse dependency to ensure we delete all
   // records before we delete their type.
   const customObjectInstanceChanges: [ChangeId, Change<InstanceElement>][] = Array.from(changes.entries())
@@ -52,17 +67,24 @@ const dataRecordToAssociatedType: DependencyChanger = async changes => {
       isInstanceOfCustomObjectSync(getChangeData(change))
     )) as [ChangeId, Change<InstanceElement>][]
   const typeChanges = Array.from(changes.entries())
-    .filter(([, change]) => isCustomObjectSync(getChangeData(change)))
+    .filter(([, change]) => isCustomObjectChange(change))
+  // There might be multiple changes on the same type (e.g. multiple Field changes), in which case only one of them will
+  // make it into typeElemIdToChangeIdMap, and consequently there will only be a dependency on that one change instead
+  // of on all the changes of that type. This should be OK as long as all metadata changes are in the same group.
   const typeElemIdToChangeIdMap = new Map<ElemID, ChangeId>(typeChanges
-    .map(([changeId, change]) => [getChangeData(change).elemID, changeId]))
+    .map(([changeId, change]) => (
+      [getAffectedType(getChangeData(change) as Field | ObjectType).elemID, changeId]
+    )))
 
-  return customObjectInstanceChanges
+  const deps = customObjectInstanceChanges
     .map(instanceChange => generateInstanceToTypeDep(
       instanceChange[0],
       getChangeData(instanceChange[1]),
       typeElemIdToChangeIdMap
     ))
     .filter(isDefined)
+  log.info('Created deps: %o', deps)
+  return deps
 }
 
 const DEPENDENCY_CHANGERS: DependencyChanger[] = [

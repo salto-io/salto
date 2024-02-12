cf2a5999f49ce30b1e7a3a270fdbad6fdc94cfe0
Daniel Barros <bros.ar@gmail.com>
2024-02-12 10:15:37 +0200

    some logs

--- packages/adapter-components/src/deployment/deployment.ts
+++ packages/adapter-components/src/deployment/deployment.ts
@@ -1,18 +1,21 @@
 /*
- *                      Copyright 2024 Salto Labs Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+*                      Copyright 2024 Salto Labs Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/* eslint-disable no-console */
+
 import _ from 'lodash'
 import {
   ActionName,
@@ -192,6 +195,7 @@ export const deployChange = async ({
       data: endpoint.omitRequestBody ? undefined : data,
       queryParams,
     })
+    console.log('inner response', response)
     return response.data
   } catch (error) {
     if (

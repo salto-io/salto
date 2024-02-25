c7ec8ee6cd91a59941a40fcfb873fa319f0a7e07
Adam Fineberg <adam.fineberg@salto.io>
2024-02-25 17:29:23 +0200

    Enable nestStandaloneInstances for Zendesk

--- packages/zendesk-adapter/src/config.ts
+++ packages/zendesk-adapter/src/config.ts
@@ -2798,8 +2798,7 @@ export const DEFAULT_CONFIG: ZendeskConfig = {
         fieldsToOmit: FIELDS_TO_OMIT,
         fieldsToHide: FIELDS_TO_HIDE,
         serviceIdField: DEFAULT_SERVICE_ID_FIELD,
-        // TODO: change this to true for SALTO-3593.
-        nestStandaloneInstances: false,
+        nestStandaloneInstances: true,
       },
     },
     types: DEFAULT_TYPES,

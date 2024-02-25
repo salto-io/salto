1794cd4bd1408e79d015243dd9d2e43b08c4edd0
Daniel Barros <bros.ar@gmail.com>
2024-02-25 10:50:34 +0200

    comments

--- packages/zendesk-adapter/src/fix_elements/remove_dup_users.ts
+++ packages/zendesk-adapter/src/fix_elements/remove_dup_users.ts
@@ -18,63 +18,50 @@
 import { ChangeError, InstanceElement, isInstanceElement } from '@salto-io/adapter-api'
 import { values } from '@salto-io/lowerdash'
 import _ from 'lodash'
-import { resolvePath } from '@salto-io/adapter-utils'
 import { FixElementsHandler } from './types'
 import { USER_SEGMENT_TYPE_NAME } from '../constants'
-import { ValueReplacer } from '../replacers_utils'
 
-const dupUsersRemovalWarning = (instance: InstanceElement, dupUsers: string[]): ChangeError => ({
-  elemID: instance.elemID,
+type UsersList = string[] | number[]
+type FixedElementResponse = { fixedInstance: InstanceElement; dupUsers: UsersList }
+
+const dupUsersRemovalWarning = (fixedElem: FixedElementResponse): ChangeError => ({
+  elemID: fixedElem.fixedInstance.elemID,
   severity: 'Warning',
-  message: `Duplicate appearances of ${dupUsers.length} username${dupUsers.length > 1 ? 's' : ''} in instance fields`,
-  detailedMessage: `The following usernames appear multiple times in an instance field: ${dupUsers.join(', ')}.\nIf you continue, the duplicate entries will be removed from the list.\n`,
+  message: `Duplicate appearances of ${fixedElem.dupUsers.length} username${fixedElem.dupUsers.length > 1 ? 's' : ''} in instance fields`,
+  detailedMessage: `The following usernames appear multiple times in an instance field: ${fixedElem.dupUsers.join(', ')}.\nIf you continue, the duplicate entries will be removed from the list.\n`,
 })
 
-const replaceUserSegmentUserIDs: ValueReplacer = (instance, mapping) => {
-  const fieldValuePath = instance.elemID.createNestedID('added_user_ids')
-  if (mapping !== undefined) {
-    instance.value.added_user_ids = Object.values(mapping)
-  }
-  const userIDs: string[] = instance.value.added_user_ids
-  return (userIDs || []).map((_value, i) => fieldValuePath.createNestedID(i.toString()))
-}
-
-const TYPE_NAME_TO_REPLACER_FOR_DUPS: Record<string, ValueReplacer> = {
-  [USER_SEGMENT_TYPE_NAME]: replaceUserSegmentUserIDs,
-}
-
-const removeDupUsers = (
-  instance: InstanceElement,
-): undefined | { fixedInstance: InstanceElement; dupUsers: string[] } => {
-  const userPaths = TYPE_NAME_TO_REPLACER_FOR_DUPS[instance.elemID.typeName]?.(instance)
-  const users: string[] = userPaths.map(path => resolvePath(instance, path)).filter(values.isDefined)
+const removeDupUsersFromUserSegment = (instance: InstanceElement): FixedElementResponse | undefined => {
+  const users: string[] = instance.value.added_user_ids || []
   const dupUsers = _.uniq(users.filter((item: string, index) => users.indexOf(item) !== index))
+
   if (_.isEmpty(dupUsers)) {
     return undefined
   }
 
-  const newUsersMapping: Record<string, string> = {}
-  users.forEach(user => {
-    newUsersMapping[user] = user
-  })
-
   const fixedInstance = instance.clone()
-  TYPE_NAME_TO_REPLACER_FOR_DUPS[instance.elemID.typeName]?.(fixedInstance, newUsersMapping)
+  fixedInstance.value.added_user_ids = _.uniq(users)
+
   return { fixedInstance, dupUsers }
 }
 
+const TYPE_NAME_TO_DUP_REMOVER: Record<string, (instance: InstanceElement) => FixedElementResponse | undefined> = {
+  [USER_SEGMENT_TYPE_NAME]: removeDupUsersFromUserSegment,
+}
+
+const removeDupUsers = (instance: InstanceElement): undefined | FixedElementResponse =>
+  TYPE_NAME_TO_DUP_REMOVER[instance.elemID.typeName]?.(instance)
+
 const isRelevantElement = (element: unknown): element is InstanceElement =>
-  isInstanceElement(element) && Object.keys(TYPE_NAME_TO_REPLACER_FOR_DUPS).includes(element.elemID.typeName)
+  isInstanceElement(element) && Object.keys(TYPE_NAME_TO_DUP_REMOVER).includes(element.elemID.typeName)
 
 /**
  * This fixer makes sure that there are no duplicate users in the same field
  */
 export const removeDupUsersHandler: FixElementsHandler = () => async elements => {
   const fixedElementsWithUserCount = elements.filter(isRelevantElement).map(removeDupUsers).filter(values.isDefined)
+  const errors = fixedElementsWithUserCount.map(dupUsersRemovalWarning)
 
-  const errors = fixedElementsWithUserCount.map(({ fixedInstance, dupUsers }) =>
-    dupUsersRemovalWarning(fixedInstance, dupUsers),
-  )
   return {
     fixedElements: fixedElementsWithUserCount.map(({ fixedInstance }) => fixedInstance),
     errors,

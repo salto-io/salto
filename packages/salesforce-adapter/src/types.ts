30b86368b2ff360a8d180a5fd77838f8026bd343
tomsellek <117099820+tomsellek@users.noreply.github.com>
2024-02-26 16:45:42 -0500

    SALTO-4690 - Salesforce: CV for modification/deletion of CurrencyIsoCode instance (#5465)

--- packages/salesforce-adapter/src/types.ts
+++ packages/salesforce-adapter/src/types.ts
@@ -158,6 +158,7 @@ export type ChangeValidatorName =
   | 'standardFieldOrObjectAdditionsOrDeletions'
   | 'deletedNonQueryableFields'
   | 'instanceWithUnknownType'
+  | 'artificialTypes'
 
 type ChangeValidatorConfig = Partial<Record<ChangeValidatorName, boolean>>
 
@@ -871,6 +872,7 @@ const changeValidatorConfigType =
       },
       deletedNonQueryableFields: { refType: BuiltinTypes.BOOLEAN },
       instanceWithUnknownType: { refType: BuiltinTypes.BOOLEAN },
+      artificialTypes: { refType: BuiltinTypes.BOOLEAN },
     },
     annotations: {
       [CORE_ANNOTATIONS.ADDITIONAL_PROPERTIES]: false,

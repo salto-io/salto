48ad4dd1e236563d963bd435ea488d8c2dfcb803
Eden Hassid <edenhassid@gmail.com>
2024-03-17 11:58:34 +0200

    add a test for coverage

--- packages/dummy-adapter/test/adapter.test.ts
+++ packages/dummy-adapter/test/adapter.test.ts
@@ -142,4 +142,21 @@ describe('dummy adapter', () => {
       expect(await adapterWithDeployModifiers.deployModifiers.changeValidator?.([myInst1Change])).toHaveLength(0)
     })
   })
+  describe('fixElements', () => {
+    it('should return correct value', async() => {
+      const mockReporter = { reportProgress: jest.fn() }
+      const fetchResult = await adapter.fetch({ progressReporter: mockReporter })
+      const fullInst1 = fetchResult.elements.find(e => e.elemID.getFullName() === 'dummy.Full.instance.FullInst1')
+      if (!isInstanceElement(fullInst1)){
+        return
+      }
+      const cloned = fullInst1.clone()
+      delete cloned.value.strField
+      const fixed = await adapter.fixElements(fetchResult.elements)
+      expect(fixed.fixedElements).toEqual([
+        cloned
+      ])
+      expect(fixed.errors).toHaveLength(1)
+    })
+  })
 })

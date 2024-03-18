abdae1981ec3c741326ac0cc8b4320f878d79e84
Tom Sellek <tom.sellek@salto.io>
2024-03-18 11:59:13 -0400

    Unit tests

--- packages/dummy-adapter/test/adapter_creator.test.ts
+++ packages/dummy-adapter/test/adapter_creator.test.ts
@@ -13,11 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import fs from 'fs'
+import { logger } from '@salto-io/logging'
 import { ObjectType, InstanceElement } from '@salto-io/adapter-api'
-import { buildElementsSourceFromElements } from '@salto-io/adapter-utils'
+import { buildElementsSourceFromElements, safeJsonStringify } from '@salto-io/adapter-utils'
 import { adapter } from '../src/adapter_creator'
 import { defaultParams, DUMMY_ADAPTER } from '../src/generator'
 import DummyAdapter from '../src/adapter'
+
+const log = logger(module)
+
+jest.mock('fs', () => ({
+  ...jest.requireActual('fs'),
+  readFileSync: jest.fn(),
+}))
+
+const mockedFs = fs as jest.Mocked<typeof fs>
 
 describe('adapter creator', () => {
   it('should return a config containing all of the generator params', () => {
@@ -49,5 +60,54 @@ describe('adapter creator', () => {
         elementsSource: buildElementsSourceFromElements([]),
       }),
     ).toBeInstanceOf(DummyAdapter)
+  })
+  describe('loadElementsFromFolder', () => {
+    describe('When the path does not exist', () => {
+      it('should throw', async () => {
+        await expect(async () => {
+          await adapter.loadElementsFromFolder?.({
+            baseDir: 'this_path_does_not_exist',
+            elementsSource: buildElementsSourceFromElements([]),
+          })
+        }).rejects.toThrow()
+      })
+    })
+    describe('When the path exists and contains a valid config', () => {
+      let configFilePath: string
+      const adapterOpsFetchSpy = jest.spyOn(DummyAdapter.prototype, 'fetch')
+      beforeEach(async () => {
+        const originalReadFileSync = jest.requireActual('fs').readFileSync
+        mockedFs.readFileSync
+          .mockImplementationOnce((path): string => {
+            configFilePath = path as string
+            return safeJsonStringify({
+              seed: 0,
+              numOfPrimitiveTypes: 1,
+              numOfTypes: 1,
+              numOfObjs: 1,
+              numOfRecords: 1,
+              fakeConfig: true,
+            })
+          })
+          .mockImplementation((path, encoding) => {
+            log.debug('Redirecting %s', path)
+            return originalReadFileSync(path, encoding)
+          })
+        await adapter.loadElementsFromFolder?.({
+          baseDir: 'some_path',
+          elementsSource: buildElementsSourceFromElements([]),
+        })
+      })
+      afterEach(() => {
+        jest.clearAllMocks()
+        jest.resetAllMocks()
+      })
+      it('should fetch elements from the correct dir', () => {
+        expect(configFilePath).toEqual('some_path/dummy.nacl')
+      })
+      it('should call fetch for the provided dir', () => {
+        expect(adapterOpsFetchSpy).toHaveBeenCalled()
+      })
+    })
   })
 })

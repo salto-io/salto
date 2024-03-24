f7014a3373466534ca12638ee8bafaf2d864756c
Israel Braverman <israel.braverman@salto.io>
2024-03-24 16:36:33 +0200

    SALTO-5441: create change validator for issue layout

--- packages/jira-adapter/src/config/config.ts
+++ packages/jira-adapter/src/config/config.ts
@@ -210,6 +210,7 @@ export type ChangeValidatorName =
   | 'readOnlyWorkflow'
   | 'dashboardGadgets'
   | 'dashboardLayout'
+  | 'issueLayouts'
   | 'permissionType'
   | 'automations'
   | 'activeSchemeDeletion'
@@ -269,6 +270,7 @@ const changeValidatorConfigType = createMatchingObjectType<ChangeValidatorConfig
     readOnlyWorkflow: { refType: BuiltinTypes.BOOLEAN },
     dashboardGadgets: { refType: BuiltinTypes.BOOLEAN },
     dashboardLayout: { refType: BuiltinTypes.BOOLEAN },
+    issueLayouts: { refType: BuiltinTypes.BOOLEAN },
     permissionType: { refType: BuiltinTypes.BOOLEAN },
     automations: { refType: BuiltinTypes.BOOLEAN },
     activeSchemeDeletion: { refType: BuiltinTypes.BOOLEAN },

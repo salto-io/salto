107629d2cf821769fc20a346c4d726a3ca735367
ido amit <ido.amit@salto.io>
2024-04-04 10:24:01 +0300

    SALTO-5732-FixAssetsComponentInAutomation

--- packages/jira-adapter/src/filters/automation/automation_deployment.ts
+++ packages/jira-adapter/src/filters/automation/automation_deployment.ts
@@ -117,7 +117,7 @@ export type Component = {
 const ASSET_COMPONENT_SCHEME = Joi.object({
   value: Joi.object({
     schemaId: Joi.required(),
-  }).unknown(true),
+  }).unknown(true).required(),
 }).unknown(true)
 
 export const isAssetComponent = createSchemeGuard<Component>(ASSET_COMPONENT_SCHEME)

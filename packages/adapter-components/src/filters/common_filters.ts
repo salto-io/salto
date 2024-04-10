548d39b5433a5e0696fe2481673d0e0a48b59f0d
Neta Marcus <netam.code@gmail.com>
2024-04-10 11:52:26 +0300

    SALTO-5406 pass in custom serialization mapping to delayed deploy resolution as well

--- packages/adapter-components/src/filters/common_filters.ts
+++ packages/adapter-components/src/filters/common_filters.ts
@@ -16,15 +16,14 @@
 import { createSaltoElementErrorFromError, isSaltoElementError, isSaltoError } from '@salto-io/adapter-api'
 import { APIDefinitionsOptions, ApiDefinitions, ResolveCustomNameMappingOptionsType, UserConfig } from '../definitions'
 import { AdapterFilterCreator, FilterResult } from '../filter_utils'
-import { FieldReferenceDefinition, FieldReferenceResolver } from '../references'
+import { FieldReferenceDefinition } from '../references'
 import { hideTypesFilterCreator } from './hide_types'
 import { defaultDeployFilterCreator } from './default_deploy'
-import { fieldReferencesFilterCreator } from './field_references'
+import { FieldReferenceResolverCreator, fieldReferencesFilterCreator } from './field_references'
 import { queryFilterCreator } from './query'
 import {
   ResolveReferenceSerializationStrategyLookup,
   ResolveReferenceContextStrategiesType,
-  ResolveReferenceIndexNames,
 } from '../definitions/system/api'
 import { referencedInstanceNamesFilterCreator } from './referenced_instance_names'
 import { serviceUrlFilterCreator } from './service_url'
@@ -46,16 +45,7 @@ export const createCommonFilters = <
   >[]
   config: Co
   definitions: ApiDefinitions<Options>
-  fieldReferenceResolverCreator?: (
-    def: FieldReferenceDefinition<
-      ResolveReferenceContextStrategiesType<Options>,
-      ResolveReferenceSerializationStrategyLookup<Options>
-    >,
-  ) => FieldReferenceResolver<
-    ResolveReferenceContextStrategiesType<Options>,
-    ResolveReferenceSerializationStrategyLookup<Options>,
-    ResolveReferenceIndexNames<Options>
-  >
+  fieldReferenceResolverCreator?: FieldReferenceResolverCreator<Options>
 }): Record<string, AdapterFilterCreator<Co, FilterResult, {}, Options>> => ({
   // TODO SALTO-5421 finish upgrading filters to new def structure and add remaining shared filters
   hideTypes: hideTypesFilterCreator(),
@@ -75,5 +65,6 @@ export const createCommonFilters = <
       }
       return createSaltoElementErrorFromError({ error, severity: 'Error', elemID })
     },
+    fieldReferenceResolverCreator,
   }),
 })

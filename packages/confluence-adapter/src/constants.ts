24a6fa3e92a3d9f6fdec18403ef0a3da31bcfa81
omrifarkash <63018798+omrifarkash@users.noreply.github.com>
2024-04-14 14:49:10 +0300

    SALTO-5742 confluence template content to be template expression (#5745)

--- packages/confluence-adapter/src/constants.ts
+++ packages/confluence-adapter/src/constants.ts
@@ -14,3 +14,13 @@
  * limitations under the License.
  */
 export const ADAPTER_NAME = 'confluence'
+export const TEMPLATE_TYPE_NAME = 'template'
+export const GLOBAL_TEMPLATE_TYPE_NAME = 'global_template'
+export const TEMPLATE_TYPE_NAMES = [TEMPLATE_TYPE_NAME, GLOBAL_TEMPLATE_TYPE_NAME]
+export const SPACE_TYPE_NAME = 'space'
+export const PAGE_TYPE_NAME = 'page'
+export const LABEL_TYPE_NAME = 'label'
+export const BLOG_POST_TYPE_NAME = 'blog_post'
+export const SPACE_SETTINGS_TYPE_NAME = 'space_settings'
+export const RESTRICTION_TYPE_NAME = 'restriction'
+export const PERMISSION_TYPE_NAME = 'permission'
--- packages/confluence-adapter/src/error_utils.ts
+++ packages/confluence-adapter/src/error_utils.ts
@@ -17,27 +17,46 @@
 import _ from 'lodash'
 import { isSaltoElementError, isSaltoError } from '@salto-io/adapter-api'
 import { deployment } from '@salto-io/adapter-components'
+import { logger } from '@salto-io/logging'
+
+const log = logger(module)
+
+type SuppressedErrorChecker = (err: Error) => boolean
+
+const isJavaNullPointerErrorOrUndefined: SuppressedErrorChecker = err => {
+  const status = _.get(err, 'response.status')
+  const message = _.get(err, 'response.data.message')
+  return status === 500 && message.startsWith('java.lang.NullPointerException: Cannot invoke')
+}
+
+const SUPPRESSED_ERRORS_CHECKER_LIST: SuppressedErrorChecker[] = [isJavaNullPointerErrorOrUndefined]
+
+const shouldSuppressError = (err: Error): boolean => SUPPRESSED_ERRORS_CHECKER_LIST.some(checkFunc => checkFunc(err))
+
+const getWrongVersionErrorOrUndefined = (err: Error): string | undefined => {
+  const errorsArray = _.get(err, 'response.data.errors')
+  const status = _.get(err, 'response.status')
+  if (
+    status === 409 &&
+    Array.isArray(errorsArray) &&
+    errorsArray.length > 0 &&
+    _.isString(errorsArray[0].title) &&
+    errorsArray[0].title.startsWith('Version')
+  ) {
+    return errorsArray[0].title
+  }
+  return undefined
+}
+const getErrorMessage = (err: Error): string => getWrongVersionErrorOrUndefined(err) ?? err.message
 
 export const customConvertError: deployment.ConvertError = (elemID, error) => {
   if (isSaltoError(error) && isSaltoElementError(error)) {
     return error
   }
-  const isWrongVersionError = (
-    err: unknown,
-  ): err is { response: { data: { errors: { title: string }[] }; status: string } } => {
-    const errorsArray = _.get(err, 'response.data.errors')
-    const status = _.get(err, 'response.status')
-    return (
-      status === 409 &&
-      Array.isArray(errorsArray) &&
-      errorsArray.length > 0 &&
-      _.isString(errorsArray[0].title) &&
-      errorsArray[0].title.startsWith('Version')
-    )
+  if (shouldSuppressError(error)) {
+    log.debug('Suppressing error: %s', error)
+    return undefined
   }
-  const extractErrorMessageFromErrorArray = (err: unknown): string | undefined =>
-    isWrongVersionError(err) ? err.response.data.errors[0].title : undefined
 
-  const message = extractErrorMessageFromErrorArray(error) ?? error.message
-  return { elemID, message, severity: 'Error' }
+  return { elemID, message: getErrorMessage(error), severity: 'Error' }
 }

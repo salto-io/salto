438a1612d8c76046d70bebcef3b577d6c59442b6
Ido Zak <ido.zak@salto.io>
2024-04-24 09:12:30 +0300

    prettier

--- packages/jira-adapter/test/filters/layouts/issue_layout.test.ts
+++ packages/jira-adapter/test/filters/layouts/issue_layout.test.ts
@@ -326,12 +326,12 @@ describe('issue layout filter', () => {
     it('should not fail if the project does not have an issueTypeScheme', async () => {
       projectInstance1.value.issueTypeScheme = undefined
       const res = await getLayoutRequestsAsync(client, config, fetchQuery, elements)
-      expect(res).toEqual({11111: {}})
+      expect(res).toEqual({ 11111: {} })
     })
     it('should not fail if issueTypeScheme does not have issueTypeIds', async () => {
       issueTypeSchemeInstance1.value.issueTypeIds = undefined
       const res = await getLayoutRequestsAsync(client, config, fetchQuery, elements)
-      expect(res).toEqual({11111: {}})
+      expect(res).toEqual({ 11111: {} })
     })
     it('should not fetch issue layouts if it disabled', async () => {
       const configWithEnableFalse = getDefaultConfig({ isDataCenter: false })
@@ -520,8 +520,7 @@ describe('issue layout filter', () => {
     })
     it('should not fail if a project does not have any screens', async () => {
       adapterContext.layoutsPromise = {
-        11111: {
-        },
+        11111: {},
       }
       await layoutFilter.onFetch(elements)
       expect(elements.filter(isInstanceElement).find(e => e.elemID.typeName === ISSUE_LAYOUT_TYPE)).toBeUndefined()

8ced0346b6338b93ba87d8081b30ee94f37230b4
Uri Bar <uri.bar@salto.io>
2024-05-08 10:57:22 +0300

    wip

---
+++ packages/google-workspace-adapter/e2e_test/jest_environment.ts
@@ -0,0 +1,54 @@
+/*
+ *                      Copyright 2024 Salto Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {
+  createEnvUtils,
+  CredsSpec,
+  JestEnvironmentConstructorArgs,
+  SaltoE2EJestEnvironment,
+} from '@salto-io/e2e-credentials-store'
+import { logger } from '@salto-io/logging'
+import { Credentials } from '../src/auth'
+
+const log = logger(module)
+
+export const credsSpec = (envName?: string): CredsSpec<Required<Credentials>> => {
+  const addEnvName = (varName: string): string => (envName === undefined ? varName : [varName, envName].join('_'))
+  const googleWorkspaceClientIdVarName = addEnvName('GOOGLE_WORKSPACE_CLIENT_ID')
+  const googleWorkspaceClientSecretVarName = addEnvName('GOOGLE_WORKSPACE_CLIENT_SECRET')
+  const googleWorkspaceRefreshTokenVarName = addEnvName('GOOGLE_WORKSPACE_REFRESH_TOKEN')
+  return {
+    envHasCreds: env => googleWorkspaceClientIdVarName in env,
+    fromEnv: env => {
+      const envUtils = createEnvUtils(env)
+      return {
+        clientId: envUtils.required(googleWorkspaceClientIdVarName),
+        clientSecret: envUtils.required(googleWorkspaceClientSecretVarName),
+        refreshToken: envUtils.required(googleWorkspaceRefreshTokenVarName),
+      }
+    },
+    validate: async (_creds: Credentials): Promise<void> => {
+      // TODO
+    },
+    typeName: 'google_workspace',
+    globalProp: envName ? `google_workspace_${envName}` : 'google_workspace',
+  }
+}
+
+export default class GoogleWorkspaceE2EJestEnvironment extends SaltoE2EJestEnvironment {
+  constructor(...args: JestEnvironmentConstructorArgs) {
+    super({ logBaseName: log.namespace }, ...args)
+  }
+}

3a9dcaa5365111d8051704e80478a69b9c47ee21
benisraeladi <37732337+benisraeladi@users.noreply.github.com>
2024-05-12 10:38:17 +0300

    Revert "SALTO-5516 order apps field in workspace by ID as well as position" (#5887)
    This reverts commit ae857deb19b9083f3d905a73ae1a988a8ccf8e69.

--- packages/zendesk-adapter/src/filters/unordered_lists.ts
+++ packages/zendesk-adapter/src/filters/unordered_lists.ts
@@ -265,9 +265,8 @@ const orderAppInstallationsInWorkspace = (instances: InstanceElement[]): void =>
     .forEach(workspace => {
       const appsList = workspace.value.apps
       if (_.isArray(appsList)) {
-        workspace.value.apps = _.sortBy(appsList, 'position', app =>
-          isReferenceExpression(app.id) ? app.id.elemID.getFullName() : undefined,
-        )
+        // _.sortBy is stable, so the order of apps with the same position will not change
+        workspace.value.apps = _.sortBy(appsList, ['position'])
       } else if (appsList !== undefined) {
         log.warn(
           `orderAppInstallationsInWorkspace - app installations are not a list in ${appsList.elemID.getFullName()}`,

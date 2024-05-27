2f9e6842ed052971fb30525ca400c7fd2f66f061
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-05-27 16:54:55 +0300

    add type name to error handler

--- packages/adapter-components/src/definitions/system/fetch/index.ts
+++ packages/adapter-components/src/definitions/system/fetch/index.ts
@@ -18,7 +18,7 @@
 export { ContextCombinationDefinition } from './dependencies'
 export { FetchApiDefinitions, InstanceFetchApiDefinitions, FetchApiDefinitionsOptions } from './fetch'
 export { FetchRequestDefinition } from './request'
-export { ResourceTransformFunc } from './resource'
+export { ResourceTransformFunc, OnErrorHandlerAction } from './resource'
 export {
   ElementFieldCustomization,
   FieldIDPart,

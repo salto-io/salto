3bd542d844236359c76932dde82bbce8de4bcde6
Eshed Gal-Or <eshed.gal-or@salto.io>
2024-06-09 17:15:26 +0300

    import yarn plugins if necessary

--- build_utils/turbo_run.sh
+++ build_utils/turbo_run.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
+TURBO_CONCURRENCY="${TURBO_CONCURRENCY:-8}"
 
-if [ -z "$TURBO_TOKEN" ]; then
-  yarn run "$@"
-else
-  yarn turbo run "$@" \
---token="${TURBO_TOKEN:-NOPE}" \
---team="${TURBO_TEAM:-SaltoDev}" \
---concurrency="${TURBO_CONCURRENCY:-4}"
+if [ -n "$CI" ]; then
+
+  # new TURBO UI crashes when running in CI context
+  export TURBO_UI=false
+
+  # avoid creating local cache in CI (because we don't want to persist it)
+  export TURBO_REMOTE_ONLY=true
+
+  # running in CI with 100% CPU will OOM
+  TURBO_CONCURRENCY="50%"
+
+  container_cpu_quota="/sys/fs/cgroup/cpu/cpu.cfs_quota_us"
+  container_cpu_scale="/sys/fs/cgroup/cpu/cpu.cfs_period_us"
+
+  if [ -f "$container_cpu_quota" ] && [ -f "$container_cpu_scale" ]; then
+
+    # if CI runs in containers, so finding the total allotted CPU is trickier
+    TURBO_CONCURRENCY="$(awk -v quota="$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)" -v period="$(cat /sys/fs/cgroup/cpu/cpu.cfs_period_us)" 'BEGIN { printf "%.0f\n", (quota / period) * 0.5 }')"
+
+  fi
+
 fi
 
+yarn turbo run "$@" \
+  --token="${TURBO_TOKEN:-NOPE}" \
+  --team="${TURBO_TEAM:-SaltoDev}" \
+  --concurrency="$TURBO_CONCURRENCY"

3629b8359a395a18e4435ec7d3c99772c5dbc54b
Daniel Barros <bros.ar@gmail.com>
2024-06-09 14:39:19 +0300

    formatting

--- packages/adapter-components/src/fetch/element/instance_utils.ts
+++ packages/adapter-components/src/fetch/element/instance_utils.ts
@@ -170,17 +170,6 @@ export const getInstanceCreationFunctions = <Options extends FetchApiDefinitions
   return { toElemName, toPath }
 }
 
-const removeNullValuesTransformFunc: TransformFuncSync = ({ value }) => (value === null ? undefined : value)
-
-const removeNullValues = (values: Values, type: ObjectType, allowEmpty = false): Values =>
-  transformValuesSync({
-    values,
-    type,
-    transformFunc: removeNullValuesTransformFunc,
-    strict: false,
-    allowEmpty,
-  }) ?? {}
-
 /**
  * Generate an instance for a single entry returned for a given type, and set its elem id and path.
  * Assuming the entry is already in its final structure (after running to InstanceValue).

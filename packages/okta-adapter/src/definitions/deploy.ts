77401330aa5b8a61c3cabb539eda13a7653aa63c
Amir Rachum <amir.rachum@salto.io>
2024-06-17 11:58:22 +0300

    Fix modify.

--- packages/okta-adapter/src/definitions/deploy.ts
+++ packages/okta-adapter/src/definitions/deploy.ts
@@ -43,7 +43,7 @@ const createCustomizations = (): Record<string, InstanceDeployApiDefinitions> =>
                 method: 'put',
               },
               context: {
-                groupId: 'id',
+                groupId: '{id}',
               },
             },
           },

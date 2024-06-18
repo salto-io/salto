1b77e0fccfb359c10a1aecc432e7ef8a36a1805d
Eshed Gal-Or <eshed.gal-or@salto.io>
2024-06-18 17:39:24 +0300

    fix vscode package step

--- packages/vscode/scripts/package.sh
+++ packages/vscode/scripts/package.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+
 set -xeuo pipefail
 shopt -s extglob
 
+yarn workspaces focus salto-vscode
+
 mkdir -p ./tmp_pkg && cp ../../LICENSE . && vsce package --yarn -o ./tmp_pkg/salto.vsix && rm -f LICENSE
-cd ./tmp_pkg && unzip salto.vsix
-cp -r ../node_modules/!(@salto-io) extension/node_modules/
+
+pushd ./tmp_pkg 
+unzip salto.vsix
+mkdir -p extension/node_modules/
+rsync -a --exclude '@salto-io' ../../../node_modules extension/node_modules/
 zip -ur salto.vsix extension
-cd ..
+popd
+
 mkdir -p ./pkg
-cp ./tmp_pkg/salto.vsix ./pkg/salto.vsix
+mv ./tmp_pkg/salto.vsix ./pkg/salto.vsix
+
 rm -rf ./tmp_pkg

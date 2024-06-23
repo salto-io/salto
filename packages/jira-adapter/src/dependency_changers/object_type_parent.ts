e59427eb6a5a99f0912c6e74e1d57998b871c571
IdoZakk <109526049+IdoZakk@users.noreply.github.com>
2024-06-23 17:18:20 +0300

    SALTO-5981: Change dependency of object types (#6070)
    * SALTO-5981: Change dependency of object types

---
+++ packages/jira-adapter/src/dependency_changers/object_type_parent.ts
@@ -0,0 +1,58 @@
+/*
+ *                      Copyright 2024 Salto Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  DependencyChanger,
+  InstanceElement,
+  RemovalChange,
+  dependencyChange,
+  getChangeData,
+  isInstanceChange,
+  isRemovalChange,
+} from '@salto-io/adapter-api'
+import { collections } from '@salto-io/lowerdash'
+import { deployment } from '@salto-io/adapter-components'
+import { OBJECT_TYPE_TYPE } from '../constants'
+
+type SetId = collections.set.SetId
+type RemovalInstanceChangeWithKey = deployment.dependency.ChangeWithKey<RemovalChange<InstanceElement>>
+
+/*
+ * This dependency changer is used to reverse the dependency between object types and their non salto parent on removal.
+ * This way the behavior will be the same as in salto's parents. Without it a cycle will be created with the order object
+ */
+export const objectTypeParentDependencyChanger: DependencyChanger = async changes => {
+  const objectTypeChanges = Array.from(changes.entries())
+    .map(([key, change]) => ({ key, change }))
+    .filter(({ change }) => isRemovalChange(change))
+    .filter(({ change }) => getChangeData(change).elemID.typeName === OBJECT_TYPE_TYPE)
+    .filter((change): change is RemovalInstanceChangeWithKey => isInstanceChange(change.change))
+
+  const fullNameToChangeKey = Object.fromEntries(
+    objectTypeChanges.map(({ key, change }) => [getChangeData(change).elemID.getFullName(), key]),
+  )
+  return objectTypeChanges
+    .filter(({ change }) => getChangeData(change).value.parentObjectTypeId?.elemID?.typeName === OBJECT_TYPE_TYPE)
+    .flatMap(({ change, key }) => {
+      const parentObjectTypeKey = fullNameToChangeKey[
+        getChangeData(change).value.parentObjectTypeId.elemID.getFullName()
+      ] as SetId
+
+      return parentObjectTypeKey === undefined
+        ? []
+        : [dependencyChange('add', key, parentObjectTypeKey), dependencyChange('remove', parentObjectTypeKey, key)]
+    })
+}

3770f98fbeed5bf842b07652ad2f8c208dda90c3
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-06-26 19:19:02 +0300

    SALTO-5783:  Support deploying Users in Okta (#6121)

--- packages/okta-adapter/src/definitions/fetch/fetch.ts
+++ packages/okta-adapter/src/definitions/fetch/fetch.ts
@@ -35,7 +35,6 @@ import { isGroupPushEntry } from '../../filters/group_push'
 import { extractSchemaIdFromUserType } from './types/user_type'
 import { isNotMappingToAuthenticatorApp } from './types/profile_mapping'
 import { assignPolicyIdsToApplication } from './types/application'
-import { OMIT_CREDS_HEADER } from '../../user_utils'
 
 const DEFAULT_FIELDS_TO_OMIT: Record<string, definitions.fetch.ElementFieldCustomization> = {
   created: { omit: true },
@@ -989,8 +988,8 @@ const createCustomizations = ({
     requests: [
       {
         endpoint: {
-          path: '/api/v1/users',
-          headers: OMIT_CREDS_HEADER,
+          // The search query is needed to fetch deprovisioned users
+          path: '/api/v1/users?search=id+pr',
         },
       },
     ],
@@ -1009,6 +1008,14 @@ const createCustomizations = ({
         activated: { omit: true },
         _links: { omit: true },
         type: { fieldType: 'UserTypeRef' },
+      },
+    },
+  },
+  UserCredentials: {
+    element: {
+      fieldCustomizations: {
+        recovery_question: { omit: true },
+        password: { omit: true },
       },
     },
   },

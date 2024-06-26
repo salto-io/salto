40b3378a17b9b3ad72d09fd518857f4f58e1abda
Amir Rachum <amir.rachum@salto.io>
2024-06-26 16:01:23 +0300

    Fix test.

--- packages/intercom-adapter/test/fetch/transforms/replaceFieldWithNestedValue.test.ts
+++ packages/intercom-adapter/test/fetch/transforms/replaceFieldWithNestedValue.test.ts
@@ -24,27 +24,27 @@ describe('replaceFieldWithNestedValue', () => {
   }
 
   describe('When the value is not an object', () => {
-    it('Should throw an error', () => {
+    it('Should throw an error', async () => {
       const fieldAdjustment = { fieldName: 'field1', nestedField: 'nestedField1', fallbackValue: 'fallback1' }
-      expect(() => replaceFieldWithNestedValue(fieldAdjustment)({ ...generatedItem, value: 'item1' })).toThrow(
+      await expect(async () => replaceFieldWithNestedValue(fieldAdjustment)({ ...generatedItem, value: 'item1' })).rejects.toThrow(
         'Unexpected item value: "item1", expected object',
       )
     })
   })
 
   describe('When the field does not exist in the object', () => {
-    it('Should return the object with the fallback value', () => {
+    it('Should return the object with the fallback value', async () => {
       const fieldAdjustment = { fieldName: 'field1', nestedField: 'nestedField1', fallbackValue: 'fallback1' }
-      const result = replaceFieldWithNestedValue(fieldAdjustment)(generatedItem)
+      const result = await replaceFieldWithNestedValue(fieldAdjustment)(generatedItem)
       expect(result.value).toEqual({ field1: 'fallback1' })
     })
   })
 
   describe('When the field exists in the object', () => {
-    it('Should return the object with the field replaced with the nested field', () => {
+    it('Should return the object with the field replaced with the nested field', async () => {
       const fieldAdjustment = { fieldName: 'field1', nestedField: 'nestedField1', fallbackValue: 'fallback1' }
       const value = { field1: { nestedField1: 'value1' } }
-      const result = replaceFieldWithNestedValue(fieldAdjustment)({ ...generatedItem, value })
+      const result = await replaceFieldWithNestedValue(fieldAdjustment)({ ...generatedItem, value })
       expect(result.value).toEqual({ field1: 'value1' })
     })
   })

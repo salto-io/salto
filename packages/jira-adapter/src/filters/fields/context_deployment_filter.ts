8f7d6e2e4aa1095ac9b7c76df01aee402ec4cc3e
Seggev Haimovich <seggevhaimovich@gmail.com>
2024-07-04 10:47:04 +0300

    all works, still without CV on same option value

--- packages/jira-adapter/src/filters/fields/context_deployment_filter.ts
+++ packages/jira-adapter/src/filters/fields/context_deployment_filter.ts
@@ -13,14 +13,105 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Element, getChangeData, isInstanceChange, isRemovalChange } from '@salto-io/adapter-api'
+import {
+  Element,
+  InstanceElement,
+  SaltoElementError,
+  createSaltoElementError,
+  getChangeData,
+  isInstanceChange,
+  isRemovalChange,
+  isModificationChange,
+  isReferenceExpression,
+  Change,
+  toChange,
+  Values,
+  ReferenceExpression,
+  DeployResult,
+} from '@salto-io/adapter-api'
 import _ from 'lodash'
-import { hasValidParent } from '@salto-io/adapter-utils'
+import { values } from '@salto-io/lowerdash'
+import { getParent, hasValidParent } from '@salto-io/adapter-utils'
 import { FilterCreator } from '../../filter'
 import { deployContextChange, setContextDeploymentAnnotations } from './contexts'
 import { deployChanges } from '../../deployment/standard_deployment'
-import { FIELD_CONTEXT_TYPE_NAME, FIELD_TYPE_NAME } from './constants'
+import { FIELD_CONTEXT_OPTION_TYPE_NAME, FIELD_CONTEXT_TYPE_NAME, FIELD_TYPE_NAME } from './constants'
 import { findObject, setFieldDeploymentAnnotations } from '../../utils'
+import { CONTEXT_NAME_FIELD } from './field_context_option_split'
+
+type BeforeOrAfter = 'before' | 'after'
+
+const getOptionFromReference = (
+  optionRef: ReferenceExpression,
+  index: number,
+  optionFullNameToChangeRecord: Record<string, Change<InstanceElement>>,
+  beforeOrAfter: BeforeOrAfter,
+): [string, Values] | undefined => {
+  if (Object.prototype.hasOwnProperty.call(optionFullNameToChangeRecord, optionRef.elemID.getFullName())) {
+    const optionChange = optionFullNameToChangeRecord[optionRef.elemID.getFullName()]
+    if (Object.prototype.hasOwnProperty.call(optionChange.data, beforeOrAfter)) {
+      const option = _.get(optionChange.data, beforeOrAfter)
+      const optionValue = _.omit(option.value, CONTEXT_NAME_FIELD)
+      return [optionValue.value, { ...optionValue, position: index + 1 }]
+    }
+    return undefined
+  }
+  const option = optionRef.value
+  return [option.value.value, { ...option.value, position: index + 1 }]
+}
+const setOptionsToOriginal = (
+  context: InstanceElement | undefined,
+  beforeOrAfter: BeforeOrAfter,
+  optionFullNameToChangeRecord: Record<string, Change<InstanceElement>>,
+): void => {
+  if (context === undefined) {
+    return
+  }
+  if (!Array.isArray(context.value.options)) {
+    return
+  }
+  context.value.options = Object.fromEntries(
+    context.value.options
+      .filter(isReferenceExpression)
+      .map((optionRef, index) => getOptionFromReference(optionRef, index, optionFullNameToChangeRecord, beforeOrAfter))
+      .filter(values.isDefined),
+  )
+}
+const getContextChangeWithFixedOptions = (
+  context: Change<InstanceElement>,
+  optionFullNameToChangeRecord: Record<string, Change<InstanceElement>>,
+): Change<InstanceElement> => {
+  if (isRemovalChange(context)) {
+    return context
+  }
+  const before = isModificationChange(context) ? context.data.before.clone() : undefined
+  const after = context.data.after.clone()
+  setOptionsToOriginal(before, 'before', optionFullNameToChangeRecord)
+  setOptionsToOriginal(after, 'after', optionFullNameToChangeRecord)
+  return toChange({ before, after })
+}
+
+const getOptionDeplymentResults = (
+  deployResult: DeployResult,
+  optionChanges: Change<InstanceElement>[],
+): { successOptionChanges: Change<InstanceElement>[]; optionErrors: SaltoElementError[] } => {
+  const successContextChanges = new Set<string>(
+    deployResult.appliedChanges.filter(isInstanceChange).map(change => getChangeData(change).elemID.getFullName()),
+  )
+
+  const [successOptionChanges, failedOptionChanges] = _.partition(optionChanges, change =>
+    successContextChanges.has(getParent(getChangeData(change)).elemID.getFullName()),
+  )
+
+  const optionErrors = failedOptionChanges.map(change =>
+    createSaltoElementError({
+      message: 'Failed to deploy context option change',
+      severity: 'Error',
+      elemID: getChangeData(change).elemID,
+    }),
+  )
+  return { successOptionChanges, optionErrors }
+}
 
 const filter: FilterCreator = ({ client, config, paginator, elementsSource }) => ({
   name: 'contextDeploymentFilter',
@@ -35,23 +126,37 @@ const filter: FilterCreator = ({ client, config, paginator, elementsSource }) =>
       await setContextDeploymentAnnotations(fieldContextType)
     }
   },
-
   deploy: async changes => {
     const [relevantChanges, leftoverChanges] = _.partition(
       changes,
-      change => isInstanceChange(change) && getChangeData(change).elemID.typeName === FIELD_CONTEXT_TYPE_NAME,
+      change =>
+        isInstanceChange(change) &&
+        (getChangeData(change).elemID.typeName === FIELD_CONTEXT_TYPE_NAME ||
+          getChangeData(change).elemID.typeName === FIELD_CONTEXT_OPTION_TYPE_NAME),
     )
-    const deployResult = await deployChanges(relevantChanges.filter(isInstanceChange), async change => {
+    const instanceChanges = relevantChanges.filter(isInstanceChange)
+    const [optionChanges, contextChanges] = _.partition(
+      instanceChanges,
+      change => getChangeData(change).elemID.typeName === FIELD_CONTEXT_OPTION_TYPE_NAME,
+    )
+    const optionFullNameToChangeDict = _.keyBy(optionChanges, change => getChangeData(change).elemID.getFullName())
+    const deployResult = await deployChanges(contextChanges.filter(isInstanceChange), async change => {
       // field contexts without fields cant be removed because they don't exist,
       // modification changes are also not allowed but will not crash.
       if (hasValidParent(getChangeData(change)) || !isRemovalChange(change)) {
-        await deployContextChange(change, client, config.apiDefinitions, paginator, elementsSource)
+        const changeToDeploy = getContextChangeWithFixedOptions(change, optionFullNameToChangeDict)
+        await deployContextChange(changeToDeploy, client, config.apiDefinitions, paginator, elementsSource)
       }
     })
 
+    const { successOptionChanges, optionErrors } = getOptionDeplymentResults(deployResult, optionChanges)
+
     return {
       leftoverChanges,
-      deployResult,
+      deployResult: {
+        errors: [...deployResult.errors, ...optionErrors],
+        appliedChanges: [...deployResult.appliedChanges, ...successOptionChanges],
+      },
     }
   },
 })
---
+++ packages/jira-adapter/src/filters/fields/field_context_option_split.ts
@@ -0,0 +1,122 @@
+/*
+ *                      Copyright 2024 Salto Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  BuiltinTypes,
+  CORE_ANNOTATIONS,
+  ElemIdGetter,
+  Field,
+  InstanceElement,
+  isInstanceElement,
+  ListType,
+  ObjectType,
+  ReferenceExpression,
+  Values,
+} from '@salto-io/adapter-api'
+import { config as configUtils, elements as adapterElements } from '@salto-io/adapter-components'
+import { naclCase, pathNaclCase } from '@salto-io/adapter-utils'
+import _ from 'lodash'
+import { DEFAULT_API_DEFINITIONS } from '../../config/api_config'
+import { FilterCreator } from '../../filter'
+import { findObject, setTypeDeploymentAnnotations } from '../../utils'
+import { FIELD_CONTEXT_OPTION_TYPE_NAME, FIELD_CONTEXT_TYPE_NAME } from './constants'
+import { getOptionsFromContext } from './context_options'
+
+const { getTransformationConfigByType } = configUtils
+const { toBasicInstance } = adapterElements
+
+const FIELD_CONTEXT_OPTIONS_FILE_NAME = 'Options'
+export const CONTEXT_NAME_FIELD = 'contextName'
+
+const getOptionInstancesFromContext = async (
+  context: InstanceElement,
+  fieldContextOptionType: ObjectType,
+  getElemIdFunc: ElemIdGetter | undefined,
+): Promise<InstanceElement[]> =>
+  Promise.all(
+    getOptionsFromContext(context).map(async (option: Values) => {
+      option[CONTEXT_NAME_FIELD] = context.elemID.name
+      const optionInstance = await toBasicInstance({
+        entry: option,
+        type: fieldContextOptionType,
+        transformationConfigByType: getTransformationConfigByType(DEFAULT_API_DEFINITIONS.types),
+        transformationDefaultConfig: DEFAULT_API_DEFINITIONS.typeDefaults.transformation,
+        defaultName: naclCase(`${context.elemID.name}_${option.value}`),
+        getElemIdFunc,
+        parent: context,
+      })
+      optionInstance.path = context.path && [
+        ...context.path,
+        pathNaclCase(naclCase(`${context.value.name}_${FIELD_CONTEXT_OPTIONS_FILE_NAME}`)),
+      ]
+
+      return optionInstance
+    }),
+  )
+
+const filter: FilterCreator = ({ config, getElemIdFunc }) => ({
+  name: 'fieldContextOptionsSplitFilter',
+  onFetch: async elements => {
+    if (!config.fetch.splitFieldContext) {
+      return
+    }
+
+    const fieldContextType = findObject(elements, FIELD_CONTEXT_TYPE_NAME)
+    const fieldContextOptionType = findObject(elements, FIELD_CONTEXT_OPTION_TYPE_NAME)
+
+    if (fieldContextType === undefined || fieldContextOptionType === undefined) {
+      return
+    }
+
+    fieldContextType.fields.options = new Field(fieldContextType, 'options', new ListType(BuiltinTypes.STRING))
+
+    fieldContextOptionType.fields[CONTEXT_NAME_FIELD] = new Field(
+      fieldContextOptionType,
+      CONTEXT_NAME_FIELD,
+      BuiltinTypes.STRING,
+      {
+        [CORE_ANNOTATIONS.HIDDEN_VALUE]: true,
+      },
+    )
+    setTypeDeploymentAnnotations(fieldContextOptionType) // TODO: what do I need this for?
+
+    const contexts = elements
+      .filter(isInstanceElement)
+      .filter(instance => instance.elemID.typeName === FIELD_CONTEXT_TYPE_NAME)
+
+    const options = _.flatten(
+      await Promise.all(
+        contexts
+          .filter(context => context.value.options !== undefined)
+          .flatMap(async context => {
+            const contextOptions = await getOptionInstancesFromContext(context, fieldContextOptionType, getElemIdFunc)
+            context.value.options = contextOptions.map(option => new ReferenceExpression(option.elemID, option, option))
+            return contextOptions
+          }),
+      ),
+    )
+
+    contexts.forEach(context => {
+      if (context.path !== undefined) {
+        context.path = [...context.path, context.path[context.path.length - 1]]
+      }
+    })
+
+    options.forEach(option => elements.push(option))
+  },
+})
+
+export default filter

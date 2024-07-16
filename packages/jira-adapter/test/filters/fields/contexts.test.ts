3bc4cc8aa52a48947a49c1150ffd74ae1eef9219
kami-delaroz <batel.delaroz@salto.io>
2024-07-16 16:43:16 +0300

    prettier

--- packages/jira-adapter/test/filters/fields/contexts.test.ts
+++ packages/jira-adapter/test/filters/fields/contexts.test.ts
@@ -112,7 +112,14 @@ describe('deployContextChange', () => {
       change: await resolveChangeElement(change, getLookUpName),
       client,
       endpointDetails: getDefaultConfig({ isDataCenter: false }).apiDefinitions.types.CustomFieldContext.deployRequests,
-      fieldsToIgnore: ['defaultValue', 'options', 'isGlobalContext', 'AssetsObjectFieldConfiguration', 'issueTypeIds', 'projectIds'],
+      fieldsToIgnore: [
+        'defaultValue',
+        'options',
+        'isGlobalContext',
+        'AssetsObjectFieldConfiguration',
+        'issueTypeIds',
+        'projectIds',
+      ],
     })
   })
 

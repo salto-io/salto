97d98fa965164955abaf5db8e3d9ac57ad08dec0
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-07-23 08:27:05 +0800

    fix users pagination

--- packages/okta-adapter/src/definitions/requests/clients.ts
+++ packages/okta-adapter/src/definitions/requests/clients.ts
@@ -61,9 +61,12 @@ export const createClientDefinitions = (
               queryArgs: { limit: '200' }, // maximum page size allowed
             },
           },
-          User: {
+          '/api/v1/users': {
             get: {
-              queryArgs: { limit: '200' }, // maximum page size allowed
+              queryArgs: { 
+                limit: '200', // maximum page size allowed
+                search: 'id pr' // The search query is needed to fetch deprovisioned users
+              },
             },
           },
         },

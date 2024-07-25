44d37bef8e9169481cf2011859654caf317e8498
Amir Rachum <amir.rachum@salto.io>
2024-07-25 16:22:21 +0300

    Update packages/okta-adapter/src/definitions/deploy/deploy.ts
    Co-authored-by: shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>

--- packages/okta-adapter/src/definitions/deploy/deploy.ts
+++ packages/okta-adapter/src/definitions/deploy/deploy.ts
@@ -77,10 +77,9 @@ const createCustomizations = (): Record<string, InstanceDeployApiDefinitions> =>
         customizations: {
           add: [
             {
-              // BrandThemes are created automatically by Okta when a Brand is
-              // created, but we're allowing to "add" a theme alongside its
-              // Brand. We first send a GET query to get its ID, and then we
-              // perform a "modify" action.
+              // BrandThemes are created automatically by Okta when a Brand is created,
+              // but we're allowing to "add" a theme alongside its Brand.
+              // We first send a GET query to get its ID, and then we perform a "modify" action.
               request: {
                 endpoint: {
                   path: '/api/v1/brands/{brandId}/themes',

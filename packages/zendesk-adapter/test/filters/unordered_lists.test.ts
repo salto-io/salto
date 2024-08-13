576e9fff0a1f4d2957bef82bdb34980292c0eb4a
Adam Fineberg <adam.fineberg@salto.io>
2024-08-13 15:04:57 +0300

    SALTO-6265 Add ticket form restricted_brand_ids sorting (#6390)
    ---------
    
    Co-authored-by: Eden Hassid <edenhassid@gmail.com>

--- packages/zendesk-adapter/test/filters/unordered_lists.test.ts
+++ packages/zendesk-adapter/test/filters/unordered_lists.test.ts
@@ -23,6 +23,7 @@ import {
 } from '@salto-io/adapter-api'
 import { filterUtils } from '@salto-io/adapter-components'
 import {
+  BRAND_TYPE_NAME,
   GROUP_TYPE_NAME,
   MACRO_TYPE_NAME,
   ROUTING_ATTRIBUTE_TYPE_NAME,
@@ -58,11 +59,14 @@ describe('Unordered lists filter', () => {
     const dynamicContentItemVariantType = new ObjectType({
       elemID: new ElemID(ZENDESK, DYNAMIC_CONTENT_ITEM_VARIANT_TYPE_NAME),
     })
+    const brandType = new ObjectType({ elemID: new ElemID(ZENDESK, BRAND_TYPE_NAME) })
     const ticketFieldOneInstance = new InstanceElement('fieldA', ticketFieldType, { raw_title: 'a' })
     const ticketFieldThreeInstance = new InstanceElement('fieldC', ticketFieldType, { raw_title: 'c' })
     const invalidTicketFieldInstance = new InstanceElement('invalid field', ticketFieldType, {})
     const customOneInstance = new InstanceElement('customA', ticketCustomFieldType, { value: 'a' })
     const customThreeInstance = new InstanceElement('customC', ticketCustomFieldType, { value: 'c' })
+    const brandOneInstance = new InstanceElement('zzzBrand', brandType, { name: 'zzzBrand' })
+    const brandTwoInstance = new InstanceElement('heyBrand', brandType, { name: 'heyBrand' })
     const validTicketFormInstance = new InstanceElement('valid form', ticketFormType, {
       agent_conditions: [
         {
@@ -109,6 +113,10 @@ describe('Unordered lists filter', () => {
         {
           value: 'b',
         },
+      ],
+      restricted_brand_ids: [
+        new ReferenceExpression(brandOneInstance.elemID, brandOneInstance),
+        new ReferenceExpression(brandTwoInstance.elemID, brandTwoInstance),
       ],
     })
     const invalidTicketFormInstance = new InstanceElement('invalid form', ticketFormType, {
@@ -516,6 +524,9 @@ describe('Unordered lists filter', () => {
       expect(instances[0].value.end_user_conditions[0].child_fields).toHaveLength(2)
       expect(instances[0].value.end_user_conditions[0].child_fields[0].id.elemID.name).toEqual('fieldA')
       expect(instances[0].value.end_user_conditions[0].child_fields[1].id.elemID.name).toEqual('fieldC')
+      expect(instances[0].value.restricted_brand_ids).toHaveLength(2)
+      expect(instances[0].value.restricted_brand_ids[0].elemID.name).toEqual('heyBrand')
+      expect(instances[0].value.restricted_brand_ids[1].elemID.name).toEqual('zzzBrand')
     })
     it('should not change order the form is invalid', async () => {
       const instances = elements.filter(isInstanceElement).filter(e => e.elemID.name === 'invalid form')

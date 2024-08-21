1d5604622c630e53f6fce1628c2de059f9fe6cc6
tamtamirr <tamir.ohayon@salto.io>
2024-08-21 11:00:19 +0300

    update pending deploy report messagE

--- packages/salesforce-adapter/src/adapter_creator.ts
+++ packages/salesforce-adapter/src/adapter_creator.ts
@@ -1,9 +1,17 @@
 /*
- * Copyright 2024 Salto Labs Ltd.
- * Licensed under the Salto Terms of Use (the "License");
- * You may not use this file except in compliance with the License.  You may obtain a copy of the License at https://www.salto.io/terms-of-use
+ *                      Copyright 2024 Salto Labs Ltd.
  *
- * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 import _ from 'lodash'
 import { logger } from '@salto-io/logging'
@@ -253,7 +261,7 @@ export const createDeployProgressReporter = (
     if (deployResult) {
       metadataProgress =
         deployResult.status === METADATA_DEPLOY_PENDING_STATUS
-          ? 'Metadata: Waiting on another deploy or automated process to finish'
+          ? 'Metadata: Waiting on another deploy or automated process to finish in Salesforce'
           : `${deployResult.numberComponentsDeployed}/${deployResult.numberComponentsTotal} Metadata Components, ${deployResult.numberTestsCompleted}/${deployResult.numberTestsTotal} Tests.${linkToSalesforce(deployResult)}`
     }
     if (deployedDataInstances > 0) {

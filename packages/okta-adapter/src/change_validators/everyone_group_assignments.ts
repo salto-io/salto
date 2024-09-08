3c1b7d6d5f91cf7466f8eddd5859c644ff8260d0
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-09-08 09:37:37 +0300

    group assignment validator

---
+++ packages/okta-adapter/src/change_validators/everyone_group_assignments.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024 Salto Labs Ltd.
+ * Licensed under the Salto Terms of Use (the "License");
+ * You may not use this file except in compliance with the License.  You may obtain a copy of the License at https://www.salto.io/terms-of-use
+ *
+ * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
+ */
+import { ChangeValidator, getChangeData, isInstanceChange, isModificationChange } from '@salto-io/adapter-api'
+import { GROUP_MEMBERSHIP_TYPE_NAME } from '../constants'
+
+/**
+ * Assignments to the "Everyone" group are managed by Okta and cannot be modified.
+ */
+export const everyoneGroupAssignments: ChangeValidator = async changes =>
+  changes
+    .filter(isInstanceChange)
+    .filter(isModificationChange)
+    .filter(change => getChangeData(change).elemID.typeName === GROUP_MEMBERSHIP_TYPE_NAME)
+    .map(getChangeData)
+    .filter(instance => instance.elemID.name === 'Everyone')
+    .map(instance => ({
+      elemID: instance.elemID,
+      severity: 'Error',
+      message: 'Assignment to the "Everyone" group are managed by Okta.',
+      detailedMessage:
+        'Group assignments to the "Everyone" group are managed by Okta. Any users added in this deployment will be auto assigned to this group.',
+    }))
--- packages/okta-adapter/src/user_config.ts
+++ packages/okta-adapter/src/user_config.ts
@@ -66,6 +66,7 @@ const changeValidatorNames = [
   'userStatusChanges',
   'disabledAuthenticatorsInMfaPolicy',
   'oidcIdentityProvider',
+  'everyoneGroupAssignments',
 ] as const
 
 export type ChangeValidatorName = (typeof changeValidatorNames)[number]

68eabe04eb90e71c9f6d6d2eaa1de3f741d11cd2
tamtamirr <tamir.ohayon@salto.io>
2024-09-15 17:06:02 +0300

    add UTs

---
+++ packages/salesforce-adapter/test/filters/wave_static_files.test.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 Salto Labs Ltd.
+ * Licensed under the Salto Terms of Use (the "License");
+ * You may not use this file except in compliance with the License.  You may obtain a copy of the License at https://www.salto.io/terms-of-use
+ *
+ * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
+ */
+import { ElemID, InstanceElement, ObjectType, StaticFile } from '@salto-io/adapter-api'
+import filterCreator, { WAVE_TYPES_WITH_STATIC_FILES } from '../../src/filters/wave_static_files'
+import { INSTANCE_FULL_NAME_FIELD, RECORDS_PATH, SALESFORCE } from '../../src/constants'
+import { defaultFilterContext } from '../utils'
+import { FilterWith } from './mocks'
+import { buildFetchProfile } from '../../src/fetch_profile/fetch_profile'
+
+describe('waveStaticFilesFilter', () => {
+  let filter: FilterWith<'onFetch'>
+  beforeEach(() => {
+    filter = filterCreator({
+      config: {
+        ...defaultFilterContext,
+        fetchProfile: buildFetchProfile({
+          fetchParams: {
+            optionalFeatures: {
+              waveMetadataSupport: true,
+            },
+          },
+        }),
+      },
+    }) as FilterWith<'onFetch' | 'preDeploy' | 'onDeploy'>
+  })
+  describe('onFetch', () => {
+    const content = 'eyJuYW1lIjoiSm9obiIsImFnZSI6MzB9'
+    it.each(WAVE_TYPES_WITH_STATIC_FILES)('should convert %s instance content to static file', async typeName => {
+      const instance = new InstanceElement(
+        'TestInstance',
+        new ObjectType({
+          elemID: new ElemID(SALESFORCE, typeName),
+          annotations: {
+            metadataType: typeName,
+          },
+        }),
+        {
+          [INSTANCE_FULL_NAME_FIELD]: 'TestInstance',
+          content,
+        },
+      )
+      await filter.onFetch([instance])
+      expect(instance.value.content).toEqual(
+        new StaticFile({
+          filepath: `${SALESFORCE}/${RECORDS_PATH}/${typeName}/TestInstance`,
+          content: Buffer.from(content, 'base64'),
+        }),
+      )
+    })
+  })
+})

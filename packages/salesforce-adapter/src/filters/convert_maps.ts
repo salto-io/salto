feeb317e23ab64296a39a1e19de8c417b4e541d5
Amir Rachum <amir.rachum@salto.io>
2024-09-18 11:35:40 +0300

    save

--- packages/salesforce-adapter/src/filters/convert_maps.ts
+++ packages/salesforce-adapter/src/filters/convert_maps.ts
@@ -145,11 +145,6 @@ export const metadataTypeToFieldToMapDef: Record<string, Record<string, MapDef>>
   [MUTING_PERMISSION_SET_METADATA_TYPE]: PERMISSIONS_SET_MAP_FIELD_DEF,
   [LIGHTNING_COMPONENT_BUNDLE_METADATA_TYPE]: LIGHTNING_COMPONENT_BUNDLE_MAP,
   [SHARING_RULES_TYPE]: SHARING_RULES_MAP_FIELD_DEF,
-  StandardValueSet: {
-    standardValue: {
-      key: 'fullName',
-    }
-  },
   GlobalValueSet: {
     customValue: {
       key: 'fullName',
@@ -444,12 +439,12 @@ const filter: LocalFilterCreator = ({ config }) => ({
 
     const fields = elements.filter(isCustomObjectSync).flatMap(obj => Object.values(obj.fields))
     await awu(Object.entries(fieldTypeToAnnotationToMapDef)).forEach(async ([fieldType, annotationToMapDef]) => {
-      const fieldsToConvert = fields.filter(field => field.elemID.typeName === fieldType)
+      const fieldsToConvert = fields.filter(field => field.refType.elemID.typeName === fieldType)
       if (fieldsToConvert.length === 0) {
         return
       }
       const nonUniqueMapFields = await convertElementFieldsToMaps(
-        fieldsToConvert.map(field => field.parent),
+        fieldsToConvert,
         annotationToMapDef,
       )
       log.debug('converted fields to maps: %s', nonUniqueMapFields)

3d795b1b67b27729497b795a6978218b822d1397
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-09-20 16:47:57 +0300

    fix almost bug

--- packages/okta-adapter/src/filters/policy_priority.ts
+++ packages/okta-adapter/src/filters/policy_priority.ts
@@ -218,7 +218,7 @@ const updatePriorityField = async ({
     )
     return
   }
-  const data = _.omit({ ...response.data, priority: requiredPriority }, fieldsToOmit)
+  const data = { ..._.omit(response.data, fieldsToOmit), priority: requiredPriority }
   try {
     await client.put({ url: path, data })
   } catch (error) {

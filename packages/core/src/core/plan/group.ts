443582cb16170cae3f7beaffbcd32552ac2a020c
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-09-22 22:52:11 +0300

    log

--- packages/core/src/core/plan/group.ts
+++ packages/core/src/core/plan/group.ts
@@ -134,10 +134,12 @@ export const buildGroupedGraphFromDiffGraph = (
   }
 
   const diffGraphWithoutRedundantFieldNodes = removeRedundantFieldNodes(diffGraph, groupKey)
+  const shouldFailOnCircularDependency = getCoreFlagBool(CORE_FLAGS.failPlanOnCircularDependencies)
+  log.debug('building acyclic grouped graph with failPlanOnCircularDependencies value: %s', shouldFailOnCircularDependency)
   return buildAcyclicGroupedGraph({
     source: diffGraphWithoutRedundantFieldNodes,
     groupKey,
-    shouldFailOnCircularDependency: getCoreFlagBool(CORE_FLAGS.failPlanOnCircularDependencies),
+    shouldFailOnCircularDependency,
     disjointGroups,
   })
 }

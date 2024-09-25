f335ae6013e90fbe08d6cf94da14b112c9e1cbe8
tamtamirr <tamir.ohayon@salto.io>
2024-09-25 18:10:18 +0300

    add filter

---
+++ packages/salesforce-adapter/src/filters/profiles_and_permission_sets_broken_paths.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 Salto Labs Ltd.
+ * Licensed under the Salto Terms of Use (the "License");
+ * You may not use this file except in compliance with the License.  You may obtain a copy of the License at https://www.salto.io/terms-of-use
+ *
+ * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
+ */
+import { logger } from '@salto-io/logging'
+import { inspectValue } from '@salto-io/adapter-utils'
+import { ElemID, InstanceElement } from '@salto-io/adapter-api'
+import { collections } from '@salto-io/lowerdash'
+import { ArtificialTypes } from '../constants'
+import { LocalFilterCreator } from '../filter'
+import { buildElementsSourceForFetch } from './utils'
+import {
+  getProfilesAndPsBrokenReferenceFields,
+  isProfileOrPermissionSetInstance,
+} from '../custom_references/profiles_and_permission_sets'
+
+const log = logger(module)
+const { toArrayAsync } = collections.asynciterable
+
+const filter: LocalFilterCreator = ({ config }) => ({
+  name: 'profilesAndPermissionSetsBrokenPaths',
+  onFetch: async elements => {
+    const elementsSource = buildElementsSourceForFetch(elements, config)
+    const profilesAndPermissionSets = (await toArrayAsync(await elementsSource.getAll())).filter(
+      isProfileOrPermissionSetInstance,
+    )
+    if (profilesAndPermissionSets.length === 0) {
+      return
+    }
+    const { paths } = await getProfilesAndPsBrokenReferenceFields({ elementsSource, profilesAndPermissionSets, config })
+    const uniquePaths = new Set(paths)
+    if (uniquePaths.size === 0) {
+      return
+    }
+    log.debug('Profiles and PermissionSets broken paths: %s', inspectValue(uniquePaths, { maxArrayLength: 100 }))
+    elements.push(
+      new InstanceElement(ElemID.CONFIG_NAME, ArtificialTypes.ProfilesAndPermissionSetsBrokenPaths, {
+        paths: uniquePaths,
+      }),
+    )
+  },
+})
+
+export default filter

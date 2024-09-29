cad0a6e5682c17ac4aaeb1a4b30b8a67c15a9549
tamtamirr <tamir.ohayon@salto.io>
2024-09-29 12:30:19 +0300

    lint fix

--- packages/lowerdash/test/objects.test.ts
+++ packages/lowerdash/test/objects.test.ts
@@ -5,8 +5,8 @@
  *
  * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
  */
+import _ from 'lodash'
 import { cleanEmptyObjects, concatObjects } from '../src/objects'
-import _ from "lodash";
 
 describe('concatObjects', () => {
   type testType = {
@@ -18,17 +18,17 @@ describe('concatObjects', () => {
     {
       a: ['a', 'b'],
       b: ['1', '2'],
-      c: undefined
+      c: undefined,
     },
     {
       a: ['c', 'd'],
       b: undefined,
-      c: [{name: 'name1'}, {name: 'name2'}]
+      c: [{ name: 'name1' }, { name: 'name2' }],
     },
     {
       a: undefined,
       b: ['3', '4'],
-      c: [{name: 'name1'}, {name: 'name4'}]
+      c: [{ name: 'name1' }, { name: 'name4' }],
     },
   ]
 
@@ -37,16 +37,16 @@ describe('concatObjects', () => {
       expect(concatObjects(objects)).toEqual({
         a: ['a', 'b', 'c', 'd'],
         b: ['1', '2', '3', '4'],
-        c: [{name: 'name1'}, {name: 'name2'}, {name: 'name1'}, {name: 'name4'}]
+        c: [{ name: 'name1' }, { name: 'name2' }, { name: 'name1' }, { name: 'name4' }],
       })
     })
   })
   describe('when uniqueFnByKey is provided', () => {
     it('Should return an object with unique array values concatenated', () => {
-      expect(concatObjects(objects, {c: values => _.uniqBy(values, 'name')})).toEqual({
+      expect(concatObjects(objects, { c: values => _.uniqBy(values, 'name') })).toEqual({
         a: ['a', 'b', 'c', 'd'],
         b: ['1', '2', '3', '4'],
-        c: [{name: 'name1'}, {name: 'name2'}, {name: 'name4'}]
+        c: [{ name: 'name1' }, { name: 'name2' }, { name: 'name4' }],
       })
     })
   })

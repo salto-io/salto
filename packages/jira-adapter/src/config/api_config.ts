34e902cb7a246bc7c0bd2a25f228f65d81163e2d
kami-delaroz <batel.delaroz@salto.io>
2024-11-07 16:32:59 +0200

    SALTO-6864 add isLocked to Field id to avoid collisions

--- packages/jira-adapter/src/config/api_config.ts
+++ packages/jira-adapter/src/config/api_config.ts
@@ -259,7 +259,7 @@ const DEFAULT_TYPE_CUSTOMIZATIONS: JiraApiConfig['types'] = {
         { fieldName: 'contextIssueTypes', fieldType: 'list<IssueTypeToContextMapping>' },
         { fieldName: 'contextProjects', fieldType: 'list<CustomFieldContextProjectMapping>' },
       ],
-      fileNameFields: ['name'],
+      fileNameFields: ['name', 'isLocked'],
     },
     deployRequests: {
       add: {

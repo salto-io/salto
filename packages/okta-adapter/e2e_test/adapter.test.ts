01cf406e1f5e87623abfcc25b5c2213269f6af26
shir-reifenberg <74004474+shir-reifenberg@users.noreply.github.com>
2024-11-17 07:12:49 +0200

    fix (#6758)

--- packages/okta-adapter/e2e_test/adapter.test.ts
+++ packages/okta-adapter/e2e_test/adapter.test.ts
@@ -651,8 +651,7 @@ const getHiddenFieldsToOmit = (
 }
 
 describe('Okta adapter E2E', () => {
-  // eslint-disable-next-line jest/no-disabled-tests
-  describe.skip('fetch and deploy', () => {
+  describe('fetch and deploy', () => {
     let credLease: CredsLease<Credentials>
     let adapterAttr: Reals
     const testSuffix = uuidv4().slice(0, 8)

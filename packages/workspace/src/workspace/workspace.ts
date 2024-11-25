217f381381044e49598bdb74222cadf3704caf5c
Almog Mesilaty <almog.mesilaty@salto.io>
2024-11-25 16:59:07 +0200

    Removed check for undefined

--- packages/workspace/src/workspace/workspace.ts
+++ packages/workspace/src/workspace/workspace.ts
@@ -123,10 +123,7 @@ export const isValidEnvName = (envName: string): boolean =>
   /^[a-z0-9-_.!\s]+$/i.test(envName) && envName.length <= MAX_ENV_NAME_LEN
 
 export const isValidAccountName = (accountName: string): boolean =>
-  !_.isUndefined(accountName) &&
-  accountName.length <= MAX_ACCOUNT_NAME_LENGTH &&
-  naclCase(accountName) === accountName &&
-  /^\D.*$/i.test(accountName)
+  accountName.length <= MAX_ACCOUNT_NAME_LENGTH && naclCase(accountName) === accountName && /^\D.*$/i.test(accountName)
 
 export type DateRange = {
   start: Date

96f33c7ba96a2b19dacc994131200603273e17f4
Ariel Chinn <ariel.chinn@salto.io>
2024-11-26 16:30:01 +0200

    SALTO-6962: Improve formula deps filter (#6830)

--- packages/salesforce-adapter/src/filters/utils.ts
+++ packages/salesforce-adapter/src/filters/utils.ts
@@ -455,8 +455,8 @@ export const instanceInternalId = (instance: InstanceElement): string =>
 
 export const hasApiName = (elem: Element): boolean => apiName(elem) !== undefined
 
-export const extractFlatCustomObjectFields = async (elem: Element): Promise<Element[]> =>
-  (await isCustomObject(elem)) && isObjectType(elem) ? [elem, ...Object.values(elem.fields)] : [elem]
+export const extractFlatCustomObjectFields = (elem: Element): Element[] =>
+  [elem].concat(isCustomObjectSync(elem) ? Object.values(elem.fields) : [])
 
 export type QueryOperator = '>' | '<' | '=' | 'IN' // 'IN' is for values that can be split across multiple queries
 export type SoqlQuery = {

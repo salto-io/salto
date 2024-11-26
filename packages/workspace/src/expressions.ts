f2ce874a8354ceda32b796e4368b28193af8fa5a
Ori Moisis <ori.moisis@salto.io>
2024-11-26 11:15:20 +0200

    CR comments

--- packages/workspace/src/expressions.ts
+++ packages/workspace/src/expressions.ts
@@ -306,19 +306,16 @@ export const resolve = (
 
       // Since fields technically reference their parent type with the .parent property
       // we need to make sure to resolve all field parents as well
-      const fieldParents = elementsToResolve.filter(isField).map(field => field.parent)
+      const fieldParents = _.uniqBy(
+        elementsToResolve.filter(isField).map(field => field.parent),
+        elem => elem.elemID.getFullName(),
+      )
 
       // We assume that if we got a field and its parent type in the same resolve call, the field's
       // parent will point to the same object type that we got to resolve, so we don't need to resolve
       // both.
       const elementIDsToResolve = new Set(elements.map(elem => elem.elemID.getFullName()))
-      const fieldParentsToResolve = fieldParents.filter(elem => {
-        if (!elementIDsToResolve.has(elem.elemID.getFullName())) {
-          elementIDsToResolve.add(elem.elemID.getFullName())
-          return true
-        }
-        return false
-      })
+      const fieldParentsToResolve = fieldParents.filter(elem => !elementIDsToResolve.has(elem.elemID.getFullName()))
       const allElementsToResolve = elementsToResolve.concat(fieldParentsToResolve)
 
       const resolvedElements = new Map(

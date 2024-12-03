308741e699b2205067a6cedd9048e474bce2257b
Almog Mesilaty <almog.mesilaty@salto.io>
2024-12-03 16:28:48 +0200

    Added holiday CV

---
+++ packages/zendesk-adapter/src/change_validators/business_hours_schedule_holiday.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2024 Salto Labs Ltd.
+ * Licensed under the Salto Terms of Use (the "License");
+ * You may not use this file except in compliance with the License.  You may obtain a copy of the License at https://www.salto.io/terms-of-use
+ *
+ * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
+ */
+
+import _ from 'lodash'
+import {
+  ChangeValidator,
+  getChangeData,
+  InstanceElement,
+  isAdditionOrModificationChange,
+  isInstanceChange,
+} from '@salto-io/adapter-api'
+import { BUSINESS_HOUR_SCHEDULE_HOLIDAY } from '../constants'
+
+type BusinessHoursScheduleHoliday = {
+  start_date: string
+  end_date: string
+}
+
+const hasStartDate = (value: unknown): boolean => _.isString(_.get(value, 'start_date'))
+
+const hasEndDate = (value: unknown): boolean => _.isString(_.get(value, 'end_date'))
+
+const isBusinessHoursScheduleHoliday = (value: unknown): value is BusinessHoursScheduleHoliday =>
+  _.isObject(value) && hasStartDate(value) && hasEndDate(value)
+
+const isHolidaySpanTooLong = (instance: InstanceElement): boolean => {
+  const values: unknown = instance.value
+  if (!isBusinessHoursScheduleHoliday(values)) {
+    return false
+  }
+  return true
+}
+
+export const businessHoursScheduleHolidayChangeValidator: ChangeValidator = async changes =>
+  changes
+    .filter(isAdditionOrModificationChange)
+    .filter(isInstanceChange)
+    .map(getChangeData)
+    .filter(instance => instance.elemID.typeName === BUSINESS_HOUR_SCHEDULE_HOLIDAY)
+    .map(instance => ({
+      elemID: instance.elemID,
+      severity: 'Error',
+      message: `Element ${instance.elemID.getFullName()} cannot be deployed.`,
+      detailedMessage: `Element ${instance.elemID.getFullName()} is a Zendesk Guide element which isn't related to a brand, and therefore cannot be deployed.`,
+    }))

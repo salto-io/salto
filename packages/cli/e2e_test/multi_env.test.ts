e15afaa585a6adf5780677f8561c9d6b5ec0c297
Ariel Chinn <ariel.chinn@salto.io>
2024-12-04 18:51:02 +0200

    Skipping workspace validation

--- packages/cli/e2e_test/multi_env.test.ts
+++ packages/cli/e2e_test/multi_env.test.ts
@@ -9,7 +9,7 @@ import { SalesforceClient, UsernamePasswordCredentials } from '@salto-io/salesfo
 // eslint-disable-next-line no-restricted-imports
 import { testHelpers as salesforceTestHelpers } from '@salto-io/salesforce-adapter/dist/e2e_test/jest_environment'
 import path from 'path'
-import { Plan } from '@salto-io/core'
+import { Plan, loadLocalWorkspace } from '@salto-io/core'
 import { parser } from '@salto-io/parser'
 import { logger } from '@salto-io/logging'
 import { strings, collections } from '@salto-io/lowerdash'
@@ -38,7 +38,6 @@ import {
   getNaclFileElements,
   cleanup as workspaceHelpersCleanup,
   getCurrentEnv,
-  loadValidWorkspace,
 } from './helpers/workspace'
 import './helpers/jest_matchers'
 import * as templates from './helpers/templates'
@@ -287,7 +286,7 @@ describe.each([
       let elementsWithHidden: readonly Element[]
       beforeAll(async () => {
         await runSetEnv(baseDir, ENV2_NAME)
-        const workspace = await loadValidWorkspace(baseDir)
+        const workspace = await loadLocalWorkspace({ path: baseDir })
         visibleElements = await awu(await (await workspace.elements(false)).getAll()).toArray()
         elementsWithHidden = await awu(await (await workspace.elements(true)).getAll()).toArray()
       })

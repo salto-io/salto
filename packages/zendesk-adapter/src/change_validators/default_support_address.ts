0b4214e023fa95171095bf8534a185402efbba68
Rotem Cohen <rotemcohen@Rotems-MacBook-Pro.local>
2024-12-04 10:59:42 +0200

    lint fixes

--- packages/zendesk-adapter/src/change_validators/default_support_address.ts
+++ packages/zendesk-adapter/src/change_validators/default_support_address.ts
@@ -21,32 +21,29 @@ const createDefaultSupportAddressError = (id: ElemID): ChangeError => ({
   elemID: id,
   severity: 'Error',
   message: "Email: Cannot be a default until it's forwarding is verified",
-  detailedMessage: `${id.getFullName()} has \'default\' field \'true\' and \'forwarding_status\' field that is not \'verified\'`,
+  detailedMessage: `${id.getFullName()} has default field true and forwarding_status field that is not verified`,
 })
 /*
  * This change validator checks that an Email is not set as default if it's forward status is not verified
  * NOTICE: zendesk doesn't allow the field 'default' to be false, and requires to delete it. This CV doesn't check this issue.
  */
-export const defaultSupportAddressValidator: ChangeValidator = async changes => {
-  return changes
+export const defaultSupportAddressValidator: ChangeValidator = async changes =>
+  changes
     .filter(isAdditionOrModificationChange)
     .map(getChangeData)
-    .filter(c => {
-      return TYPES_TO_CHECK.has(c.elemID.typeName)
-    })
+    .filter(c => TYPES_TO_CHECK.has(c.elemID.typeName))
     .filter(ch => {
       const forwardingValue = _.get(ch, 'value').forwarding_status
-      if (!forwardingValue || forwardingValue == 'verified') {
+      if (!forwardingValue || forwardingValue === 'verified') {
         return false
       }
       const defaultValue = _.get(ch, 'value').default
       if (!defaultValue) {
         return false
       }
-      if (defaultValue == true) {
+      if (defaultValue === true) {
         return true
       }
       return false
     })
     .map(a => createDefaultSupportAddressError(a.elemID))
-}

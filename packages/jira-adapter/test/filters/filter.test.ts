aaa687d4d9cddeb8718d9e39c028ed9c2f921f03
yuval.givon <yuval.givon@salto.io>
2024-12-16 15:07:31 +0200

    UT

--- packages/jira-adapter/test/filters/filter.test.ts
+++ packages/jira-adapter/test/filters/filter.test.ts
@@ -5,16 +5,62 @@
  *
  * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
  */
-import { InstanceElement, toChange } from '@salto-io/adapter-api'
-import { createEmptyType, getFilterParams } from '../utils'
+import {
+  CORE_ANNOTATIONS,
+  InstanceElement,
+  toChange,
+  BuiltinTypes,
+  ObjectType,
+  ElemID,
+  Change,
+} from '@salto-io/adapter-api'
+import _ from 'lodash'
+import { MockInterface } from '@salto-io/test-utils'
+import { deployment, client as clientUtils } from '@salto-io/adapter-components'
+import { createEmptyType, getFilterParams, mockClient } from '../utils'
 import filtersFilter from '../../src/filters/filter'
 import { Filter } from '../../src/filter'
+import { FILTER_TYPE_NAME, JIRA } from '../../src/constants'
+import JiraClient from '../../src/client/client'
+import { getDefaultConfig, JiraConfig } from '../../src/config/config'
+
+jest.mock('@salto-io/adapter-components', () => {
+  const actual = jest.requireActual('@salto-io/adapter-components')
+  return {
+    ...actual,
+    deployment: {
+      ...actual.deployment,
+      deployChange: jest.fn(),
+    },
+  }
+})
 
 describe('filtersFilter', () => {
   let filter: Filter
+  let type: ObjectType
+  let connection: MockInterface<clientUtils.APIConnection>
+  let client: JiraClient
+  let config: JiraConfig
 
   beforeEach(async () => {
-    filter = filtersFilter(getFilterParams())
+    const { connection: conn, client: cli, paginator } = mockClient()
+    connection = conn
+    client = cli
+    config = _.cloneDeep(getDefaultConfig({ isDataCenter: false }))
+    filter = filtersFilter(getFilterParams({ client, paginator, config }))
+    type = new ObjectType({
+      elemID: new ElemID(JIRA, FILTER_TYPE_NAME),
+      fields: {
+        owner: { refType: BuiltinTypes.STRING },
+      },
+    })
+  })
+  it('should turn owner field CREATABLE & UPDATEABLE annotations to true on fetch', async () => {
+    await filter.onFetch?.([type])
+    expect(type.fields.owner.annotations).toEqual({
+      [CORE_ANNOTATIONS.UPDATABLE]: true,
+      [CORE_ANNOTATIONS.CREATABLE]: true,
+    })
   })
   it('should replace user field on pre deploy', async () => {
     const instance = new InstanceElement('instance', createEmptyType('Filter'), {
@@ -86,6 +132,73 @@ describe('filtersFilter', () => {
       },
     ])
   })
+  describe('deploy', () => {
+    let change: Change<InstanceElement>
+    const deployChangeMock = deployment.deployChange as jest.MockedFunction<typeof deployment.deployChange>
+    const instance = new InstanceElement('instance', createEmptyType('Filter'), {
+      id: '1',
+      owner: '123',
+    })
+
+    beforeEach(() => {
+      deployChangeMock.mockReset()
+    })
+
+    describe('addition', () => {
+      beforeEach(() => {
+        change = toChange({ after: instance })
+      })
+      it('should add the owner to the new instance', async () => {
+        await filter.deploy?.([change])
+
+        expect(deployChangeMock).toHaveBeenCalledWith({
+          change,
+          client,
+          endpointDetails: getDefaultConfig({ isDataCenter: false }).apiDefinitions.types[FILTER_TYPE_NAME]
+            .deployRequests,
+          fieldsToIgnore: ['owner'],
+        })
+
+        expect(connection.put).toHaveBeenCalledWith(
+          `/rest/api/3/filter/${instance.value.id}/owner`,
+          {
+            accountId: instance.value.owner,
+          },
+          undefined,
+        )
+      })
+    })
+
+    describe('modification', () => {
+      let instanceBefore: InstanceElement
+      beforeEach(() => {
+        instanceBefore = instance.clone()
+        change = toChange({ before: instanceBefore, after: instance })
+      })
+      it('should deploy owner when changed', async () => {
+        instance.value.owner = '234'
+        instance.value.description = 'test'
+
+        await filter.deploy?.([change])
+
+        expect(deployChangeMock).toHaveBeenCalledWith({
+          change,
+          client,
+          endpointDetails: getDefaultConfig({ isDataCenter: false }).apiDefinitions.types[FILTER_TYPE_NAME]
+            .deployRequests,
+          fieldsToIgnore: ['owner'],
+        })
+
+        expect(connection.put).toHaveBeenCalledWith(
+          `/rest/api/3/filter/${instance.value.id}/owner`,
+          {
+            accountId: instance.value.owner,
+          },
+          undefined,
+        )
+      })
+    })
+  })
   it('should return user field after deploy', async () => {
     const instance = new InstanceElement('instance', createEmptyType('Filter'), {
       sharePermissions: [
@@ -155,7 +268,6 @@ describe('filtersFilter', () => {
       },
     ])
   })
-
   it('should do nothing if not a filter type', async () => {
     const instance = new InstanceElement('instance', createEmptyType('Other'), {
       editPermissions: [

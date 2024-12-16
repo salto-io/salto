e579725125c30f6d92cc83013f0f58926339791d
Ariel Chinn <ariel.chinn@salto.io>
2024-12-16 17:45:15 +0200

    SALTO-6830: Sorting converted map values in pre deploy
    This will give us consistent ordering in SFDX and shouldn't affect deployments (the current order we send is basically random).

--- packages/salesforce-adapter/test/filters/convert_maps.test.ts
+++ packages/salesforce-adapter/test/filters/convert_maps.test.ts
@@ -630,6 +630,7 @@ describe('Convert maps filter', () => {
   describe('Convert inner field to map', () => {
     let lwcBefore: InstanceElement
     let lwcAfter: InstanceElement
+    let lwcSorted: InstanceElement
     let elements: Element[]
     type FilterType = FilterWith<'onFetch' | 'preDeploy'>
     let filter: FilterType
@@ -641,6 +642,18 @@ describe('Convert maps filter', () => {
             lwcResource: [
               { filePath: 'lwc/dir/lwc.js', source: 'lwc.ts' },
               { filePath: 'lwc/dir/__mocks__/lwc.js', source: 'lwc.ts' },
+            ],
+          },
+        },
+        mockTypes.LightningComponentBundle,
+      )
+      lwcSorted = createInstanceElement(
+        {
+          fullName: 'lwc',
+          lwcResources: {
+            lwcResource: [
+              { filePath: 'lwc/dir/__mocks__/lwc.js', source: 'lwc.ts' },
+              { filePath: 'lwc/dir/lwc.js', source: 'lwc.ts' },
             ],
           },
         },
@@ -677,7 +690,7 @@ describe('Convert maps filter', () => {
         await filter.preDeploy([toChange({ after: lwcDeploy })])
       })
       it('should return inner field back to list', async () => {
-        expect(lwcDeploy).toEqual(lwcBefore)
+        expect(lwcDeploy).toEqual(lwcSorted)
       })
     })
   })

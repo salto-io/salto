d952abbfe9efa90d2a55165f7385249be6059c21
Almog Mesilaty <almog.mesilaty@salto.io>
2024-12-17 10:04:38 +0200

    Created CV file

---
+++ packages/salesforce-adapter/src/change_validators/flow_have_referenced_elements.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2024 Salto Labs Ltd.
+ * Licensed under the Salto Terms of Use (the "License");
+ * You may not use this file except in compliance with the License.  You may obtain a copy of the License at https://www.salto.io/terms-of-use
+ *
+ * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
+ */
+
+import { ChangeValidator, getChangeData, isAdditionOrModificationChange, isInstanceChange } from '@salto-io/adapter-api'
+import { isInstanceOfTypeSync } from '../filters/utils'
+import { FLOW_METADATA_TYPE } from '../constants'
+
+const changeValidator: ChangeValidator = async changes =>
+  changes
+    .filter(isAdditionOrModificationChange)
+    .filter(isInstanceChange)
+    .map(getChangeData)
+    .filter(isInstanceOfTypeSync(FLOW_METADATA_TYPE))

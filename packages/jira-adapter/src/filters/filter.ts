26b54edd49f31f2894553ea737b97ca8771c3e52
IdoZakk <109526049+IdoZakk@users.noreply.github.com>
2024-12-17 14:55:48 +0200

    SALTO-7062: Deployment of filters with user share permissions (#6940)
    * SALTO-7062: Deployment of filters with user share permissions

--- packages/jira-adapter/src/filters/filter.ts
+++ packages/jira-adapter/src/filters/filter.ts
@@ -13,50 +13,61 @@ import {
   ChangeDataType,
   Value,
 } from '@salto-io/adapter-api'
+import { collections } from '@salto-io/lowerdash'
+import { createSchemeGuard } from '@salto-io/adapter-utils'
+import Joi from 'joi'
 import { FilterCreator } from '../filter'
+import { FILTER_TYPE_NAME, SHARE_PERMISSION_FIELDS } from '../constants'
 
+const { makeArray } = collections.array
+const USER = 'user'
+
+type Permission = {
+  type: string
+  user: Value
+}
+
+const USER_PERMISSION_SCHEME = Joi.object({
+  type: Joi.string().valid(USER).required(),
+  user: Joi.any().required(),
+})
+
+const isUserPermission = createSchemeGuard<Permission>(USER_PERMISSION_SCHEME)
+
+type ChangeFormatFunction = (permission: Permission) => void
+
+const toDeploymentFormat = (permission: Permission): void => {
+  permission.user = {
+    accountId: permission.user,
+  }
+}
+
+const toNaclFormat = (permission: Permission): void => {
+  permission.user = permission.user.accountId
+}
+
+const changePermissionFieldFormat = (changes: Change<ChangeDataType>[], changeFunction: ChangeFormatFunction): void => {
+  changes
+    .filter(isInstanceChange)
+    .filter(isAdditionOrModificationChange)
+    .map(getChangeData)
+    .filter(instance => instance.elemID.typeName === FILTER_TYPE_NAME)
+    .forEach(instance => {
+      SHARE_PERMISSION_FIELDS.forEach(fieldName => {
+        makeArray(instance.value[fieldName]).filter(isUserPermission).forEach(changeFunction)
+      })
+    })
+}
+
+// handles the permissions format change. The share permission is not relevant for dc, as there is no option for it there
 const filter: FilterCreator = () => ({
   name: 'filtersFilter',
   preDeploy: async (changes: Change<ChangeDataType>[]) => {
-    changes
-      .filter(isInstanceChange)
-      .filter(isAdditionOrModificationChange)
-      .map(change => getChangeData(change))
-      .filter(element => element.elemID.typeName === 'Filter')
-      .filter(
-        element =>
-          Object.prototype.hasOwnProperty.call(element.value, 'editPermissions') &&
-          Array.isArray(element.value.editPermissions),
-      )
-      .forEach(element => {
-        element.value.editPermissions
-          .filter((permission: Value) => permission.type === 'user')
-          .forEach((permission: Value) => {
-            permission.user = {
-              accountId: permission.user,
-            }
-          })
-      })
+    changePermissionFieldFormat(changes, toDeploymentFormat)
   },
 
   onDeploy: async (changes: Change<ChangeDataType>[]) => {
-    changes
-      .filter(isInstanceChange)
-      .filter(isAdditionOrModificationChange)
-      .map(change => getChangeData(change))
-      .filter(element => element.elemID.typeName === 'Filter')
-      .filter(
-        element =>
-          Object.prototype.hasOwnProperty.call(element.value, 'editPermissions') &&
-          Array.isArray(element.value.editPermissions),
-      )
-      .forEach(element => {
-        element.value.editPermissions
-          .filter((permission: Value) => permission.type === 'user')
-          .forEach((permission: Value) => {
-            permission.user = permission.user.accountId
-          })
-      })
+    changePermissionFieldFormat(changes, toNaclFormat)
   },
 })
 

77686d0296509cc41b5fe71ac1b08e231adde239
Rotem Cohen <rotem.cohen@salto.io>
2025-01-01 15:20:16 +0200

    fixed tests

--- packages/salesforce-adapter/test/custom_references/omit_non_existing_fields.test.ts
+++ packages/salesforce-adapter/test/custom_references/omit_non_existing_fields.test.ts
@@ -6,122 +6,141 @@
  * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
  */
 
-import { ElemID, Field, InstanceElement, ObjectType } from '@salto-io/adapter-api'
+import { ElemID, Field, FixElementsFunc, InstanceElement, ObjectType } from '@salto-io/adapter-api'
 import { buildElementsSourceFromElements } from '@salto-io/adapter-utils'
-import { API_NAME, CUSTOM_OBJECT, METADATA_TYPE, SALESFORCE } from '../../src/constants'
+import { CUSTOM_METADATA_SUFFIX, SALESFORCE } from '../../src/constants'
 import { omitNonExistingFieldsHandler } from '../../src/custom_references/omit_non_existing_fields'
 import { mockTypes } from '../mock_elements'
+import { createCustomMetadataType, createCustomObjectType } from '../utils'
 
-describe('custom metadata and data types non existing fields', () => {
-  const customObjectType = new ObjectType({
-    elemID: new ElemID(SALESFORCE, 'customObjectType'),
-    annotations: { [API_NAME]: 'api__c', [METADATA_TYPE]: CUSTOM_OBJECT },
-    fields: {
-      fullName: new Field(
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-        'fullName',
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-      ),
-      sdf__c: new Field(
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-        'anotherField__c',
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-      ),
-    },
-  })
-  const metadataType = new ObjectType({
-    elemID: new ElemID(SALESFORCE, 'metadataType'),
-    annotations: { [API_NAME]: 'api__mdt' },
-    fields: {
-      id: new Field(
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-        'fullName',
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-      ),
-      sdf__c: new Field(
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-        'anotherField__c',
-        new ObjectType({ elemID: new ElemID(SALESFORCE, 'testTypeField') }),
-      ),
-    },
-  })
-  const flowType = mockTypes.Flow
-  const customObjectElementExtraValues = new InstanceElement('instance', customObjectType, {
-    fullName: 'asd',
-    sdf__c: 'sdf',
-    dfg__c: 'dfg',
-    qwerty: 'qwert',
-    id: 'fake',
-    customObjectElementExtraValues: 'fake',
-  })
-  const customObjectElement = new InstanceElement('instance', customObjectType, {
-    fullName: 'asd',
-    sdf__c: 'sdf',
-  })
-  const dataTypeElementExtraValues = new InstanceElement('instance', metadataType, {
-    id: 'asd',
-    sdf__c: 'sdf',
-    dfg__c: 'dfg',
-    qwerty: 'qwert',
-    fullName: 'fake',
-    dataTypeElementExtraValues: 'fake',
-  })
-  const dataTypeElement = new InstanceElement('instance', metadataType, {
-    id: 'asd',
-    sdf__c: 'sdf',
-  })
-  const flowElement = new InstanceElement('instance', flowType, {
-    status: 'asd',
-    fake__c: 'sdf',
-    flowElement: 'fake',
-  })
-  const elementsSource = buildElementsSourceFromElements([])
-  const fixElementsFunc = omitNonExistingFieldsHandler.removeWeakReferences({
-    elementsSource,
-    config: {
-      fetch: {
-        metadata: {
-          include: [
-            {
-              metadataType: '.*',
-              name: '.*',
-              namespace: '',
-            },
-          ],
+describe('custom object and custom metadata types non existing fields', () => {
+  let field: Field
+  let customObjectType: ObjectType
+  let metadataType: ObjectType
+  let flowType: ObjectType
+  let customObjectElementExtraValues: InstanceElement
+  let customObjectElement: InstanceElement
+  let customMetaDataTypeElementExtraValues: InstanceElement
+  let customMetaDataTypeElement: InstanceElement
+  let flowElement: InstanceElement
+  let fixElementsFunc: FixElementsFunc
+  beforeEach(() => {
+    field = new Field(
+      new ObjectType({ elemID: new ElemID(SALESFORCE, 'testField') }),
+      'fieldName',
+      new ObjectType({ elemID: new ElemID(SALESFORCE, 'testField') }),
+    )
+    fixElementsFunc = omitNonExistingFieldsHandler.removeWeakReferences({
+      elementsSource: buildElementsSourceFromElements([]),
+      config: {
+        fetch: {
+          metadata: {
+            include: [
+              {
+                metadataType: '.*',
+                name: '.*',
+                namespace: '',
+              },
+            ],
+          },
         },
       },
-    },
+    })
+
+    customObjectType = createCustomObjectType('customObjectType', {
+      fields: {
+        customObjectElementExtraValues__c: field,
+      },
+    })
+    metadataType = createCustomMetadataType(['metadataType', CUSTOM_METADATA_SUFFIX].join(''), {
+      fields: {
+        dataTypeElementExtraValues__c: field,
+        fullName: field,
+        sdf__c: field,
+      },
+    })
+    flowType = mockTypes.Flow
   })
 
-  describe('when receiving multiple elements', () => {
-    describe("when some have only values that exist in the type's fields, and some have values that don't exist in the type's fields", () => {
-      it("should omit values that don't exist in the type's fields not change the rest", async () => {
-        const { fixedElements, errors } = await fixElementsFunc([
-          customObjectElement,
-          customObjectElementExtraValues,
-          dataTypeElement,
-          dataTypeElementExtraValues,
-          flowElement,
-        ])
-        expect(fixedElements).toHaveLength(2)
-        const ele0 = fixedElements[0] as InstanceElement
-        expect(Object.keys(ele0.value)).toContain('fullName')
-        expect(Object.keys(ele0.value)).toContain('sdf__c')
-        expect(Object.keys(ele0.value)).not.toContain('qwerty')
-        expect(Object.keys(ele0.value)).not.toContain('dfg__c')
-        expect(Object.keys(ele0.value)).not.toContain('id')
-        expect(Object.keys(ele0.value)).not.toContain('customObjectElementExtraValues')
-        const ele1 = fixedElements[1] as InstanceElement
-        expect(Object.keys(ele1.value)).toContain('id')
-        expect(Object.keys(ele1.value)).toContain('sdf__c')
-        expect(Object.keys(ele1.value)).not.toContain('qwerty')
-        expect(Object.keys(ele1.value)).not.toContain('dfg__c')
-        expect(Object.keys(ele1.value)).not.toContain('fullName')
-        expect(Object.keys(ele1.value)).not.toContain('dataTypeElementExtraValues')
-        expect(errors).toHaveLength(2)
-        expect(errors[0].elemID).toEqual(customObjectElementExtraValues.elemID)
-        expect(errors[1].elemID).toEqual(dataTypeElement.elemID)
+  describe("when custom object with values that are not defined in the type's fields", () => {
+    it('should omit those values', async () => {
+      customObjectElementExtraValues = new InstanceElement('instance', customObjectType, {
+        Id: 'asd',
+        Name: 'sdf',
+        customObjectElementExtraValues__c: 'fake',
+        dfg__c: 'dfg',
+        qwerty: 'qwert',
+        id: 'fake',
       })
+      const { fixedElements, errors } = await fixElementsFunc([customObjectElementExtraValues])
+      expect(fixedElements).toHaveLength(1)
+      const ele = fixedElements[0] as InstanceElement
+      expect(Object.keys(ele.value)).toContain('Name')
+      expect(Object.keys(ele.value)).toContain('Id')
+      expect(Object.keys(ele.value)).toContain('customObjectElementExtraValues__c')
+      expect(Object.keys(ele.value)).not.toContain('qwerty')
+      expect(Object.keys(ele.value)).not.toContain('dfg__c')
+      expect(Object.keys(ele.value)).not.toContain('id')
+      expect(errors).toHaveLength(1)
+      expect(errors[0].elemID).toEqual(customObjectElementExtraValues.elemID)
+    })
+  })
+  describe("when custom object with only values that are defined in the type's fields", () => {
+    it('should not change the instance', async () => {
+      customObjectElement = new InstanceElement('instance', customObjectType, {
+        Id: 'asd',
+        Name: 'sdf',
+      })
+      const { fixedElements, errors } = await fixElementsFunc([customObjectElement])
+      expect(fixedElements).toHaveLength(0)
+      expect(errors).toHaveLength(0)
+    })
+  })
+  describe("when custom metadata type with values that are not defined in the type's fields", () => {
+    it('should omit those values', async () => {
+      customMetaDataTypeElementExtraValues = new InstanceElement('instance', metadataType, {
+        fullName: 'real',
+        dataTypeElementExtraValues__c: 'real',
+        sdf__c: 'real',
+        id: 'fake',
+        dfg__c: 'dfg',
+        qwerty: 'qwert',
+      })
+      const { fixedElements, errors } = await fixElementsFunc([customMetaDataTypeElementExtraValues])
+      expect(fixedElements).toHaveLength(1)
+      const ele = fixedElements[0] as InstanceElement
+      expect(Object.keys(ele.value)).toContain('fullName')
+      expect(Object.keys(ele.value)).toContain('sdf__c')
+      expect(Object.keys(ele.value)).toContain('dataTypeElementExtraValues__c')
+      expect(Object.keys(ele.value)).not.toContain('id')
+      expect(Object.keys(ele.value)).not.toContain('qwerty')
+      expect(Object.keys(ele.value)).not.toContain('dfg__c')
+      expect(errors).toHaveLength(1)
+      expect(errors[0].elemID).toEqual(customMetaDataTypeElementExtraValues.elemID)
+    })
+  })
+  describe("when custom metadata type with only values that are defined in the type's fields", () => {
+    it('should not change the instance', async () => {
+      customMetaDataTypeElement = new InstanceElement('instance', metadataType, {
+        fullName: 'asd',
+        sdf__c: 'sdf',
+        dataTypeElementExtraValues__c: 'real',
+      })
+      const { fixedElements, errors } = await fixElementsFunc([customMetaDataTypeElement])
+      expect(fixedElements).toHaveLength(0)
+      expect(errors).toHaveLength(0)
+    })
+  })
+  describe('when instance is neither custom metadata type nor custom object', () => {
+    it("should not change the instance regardless of it's values", async () => {
+      flowElement = new InstanceElement('instance', flowType, {
+        status: 'asd',
+        fake__c: 'sdf',
+        flowElement: 'fake',
+      })
+      const { fixedElements, errors } = await fixElementsFunc([flowElement])
+      expect(fixedElements).toHaveLength(0)
+      expect(errors).toHaveLength(0)
     })
   })
 })

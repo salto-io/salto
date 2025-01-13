4ebeaf5019c8db214fc6dea6b83793bc98cd7305
Amir Rachum <amir.rachum@salto.io>
2025-01-13 13:15:03 +0200

    save

--- packages/adapter-components/src/fetch/element/element.ts
+++ packages/adapter-components/src/fetch/element/element.ts
@@ -18,6 +18,7 @@ import {
   hideAndOmitFields,
   overrideFieldTypes,
   createRemainingTypes,
+  addImportantValues,
 } from './type_utils'
 import { ElementAndResourceDefFinder } from '../../definitions/system/fetch/types'
 import { FetchApiDefinitionsOptions } from '../../definitions/system/fetch'
@@ -166,6 +167,7 @@ export const getElementGenerator = <Options extends FetchApiDefinitionsOptions>(
     // omit fields based on the adjusted types
     omitAllInstancesValues({ instances, defQuery })
     hideAndOmitFields({ definedTypes, defQuery, finalTypeNames })
+    addImportantValues({ definedTypes, defQuery, finalTypeNames })
 
     // only return types that are reachable from instances or definitions
     const filteredTypes = getReachableTypes({ instances, types: Object.values(definedTypes), defQuery })

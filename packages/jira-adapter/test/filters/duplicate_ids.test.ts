0dda3081de88d46633a06f807000f83ca64e85af
kami-delaroz <batel.delaroz@salto.io>
2025-01-13 18:09:58 +0200

    -

--- packages/jira-adapter/test/filters/duplicate_ids.test.ts
+++ packages/jira-adapter/test/filters/duplicate_ids.test.ts
@@ -159,14 +159,14 @@ describe('duplicateIdsFilter', () => {
           message: `The following elements had duplicate names in Jira. It is strongly recommended to rename these instances so they are unique in Jira, then re-fetch.
 If changing the names is not possible, you can add the fetch.fallbackToInternalId option to the configuration file; that will add their internal ID to their names and fetch them. Read more here: https://help.salto.io/en/articles/6927157-salto-id-collisions
 dup (jira.Status.instance.dup),
-instance alias (jira.Status.instance.dup). View in the service - https://dup_1_someurl.com,
-another instance alias (jira.Status.instance.dup). View in the service - https://dup_2_someurl.com,
+instance alias (jira.Status.instance.dup). View in the service - https://dup_1_someurl.com ,
+another instance alias (jira.Status.instance.dup). View in the service - https://dup_2_someurl.com ,
 instance alias (jira.Status.instance.dup)`,
           detailedMessage: `The following elements had duplicate names in Jira. It is strongly recommended to rename these instances so they are unique in Jira, then re-fetch.
 If changing the names is not possible, you can add the fetch.fallbackToInternalId option to the configuration file; that will add their internal ID to their names and fetch them. Read more here: https://help.salto.io/en/articles/6927157-salto-id-collisions
 dup (jira.Status.instance.dup),
-instance alias (jira.Status.instance.dup). View in the service - https://dup_1_someurl.com,
-another instance alias (jira.Status.instance.dup). View in the service - https://dup_2_someurl.com,
+instance alias (jira.Status.instance.dup). View in the service - https://dup_1_someurl.com ,
+another instance alias (jira.Status.instance.dup). View in the service - https://dup_2_someurl.com ,
 instance alias (jira.Status.instance.dup)`,
           severity: 'Warning',
         },

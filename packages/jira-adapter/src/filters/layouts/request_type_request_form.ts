c6a274798d84133d2223ef34048510faacba35cd
yuval.givon <yuval.givon@salto.io>
2025-01-20 12:30:31 +0200

    cr

--- packages/jira-adapter/src/filters/layouts/request_type_request_form.ts
+++ packages/jira-adapter/src/filters/layouts/request_type_request_form.ts
@@ -6,19 +6,46 @@
  * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
  */
 
-import { Element, isInstanceElement, Value, Values } from '@salto-io/adapter-api'
+import { Element, isInstanceElement } from '@salto-io/adapter-api'
+import { createSchemeGuard } from '@salto-io/adapter-utils'
+import Joi from 'joi'
 import { ISSUE_VIEW_TYPE, REQUEST_FORM_TYPE } from '../../constants'
 import { FilterCreator } from '../../filter'
 import { fetchRequestTypeDetails } from './layout_service_operations'
 
-const deleteEmptyProperties = (fields: Values[]): void => {
+type requestTypeRequestFormData = {
+  properties?: Record<string, string>
+}
+
+type requestTypeRequestFormItemComponent = {
+  data: requestTypeRequestFormData
+}
+
+const REQUEST_TYPE_REQUEST_FORM_ITEM_COMPONENT_SCHEME = Joi.object({
+  data: Joi.object({
+    properties: Joi.array()
+      .items(
+        Joi.object({
+          key: Joi.string().required(),
+          value: Joi.string().required(),
+        }),
+      )
+      .required(),
+  }).unknown(true),
+})
+  .unknown(true)
+  .required()
+
+const requestTypeRequestFormItemComponent = createSchemeGuard<requestTypeRequestFormItemComponent>(
+  REQUEST_TYPE_REQUEST_FORM_ITEM_COMPONENT_SCHEME,
+)
+
+const deleteEmptyProperties = (fields: requestTypeRequestFormData[]): void => {
   fields.forEach(field => {
-    if (field.properties !== undefined) {
-      field.properties = Object.entries(field.properties).filter(([_key, value]) => value !== '')
-      if (field.properties.length === 0) {
+    if (field.properties) {
+      field.properties = Object.fromEntries(Object.entries(field.properties).filter(([_key, value]) => value !== ''))
+      if (Object.keys(field.properties).length === 0) {
         delete field.properties
-      } else {
-        field.properties = Object.fromEntries(field.properties)
       }
     }
   })
@@ -30,7 +57,9 @@ const filterEmptyProperties = (elements: Element[]): void => {
     .filter(isInstanceElement)
     .forEach(instance => {
       if (Array.isArray(instance.value.issueLayoutConfig?.items)) {
-        deleteEmptyProperties(instance.value.issueLayoutConfig.items.map((item: Value) => item.data ?? {}))
+        deleteEmptyProperties(
+          instance.value.issueLayoutConfig.items.map((item: requestTypeRequestFormItemComponent) => item.data ?? {}),
+        )
       }
     })
 }

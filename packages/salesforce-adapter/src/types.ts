d6687bba0892b31865aaff3a86a87c483c7e9de9
Almog Mesilaty <almog.mesilaty@salto.io>
2025-01-27 19:00:43 +0200

    Added UTs

--- packages/salesforce-adapter/src/types.ts
+++ packages/salesforce-adapter/src/types.ts
@@ -189,6 +189,7 @@ const CHANGE_VALIDATORS = [
   'flowReferencedElements',
   'liveChatButtonRoutingType',
   'flexiPageUnusedOrMissingFacets',
+  'uniqueFlowElementName',
 ] as const
 const DEPRECATED_CHANGE_VALIDATORS = ['multipleDefaults'] as const
 export type ChangeValidatorName = (typeof CHANGE_VALIDATORS)[number]

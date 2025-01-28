46729b0c04ac568e971543e906845e4a574c7ffe
Almog Mesilaty <almog.mesilaty@salto.io>
2025-01-28 09:51:06 +0200

    CR changes

--- packages/salesforce-adapter/src/change_validators/unique_flow_element_name.ts
+++ packages/salesforce-adapter/src/change_validators/unique_flow_element_name.ts
@@ -14,7 +14,7 @@ import {
   isAdditionOrModificationChange,
 } from '@salto-io/adapter-api'
 import { collections } from '@salto-io/lowerdash'
-import { TransformFuncSync, transformValuesSync } from '@salto-io/adapter-utils'
+import { WALK_NEXT_STEP, walkOnElement, WalkOnFunc } from '@salto-io/adapter-utils'
 import { isInstanceOfTypeSync } from '../filters/utils'
 import { FLOW_METADATA_TYPE } from '../constants'
 
@@ -23,26 +23,24 @@ const { DefaultMap } = collections.map
 const createChangeErrors = (element: InstanceElement): ChangeError[] => {
   const duplicates = new Set<string>()
   const nameToElemIds = new DefaultMap<string, ElemID[]>(() => [])
-  const mapNameToElemIds: TransformFuncSync = ({ value, path, field }) => {
-    if (field === undefined || path === undefined) return value
-    if (field.name === 'name' && nameToElemIds.get(value).length > 0) {
+  const mapNameToElemIds: WalkOnFunc = ({ value, path }) => {
+    if (path === undefined || path.name !== 'name') return WALK_NEXT_STEP.RECURSE
+    if (nameToElemIds.get(value).length > 0) {
       duplicates.add(value)
     }
     nameToElemIds.get(value).push(path)
-    return value
+    return WALK_NEXT_STEP.RECURSE
   }
-  transformValuesSync({
-    values: element.value,
-    pathID: element.elemID,
-    type: element.getTypeSync(),
-    transformFunc: mapNameToElemIds,
+  walkOnElement({
+    element,
+    func: mapNameToElemIds,
   })
   return Array.from(duplicates).flatMap(name =>
     nameToElemIds.get(name).map(elemId => ({
       elemID: elemId,
       severity: 'Warning',
-      message: 'Duplicate FlowElement Name',
-      detailedMessage: `The name "${name}" must be unique within the Flow.`,
+      message: 'Duplicate Name in Flow',
+      detailedMessage: `The name "${name}" is used multiple times within this Flow.`,
     })),
   )
 }

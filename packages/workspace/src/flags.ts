5192e227754694735d0e41f040f5e2f9e8622497
Amir Rachum <amir.rachum@salto.io>
2025-01-29 10:00:07 +0200

    SALTO-7376: Cleanup `SALTO_COMPUTE_PLAN_ON_FETCH` flag.

--- packages/workspace/src/flags.ts
+++ packages/workspace/src/flags.ts
@@ -24,8 +24,5 @@ export const getSaltoFlagBool = (flagName: string): boolean => {
 export const WORKSPACE_FLAGS = {
   createFilenamesToElementIdsMapping: 'CREATE_FILENAMES_TO_ELEMENT_IDS_MAPPING',
   useSplitSourceMapInUpdate: 'USE_SPLIT_SOURCE_MAP_IN_UPDATE',
-  // Killswitch for the new fetch diff computation logic. Activating this restores the old logic.
-  // TODO(SALTO-6992): Remove this killswitch after 2025-01-30
-  computePlanOnFetch: 'COMPUTE_PLAN_ON_FETCH',
   skipStaticFilesCacheUpdate: 'SKIP_STATIC_FILES_CACHE_UPDATE',
 } as const

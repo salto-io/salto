de6c57bf98d70e6bc2f981caf3b57ccb8a35c272
Shay Cohen <shay.cohen@salto.io>
2025-02-04 15:37:55 +0200

    add order error

--- packages/jira-adapter/src/change_validators/field_contexts/option_removal.ts
+++ packages/jira-adapter/src/change_validators/field_contexts/option_removal.ts
@@ -7,22 +7,26 @@
  */
 import {
   Change,
+  ChangeError,
   ChangeValidator,
   ElemID,
   getChangeData,
   InstanceElement,
   isInstanceChange,
+  isInstanceElement,
+  isModificationChange,
   isRemovalChange,
   SeverityLevel,
 } from '@salto-io/adapter-api'
+import _ from 'lodash'
 import { logger } from '@salto-io/logging'
 import { createSchemeGuard } from '@salto-io/adapter-utils'
 import Joi from 'joi'
 import { values } from '@salto-io/lowerdash'
 import JiraClient from '../../client/client'
-import { getContextAndFieldIds } from '../../common/fields'
+import { getContextAndFieldIds, getContextParent } from '../../common/fields'
 import { JiraConfig } from '../../config/config'
-import { FIELD_CONTEXT_OPTION_TYPE_NAME } from '../../filters/fields/constants'
+import { FIELD_CONTEXT_OPTION_TYPE_NAME, OPTIONS_ORDER_TYPE_NAME } from '../../filters/fields/constants'
 import { removeCustomFieldPrefix } from '../../filters/jql/template_expression_generator'
 
 const log = logger(module)
@@ -105,9 +109,35 @@ const isActiveOption = async (
 const optionMigrationLink = (client: JiraClient, fieldOptionContext: FieldOptionContext): string =>
   `${client.baseUrl}/secure/admin/EditCustomFieldOptions!remove.jspa?fieldConfigId=${fieldOptionContext.contextId}&selectedValue=${fieldOptionContext.optionId}`
 
+const getFiledContextOrderErrors = (
+  activeOptionRemovals: FieldOptionContext[],
+  contextIdToOrder: Record<string, InstanceElement>,
+): ChangeError[] =>
+  activeOptionRemovals
+    .map(fieldOptionContext => ({
+      fieldOptionContext,
+      fieldContextOptionsOrder: contextIdToOrder[fieldOptionContext.contextId],
+    }))
+    .filter(({ fieldContextOptionsOrder }) => fieldContextOptionsOrder !== undefined)
+    .map(({ fieldOptionContext, fieldContextOptionsOrder }) => ({
+      elemID: fieldContextOptionsOrder.elemID,
+      severity: 'Error' as SeverityLevel,
+      message: "This order is not referencing all it's options",
+      detailedMessage: `This order cannot be deployed because it depends on removing the option '${fieldOptionContext.optionValue}', which cannot be deployed because it is still in use by existing issues.`,
+    }))
+
+const getFieldContextOptionErrors = (activeOptionRemovals: FieldOptionContext[], client: JiraClient): ChangeError[] =>
+  activeOptionRemovals.map(fieldOptionContext => ({
+    elemID: fieldOptionContext.optionElemID,
+    severity: 'Error' as SeverityLevel,
+    message: 'Cannot remove field context option as it is in use by issues',
+    detailedMessage: `The option "${fieldOptionContext.optionValue}" of field "${fieldOptionContext.fieldName}" is in use by issues. Please migrate the issues to another option in Jira UI via ${optionMigrationLink(client, fieldOptionContext)}, and then refresh the deployment.`,
+  }))
+
 /**
- * This validator checks if there are any active issues that use the context option that is being removed.
- * If there are, the validator will return an error with a message that explains that the user needs to migrate the issues in Jira UI.
+ * Validates whether any issues are using the context option that is being removed.
+ * If these issues exist, the validator returns an error on the relevant `fieldContextOption`
+ * and `fieldContextOrder` changes, prompting the user to migrate the issues in the Jira UI.
  */
 export const fieldContextOptionRemovalValidator: (config: JiraConfig, client: JiraClient) => ChangeValidator =
   (config, client) => async changes => {
@@ -118,9 +148,11 @@ export const fieldContextOptionRemovalValidator: (config: JiraConfig, client: Ji
       .filter(isInstanceChange)
       .filter(isRemovalChange)
       .filter(change => change.data.before.elemID.typeName === FIELD_CONTEXT_OPTION_TYPE_NAME)
+    if (_.isEmpty(optionRemovalChanges)) {
+      return []
+    }
 
     const optionNameToFieldOptionContext = getOptionNameToFieldOptionContext(optionRemovalChanges)
-
     const activeOptionRemovals = (
       await Promise.all(
         optionRemovalChanges.map(getChangeData).map(async option => {
@@ -136,10 +168,23 @@ export const fieldContextOptionRemovalValidator: (config: JiraConfig, client: Ji
     )
       .filter(isDefined)
       .filter(option => option.isActive)
-    return activeOptionRemovals.map(fieldOptionContext => ({
-      elemID: fieldOptionContext.optionElemID,
-      severity: 'Error' as SeverityLevel,
-      message: 'Cannot remove field context option as it is in use by issues',
-      detailedMessage: `The option "${fieldOptionContext.optionValue}" of field "${fieldOptionContext.fieldName}" is in use by issues. Please migrate the issues to another option in Jira UI via ${optionMigrationLink(client, fieldOptionContext)}, and then refresh the deployment.`,
-    }))
+
+    if (_.isEmpty(activeOptionRemovals)) {
+      return []
+    }
+
+    const relevantOrderInstances = changes
+      .filter(isModificationChange)
+      .filter(change => change.data.before.elemID.typeName === OPTIONS_ORDER_TYPE_NAME)
+      .map(getChangeData)
+      .filter(isInstanceElement)
+
+    const contextIdToOrder = _.keyBy(relevantOrderInstances, change => getContextParent(change).value.id) as Record<
+      string,
+      InstanceElement
+    >
+    const orderChangeErrors = getFiledContextOrderErrors(activeOptionRemovals, contextIdToOrder)
+    const optionRemovalErrors = getFieldContextOptionErrors(activeOptionRemovals, client)
+
+    return optionRemovalErrors.concat(orderChangeErrors)
   }

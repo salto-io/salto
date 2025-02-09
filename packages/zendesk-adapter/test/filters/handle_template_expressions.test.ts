afefe5e5a955f3d0f43fb228193c66bc8cd63c4a
Adam Fineberg <adam.fineberg@salto.io>
2025-02-09 13:18:26 +0200

    Update tests

--- packages/zendesk-adapter/test/filters/handle_template_expressions.test.ts
+++ packages/zendesk-adapter/test/filters/handle_template_expressions.test.ts
@@ -370,7 +370,7 @@ describe('handle templates filter', () => {
     'articleTranslation',
     new ObjectType({ elemID: new ElemID(ZENDESK, ARTICLE_TRANSLATION_TYPE_NAME) }),
     {
-      body: `"/hc/test/test/articles/${article.value.id}/test"\n"hc/test/test/articles/${macro1.value.id}/test\n"hc/test/test/sections/${article.value.id}/test`,
+      body: `"/hc/test/test/articles/${article.value.id}/test"\n"hc/test/test/articles/${macro1.value.id}/test\n"hc/test/test/sections/${article.value.id}/test\n"hc/test/no-references/index.html"`,
     },
   )
 
@@ -760,7 +760,7 @@ describe('handle templates filter', () => {
         .filter(isInstanceElement)
         .find(i => i.elemID.name === 'articleTranslation')
       expect(fetchedArticleTranslation?.value.body).toEqual(
-        `"/hc/test/test/articles/${article.value.id}/test"\n"hc/test/test/articles/${macro1.value.id}/test\n"hc/test/test/sections/${article.value.id}/test`,
+        `"/hc/test/test/articles/${article.value.id}/test"\n"hc/test/test/articles/${macro1.value.id}/test\n"hc/test/test/sections/${article.value.id}/test\n"hc/test/no-references/index.html"`,
       )
     })
     it('should resolve urls correctly when config flags are on', async () => {
@@ -783,7 +783,7 @@ describe('handle templates filter', () => {
             new ReferenceExpression(macro1.elemID, macro1),
             '/test\n"hc/test/test/sections/',
             new ReferenceExpression(article.elemID, article),
-            '/test',
+            '/test\n"hc/test/no-references/index.html"',
           ],
         }),
       )

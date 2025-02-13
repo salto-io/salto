8df65093fc1e273f7b079a535789f0e3b0aa4825
Amir Rachum <amir.rachum@salto.io>
2025-02-13 13:37:07 +0200

    SALTO-7272: Compute config changes with `calculateDiff` instead of `getPlan` (#7187)

--- packages/cli/src/callbacks.ts
+++ packages/cli/src/callbacks.ts
@@ -15,9 +15,11 @@ import {
   InstanceElement,
   isPrimitiveType,
   PrimitiveTypes,
+  Change,
 } from '@salto-io/adapter-api'
-import { FetchChange, PlanItem } from '@salto-io/core'
+import { FetchChange } from '@salto-io/core'
 import { collections } from '@salto-io/lowerdash'
+import { toDetailedChangeFromBaseChange } from '@salto-io/adapter-utils'
 import {
   formatFetchChangeForApproval,
   formatShouldContinueWithWarning,
@@ -66,9 +68,14 @@ export const shouldAbortWorkspaceInCaseOfValidationError = async (numErrors: num
 export const shouldUpdateConfig = async (
   { stdout }: CliOutput,
   introMessage: string,
-  change: PlanItem,
+  change: Change,
 ): Promise<boolean> => {
-  stdout.write(formatConfigChangeNeeded(introMessage, await formatDetailedChanges([change.detailedChanges()], true)))
+  stdout.write(
+    formatConfigChangeNeeded(
+      introMessage,
+      await formatDetailedChanges([[toDetailedChangeFromBaseChange(change)]], true),
+    ),
+  )
   return getUserBooleanInput(Prompts.SHOULD_UPDATE_CONFIG)
 }
 

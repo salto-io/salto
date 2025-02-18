35ce3582791272a14d218503baf5688c3551267a
Ido Zak <ido.zak@salto.io>
2025-02-18 11:49:42 +0200

    SALTO-7470: second project scope context

--- packages/jira-adapter/src/change_validators/field_contexts/second_global_context.ts
+++ packages/jira-adapter/src/change_validators/field_contexts/second_global_context.ts
@@ -8,16 +8,16 @@
 import {
   ChangeError,
   ChangeValidator,
-  CORE_ANNOTATIONS,
   ElemID,
   getChangeData,
-  InstanceElement,
   isAdditionChange,
   isAdditionOrModificationChange,
   isInstanceChange,
+  isReferenceExpression,
+  ReferenceExpression,
   SeverityLevel,
 } from '@salto-io/adapter-api'
-import { getInstancesFromElementSource, getParentElemID } from '@salto-io/adapter-utils'
+import { getElementPrettyName, getInstancesFromElementSource, getParentElemID } from '@salto-io/adapter-utils'
 import { logger } from '@salto-io/logging'
 import _ from 'lodash'
 import { FIELD_CONTEXT_TYPE_NAME } from '../../filters/fields/constants'
@@ -26,16 +26,23 @@ import { AddOrModifyInstanceChange } from '../../common/general'
 
 const log = logger(module)
 
-const createFieldContextErrorMessage = (elemID: ElemID, field: InstanceElement): ChangeError => ({
+const createSecondGlobalContextErrorMessage = (elemID: ElemID, fieldName: string): ChangeError => ({
   elemID,
   severity: 'Error' as SeverityLevel,
   message: 'A field can only have a single global context',
-  detailedMessage: `Can't deploy this global context because the deployment will result in more than a single global context for field ${field.annotations[CORE_ANNOTATIONS.ALIAS] ?? field.elemID.name}.`,
+  detailedMessage: `Can't deploy this global context because the deployment will result in more than a single global context for field ${fieldName}.`,
 })
 
-export const fieldSecondGlobalContextValidator: ChangeValidator = async (changes, elementSource) => {
+const createProjectScopeErrorMessage = (elemID: ElemID, fieldName: string, projectName: string): ChangeError => ({
+  elemID,
+  severity: 'Error' as SeverityLevel,
+  message: 'A field can only have a single context per project',
+  detailedMessage: `Can't deploy this project scoped context because the deployment will result in more than one context for field ${fieldName} with a scope for project ${projectName}`,
+})
+
+export const fieldSecondContextValidator: ChangeValidator = async (changes, elementSource) => {
   if (elementSource === undefined) {
-    log.error('Failed to run fieldSecondGlobalContextValidator because element source is undefined')
+    log.error('Failed to run fieldSecondContextValidator because element source is undefined')
     return []
   }
 
@@ -47,21 +54,67 @@ export const fieldSecondGlobalContextValidator: ChangeValidator = async (changes
   if (contextChanges.length === 0) {
     return []
   }
-  const fieldToGlobalContextCount = _.countBy(
-    (await getInstancesFromElementSource(elementSource, [FIELD_CONTEXT_TYPE_NAME])).filter(isGlobalContext),
+
+  const contextsByFieldName = _.groupBy(
+    await getInstancesFromElementSource(elementSource, [FIELD_CONTEXT_TYPE_NAME]),
     instance => getParentElemID(instance).getFullName(),
+  )
+
+  const globalContextCountByFieldName = _.mapValues(
+    contextsByFieldName,
+    instances => instances.filter(isGlobalContext).length,
+  )
+  const contextCountByFieldNameAndProject = _.mapValues(contextsByFieldName, instances =>
+    _.chain(instances)
+      .flatMap(context => (context.value.projectIds ? context.value.projectIds : []))
+      .filter(isReferenceExpression)
+      .map(projectReference => projectReference.elemID.getFullName())
+      .countBy()
+      .value(),
   )
 
   const addedGlobalContext = (change: AddOrModifyInstanceChange): boolean =>
     isGlobalContext(change.data.after) && (isAdditionChange(change) || !isGlobalContext(change.data.before))
 
-  return Promise.all(
+  const secondGlobalErrors = Promise.all(
     contextChanges
       .filter(addedGlobalContext)
       .map(getChangeData)
-      .filter(instance => fieldToGlobalContextCount[getParentElemID(instance).getFullName()] > 1)
+      .filter(instance => globalContextCountByFieldName[getParentElemID(instance).getFullName()] > 1)
       .map(async instance =>
-        createFieldContextErrorMessage(instance.elemID, await elementSource.get(getParentElemID(instance))),
+        createSecondGlobalContextErrorMessage(
+          instance.elemID,
+          getElementPrettyName(await elementSource.get(getParentElemID(instance))),
+        ),
       ),
   )
+
+  const secondProjectErrors = Promise.all(
+    contextChanges
+      .filter(isAdditionOrModificationChange)
+      .map(getChangeData)
+      .filter(instance => instance.value.projectIds && instance.value.projectIds.length > 0)
+      .map(instance => ({
+        instance,
+        secondProjectScopes: instance.value.projectIds
+          .filter(isReferenceExpression)
+          .map((projectReference: ReferenceExpression) => projectReference.elemID.getFullName())
+          .filter(
+            (projectName: string) =>
+              contextCountByFieldNameAndProject[getParentElemID(instance).getFullName()][projectName] > 1,
+          ),
+      }))
+      .filter(({ secondProjectScopes }) => secondProjectScopes.length > 0)
+      .map(async ({ instance, secondProjectScopes }) => {
+        const fieldName = getElementPrettyName(await elementSource.get(getParentElemID(instance)))
+        return secondProjectScopes.map((projectFullName: string) =>
+          createProjectScopeErrorMessage(instance.elemID, fieldName, projectFullName),
+        )
+      }),
+  )
+
+  return Promise.all([secondGlobalErrors, secondProjectErrors]).then(([globalErrors, projectErrors]) => [
+    ...globalErrors,
+    ...projectErrors.flat(),
+  ])
 }

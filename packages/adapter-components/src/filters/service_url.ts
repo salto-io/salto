85ef1e6efb61043ad3c7e8716dbf7643dc4271f5
Amir Rachum <amir.rachum@salto.io>
2025-02-24 15:26:36 +0200

    :art: formatting

--- packages/adapter-components/src/filters/service_url.ts
+++ packages/adapter-components/src/filters/service_url.ts
@@ -68,7 +68,7 @@ export const addUrlToInstance: <Options extends FetchApiDefinitionsOptions = {}>
     Object.entries(parentVal).forEach(([key, value]) => {
       // Fancy one-liner for doing "result._parent[idx][key] = value" while initializing all intermediate missing objects.
       // eslint-disable-next-line no-underscore-dangle
-      ((result._parent ??= {})[idx] ??= {})[key] = value
+      ;((result._parent ??= {})[idx] ??= {})[key] = value
     })
     return result
   }, {})

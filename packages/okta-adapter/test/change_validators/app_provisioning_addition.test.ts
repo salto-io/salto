efe23d4c488a73e10fab60d6f248225e35699e39
Ido Amit <idoamit8@gmail.com>
2025-02-26 09:54:06 +0200

    review

--- packages/okta-adapter/test/change_validators/app_provisioning_addition.test.ts
+++ packages/okta-adapter/test/change_validators/app_provisioning_addition.test.ts
@@ -5,38 +5,35 @@
  *
  * CERTAIN THIRD PARTY SOFTWARE MAY BE CONTAINED IN PORTIONS OF THE SOFTWARE. See NOTICE FILE AT https://github.com/salto-io/salto/blob/main/NOTICES
  */
-
-import { ObjectType, ElemID, InstanceElement, toChange } from '@salto-io/adapter-api'
-import { OKTA, APPLICATION_TYPE_NAME, APP_PROVISIONING_FIELD_NAMES } from '../../src/constants'
-import { appProvisioningAdditionValidator } from '../../src/change_validators/app_provisioning_addition'
+import { ObjectType, ElemID, InstanceElement, toChange } from '@salto-io/adapter-api';
+import { OKTA, APPLICATION_TYPE_NAME, APP_PROVISIONING_FIELD_NAMES } from '../../src/constants';
+import { appProvisioningAdditionValidator } from '../../src/change_validators/app_provisioning_addition';
 
 describe('appProvisioningAdditionValidator', () => {
-  const appType = new ObjectType({ elemID: new ElemID(OKTA, APPLICATION_TYPE_NAME) })
+  const appType = new ObjectType({ elemID: new ElemID(OKTA, APPLICATION_TYPE_NAME) });
 
   describe('When testing addition changes', () => {
-    APP_PROVISIONING_FIELD_NAMES.forEach(fieldName => {
-      it(`should return error when ${fieldName} is added`, async () => {
+    describe.each(APP_PROVISIONING_FIELD_NAMES)('when %s is added', (fieldName) => {
+      it('should return error', async () => {
         const app = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
           [fieldName]: {},
-        })
-        const changes = [toChange({ after: app })]
-        const changeErrors = await appProvisioningAdditionValidator(changes)
-        expect(changeErrors).toHaveLength(1)
+        });
+        const changes = [toChange({ after: app })];
+        const changeErrors = await appProvisioningAdditionValidator(changes);
+        expect(changeErrors).toHaveLength(1);
         expect(changeErrors).toEqual([
           {
             elemID: app.elemID,
             severity: 'Error',
             message: 'Application provisioning cannot be added',
-            detailedMessage:
-              'To deploy this application, remove the provisioning configuration first. Then, enable provisioning within the service and perform a fetch.',
-          },
-        ])
-      })
-    })
+            detailedMessage: 'To deploy this application, you must first remove the provisioning configuration. This is required because provisioning settings cannot be modified directly through Salto. After deployment, enable provisioning in Okta and perform a fetch to sync the latest configuration. For a step-by-step guide, visit https://help.salto.io/en/articles/10199943-deploying-okta-applications-using-salto and refer to the "Application provisioning settings" section.', }
+          ]);
+      });
+    });
 
     it('should not return error when no provisioning fields are present', async () => {
       const app = new InstanceElement('app1', appType, {
@@ -44,99 +41,98 @@ describe('appProvisioningAdditionValidator', () => {
         label: 'app1',
         status: 'INACTIVE',
         accessPolicy: 'accessPolicyId',
-      })
-      const changes = [toChange({ after: app })]
-      const changeErrors = await appProvisioningAdditionValidator(changes)
-      expect(changeErrors).toHaveLength(0)
-    })
-  })
+      });
+      const changes = [toChange({ after: app })];
+      const changeErrors = await appProvisioningAdditionValidator(changes);
+      expect(changeErrors).toHaveLength(0);
+    });
+  });
 
   describe('When testing modification changes', () => {
-    APP_PROVISIONING_FIELD_NAMES.forEach(fieldName => {
-      it(`should return error when ${fieldName} is added in modification`, async () => {
+    describe.each(APP_PROVISIONING_FIELD_NAMES)('when %s is added in modification', (fieldName) => {
+      it('should return error', async () => {
         const before = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
-        })
+        });
         const after = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
           [fieldName]: {},
-        })
-        const changes = [toChange({ before, after })]
-        const changeErrors = await appProvisioningAdditionValidator(changes)
-        expect(changeErrors).toHaveLength(1)
+        });
+        const changes = [toChange({ before, after })];
+        const changeErrors = await appProvisioningAdditionValidator(changes);
+        expect(changeErrors).toHaveLength(1);
         expect(changeErrors).toEqual([
           {
             elemID: after.elemID,
             severity: 'Error',
             message: 'Application provisioning cannot be added',
-            detailedMessage:
-              'To deploy this application, remove the provisioning configuration first. Then, enable provisioning within the service and perform a fetch.',
-          },
-        ])
-      })
+            detailedMessage: 'To deploy this application, you must first remove the provisioning configuration. This is required because provisioning settings cannot be modified directly through Salto. After deployment, enable provisioning in Okta and perform a fetch to sync the latest configuration. For a step-by-step guide, visit https://help.salto.io/en/articles/10199943-deploying-okta-applications-using-salto and refer to the "Application provisioning settings" section.', }
+          ]);
+      });
 
-      it(`should not return error when ${fieldName} is modified`, async () => {
+      it('should not return error when modified', async () => {
         const before = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
           [fieldName]: { someConfig: 'old' },
-        })
+        });
         const after = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
           [fieldName]: { someConfig: 'new' },
-        })
-        const changes = [toChange({ before, after })]
-        const changeErrors = await appProvisioningAdditionValidator(changes)
-        expect(changeErrors).toHaveLength(0)
-      })
+        });
+        const changes = [toChange({ before, after })];
+        const changeErrors = await appProvisioningAdditionValidator(changes);
+        expect(changeErrors).toHaveLength(0);
+      });
 
-      it(`should not return error when ${fieldName} is removed`, async () => {
+      it('should not return error when removed', async () => {
         const before = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
           [fieldName]: {},
-        })
+        });
         const after = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
-        })
-        const changes = [toChange({ before, after })]
-        const changeErrors = await appProvisioningAdditionValidator(changes)
-        expect(changeErrors).toHaveLength(0)
-      })
-      it(`should not return error when ${fieldName} isn't changed`, async () => {
+        });
+        const changes = [toChange({ before, after })];
+        const changeErrors = await appProvisioningAdditionValidator(changes);
+        expect(changeErrors).toHaveLength(0);
+      });
+
+      it('should not return error when unchanged', async () => {
         const before = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'INACTIVE',
           accessPolicy: 'accessPolicyId',
           [fieldName]: {},
-        })
+        });
         const after = new InstanceElement('app1', appType, {
           id: '1',
           label: 'app1',
           status: 'ACTIVE',
           [fieldName]: {},
-        })
-        const changes = [toChange({ before, after })]
-        const changeErrors = await appProvisioningAdditionValidator(changes)
-        expect(changeErrors).toHaveLength(0)
-      })
-    })
-  })
-})
+        });
+        const changes = [toChange({ before, after })];
+        const changeErrors = await appProvisioningAdditionValidator(changes);
+        expect(changeErrors).toHaveLength(0);
+      });
+    });
+  });
+});
